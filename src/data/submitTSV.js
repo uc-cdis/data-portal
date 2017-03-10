f9a4362c3f2482bc46b6999fddf8bb597ded9f4c
philloooo <phillis.tt@gmail.com>
2017-03-09 18:15:14 -0600

    fix(filereader): fix filereader in ie

--- src/data/submitTSV.js
+++ src/data/submitTSV.js
@@ -61,6 +61,25 @@ const SubmitTSVComponent = ({ path, submission, onUploadClick, onSubmitClick, on
     console.log(file_type);
     reader.readAsBinaryString(f);
     reader.onload = ((e) => onUploadClick(e.target.result, file_type));
+		// Extend file reader
+    if (!FileReader.prototype.readAsBinaryString) {
+			FileReader.prototype.readAsBinaryString = function (fileData) {
+				 var binary = "";
+				 var pt = this;
+				 var reader = new FileReader();      
+				 reader.onload = function (e) {
+						 var bytes = new Uint8Array(reader.result);
+						 var length = bytes.byteLength;
+						 for (var i = 0; i < length; i++) {
+								 binary += String.fromCharCode(bytes[i]);
+						 }
+					//pt.result  - readonly so assign binary
+					pt.content = binary;
+					$(pt).trigger('onload');
+			}
+			reader.readAsArrayBuffer(fileData);
+			}
+		}
   }
   let onSubmitClickEvent = () => {
     onSubmitClick();
