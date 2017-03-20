d489ca8744dee798b8014b13c6eec0502e2331ff
Thanh Nguyen <thanh.nguyen@uclouvain.be>
2017-03-20 18:45:09 -0500

    feat(access_key): add page to manage access key

--- src/data/reducers.js
+++ src/data/reducers.js
@@ -35,9 +35,28 @@ const user = (state={}, action) => {
 const cloud_access = (state={}, action) => {
   switch (action.type){
     case 'RECEIVE_CLOUD_ACCESS':
-      return {...state, ...action.cloud_access, access_key_pairs: action.access_keys};
+      return {...state, user: action.user, access_key_pairs: action.access_keys};
+    case 'CLOUD_ACCESS_ERROR':
+      return {...state, user: action.user, cloud_access_error: action.error};
     case 'RECEIVE_MIDDLEWARE_LOGIN':
       return {...state, login:state.result, error:state.error};
+    case 'REQUEST_DELETE_KEY':
+      return {...state, ...action.cloud_access, request_delete_key: action.access_key_id};
+    case 'CREATE_SUCCEED':
+      return {...state, ...action.cloud_access, access_key_pair: action.access_key_pair,
+        str_access_key_pair: action.str_access_key_pair, create_error: null};
+    case 'CREATE_USER_SUCCEED':
+      return {...state, user: action.user, create_error: null};
+    case 'CREATE_FAIL':
+      return {...state, create_error: action.error};
+    case 'DELETE_KEY_SUCCEED':
+      return {...state, ...action.cloud_access, access_key_pairs: action.access_keys, delete_error: null};
+    case 'DELETE_KEY_FAIL':
+      return {...state, delete_error: action.error};
+    case 'CLEAR_DELETE_KEY_SESSION':
+      return {...state, delete_error: null};
+    case 'CLEAR_CREATION_SESSION':
+      return {...state, access_key_pair: null, str_access_key_pair: null, delete_error: null};
     case 'FETCH_ERROR':
       return {...state, fetch_error: action.error};
     default:
@@ -109,7 +128,7 @@ const removeDeletedNode = (state, id) =>{
   // graphql response should always be {data: {node_type: [ nodes ] }}
   let node_type = Object.keys(search_result['data'])[0];
   let entities = search_result['data'][node_type];
-  search_result['data'][node_type] = entities.filter((entity) => entity['id'] != id);
+  search_result['data'][node_type] = entities.filter((entity) => entity['id'] !== id);
   return search_result;
 };
 
