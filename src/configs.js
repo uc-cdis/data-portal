017eca8d8df6ee4695217ec1f68a56af1c58b95e
philloooo <phillis.tt@gmail.com>
2017-05-25 22:30:47 -0500

    feat(csrf): add csrf token header

--- src/configs.js
+++ src/configs.js
@@ -1,5 +1,6 @@
 export * from './localconf.js';
-
-export const headers = new Headers({
+var csrftoken = document.cookie.replace(/(?:(?:^|.*;\s*)csrftoken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+export const headers = {
   'Accept': 'application/json',
-  'Content-Type': 'application/json'});
+  'Content-Type': 'application/json',
+  'x-csrf-token': csrftoken};
