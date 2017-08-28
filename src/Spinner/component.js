5381b05365950f6bc6801d9efa0286f858b7f208
Thanh Nguyen <thanh.nguyen@uclouvain.be>
2017-08-28 07:52:13 -0500

    fix(static): change to static component

--- src/Spinner/component.js
+++ src/Spinner/component.js
@@ -2,23 +2,20 @@ import React, { PropTypes } from 'react'
 import { connect } from 'react-redux';
 import { LoadingSpinnerWrap, LoadingSpinnerSVG } from './style'
 
-export const LoadingSpinnerComponent = () => {
-  return (
-    <LoadingSpinnerWrap>
-      <LoadingSpinnerSVG width='60' height='20' viewBox='0 0 60 20'>
-        <circle cx='7' cy='15' r='4' />
-        <circle cx='30' cy='15' r='4' />
-        <circle cx='53' cy='15' r='4' />
-      </LoadingSpinnerSVG>
-    </LoadingSpinnerWrap>
-  )
-};
+class LoadingSpinner extends React.Component {
+  render () {
+    return (
+      <div>
+        <LoadingSpinnerWrap>
+          <LoadingSpinnerSVG width='60' height='20' viewBox='0 0 60 20'>
+            <circle cx='7' cy='15' r='4' />
+            <circle cx='30' cy='15' r='4' />
+            <circle cx='53' cy='15' r='4' />
+          </LoadingSpinnerSVG>
+        </LoadingSpinnerWrap>
+      </div>
+    );
+  }
+}
 
-const mapStateToProps = (state) => ({
-});
-
-const mapDispatchToProps = (dispatch) => ({
-});
-
-const LoadingSpinner = connect(mapStateToProps, mapDispatchToProps)(LoadingSpinnerComponent);
 export default LoadingSpinner;
