7cebf636c1f74e2176cbf9edd836a9bda6cbcc26
Alex Espinosa <arespinos@gmail.com>
2017-09-05 14:21:16 -0500

    fix(submitForm): fix indentation

--- src/Submission/submitForm.js
+++ src/Submission/submitForm.js
@@ -267,22 +267,22 @@ class SubmitFormContainer extends Component {
   			return otherKeys;
 		}
 
-    const removeEmptyProps = (object) =>{
-      for(let prop in object){
-        if(object[prop] == '' || object[prop] == null){
-          object = objectWithoutKey(object, prop)
-        }
-        if(typeof object[prop] === 'object' || typeof object[prop] === 'array'){
-          object[prop] = removeEmptyProps(object[prop])
-        }
-      }
-      return(object);
-    }
+	    const removeEmptyProps = (object) =>{
+	      for(let prop in object){
+	        if(object[prop] == '' || object[prop] == null){
+	          object = objectWithoutKey(object, prop)
+	        }
+	        if(typeof object[prop] === 'object' || typeof object[prop] === 'array'){
+	          object[prop] = removeEmptyProps(object[prop])
+	        }
+	      }
+	      return(object);
+	    }
     
-    let cleared_form = removeEmptyProps(this.state.form);
+    	let cleared_form = removeEmptyProps(this.state.form);
 		let value = json_to_string(cleared_form);
 		this.props.onUploadClick(value, 'application/json');
-  };
+    };
 
   	onChangeAnyOf = (name, event, properties) =>{
   		const target = event.target;
