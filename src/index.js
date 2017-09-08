703376859aa9ae8fe58dd60464439884bd2e8519
Reuben Pasquini <reuben@Reubens-MacBook-Air.local>
2017-09-08 13:56:36 -0500

    refactor(reduxStore): add reduxStore.js helper
    reduxStore helps with data handoff between
    relay and redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import Relay from 'react-relay';
-import { dict } from './dictionary.js';
-import { persistStore, autoRehydrate } from 'redux-persist';
 import { render } from 'react-dom';
 import GraphiQL from 'graphiql';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { createStore, applyMiddleware, compose } from 'redux';
-import reducers from './reducers';
 import { requireAuth, enterHook, fetchUser } from './actions';
 import { clearResultAndQuery } from './QueryNode/actions';
 import Login from './Login/component';
@@ -38,53 +33,17 @@ import { ThemeProvider } from 'styled-components';
 import browserHistory from './history';
 import { theme, Box } from './theme';
 import { clearCounts } from './DataModelGraph/actions'
-import { required_certs } from './configs';
 import { asyncSetInterval, withBoxAndNav, withAuthTimeout } from './utils';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { getReduxStore } from './reduxStore.js';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-let store;
-const configureStore = function () {
-  if ( store ) {
-    console.log( "WARNING: attempt to reconfigure store" );
-    return;  // only run this once!
-  }
-  return new Promise((resolve, reject) => {
-    try {
-      if (dev === true) {
-        let data = {};
-        if (mock_store) {
-          data = {user: {username: 'test', certificates_uploaded: required_certs }, submission: {dictionary: dict, node_types: Object.keys(dict).slice(2) }, status: {}};
-        }
-        const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-        store = compose(applyMiddleware(thunk, routerMiddleware(browserHistory)), autoRehydrate())(createStore)(
-          reducers,
-          data,
-          window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-        );
-      }
-      else {
-        store = compose(applyMiddleware(thunk, routerMiddleware(browserHistory)), autoRehydrate())(createStore)(
-          reducers,
-          {user: {}, status: {}},
-          autoRehydrate()
-        );
-      }
-
-      asyncSetInterval( () => store.dispatch(fetchUser()), 10000 );
-      
-      const persister = persistStore(store, { whitelist: ['certificate']}, () => { console.log('rehydration complete'); resolve(store)});
-    } catch (e) {
-      reject(e);
-    }
-  });
-}
 
 
 const NoMatch = () => (
@@ -102,10 +61,20 @@ const homepageQueries = {
   viewer: () => Relay.QL`query { viewer }`,  
 }
 
+let initialized = false;
 
 // render the app after the store is configured
 async function init() {
-  const store = await configureStore();
+  if ( initialized ) {
+    console.log( "WARNING: attempt to re-initialize application" );
+    return;
+  }
+  initialized = true;
+  const store = await getReduxStore();
+
+  // not necessary to wait for this? ... await store.dispatch( fetchUser() );
+  asyncSetInterval( () => store.dispatch(fetchUser()), 10000 );
+  
   const history = syncHistoryWithStore(browserHistory, store);
   history.listen(location => console.log(location.pathname));
   if (app !== 'gdc') {
