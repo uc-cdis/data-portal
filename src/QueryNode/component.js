3bbb6b03c72d11304358aa8e13f0952bbb724b19
Reuben Pasquini <reuben@frickjack.com>
2017-09-25 11:57:15 -0500

    fix(QueryNode): fix view-node popup

--- src/QueryNode/component.js
+++ src/QueryNode/component.js
@@ -129,51 +129,72 @@ const Entity = ({ value, project, onUpdatePopup, onStoreNodeInfo }) => {
   );
 };
 
-const Entities = ({ value, project, onUpdatePopup, onStoreNodeInfo }) => (
-  <ul>
-    {value.map(value => <Entity project={project} onStoreNodeInfo={onStoreNodeInfo} onUpdatePopup={onUpdatePopup} key={value.submitter_id} value={value} />)}
-  </ul>
-);
-
-const QueryNodeComponent = ({ params, ownProps, submission, query_nodes, popups, onSearchFormSubmit, onUpdatePopup, onDeleteNode, onStoreNodeInfo, onClearDeleteSession }) => {
-  const project = params.project;
+const Entities = ({value, project, onUpdatePopup, onStoreNodeInfo}) => {
   return (
+    <ul>
+      {value.map( ( value) => <Entity project={project} onStoreNodeInfo={onStoreNodeInfo} onUpdatePopup={onUpdatePopup} key={value.submitter_id} value={value} /> )}
+    </ul>
+  );
+};
+
+const QueryNodeComponent = ({params, ownProps, submission, query_nodes, popups, onSearchFormSubmit, onUpdatePopup, onDeleteNode, onStoreNodeInfo, onClearDeleteSession}) => {
+  const project = params.project;
+  const popup = (() => {
+    if ( popups.nodedelete_popup === true ) {
+      // User clicked on node 'Delete' button
+      return <Popup message={'Are you sure you want to delete this node?'} error={json_to_string(query_nodes.delete_error)} 
+        code={json_to_string(query_nodes.query_node)} 
+        onConfirm={
+          ()=>{
+            onDeleteNode({project, id:query_nodes.stored_node_info}); 
+            onUpdatePopup({view_popup: 'Waiting for delete to finish ...', nodedelete_popup:false });
+          }
+        } 
+        onCancel={()=>{ onClearDeleteSession(); onUpdatePopup({nodedelete_popup:false }); }}
+      />;
+    } else if (! popups.nodedelete_popup &&
+      popups.view_popup === true &&
+      query_nodes.query_node &&
+      query_nodes.delete_error 
+    ) {
+      // Error deleting node
+      return <Popup message={'Error deleting: ' + query_nodes.query_node.submitter_id} error={json_to_string(query_nodes.delete_error)} 
+        code={json_to_string(query_nodes.query_node)} 
+        onClose={ ()=>{ onClearDeleteSession(); onUpdatePopup({view_popup: false}); } }
+      />;
+    } else if (! popups.nodedelete_popup &&
+      typeof popups.view_popup === 'string' &&
+      query_nodes.query_node  
+    ) {
+      // Waiting for node delete to finish
+      return <Popup message={ popups.view_popup }  />;
+    } else if (! popups.nodedelete_popup &&
+      popups.view_popup  &&
+      query_nodes.query_node
+    ) { 
+      // View node button clicked
+      return <Popup message={query_nodes.query_node.submitter_id} code={json_to_string(query_nodes.query_node)} 
+        onClose={
+          ()=>{
+            onUpdatePopup({view_popup: false, nodedelete_popup:false });
+          }
+        } 
+      />;
+    } else {
+      return "";
+    }
+  })();
+
+  
+
+  
+  return  (
     <div>
-      <h3>browse <Link to={`/${project}`}>{project}</Link> </h3>
-      { // User clicked on node 'Delete' button
-        popups.nodedelete_popup === true &&
-          <Popup
-            message={'Are you sure you want to delete this node?'} error={json_to_string(query_nodes.delete_error)}
-            code={json_to_string(query_nodes.query_node)}
-            onConfirm={
-              () => {
-                onDeleteNode({ project, id: query_nodes.stored_node_info });
-                onUpdatePopup({ view_popup: 'Waiting for delete to finish ...', nodedelete_popup: false });
-              }
-            }
-            onCancel={() => { onClearDeleteSession(); onUpdatePopup({ nodedelete_popup: false }); }}
-          />
-      }
-      { // Error deleting node
-        (! popups.nodedelete_popup) &&
-        popups.view_popup === true &&
-        query_nodes.query_node &&
-        query_nodes.delete_error &&
-          <Popup
-            message={`Error deleting: ${query_nodes.query_node.submitter_id}`} error={json_to_string(query_nodes.delete_error)}
-            code={json_to_string(query_nodes.query_node)}
-            onClose={() => { onClearDeleteSession(); onUpdatePopup({ view_popup: false }); }}
-          />
-      }
-      { // Waiting for node delete to finish
-        (! popups.nodedelete_popup) &&
-        typeof popups.view_popup === 'string' &&
-        query_nodes.query_node &&
-          <Popup message={popups.view_popup} />
-      }
-      <QueryForm onSearchFormSubmit={onSearchFormSubmit} project={project} node_types={submission.node_types} />
-      { query_nodes.search_status === 'succeed: 200' &&
-          Object.entries(query_nodes.search_result.data).map(value => (<Entities project={project} onStoreNodeInfo={onStoreNodeInfo} onUpdatePopup={onUpdatePopup} node_type={value[0]} key={value[0]} value={value[1]} />))
+      <h3>browse <Link to={'/' + project}>{project}</Link> </h3>
+      {popup}
+      <QueryForm onSearchFormSubmit={onSearchFormSubmit} project={project} node_types={submission.node_types}/>
+      { query_nodes.search_status==='succeed: 200' &&
+          Object.entries(query_nodes.search_result['data']).map((value) => { return (<Entities project={project} onStoreNodeInfo={onStoreNodeInfo} onUpdatePopup={onUpdatePopup} node_type={value[0]} key={value[0]} value={value[1]}/>)})
       }
     </div>
   );
