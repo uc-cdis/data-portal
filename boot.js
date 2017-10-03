acd4f6c5a42954422b00e2dfa0e9e780484742c6
Reuben Pasquini <reuben@frickjack.com>
2017-10-03 12:11:00 -0500

    style(boot.js): eslint fixes

--- boot.js
+++ boot.js
@@ -1,8 +1,8 @@
-(function() {
-  const isDevMode = !! location.pathname.match( /^\/dev.html/ );
-  const buildSrc = isDevMode ? "https://localhost/bundle.js" : "/bundle.js"; 
-  const scriptNode = document.createElement( 'script' );
+(function () {
+  const isDevMode = !!location.pathname.match(/^\/dev.html/);
+  const buildSrc = isDevMode ? 'https://localhost/bundle.js' : '/bundle.js';
+  const scriptNode = document.createElement('script');
   scriptNode.src = buildSrc;
-  scriptNode.type = "text/javascript";
-  document.body.appendChild( scriptNode );
-})();
+  scriptNode.type = 'text/javascript';
+  document.body.appendChild(scriptNode);
+}());
