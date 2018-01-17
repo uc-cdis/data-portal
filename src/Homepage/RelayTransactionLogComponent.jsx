890a3a93c7c1ff3aa78e1c6597ff9e9276110f3a
Thanh Nguyen <thanhnd@uchicago.edu>
2018-01-16 18:48:16 -0600

    fix(size): fixed size color bar

--- src/Homepage/RelayTransactionLogComponent.jsx
+++ src/Homepage/RelayTransactionLogComponent.jsx
@@ -4,19 +4,19 @@ import environment from '../environment';
 import Spinner from '../components/Spinner';
 import { app } from '../localconf';
 import {graphql} from 'react-relay';
-import { Table, TableHead, TableRow, TableColLabel, TableCell, ColorSpan } from "./style";
+import { Table, TableHead, TableRow, TableColLabel, TableCell, ColorSpan, ColorBar } from "./style";
 import styled from "styled-components";
 
-const SuccessBar = styled(ColorSpan)`
-  background-color: '#00ee00';
+const SuccessBar = styled(ColorBar)`
+  background: #00ee00;
 `;
 
-const FailBar = styled(ColorSpan)`
-  background-color: '#ff0000';
+const FailBar = styled(ColorBar)`
+  background: #ff0000;
 `;
 
-const PendingBar = styled(ColorSpan)`
-  background-color: '#ffcc00';
+const PendingBar = styled(ColorBar)`
+  background: #ffcc00;
 `;
 
 export class RelayTransactionLogComponent extends Component{
@@ -73,9 +73,9 @@ export class TransactionLogTR extends React.Component {
         <TableCell>{getLocalTime(trans.created_datetime)}
         </TableCell>
         <TableCell>
-          {(trans.state === 'SUCCEEDED' && <ColorSpan >{trans.state}</ColorSpan>)
-          || (trans.state === 'FAILED' && <FailBar>{trans.state}</FailBar>)
-          || (trans.state === 'PENDING' && <PendingBar>{trans.state}</PendingBar>)
+          {(trans.state === 'SUCCEEDED' && <SuccessBar><ColorSpan >{trans.state}</ColorSpan></SuccessBar>)
+          || (trans.state === 'FAILED' && <FailBar><ColorSpan >{trans.state}</ColorSpan></FailBar>)
+          || (trans.state === 'PENDING' && <PendingBar><ColorSpan >{trans.state}</ColorSpan></PendingBar>)
           }
         </TableCell>
       </TableRow>);
@@ -88,9 +88,9 @@ export class TransactionLogTR extends React.Component {
         <TableCell>{getLocalTime(trans.created_datetime)}
         </TableCell>
         <TableCell>
-          {(trans.state === 'SUCCEEDED' && <SuccessBar >{trans.state}</SuccessBar>)
-          || (trans.state === 'FAILED' && <FailBar>{trans.state}</FailBar>)
-          || (trans.state === 'PENDING' && <PendingBar>{trans.state}</PendingBar>)
+          {(trans.state === 'SUCCEEDED' && <SuccessBar><ColorSpan >{trans.state}</ColorSpan></SuccessBar>)
+          || (trans.state === 'FAILED' && <FailBar><ColorSpan >{trans.state}</ColorSpan></FailBar>)
+          || (trans.state === 'PENDING' && <PendingBar><ColorSpan >{trans.state}</ColorSpan></PendingBar>)
           }
         </TableCell>
       </TableRow>);
--- src/Homepage/style.js
+++ src/Homepage/style.js
@@ -42,24 +42,33 @@ export const TableColLabel = styled.th`
   text-align:center;
 `;
 
-export const ColorSpan = styled.span`
+export const ColorBar = styled.div`
   border: 10px;
   box-sizing: border-box;
   display: inline-block;
   font-family: Roboto, sans-serif;
-  text-decoration: none;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
   margin: 0px;
   padding: 0px;
-  outline: none;
-  font-size: inherit;
-  font-weight: inherit;
   position: relative;
   height: 36px;
   line-height: 36px;
-  min-width: 88px;
+  width: 120px;
   color: rgba(0, 0, 0, 0.87);
   transition: all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms;
   border-radius: 2px;
+  user-select: none;
   overflow: hidden;
   text-align: center;
+`;
+
+export const ColorSpan = styled.span`
+  position: relative;
+  padding-left: 16px;
+  padding-right: 16px;
+  vertical-align: middle;
+  letter-spacing: 0px;
+  text-transform: uppercase;
+  font-weight: 500;
+  font-size: 14px;
 `;
