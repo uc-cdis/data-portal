0ff39d52e442d55a1f83160b6ba6a629ca4e6fb5
Yajing Tang <phillis.tt@gmail.com>
2018-04-16 14:01:56 -0500

    fix(typo): kB -> KB

--- src/Explorer/ExplorerTable.jsx
+++ src/Explorer/ExplorerTable.jsx
@@ -85,7 +85,7 @@ export class ExplorerTableComponent extends Component {
   static humanFileSize(size) {
     const i = size === 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
     const sizeStr = (size / (1024 ** i)).toFixed(2) * 1;
-    const suffix = ['B', 'kB', 'MB', 'GB', 'TB'][i];
+    const suffix = ['B', 'KB', 'MB', 'GB', 'TB'][i];
     return `${sizeStr} ${suffix}`;
   }
 
