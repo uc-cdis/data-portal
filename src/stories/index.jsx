33111424647f1f7789d40326dd64b5eb4c77841d
abgeorge7 <abgeorge@umich.edu>
2018-07-10 14:43:38 -0500

    PXD-1087 more linting

--- src/stories/index.jsx
+++ src/stories/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 
 import 'react-select/dist/react-select.css';
 import '../css/base.less';
@@ -34,4 +33,4 @@ const countItems = [
 storiesOf('CountCard', module)
   .add('default', () => (
     <CountCard countItems={countItems} />
-  ))
+  ));
