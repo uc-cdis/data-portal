24af7bbd1737c8cf0a9c44ae710da22737b6b1bf
abgeorge7 <abgeorge@umich.edu>
2018-07-10 15:16:48 -0500

    Hopefully last linting issue

--- src/stories/buttons.jsx
+++ src/stories/buttons.jsx
@@ -9,41 +9,41 @@ import IconicLink from '../components/buttons/IconicLink';
 storiesOf('IconicButton', module)
   .add('with caption', () => (
     <IconicButton
-      onClick={action('clicked')}
-      caption="Hello Button"
+    onClick={action('clicked')}
+    caption="Hello Button"
     />
   ))
   .add('with color', () => (
     <IconicButton
-      onClick={action('clicked')}
-      iconColor="#ef8523"
-      caption="When you click me, I'm orange!"
+    onClick={action('clicked')}
+    iconColor="#ef8523"
+    caption="When you click me, I'm orange!"
     />
   ));
 
 const context = {};
 
 storiesOf('IconicLink', module)
-.add('with caption', () => (
-  <StaticRouter context={context}>
-    <Route>
-      <IconicLink
-        onClick={action('clicked')}
-        link="test.com"
-        iconColor="#ef8523"
-        caption="Hello Link"
-      />
-    </Route>
-  </StaticRouter>
-))
-.add('with color', () => (
-  <StaticRouter context={context}>
-    <Route>
-      <IconicLink
-        onClick={action('clicked')}
-        link='test.com'
-        caption="When you click me, I'm orange!"
-      />
-    </Route>
-  </StaticRouter>
-));
+  .add('with caption', () => (
+    <StaticRouter context={context}>
+      <Route>
+        <IconicLink
+          onClick={action('clicked')}
+          link="test.com"
+          iconColor="#ef8523"
+          caption="Hello Link"
+        />
+      </Route>
+    </StaticRouter>
+  ))
+  .add('with color', () => (
+    <StaticRouter context={context}>
+      <Route>
+        <IconicLink
+          onClick={action('clicked')}
+          link="test.com"
+          caption="When you click me, I'm orange!"
+        />
+      </Route>
+    </StaticRouter>
+  ));
