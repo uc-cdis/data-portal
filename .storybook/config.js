3e77253bcac59c824317425bc143de2a6c890ff6
abgeorge7 <abgeorge@umich.edu>
2018-07-10 11:05:54 -0500

    PXD-1050 jsx instead of js

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from '@storybook/react';
 
 function loadStories() {
-  require('../src/stories/index.js');
+  require('../src/stories/index.jsx');
   // You can require as many stories as you need.
 }
 
--- src/stories/index.jsx
+++ src/stories/index.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import IconicButton from '../components/buttons/IconicButton.jsx';
+import IconicButton from '../components/buttons/IconicButton';
 
 storiesOf('IconicButton', module)
   .add('with caption', () => (
     <IconicButton
       onClick={action('clicked')}
-      caption='Hello Button'
+      caption="Hello Button"
     />
   ))
   .add('with color', () => (
     <IconicButton
       onClick={action('clicked')}
-      iconColor='#000'
+      iconColor="#000"
     />
   ));
