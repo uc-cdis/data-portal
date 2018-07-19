f40e72d0b6d3d7c452bd0378e8d179714825a10e
abgeorge7 <abgeorge@umich.edu>
2018-07-19 11:58:47 -0500

    PXD-998 sorting fixed - multisort available

--- src/DataExplorer/index.jsx
+++ src/DataExplorer/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import DataExplorerTable from '../components/tables/DataExplorerTable/.';
+import multisort from 'multisort';
 
 const dummyConfig = {
   timestamp: '2018-01-12T16:42:07.495Z',
   type: 'subjects',
-  keyField: 'case_id',
-  defaultSorted: [{ id: 'case_id', desc: false }],
+  keyField: 'name',
+  defaultSorted: [{ id: 'name', desc: false }],
   columns: [
     {
       show: true,
@@ -39,16 +40,6 @@ const dummyConfig = {
       canChangeShow: true,
       accessor: 'gender',
     },
-    /*
-    {
-      show: true,
-      Header: 'Vital Status',
-      type: 'string',
-      sortable: true,
-      canChangeShow: true,
-      accessor: 'vital_status',
-    },
-    */
     {
       show: true,
       Header: 'Ethnicity',
@@ -64,63 +55,7 @@ const dummyConfig = {
       sortable: true,
       canChangeShow: true,
       accessor: 'race',
-    } /*,
-    {
-      show: true,
-      Header: 'Birth Year',
-      type: 'integer',
-      sortable: true,
-      canChangeShow: true,
-      accessor: 'birth_year',
     },
-    {
-      show: true,
-      Header: 'Death Year',
-      type: 'integer',
-      sortable: true,
-      canChangeShow: true,
-      accessor: 'death_year',
-    },
-    {
-      show: true,
-      Header: 'Species',
-      type: 'string',
-      sortable: true,
-      canChangeShow: true,
-      accessor: 'species',
-    },
-    {
-      show: true,
-      Header: 'Number Visits',
-      type: 'integer',
-      sortable: true,
-      canChangeShow: true,
-      accessor: 'number_visits',
-    },
-    {
-      show: true,
-      Header: 'Lab Records',
-      type: 'integer',
-      sortable: true,
-      canChangeShow: true,
-      accessor: 'lab_records',
-    },
-    {
-      show: true,
-      Header: 'Drug Records',
-      type: 'integer',
-      sortable: true,
-      canChangeShow: true,
-      accessor: 'drug_records',
-    },
-    {
-      show: true,
-      Header: 'mRNA Array Records',
-      type: 'integer',
-      sortable: true,
-      canChangeShow: true,
-      accessor: 'mrna_records',
-    },*/
   ],
 };
 
@@ -134,16 +69,20 @@ const fetchGQL = ({ sort, offset, first }) => {
         {
           query: "{ subject { hits { edges { node { name, project, study, gender, ethnicity, race, file_type }}}}}",
         }
-      )
+      ),
     }
   ).then(
     res => res.json()
   ).then(
-    (data) => {
-      console.log('Got data:', data);
-      const clean = data.data.subject.hits.edges.map( it => it.node );
-      console.log('Got clean data:', clean);
-      return { total: clean.length, data: clean };
+    (json) => {
+      const cleanData = json.data.subject.hits.edges.map( it => it.node );
+      const searchCriteria = sort.map(s =>
+        s.order == 'desc' ? '~'.concat(s.field) : s.field.toString()
+      )
+      return {
+        total: cleanData.length,
+        data: multisort(cleanData, searchCriteria).slice(offset, offset + first)
+      };
     }
   );
 };
