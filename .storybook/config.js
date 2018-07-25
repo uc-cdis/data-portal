711f3cd21b2e360f908effa5739d2829db31f147
Abby George <abbygeorge@uchicago.edu>
2018-07-25 16:04:07 -0500

    PXD-998 (part 2) Initial filter styling (#317)
    * initial filer setup
    
    * single select filter
    
    * filter section
    
    * filter list
    
    * filter group skeleton - wip
    
    * file group + tests
    
    * chevron icon
    
    * range filters
    
    * fixed tests
    
    * linting fixes
    
    * style tweaks
    
    * more styling
    
    * linting fixes
    
    * Reverted package-lock and added to gitignore
    
    * Revert "Reverted package-lock and added to gitignore"
    
    This reverts commit 3dee5171b3b4c7876471d5687d65d1b33becb85f.
    
    * Revert DataExplorer
    
    * PR changes
    
    * missing semicolon

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,7 +5,8 @@ function loadStories() {
   require('../src/stories/buttons.jsx');
   require('../src/stories/charts.jsx');
   require('../src/stories/tables.jsx');
-  require('../src/stories/cards.jsx')
+  require('../src/stories/filters.jsx');
+  require('../src/stories/cards.jsx');
 }
 
 configure(loadStories, module);
