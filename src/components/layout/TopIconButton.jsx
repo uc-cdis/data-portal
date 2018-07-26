d87c4a4f2415eb79cba26d1fd0a774950679a671
Qingya Shu <qingya.shu@gmail.com>
2018-07-26 11:43:52 -0500

    fix(btn-text-color): remove styled component and move to less

--- src/components/layout/TopIconButton.jsx
+++ src/components/layout/TopIconButton.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import IconComponent from '../Icon';
-
-const TopButton = styled.div`
-  background-color: #3283c8;
-  &:hover {
-    border-bottom: 1px solid #ffffff;
-  }
-  color: #ffffff;
-  margin: auto 20px;
-  width: auto;
-  display: block;
-`;
+import './TopIconButton.less';
 
 const TopIconButton = ({ dictIcons, item, onActiveTab = () => {}, isActive = false }) => (
-  <TopButton
-    className={isActive ? 'body-typo button-top-active' : 'body-typo'}
+  <div
+    className={isActive ? 'top-icon-button button-top-active' : 'top-icon-button'}
     onClick={onActiveTab}
   >
     {item.name}&nbsp;{item.icon ? <IconComponent
@@ -24,9 +13,9 @@ const TopIconButton = ({ dictIcons, item, onActiveTab = () => {}, isActive = fal
       iconName={item.icon}
       height="14px"
     /> : ''}
-  </TopButton>
+  </div>
 );
-
+ 
 TopIconButton.propTypes = {
   item: PropTypes.object.isRequired,
   dictIcons: PropTypes.object.isRequired,
