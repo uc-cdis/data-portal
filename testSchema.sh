c4d056b3de5be10d1dc5d409bc136cc8de76b7c5
abgeorge7 <abgeorge@umich.edu>
2018-07-26 11:09:11 -0500

    Travis testSchema

--- testSchema.sh
+++ testSchema.sh
@@ -2,21 +2,26 @@
 
 set -e
 
-declare -A testCases
+echo "RUNNING testSchema.sh"
+declare -a testCases
 testCases=(
-[bpa]="data.bloodpac.org"
-[bhc]="data.braincommons.org"
-[gtex]="dcp.bionimbus.org"
-[dev]="dev.planx-pla.net"
-[edc]="portal.occ-data.org"
-[genomel]="genomel.bionimbus.org"
-[kf]="data.kidsfirstdrc.org"
-[kfDcfInterop]="dcf-interop.kidsfirstdrc.org"
-[ndh]="niaid.bionimbus.org"
+bpa "data.bloodpac.org"
+bhc "data.braincommons.org"
+gtex "dcp.bionimbus.org"
+dev "dev.planx-pla.net"
+edc "portal.occ-data.org"
+genomel "genomel.bionimbus.org"
+kf "data.kidsfirstdrc.org"
+kfDcfInterop "dcf-interop.kidsfirstdrc.org"
+ndh "niaid.bionimbus.org"
 )
-for APP in "${!testCases[@]}"; do
-  export APP
-  export HOSTNAME="${testCases[$APP]}"
+count=0
+while [[ count -lt ${#testCases[@]} ]]; do
+  export APP=${testCases[$count]}
+  let count+=1
+  export HOSTNAME="${testCases[$count]}"
+  let count+=1
+  echo "Testing schema: $APP - $HOSTNAME"
   npm run schema
   npm run relay
 done
