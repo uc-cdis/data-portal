893b901165790a5e258ea52cc207316461ebe70d
Qingya Shu <qingya.shu@gmail.com>
2018-08-01 17:10:23 -0500

    fix(css): move css out of jsx files

--- src/components/layout/NavButton.jsx
+++ src/components/layout/NavButton.jsx
@@ -1,39 +1,19 @@
 import React from 'react';
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import IconComponent from '../Icon';
-
-const NavDiv = styled.div`
-  padding: 16px 0px;
-  height: 80px;
-  width: 110px;
-  display: inline-block;
-  text-align: center;
-  opacity: 0.8;
-  &:hover {
-    opacity: 1;
-    border-bottom: 3px solid #ef8523;
-  }
-`;
-
-
-const NavIcon = styled.div`
-  vertical-align: middle;
-  padding-left: 4px;
-`;
+import './NavButton.less';
 
 const NavButton = ({ dictIcons, item, onActiveTab, isActive }) => (
-  <NavDiv
-    className={isActive ? 'body-typo button-active' : 'body-typo'}
+  <div
+    className={isActive ? 'body-typo button-active nav-button' : 'body-typo nav-button'}
     onClick={onActiveTab}
   >
-    <NavIcon>
+    <div className="nav-button__icon">
       <IconComponent iconName={item.icon} dictIcons={dictIcons} />
-    </NavIcon>
+    </div>
     {item.name}
-  </NavDiv>
+  </div>
 );
-
 
 NavButton.propTypes = {
   item: PropTypes.object.isRequired,
