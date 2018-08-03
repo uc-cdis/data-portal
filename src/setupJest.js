66d400ad1b8710a7d62a48535059317824ef7471
abgeorge7 <abgeorge@umich.edu>
2018-08-03 09:43:51 -0500

    added test to fail if any console warnings

--- src/setupJest.js
+++ src/setupJest.js
@@ -7,3 +7,8 @@ import { faAngleUp, faAngleDown } from '@fortawesome/free-solid-svg-icons';
 global.fetch = require('jest-fetch-mock');
 library.add(faAngleUp, faAngleDown);
 Enzyme.configure({ adapter: new Adapter() });
+
+// Tests will fail if there are any warning messages
+console.warn = jest.fn(warn => {
+  throw new Error(warn);
+});
