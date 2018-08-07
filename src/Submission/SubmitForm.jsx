1f2d7e6e2e910023f0cdbe03851f8e34d552a049
Qingya Shu <38708936+qingyashu@users.noreply.github.com>
2018-08-07 17:03:49 -0500

    PXD-1310 Fix/css refactor 3 (#327)
    * fix(css): css refactor 3/4

--- src/Submission/SubmitForm.jsx
+++ src/Submission/SubmitForm.jsx
@@ -1,179 +1,10 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
 import { Toggle } from 'material-ui';
 import PropTypes from 'prop-types';
-
-import { Dropdown } from '../theme';
+import Select from 'react-select';
 import { jsonToString } from '../utils';
-import TextInput from './TextInput';
-import OneOfInput from './OneOfInput';
-import EnumInput from './EnumInput';
-
-const RequiredNotification = styled.span`
-  color:#d45252;
-  margin:5px 0 0 0;
-  display:inline;
-  float: ${props => (props.istext ? 'right' : '')};
-`;
-
-const UploadFormButton = styled.a`
-  display:inline-block;
-  border-radius: 3px;
-  margin-bottom: 10px;
-`;
-
-const AnyOfSubProps = styled.div`
-  margin-left:50px;
-`;
-
-const AnyOfInput = ({ name, values, node, properties, required, requireds, onChange }) => {
-  // this is smelly code because it reuses logic from SubmitNodeForm,
-  // I'd like to extract some of the code into another function
-
-  const onChangeAnyOfWrapper = (event) => {
-    onChange(name, event, properties);
-  };
-
-  return (
-    <div>
-      <h6 style={{ display: 'inline' }}>{name}:</h6>
-      {required && <RequiredNotification> {'*'} </RequiredNotification>}
-      <AnyOfSubProps>
-        {properties.map((property) => {
-          let description = ('description' in node.properties[property]) ? node.properties[property].description : '';
-          if (description === '') {
-            description = ('term' in node.properties[property]) ? node.properties[property].term.description : '';
-          }
-          const requiredSubprop = (requireds.indexOf(property) > -1);
-          // we use index 0 of values because AnyOfInput is hardcoded
-          // to be an array of length 1, an upcoming feature should be to add to this array
-          return (
-            <TextInput
-              key={property}
-              name={property}
-              value={values ? values[0][property] : ''}
-              required={required && requiredSubprop}
-              description={description}
-              onChange={onChangeAnyOfWrapper}
-            />);
-        })}
-      </AnyOfSubProps>
-    </div>
-  );
-};
-
-AnyOfInput.propTypes = {
-  name: PropTypes.string.isRequired,
-  values: PropTypes.object.isRequired,
-  node: PropTypes.any.isRequired,
-  properties: PropTypes.array.isRequired,
-  required: PropTypes.bool.isRequired,
-  requireds: PropTypes.array,
-  onChange: PropTypes.func.isRequired,
-};
-
-
-AnyOfInput.defaultProps = {
-  requireds: [],
-};
-
-
-const SubmitNodeForm = ({ node, form, properties, requireds, onChange,
-  onChangeEnum, onChangeAnyOf, onUpdateFormSchema,
-  handleSubmit }) => (
-  <div>
-    <form onSubmit={handleSubmit} >
-      {properties.map((property) => {
-        let description = ('description' in node.properties[property]) ? node.properties[property].description : '';
-        if (description === '') {
-          description = ('term' in node.properties[property]) ? node.properties[property].term.description : '';
-        }
-        const required = (requireds.indexOf(property) > -1);
-
-        if (property === 'type') {
-          return null;
-        } else if ('enum' in node.properties[property]) {
-          return (
-            <EnumInput
-              key={property}
-              name={property}
-              options={node.properties[property].enum}
-              onChange={onChangeEnum}
-              required={required}
-              onUpdateFormSchema={onUpdateFormSchema}
-              propertyType='string'
-              description={description}
-            />);
-        } else if ('oneOf' in node.properties[property]) {
-          return (
-            <OneOfInput
-              key={property}
-              value={form[property]}
-              property={node.properties[property].oneOf}
-              name={property}
-              required={required}
-              description={description}
-              onChange={onChange}
-              onChangeEnum={onChangeEnum}
-              onUpdateFormSchema={onUpdateFormSchema}
-            />);
-        } else if ('anyOf' in node.properties[property]) {
-          return (
-            <AnyOfInput
-              key={property}
-              values={form[property]}
-              name={property}
-              node={node.properties[property].anyOf[0].items}
-              properties={Object.keys(node.properties[property].anyOf[0].items.properties)}
-              required={required}
-              requireds={requireds}
-              onChange={onChangeAnyOf}
-            />
-          );
-        }
-        let propertyType = node.properties[property].type;
-        if (typeof (propertyType) === 'object') {
-          /* just use the first type if it allows multiple types */
-          propertyType = propertyType[0];
-        }
-        return (
-          <TextInput
-            key={property}
-            name={property}
-            onUpdateFormSchema={onUpdateFormSchema}
-            propertyType={propertyType}
-            value={form[property]}
-            required={required}
-            description={description}
-            onChange={onChange}
-          />);
-      })}
-      <UploadFormButton>
-        <button type='submit' value='Submit' className='button-primary-white' style={{ width: '250px' }}>
-          Upload submission json from form
-        </button>
-      </UploadFormButton>
-    </form>
-  </div>
-);
-
-SubmitNodeForm.propTypes = {
-  node: PropTypes.any.isRequired,
-  form: PropTypes.object.isRequired,
-  properties: PropTypes.object.isRequired,
-  requireds: PropTypes.array,
-  onChange: PropTypes.func.isRequired,
-  onChangeEnum: PropTypes.func.isRequired,
-  onChangeAnyOf: PropTypes.func.isRequired,
-  onUpdateFormSchema: PropTypes.func.isRequired,
-  handleSubmit: PropTypes.func.isRequired,
-};
-
-
-SubmitNodeForm.defaultProps = {
-  requireds: [],
-};
-
+import SubmitNodeForm from './SubmitNodeForm';
+import './SubmitForm.less';
 
 /**
  * Form-based data submission.  The results of this form submission are subsequently
@@ -266,22 +97,22 @@ class SubmitForm extends Component {
       });
     };
 
-
     return (
       <div>
         <form>
           <Toggle label='Use Form Submission' labelStyle={{ width: '' }} onToggle={this.onFormToggle} />
-          {this.state.fill_form && <Dropdown
+          {this.state.fill_form && <Select
             name='nodeType'
             options={options}
             value={this.state.chosenNode}
             onChange={updateChosenNode}
+            className='submit-form__select'
           />}
         </form>
         {(this.state.chosenNode.value !== null) && this.state.fill_form &&
         <div>
           <h5> Properties: </h5>
-          <RequiredNotification istext> * Denotes Required Property </RequiredNotification>
+          <span className='submit-form__required-notification'> * Denotes Required Property </span>
           <br />
           <SubmitNodeForm
             node={node}
--- src/components/tables/base/TableHead.jsx
+++ src/components/tables/base/TableHead.jsx
@@ -1,53 +1,30 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-
-export const THead = styled.thead`
-  height: 36px;
-  line-height: 36px;
-  border-top: 2px solid #000000;
-  border-bottom: 1px solid #dedede;
-`;
-
-export const ColHead = styled.th`
-  letter-spacing: .02rem;
-  color: #000000;
-  font-size: 16px;
-  font-weight: @semi-bold;
-  text-align:center;
-`;
+import './Table.less';
 
 class TableHead extends Component {
   render() {
     return (
-      <THead>
+      <thead className='base-table__head'>
         <tr>
           {
             this.props.cols.map((col, i) => (
-              this.props.colStyles.length <= i ? <ColHead key={`col_${col}_${i}`}>
+              <th
+                className='base-table__column-head'
+                key={`col_${col}_${i}`}
+              >
                 {col}
-              </ColHead> :
-                <ColHead
-                  key={`col_${col}_${i}`}
-                  style={{ ...this.props.colStyles[i] }}
-                >
-                  {col}
-                </ColHead>
+              </th>
             ))
           }
         </tr>
-      </THead>
+      </thead>
     );
   }
 }
 
 TableHead.propTypes = {
   cols: PropTypes.array.isRequired,
-  colStyles: PropTypes.array,
-};
-
-TableHead.defaultProps = {
-  colStyles: [],
 };
 
 export default TableHead;
