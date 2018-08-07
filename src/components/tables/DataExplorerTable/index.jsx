70d41c185fb941c2fa6f5ac6f3b5f2c47ad56a58
abgeorge7 <abgeorge@umich.edu>
2018-08-07 10:21:35 -0500

    ported over arranger styles

--- src/components/tables/DataExplorerTable/index.jsx
+++ src/components/tables/DataExplorerTable/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Table } from '@arranger/components/dist/Arranger';
-import '@arranger/components/public/themeStyles/beagle/beagle.css';
 import './DataExplorerTable.less';
 
 class DataExplorerTable extends React.Component {
