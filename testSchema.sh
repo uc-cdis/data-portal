dd32dda342f6ec3acbff9510747b9c1fe74acbdb
Reuben Pasquini <reuben@frickjack.com>
2018-10-03 10:02:27 -0500

    fix(testSchema): to work on mac

--- testSchema.sh
+++ testSchema.sh
@@ -2,21 +2,28 @@
 
 set -e
 
-declare -A testCases
+# Do not use associative arrays - not supported on Mac!
+declare -a testCases
 testCases=(
-[bpa]="data.bloodpac.org"
-[bhc]="data.braincommons.org"
-[gtex]="dcp.bionimbus.org"
-[dev]="dev.planx-pla.net"
-[edc]="portal.occ-data.org"
-[genomel]="genomel.bionimbus.org"
-[kfDcfInterop]="dcf-interop.kidsfirstdrc.org"
-[ndh]="niaid.bionimbus.org"
+bpa "data.bloodpac.org"
+bhc "data.braincommons.org"
+gtex "dcp.bionimbus.org"
+dev "dev.planx-pla.net"
+edc "portal.occ-data.org"
+genomel "genomel.bionimbus.org"
+kfDcfInterop "dcf-interop.kidsfirstdrc.org"
+ndh "niaid.bionimbus.org"
 )
-for APP in "${!testCases[@]}"; do
+index=0
+while [[ $index -lt ${#testCases[@]} ]]; do
+  APP=${testCases[$index]}
+  let index+=1
+  HOSTNAME=${testCases[$index]}
+  let index+=1
   export APP
-  export HOSTNAME="${testCases[$APP]}"
+  export HOSTNAME
   echo "Run setup for $APP $HOSTNAME"
   npm run schema
   npm run relay
 done
+
