e3197515f90389c36517fbed353e5e8eefde21f0
Qingya Shu <qingya.shu@gmail.com>
2018-10-15 09:53:44 -0500

    fix(link): link to embed notebook and also change CSP

--- src/Workspace/index.jsx
+++ src/Workspace/index.jsx
@@ -4,7 +4,12 @@ import './Workspace.less';
 class Workspace extends React.Component {
   render() {
     return (
-      <iframe title='Workspace' frameBorder='0' className='workspace' src='/workspace/' />
+      <iframe
+        title='Workspace'
+        frameBorder='0'
+        className='workspace'
+        src='/workspace/'
+      />
     );
   }
 }
