0f410322c9f6d09d2b9a73d13f8474276eea89f0
abgeorge7 <abgeorge@umich.edu>
2018-10-16 13:42:27 -0500

    WIP

--- src/DataExplorer/DataExplorer.test.jsx
+++ src/DataExplorer/DataExplorer.test.jsx
@@ -7,4 +7,16 @@ describe('DataExplorer', () => {
     const component = mount(<DataExplorer />);
     expect(component.find(DataExplorer).length).toBe(1);
   });
+
+  it('registers mouse clicks', () => {
+
+  });
+
+  it('logs the user out after inactivity', () => {
+
+  });
+
+  it('refreshes the users token if active', () => {
+
+  });
 });
--- src/DataExplorer/index.jsx
+++ src/DataExplorer/index.jsx
@@ -4,9 +4,34 @@ import DataExplorerFilters from './DataExplorerFilters';
 import DataExplorerVisualizations from './DataExplorerVisualizations';
 import arrangerApi from '../Arranger/utils';
 import { config } from '../params';
+import { loginPath, userapiPath } from '../localconf';
 import './DataExplorer.less';
 
 class DataExplorer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { mostRecentActivityTimestamp: Date.now() };
+  }
+
+  componentDidMount() {
+    window.addEventListener('mousedown', this.updateUserActivity, false);
+    window.addEventListener('keypress', this.updateUserActivity, false);
+    setTimeout(this.refreshSession, 6000);
+  }
+
+  updateUserActivity = () => {
+    this.setState({ mostRecentActivityTimestamp: Date.now() });
+  }
+
+  refreshSession = () => {
+    if (Date.now() - this.state.mostRecentActivityTimestamp > 18000) { // If 30 min have passed
+      window.location = loginPath;
+    } else {
+      fetch(userapiPath); // hitting Fence endpoint refreshes token
+      setTimeout(this.refreshSession, 6000);
+    }
+  }
+
   render() {
     const arrangerConfig = config.arrangerConfig || {};
     const explorerTableConfig = arrangerConfig.table || {};
