afa97b73f894ce0e9b8dcdbb9639eb42173fb1ba
Qingya Shu <qingya.shu@gmail.com>
2018-10-18 15:01:13 -0500

    fix(link): change link back...

--- src/Workspace/index.jsx
+++ src/Workspace/index.jsx
@@ -8,7 +8,7 @@ class Workspace extends React.Component {
         title='Workspace'
         frameBorder='0'
         className='workspace'
-        src='/workspace/'
+        src='/lw-workspace/'
       />
     );
   }
