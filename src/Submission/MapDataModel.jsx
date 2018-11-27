69fb6a84f5d3921eea9099d7ba8fbcf5898ba133
abgeorge7 <abgeorge@umich.edu>
2018-11-27 10:30:51 -0600

    step 3 wip

--- src/Submission/MapDataModel.jsx
+++ src/Submission/MapDataModel.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Select from 'react-select';
 import BackLink from '../components/BackLink';
 import './MapDataModel.less';
 
 class MapDataModel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projectId: null,
+      nodeType: null,
+    }
+  }
+
+  selectProjectId = option => {
+    this.setState({ projectId: option ? option.value : null });
+  }
+
+  selectNodeType = option => {
+    this.setState({ nodeType: option ? option.value : null });
+  }
+
   render() {
-    console.log('props', this.props)
     return(
       <div className='map-data-model'>
         <BackLink url='/submission/files' label='Back to My Files' />
@@ -14,8 +30,60 @@ class MapDataModel extends React.Component {
         </div>
         <div className='map-data-model__form'>
           <div className='map-data-model__header'>
-            Assign Project and Node Type
+            <div className='h3-typo'>Assign Project and Node Type</div>
+            <div className='h3-typo'>Step 1 of 2</div>
           </div>
+          <div className='map-data-model__form-section'>
+            <div className='h4-typo'>Project</div>
+              <Select
+                className='map-data-model__dropdown .map-data-model__dropdown--required introduction'
+                value={this.state.projectId}
+                placeholder={'Select your project'}
+                options={[
+                  {value: 'proj1', label: 'Project 1'},
+                  {value: 'proj2', label: 'Project 2'},
+                  {value: 'proj3', label: 'Project 3'}
+                ]}
+                onChange={this.selectProjectId}
+              />
+          </div>
+          <div className='map-data-model__form-section'>
+            <div className='h4-typo'>File Node</div>
+              <Select
+                className='map-data-model__dropdown introduction'
+                value={this.state.nodeType}
+                placeholder='Select your node'
+                options={[
+                  {value: 'node1', label: 'Node 1'},
+                  {value: 'node2', label: 'Node 2'},
+                  {value: 'node3', label: 'Node 3'},
+                  {value: 'node4', label: 'Node 4'},
+                ]}
+                onChange={this.selectNodeType}
+              />
+          </div>
+          <div className='map-data-model__header'>
+            <div className='h3-typo'>Add Details</div>
+            <div className='h3-typo'>Step 2 of 2</div>
+          </div>
+          <div>The Submitter ID is a project-specific node identifier. This property
+          is the calling card, nickname, and alias of a submission. It can be used
+          in place of the UUID for identifying or recalling a node.</div>
+          {
+            this.props.filesToMap.map((file, i) => (
+              <div key={i} className='map-data-model__file-section'>
+                {file}
+                <Select
+                  className='map-data-model__dropdown'
+                  options={[{value: '1', label: 'First'}]}
+                />
+                <Select
+                  className='map-data-model__dropdown'
+                  options={[{value: '2', label: 'Second'}]}
+                />
+              </div>
+            ))
+          }
         </div>
       </div>
     )
