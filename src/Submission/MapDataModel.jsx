f7ef7de279b1ed901a85ffd1ee520101a29c8f93
abgeorge7 <abgeorge@umich.edu>
2018-12-06 14:23:45 -0600

    everything added except submit button, need to clean it up

--- src/Submission/MapDataModel.jsx
+++ src/Submission/MapDataModel.jsx
@@ -23,10 +23,10 @@ class MapDataModel extends React.Component {
     this.state = {
       projectId: null,
       nodeType: null,
-      submissions: {},
-      existingSubmitterIds: new Set(),
-      submitterIdsByType: {},
-      optionalSectionIsOpen: false,
+      requiredFields: {},
+      parentNodeType: null,
+      parentNodeId: null,
+      validParentIds: [],
     }
   }
 
@@ -35,101 +35,64 @@ class MapDataModel extends React.Component {
       this.props.history.push('/submission/files');
     }
     getProjectsList();
-    let submissions = {};
-    this.props.filesToMap.forEach(file => {
-      submissions[file.did] = {
-        submitterId: null,
-        parentSubmitterIds: {},
-      }
-    });
-    this.setState({ submissions: submissions });
   }
 
   fetchAllSubmitterIds = () => {
-    let map = {};
-    let set = new Set();
-    fetchQuery(
-      environment,
-      gqlHelper.allSubmitterIdsQuery,
-      { project_id: this.state.project }
-    ).then(data => {
-      if (data && data.datanode) {
-        data.datanode.forEach(elt => {
-          set.add(elt.submitter_id);
-          if (!map[elt.type]) {
-            map[elt.type] = new Set();
-          }
-          map[elt.type] = map[elt.type].add(elt.submitter_id);
-        })
-      }
-      this.setState({ existingSubmitterIds: set, submitterIdsByType: map }, () => console.log('map', map, 'set', set));
-    })
-  }
-
-  getOptionalNodeProperties = requiredNodeProperties => {
-    const properties = this.props.dictionary[this.state.nodeType].properties;
-    return Object.keys(properties)
-      .filter(key => {
-        return !requiredNodeProperties.includes(key)
+    let ids = [];
+    if (this.state.parentNodeType && this.state.nodeType && this.state.projectId) {
+      fetchQuery(
+        environment,
+        gqlHelper.allSubmitterIdsByTypeQuery,
+        { project_id: this.state.project,  type: this.state.parentNodeType }
+      ).then(data => {
+        console.log('data', data)
+        if (data && data.datanode) {
+          ids = data;
+        }
       })
-      .reduce((obj, key) => {
-        obj[key] = properties[key];
-        return obj;
-      }, {});
-  }
-
-  parentSubmitterIdsAreValid = (index, type) => {
-    let id = this.state.submissions[index] ? this.state.submissions[index].parentSubmitterIds[type] : [];
-      if (!(this.state.existingSubmitterIds.has(id) &&
-            this.state.submitterIdsByType[type] &&
-            this.state.submitterIdsByType[type].has(id))) {
-        return false;
-      }
-    return true;
-  }
-
-  selectProjectId = option => {
-    this.setState({ projectId: option ? option.value : null }, this.fetchAllSubmitterIds());
+    }
+    this.setState({ validParentIds: ids }, () => console.log('data', this.state.validParentIds));
   }
 
   selectNodeType = option => {
-    this.setState({ nodeType: option ? option.value : null });
+    this.setState({ nodeType: option ? option.value : null }, this.fetchAllSubmitterIds);
   }
 
-  setParentIds = (options, index, type) => {
-    let submissions = this.state.submissions;
-    submissions[index].parentSubmitterIds[type] = options;
-    this.setState({ submissions: submissions });
+  selectParentNodeId = option => {
+    this.setState({ parentNodeId: option ? option.value : null});
   }
 
-  setSubmitterId = (e, index) => {
-    let id = e.target.value;
-    let submissions = this.state.submissions;
-    submissions[index].submitterId = id;
-    this.setState({ submissions: submissions });
+  selectParentNodeType = option => {
+    this.setState({ parentNodeType: option ? option.value : null }, this.fetchAllSubmitterIds);
   }
 
-  submitterIdExists = index => {
-    let id = this.state.submissions[index] ? this.state.submissions[index].submitterId : null;
-    if (id && this.state.existingSubmitterIds.has(id)) {
-      return true;
-    }
-    return false;
+  selectProjectId = option => {
+    this.setState({ projectId: option ? option.value : null }, this.fetchAllSubmitterIds);
   }
 
-  toggleOptionalSection = () => {
-    this.setState({ optionalSectionIsOpen: !this.state.optionalSectionIsOpen });
+  selectRequiredField = (option, prop) => {
+    let fields = this.state.requiredFields;
+    fields[prop] = option ? option.value : null;
+    this.setState({ requiredFields: fields });
   }
 
   render() {
-    console.log('submission:', this.state.submissions)
+    console.log('node types', this.props.nodeTypes)
+    console.log('dictionary', this.props.dictionary)
     const projectList = this.props.projects ? Object.keys(this.props.projects) : [];
     const projectOptions = projectList.map(key => ({ value: this.props.projects[key].code, label: this.props.projects[key].name }));
     const nodeOptions = this.props.nodeTypes ? this.props.nodeTypes.map(node => ({ value: node, label: node })) : [];
-    const requiredNodeProperties = this.state.nodeType ? this.props.dictionary[this.state.nodeType].required : null;
-    const optionalNodeProperties = this.state.nodeType ? this.getOptionalNodeProperties(requiredNodeProperties) : null;
-    console.log('dictionary', this.props.dictionary)
-    return(
+    const requiredNodeProperties = this.state.nodeType ?
+      this.props.dictionary[this.state.nodeType].required.filter(prop => prop !== 'submitter_id' &&
+        !this.props.dictionary[this.state.nodeType].systemProperties.includes(prop) &&
+        !this.props.dictionary[this.state.nodeType].links.map(link => link.name).includes(prop))
+      : null;
+    const linkOptions = this.state.nodeType ?
+      this.props.dictionary[this.state.nodeType].links.map(link => ({ value: link.target_type, label: link.name }))
+      : null
+    console.log('requiredFields', this.state.requiredFields);
+
+    return (
       <div className='map-data-model'>
         <BackLink url='/submission/files' label='Back to My Files' />
         <div className='h1-typo'>
@@ -170,83 +133,65 @@ class MapDataModel extends React.Component {
                 <div className='map-data-model__required-fields-section'>
                   <div className='h4-typo'>Required Fields</div>
                   {
-                    requiredNodeProperties.map((prop, i) =>
-                      <div key={i} className='map-data-model__required-field'>
-                        <RequiredIcon />
-                        <div className='h4-typo'>{prop}</div>
-                      </div>
-                  )}
+                    requiredNodeProperties.map((prop, i) => {
+                      const type = this.props.dictionary[this.state.nodeType].properties[prop];
+                      console.log('checkmark?', this.state.requiredFields[prop])
+                      return (
+                        <div key={i} className='map-data-model__required-field'>
+                          <RequiredIcon />
+                          <div className='h4-typo'>{prop}</div>
+                          {
+                            type['enum'] ?
+                              <InputWithIcon
+                                inputClassName='map-data-model__dropdown map-data-model__dropdown--required introduction'
+                                inputValue={this.state.requiredFields[prop]}
+                                inputPlaceholderText='Select your field'
+                                inputOptions={type['enum'].map(option => ({ value: option, label: option }))}
+                                inputOnChange={(e) => this.selectRequiredField(e, prop)}
+                                iconSvg={CheckmarkIcon}
+                                shouldDisplayIcon={!!this.state.requiredFields[prop]}
+                              />
+                              : <InputWithIcon
+                                inputClassName='map-data-model__input map-data-model__input--required introduction'
+                                inputValue={this.state.requiredFields[prop]}
+                                inputOnChange={(e) => this.selectRequiredField(e, prop)}
+                                iconSvg={CheckmarkIcon}
+                                shouldDisplayIcon={!!this.state.requiredFields[prop]}
+                              />
+                          }
+                        </div>
+                      )
+                    })
+                  }
                 </div>
               ) : null
             }
           </div>
-          <div className='map-data-model__header'>
-            <div className='h3-typo'>Add Details</div>
-            <div className='h3-typo'>Step 2 of 2</div>
+          <div className='map-data-model__node-form-section'>
+            <div className='map-data-model__form-section'>
+              <div className='h4-typo'>Link(s) to Parent Node(s)</div>
+              <InputWithIcon
+                inputClassName='map-data-model__dropdown map-data-model__dropdown--required introduction'
+                inputValue={this.state.parentNodeType}
+                inputPlaceholderText='Select your parent node type'
+                inputOptions={linkOptions}
+                inputOnChange={this.selectParentNodeType}
+                iconSvg={CheckmarkIcon}
+                shouldDisplayIcon={this.state.parentNodeType !== null}
+              />
+            </div>
+            <div className='map-data-model__required-fields-section'>
+            <InputWithIcon
+              inputClassName='map-data-model__dropdown map-data-model__dropdown--required introduction'
+              inputValue={this.state.parentNodeId}
+              inputPlaceholderText='Select your parent node ID'
+              inputOptions={this.state.validParentIds}
+              inputOnChange={this.selectParentNodeId}
+              iconSvg={CheckmarkIcon}
+              shouldDisplayIcon={this.state.parentNodeId !== null}
+            />
+            </div>
           </div>
-          <div className='map-data-model__details introduction'>The Submitter ID is a project-specific node identifier. This property
-          is the calling card, nickname, and alias of a submission. It can be used
-          in place of the UUID for identifying or recalling a node.</div>
-          <table className='map-data-model__files-table'>
-            <tbody>
-              <tr className='map-data-model__files-table-header'>
-                <th className='map-data-model__files-table-header-cell'>File Name</th>
-                <th className='map-data-model__files-table-header-cell'>Submitter ID</th>
-                <th className='map-data-model__files-table-header-cell'>Link(s) to Parent Node</th>
-              </tr>
-              {
-                this.props.filesToMap.map(file => {
-                  const submitterIdIsValid = !this.submitterIdExists(file.did);
-                  return (
-                    <tr key={file.did} className='map-data-model__file-row'>
-                      <td className='map-data-model__file-row-cell map-data-model__icon-cell'>
-                        <FileIcon />
-                        {file.file_name}
-                      </td>
-                      <td className='map-data-model__file-row-cell'>
-                        <InputWithIcon
-                          inputClassName={'map-data-model__input map-data-model__input--required'.concat(!submitterIdIsValid ? ' red-border' : '')}
-                          inputOnChange={(e) => this.setSubmitterId(e, file.did)}
-                          iconSvg={InvalidIcon}
-                          shouldDisplayIcon={!submitterIdIsValid}
-                          shouldDisplayText={!submitterIdIsValid}
-                          text='This submitter ID has been used.'
-                          textClassName='invalid-text body-typo'
-                        />
-                      </td>
-                      <td className='map-data-model__file-row-cell'>
-                      {
-                        this.state.projectId && this.state.nodeType ? this.props.dictionary[this.state.nodeType].links.map(link => {
-                          const parentSubmitterIdsAreValid = this.parentSubmitterIdsAreValid(file.did, link.target_type);
-                          const submitterIds = this.state.submitterIdsByType[link.target_type];
-                          const options = submitterIds ? Array.from(submitterIds).map(id => ({ value: id, label: id })) : [];
-                          const selectedIds = this.state.submissions[file.did] && this.state.submissions[file.did].parentSubmitterIds[link.target_type]
-                            ? this.state.submissions[file.did].parentSubmitterIds[link.target_type]
-                            : null
-                          return (
-                            <React.Fragment key={`${file.did}-${link.target_type}`}>
-                              <p>{link.name}</p>
-                              <InputWithIcon
-                                inputClassName={'map-data-model__dropdown--required'.concat(!parentSubmitterIdsAreValid ? ' red-border' : '')}
-                                inputOptions={options}
-                                inputOnChange={(option) => this.setParentIds(option, file.did, link.target_type)}
-                                inputValue={selectedIds}
-                                iconSvg={InvalidIcon}
-                                text='One or more of these parent IDs does not exist.'
-                                textClassName='invalid-text body-typo'
-                                isMulti
-                              />
-                            </React.Fragment>
-                          )
-                        }) : null
-                      }
-                    </td>
-                  </tr>
-                )
-              })
-            }
-            </tbody>
-          </table>
         </div>
       </div>
     )
