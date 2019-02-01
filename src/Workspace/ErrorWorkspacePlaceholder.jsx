0a0825d10427c29044a48e312255be3e03add7b9
Qingya Shu <qingya.shu@gmail.com>
2019-02-01 10:12:15 -0600

    fix(img): add 404 img

--- src/Workspace/ErrorWorkspacePlaceholder.jsx
+++ src/Workspace/ErrorWorkspacePlaceholder.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Workspace.less';
+import NotFoundSVG from '../img/not-found.svg';
 
 class ErrorWorkspacePlaceholder extends React.Component {
   render() {
     return (
-      <h1 className='error-workspace-placeholder__error-msg'>
-        Error opening workspace...
-      </h1>
+      <div className='error-workspace-placeholder__error-msg'>
+        <h1>Error opening workspace...</h1>
+        <p>Please try again later, or contact administrator for more information.</p>
+        <NotFoundSVG />
+      </div>
     );
   }
 }
