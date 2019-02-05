33f2eb173b9cdf49cec25c6f8e0e569635e6d8d5
Qingya Shu <qingya.shu@gmail.com>
2019-02-05 13:05:32 -0600

    fix(commit): recover wrong commit

--- src/utils.js
+++ src/utils.js
@@ -2,13 +2,6 @@ import * as d3 from 'd3-scale';
 
 import { submissionApiPath } from './localconf';
 
-export const humanFileSize = (size) => {
-  const i = size === 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
-  const sizeStr = (size / (1024 ** i)).toFixed(2) * 1;
-  const suffix = ['B', 'KB', 'MB', 'GB', 'TB'][i];
-  return `${sizeStr} ${suffix}`;
-};
-
 export const getSubmitPath = (project) => {
   const path = project.split('-');
   const programName = path[0];
