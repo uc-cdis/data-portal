018ca38421a30e76906ccba04a3400b6f27eb6e9
Benjamin Rohrer <benJrohrer@gmail.com>
2019-03-08 17:33:37 -0600

    flow for workspace-token-service

--- src/Workspace/index.jsx
+++ src/Workspace/index.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import { workspaceUrl } from '../localconf';
 import './Workspace.less';
+import { fetchWithCreds } from '../actions';
 
 class Workspace extends React.Component {
+
+  componentDidMount() {
+    fetchWithCreds({
+      path: '/oauth2/check'
+      method: 'GET'
+    })
+      .then(
+        ({ status, data }) => {
+          if (status != 200) {
+            this.props.history.push('/oauth2/authorization_url')
+          }
+        }
+      )
+  }
+
   render() {
     return (
       <iframe
