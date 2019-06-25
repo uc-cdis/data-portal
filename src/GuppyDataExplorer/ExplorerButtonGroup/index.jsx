9fe02b10d4aad52095f69a030bd264537e3b9fab
Zakir Gowani <zakirgowani@gmail.com>
2019-06-25 15:11:56 -0500

    fix: guppy explorer using incorrect manifest format (#549)
    * fix: guppy explorer using incorrect manifest format

--- src/GuppyDataExplorer/ExplorerButtonGroup/index.jsx
+++ src/GuppyDataExplorer/ExplorerButtonGroup/index.jsx
@@ -98,7 +98,12 @@ class ExplorerButtonGroup extends React.Component {
       },
       [caseFieldInFileIndex, fileFieldInFileIndex],
     );
-    resultManifest = resultManifest.filter(x => typeof x[fileFieldInFileIndex] !== 'undefined');
+    resultManifest = resultManifest.filter(
+      x => typeof x[fileFieldInFileIndex] !== 'undefined'
+    );
+    for(let i = 0; i < resultManifest.length; i += 1) {
+      resultManifest[i][caseFieldInFileIndex] = [ resultManifest[i][caseFieldInFileIndex] ];
+    } 
     return resultManifest.flat();
   };
 
