a39097074116d2046d3ecfe3a02cc53155dd2c36
Michael Ingram <mpingram@uchicago.edu>
2019-11-12 16:33:16 -0600

    Fix(PXP-3514): Correctly handle unicode file uploads (#620)
    * Write test for handling utf8 uploads
    
    * Use FileReader.readAsTextString to upload files
    
    - Previous code had used FileReader.readAsBinaryBlob (with a
      polyfill for IE support.) Unfortunately, readAsBinaryBlob does
      not preserve unicode encoding, leading to bugs such as
      https://ctds-planx.atlassian.net/browse/PXP-3514.

--- src/Submission/SubmitTSV.test.jsx
+++ src/Submission/SubmitTSV.test.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import * as testData from "./__test__/data.json"
 
 import SubmitTSV from './SubmitTSV';
 
-describe('the TSV submission componet', () => {
+describe('the TSV submission component', () => {
   const testProjName = 'bogusProject';
   /* Mutes large warning:
   * "Could not load worker TypeError:
@@ -17,15 +18,16 @@ describe('the TSV submission componet', () => {
    *
    * @param {file, submit_result, submit_status} submission property passed through to <SubmitTSV>
    * @param {function} submitCallback invoked by onSubmitClick property on <SubmitTSV>
+   * @param {function} uploadCallback invoked by onUploadClick property on <SubmitTSV>
    * @return enzymejs wrapper of <SubmitTSV> with properties from params
    */
-  function buildTest(submission = { file: '', submit_result: '', submit_status: 200 }, submitCallback = () => {}) {
+  function buildTest(submission = { file: '', submit_result: '', submit_status: 200 }, submitCallback = () => { }, uploadCallback = () => { }) {
     const $dom = mount(
       <MuiThemeProvider>
         <SubmitTSV
           project={testProjName}
           submission={submission}
-          onUploadClick={() => { console.log('onUpload'); }}
+          onUploadClick={(newFile, fileType) => { console.log('onUploadClick'); uploadCallback(newFile, fileType); }}
           onSubmitClick={(project) => { console.log('onSubmitClick'); submitCallback(project); }}
           onFileChange={() => { console.log('onFileChange'); }}
         />
@@ -68,5 +70,42 @@ describe('the TSV submission componet', () => {
     expect($dom.find('label[id="cd-submit-tsv__upload-button"]').length).toBe(1);
     expect($dom.find('button[id="cd-submit-tsv__submit-button"]').length).toBe(1);
     expect($dom.find('div[id="cd-submit-tsv__result"]').length).toBe(1);
+  });
+
+  it('correctly handles utf-8 files, without corrupting multi-byte special characters', (done) => {
+    expect.assertions(3);
+    // The 'testData' file contains multi-byte special characters
+    const utf8TestData = JSON.stringify(testData);
+
+    // 2. Compare the contents of the uploaded
+    // file and the file we passed in.
+    const handleUpload = (data, fileType) => {
+      try {
+        expect(fileType).toBe('application/json');
+        // Expect the uploaded file, as a string, to be the same as the test file,
+        // as a string -- ie, for the utf8 encoding to be preserved.
+        expect(data).toEqual(utf8TestData);
+      } catch (err) {
+        // We need to use done.fail(err) because otherwise the exception thrown by
+        // expect will not be caught by this test.
+        done.fail(err);
+      }
+      done();
+    };
+
+    const { $dom } = buildTest(undefined, undefined, handleUpload);
+
+    // 1. Find the file upload button and upload our JSON file with non-ascii characters.
+    // This will trigger the 'handleUpload' callback.
+    const nonAsciiJSONFile = new File([JSON.stringify(testData)], 'test.json');
+    const $input = $dom.find('input[id="file-upload"]');
+    expect($input.length).toBe(1);
+    $input.simulate('change', {
+      target: {
+        files: [
+          nonAsciiJSONFile,
+        ],
+      },
+    });
   });
 });
