9fcd17f1573d4f09a25d025c71786339dd1cb8cc
Pauline <ribeyre@uchicago.edu>
2020-04-01 13:58:35 -0500

    smaller control panel + fix explorer checkboxes

--- src/Covid19Dashboard/ControlPanel/index.jsx
+++ src/Covid19Dashboard/ControlPanel/index.jsx
@@ -8,9 +8,9 @@ export default class ControlPanel extends PureComponent {
 
     return (
       <div className='control-panel'>
-        <h3>Confirmed cases of Coronavirus</h3>
+        <h3>Confirmed cases</h3>
         <p>
-          Data source: <a href='https://systems.jhu.edu'>Johns Hopkins University Center for Systems Science and Engineering (JHU CSSE)</a>
+          Data source: <a href='https://systems.jhu.edu'>Johns Hopkins University CSSE</a>
         </p>
 
         {/* TODO: fix or remove */}
