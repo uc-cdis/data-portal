5706af854b7242b575cfe63137e29fef1947b918
Abby George <abgeorge@umich.edu>
2020-04-05 00:46:25 -0500

    :sparkles: Show popup with timeseries graph on click

--- src/Covid19Dashboard/IllinoisMapChart/index.jsx
+++ src/Covid19Dashboard/IllinoisMapChart/index.jsx
@@ -2,16 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Range } from 'rc-slider';
 import * as ReactMapGL from 'react-map-gl';
+import {
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer,
+} from 'recharts';
 
 import ControlPanel from '../ControlPanel';
+import Popup from '../../components/Popup';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './IllinoisMapChart.less';
+
 
 import countyData from '../c_03mr20'
 
 const numberWithCommas = x => {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+const monthNames = ["Jan", "Feb", "Mar", "April", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov","Dec"];
+
+class CustomizedAxisTick extends React.Component {
+  render() {
+    const {
+      x, y, stroke, payload,
+    } = this.props;
+    const formattedDate = `${monthNames[new Date(payload.value).getMonth()]} ${new Date(payload.value).getDate()}`;
+    return (
+      <g transform={`translate(${x},${y})`}>
+        <text x={0} y={0} dy={16} textAnchor="end" fill="#666" transform="rotate(-60)">{formattedDate}</text>
+      </g>
+    );
+  }
 }
 
 class IllinoisMapChart extends React.Component {
@@ -38,11 +59,19 @@ class IllinoisMapChart extends React.Component {
       },
       hoverInfo: null,
       showLegend: true,
+      selectedDate: props.rawData && props.rawData[0] ? new Date(Math.max.apply(null, props.rawData[0].date.map(date => new Date(date)))) : null,
+      selectedLocation: null,
     };
   }
 
   componentDidMount() {
     window.addEventListener('resize', this.updateDimensions);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.rawData.length !== this.props.rawData.length) {
+      this.setState({ selectedDate: nextProps.rawData && nextProps.rawData[0] ? new Date(Math.max.apply(null, nextProps.rawData[0].date.map(date => new Date(date)))) : null });
+    }
   }
 
   // componentWillUnmount() {
@@ -93,7 +122,7 @@ class IllinoisMapChart extends React.Component {
     return null;
   }
 
-  convertDataToDict(rawData, selectedDate) {
+  convertDataToDict(rawData) {
     var filteredFeatures = {};
     rawData.reduce((res, location) => {
       if (location.project_id != 'open-JHU') {
@@ -102,18 +131,16 @@ class IllinoisMapChart extends React.Component {
         // to filter on client side
         return res;
       }
-      if (location.province_state != "Illinois"){
+      if (location.province_state != "Illinois") {
         return res;
       }
-      location.date.forEach((date, i) => {
-        if (new Date(date).getTime() != selectedDate.getTime()) {
-          return;
-        }
-        res[location.FIPS] = {
-        'confirmed': location.confirmed[i],
-        'deaths': location.deaths[i],
-        }
-      });
+      const selectedDateIndex = location.date.findIndex(x => new Date(x).getTime() === this.state.selectedDate.getTime());
+
+      res[location.FIPS] = {
+        confirmed: location.confirmed[selectedDateIndex],
+        deaths: location.deaths[selectedDateIndex],
+        allData: { ...location },
+      }
       return res;
     }, filteredFeatures);
     return filteredFeatures;
@@ -125,8 +152,17 @@ class IllinoisMapChart extends React.Component {
       features: this.counties.features.map((location) => {
         if (location.properties.FIPS in fipsData) {
           location.properties.confirmed = Number(fipsData[location.properties.FIPS].confirmed);
+          const allData = fipsData[location.properties.FIPS].allData;
+          location.properties.allData = {
+            confirmed: allData.confirmed,
+            deaths: allData.deaths,
+            date: allData.date,
+            testing: allData.testing,
+            recovered: allData.recovered,
+          }
         } else {
           location.properties.confirmed = 0;
+          location.properties.allData = {};
         }
         return location;
       }),
@@ -135,17 +171,66 @@ class IllinoisMapChart extends React.Component {
     return geoJson;
   }
 
+  _onClick = (event) => {
+    if (!event.features) { return; }
+
+    let selectedLocation = null;
+    event.features.forEach((feature) => {
+      if (feature.layer.id == 'confirmed') {
+        const state = feature.properties.STATE;
+        const county = feature.properties.COUNTYNAME;
+        const fips = feature.properties.FIPS;
+        const data = JSON.parse(feature.properties.allData);
+        selectedLocation = {
+          state,
+          county,
+          fips,
+          data,
+        };
+      }
+    });
+
+    this.setState({ selectedLocation });
+  }
+
+  formatLocationData = data => {
+    let sortedData = data.date.sort((a, b) => new Date(a) - new Date(b));
+    let max = 0;
+    sortedData = sortedData.map((date, i) => {
+      max = Math.max(max, data.confirmed[i], data.deaths[i]);
+      return {
+        date,
+        confirmed: data.confirmed[i],
+        deaths: data.deaths[i],
+      }
+    });
+    return { data: sortedData, max };
+  }
+
+  closePopup = () => {
+    this.setState({ selectedLocation: null });
+  }
+
+  renderTooltip = props => {
+    const date = new Date(props.label);
+    return (
+      <div className='map-chart__tooltip'>
+        <p>{monthNames[date.getMonth()]} {date.getDate()}, {date.getFullYear()}</p>
+        {
+          props.payload.map((data, i) => (
+            <p style={{color: data.stroke}} key={i}>{data.name}: {data.value}</p>
+          ))
+        }
+      </div>
+    )
+  }
+
   render() {
     const rawData = this.props.rawData;
-    // console.log('rawData', rawData);
+    const { selectedLocation } = this.state;
 
     if (!this.geoJson || this.geoJson.features.length == 0) {
-      // find latest date we have in the data
-      let selectedDate = new Date();
-      if (rawData.length > 0) {
-        selectedDate = new Date(Math.max.apply(null, rawData[0].date.map(date => new Date(date))));
-      }
-      const fipsData = this.convertDataToDict(rawData, selectedDate);
+      const fipsData = this.convertDataToDict(rawData);
       this.geoJson =this.convertDataToGeoJson(fipsData);
     }
 
@@ -162,7 +247,7 @@ class IllinoisMapChart extends React.Component {
       2500: '#aa5e79'
     };
     const colorsAsList = Object.entries(colors).map(item => [+item[0], item[1]]).flat();
-
+    const selectedLocationData = selectedLocation ? this.formatLocationData(selectedLocation.data) : null;
     return (
       <div className='map-chart'>
         <ReactMapGL.InteractiveMap
@@ -175,6 +260,7 @@ class IllinoisMapChart extends React.Component {
             this.setState({ viewport });
           }}
           onHover={this._onHover}
+          onClick={this._onClick}
           dragRotate={false}
           touchRotate={false}
           // maxBounds={[ // doesn't work
@@ -205,6 +291,31 @@ class IllinoisMapChart extends React.Component {
           settings={this.state}
           colors={colors}
         />
+        {
+          this.state.selectedLocation ?
+            <Popup
+              title={`${selectedLocation.county} County, ${selectedLocation.state}`}
+              onClose={() => this.closePopup()}
+            >
+                <ResponsiveContainer>
+                  <LineChart
+                    data={selectedLocationData.data}
+                    margin={{
+                      top: 5, right: 30, left: 20, bottom: 5,
+                    }}
+                  >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="date" tick={<CustomizedAxisTick />} interval={1} />
+                    <YAxis type="number" domain={[0, selectedLocationData.max || 'auto']} />
+                    <Tooltip content={this.renderTooltip} />
+                    <Legend />
+                    <Line type="monotone" dataKey="confirmed" stroke="#8884d8" activeDot={{ r: 8 }} />
+                    <Line type="monotone" dataKey="deaths" stroke="#82ca9d" />
+                  </LineChart>
+                </ResponsiveContainer>
+            </Popup>
+          : null
+        }
       </div>
     );
   }
