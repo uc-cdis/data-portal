fc62f9a924adfcb53216f48ddaf830a2c8042ac4
Pauline <ribeyre@uchicago.edu>
2020-04-10 14:37:23 -0500

    improve legend labels

--- src/Covid19Dashboard/ControlPanel/LegendPanel/index.jsx
+++ src/Covid19Dashboard/ControlPanel/LegendPanel/index.jsx
@@ -4,14 +4,42 @@ import PropTypes from 'prop-types';
 import './LegendPanel.less';
 
 class LegendPanel extends PureComponent {
+	dictToLegendList(colors) {
+		// input: {0: '#FFF', 10: '#888', 100: '#000'}
+		// output: [[ '0-9', '#FFF' ], [ '10-99', '#888' ], [ '100+', '#000' ]]
+		return Object.entries(colors).map((value, i) => {
+			const color = value[1];
+			let label = value[0];
+			if (i === Object.keys(colors).length - 1) {
+				label = `${label}+`;
+			}
+			else {
+				const nextLabel = Number(Object.keys(colors)[i + 1]);
+				if (nextLabel - 1 !== Number(label)) {
+					label = `${label} - ${nextLabel - 1}`;
+				}
+			}
+			return [label, color]
+		});
+	}
+
 	render() {
-		const { colors } = this.props;
+		const legendData = this.dictToLegendList(this.props.colors);
 		return (
 			<div className='legend-panel'>
 				<h3>Legend</h3>
 				<div>
 				{
-					Object.keys(colors).map((value, i)=> <p key={value}><span className='legend-panel__item' style={{backgroundColor: colors[value]}}/> {i === Object.keys(colors).length - 1 ? `${value}+` : value}</p>)
+					legendData.map(
+						(value) =>
+						<p key={value[1]}>
+							<span
+								className='legend-panel__item'
+								style={{backgroundColor: value[1]}}
+							/>
+							{value[0]}
+						</p>
+					)
 				}
 				</div>
 			</div>
