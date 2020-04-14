fed96c995776e4eaa55d496554c2cd6b758b02e1
Pauline <ribeyre@uchicago.edu>
2020-04-14 13:23:40 -0500

    improve img css

--- src/ResourceBrowser/Resource.jsx
+++ src/ResourceBrowser/Resource.jsx
@@ -1,46 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 
 import './ResourceBrowser.css';
 
 class Resource extends React.Component {
   render() {
     return (
-      <a
-        className='resource resource__link'
-        href={this.props.link}
-        target="_blank" // open in new tab
-      >
-        <h3 className='resource__title'>
-          {this.props.title}
-        </h3>
-        {this.props.description ?
-          <div className='resource__description'>
-            {this.props.description}
+      <div className='resource'>
+        <a
+          className='resource__link'
+          href={this.props.link}
+          target='_blank' // open in new tab
+          rel='noopener noreferrer'
+        >
+          <div className='resource__text-contents'>
+            <h3 className='resource__title'>
+              {this.props.title}
+            </h3>
+            {this.props.description ?
+              <div className='resource__description'>
+                {this.props.description}
+              </div>
+              : null}
           </div>
-        : null}
-        {this.props.imageUrl ?
-          <img
-            className='resource__image'
-            src={this.props.imageUrl} // TODO handle if no image
-          />
-        : null}
-      </a>
-    )
+          {this.props.imageUrl ?
+            <img
+              className='resource__image'
+              src={this.props.imageUrl}
+              alt={this.props.title}
+            />
+            : null}
+        </a>
+      </div>
+    );
   }
 }
 
 Resource.propTypes = {
   title: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired,
-  category: PropTypes.string,
   description: PropTypes.string,
   imageUrl: PropTypes.string,
 };
 
 Resource.defaultProps = {
-  category: '',
   description: '',
   imageUrl: '',
 };
