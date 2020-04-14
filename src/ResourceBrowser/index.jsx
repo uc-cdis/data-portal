44f7392bed315b9471e11061614ed3108825b67a
Pauline <ribeyre@uchicago.edu>
2020-04-14 12:52:45 -0500

    add resource browser

---
+++ src/ResourceBrowser/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+import Resource from './Resource';
+import { config } from '../params';
+
+import './ResourceBrowser.css';
+
+class ResourceBrowser extends React.Component {
+  render() {
+    // TODO use categories
+    const title = config.resourceBrowser.title;
+    const resources = config.resourceBrowser && config.resourceBrowser.resources || [];
+    return (
+      <div className='resource-browser'>
+        <h2 className='resource-browser__title'>
+          {title}
+        </h2>
+        <div className='resource-browser__resources'>
+          {resources.map((resource, i) =>
+            <Resource
+              key={i}
+              {...resource}
+            />
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ResourceBrowser;
