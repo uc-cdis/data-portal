8d3268f4567ae2923b166475d90a4293c55c3261
Mingfei Shao <mshao1@uchicago.edu>
2020-04-18 16:31:32 -0500

    fix/login redirect

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -52,8 +52,9 @@ class Login extends React.Component {
   }
 
   render() {
-    let next = basename;
     const location = this.props.location; // this is the react-router "location"
+    // compose next according to location.from
+    let next = (location.from) ? `${basename}${location.from}` : basename;
     const queryParams = querystring.parse(location.search ? location.search.replace(/^\?+/, '') : '');
     if (queryParams.next) {
       next = basename === '/' ? queryParams.next : basename + queryParams.next;
