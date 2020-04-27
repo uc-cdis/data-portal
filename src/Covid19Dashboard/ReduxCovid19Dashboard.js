91a87645e78593f27dc354a05d14eab1d92ff12a
Andrew Prokhorenkov <aprokh@uchicago.edu>
2020-04-27 11:48:43 -0500

    COVID-19 Commons graphs (#675)
    * feat: enable graph
    
    * fix
    
    * fix
    
    * feat: additional charts
    
    * fix & debug
    
    * fix
    
    * debug
    
    * feat: improve color scheme
    
    * feat: remove debug print
    
    * fix: return the proper data
    
    * feat: update
    
    * feat: change position of yaxis label
    
    * Update src/Covid19Dashboard/Covid19Dashboard.less
    
    Co-Authored-By: Pauline Ribeyre <ribeyre@uchicago.edu>
    
    * Update src/Covid19Dashboard/index.jsx
    
    Co-Authored-By: Pauline Ribeyre <ribeyre@uchicago.edu>
    
    * Update src/Covid19Dashboard/index.jsx
    
    Co-Authored-By: Pauline Ribeyre <ribeyre@uchicago.edu>
    
    * Update src/Covid19Dashboard/index.jsx
    
    Co-Authored-By: Pauline Ribeyre <ribeyre@uchicago.edu>
    
    * feat: addressing PR comments
    
    * doc: comment for mouse over
    
    * fix: 3-letter abbreviation for all months
    
    * feat: matplotlib default color palette
    
    * feat: description in defaultProps
    
    * feat: arrow function for addDays
    
    * fix: arrow function for addDays
    
    * fix: move description section back
    
    * feat: variable to disable graph, enabled by default
    
    Co-authored-by: Pauline Ribeyre <ribeyre@uchicago.edu>

--- src/Covid19Dashboard/ReduxCovid19Dashboard.js
+++ src/Covid19Dashboard/ReduxCovid19Dashboard.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import Covid19Dashboard from '../Covid19Dashboard';
 
-import { formatSeirData } from './dataUtils.js';
+import { readSingleColumnTSV, readMultiColumnTSV } from './dataUtils.js';
 
 const dataUrl = 'https://opendata.datacommons.io/';
 
@@ -9,7 +9,11 @@ async function handleChartData(propName, data) {
   switch (propName) {
   case 'seirObserved':
   case 'seirSimulated':
-    return formatSeirData(data);
+    return readSingleColumnTSV(data);
+  case 'top10':
+    return readMultiColumnTSV(data);
+  case 'idphDaily':
+    return readMultiColumnTSV(data); // change the name
   default:
     console.warn(`I don't know how to handle chart data for "${propName}"`); // eslint-disable-line no-console
     // return 'ERROR_FETCH_CHART_DATA';
--- src/Covid19Dashboard/dataUtils.js
+++ src/Covid19Dashboard/dataUtils.js
@@ -4,20 +4,54 @@ export function numberWithCommas(x) {
   return x ? x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') : 0;
 }
 
-export async function formatSeirData(tsvData) {
+export async function readSingleColumnTSV(tsvData) {
   let headers = null;
   let dateIndex;
   let valueIndex;
   const contents = {};
   papaparse.parse(tsvData, {
     worker: true,
+    skipEmptyLines: true,
     step(row) {
       if (!headers) {
         headers = row.data;
         dateIndex = headers.findIndex(x => x === 'date');
         valueIndex = headers.findIndex(x => x !== 'date');
-      } else if (row.data.length >= 2) { // skip empty line at end of file
+      } else {
         contents[row.data[dateIndex]] = row.data[valueIndex];
+      }
+    },
+  });
+  return contents;
+}
+
+export async function readMultiColumnTSV(tsvData) {
+  let headers = null;
+  let dateIndex;
+  const contents = {};
+  papaparse.parse(tsvData, {
+    worker: true,
+    skipEmptyLines: true,
+    step(row) {
+      if (!headers) {
+        headers = row.data;
+        dateIndex = headers.findIndex(x => x === 'date');
+        for (let index = 0; index < headers.length; index++) {
+          if (index === dateIndex) {
+            continue;
+          }
+          const col = headers[index];
+          contents[col] = {};
+        }
+      } else {
+        for (let index = 0; index < headers.length; index++) {
+          if (index === dateIndex) {
+            continue;
+          }
+          
+          const col = headers[index];
+          contents[col][row.data[dateIndex]] = row.data[index];
+        }
       }
     },
   });
