8e415de4e3db2893b17707114c398ade777b78e4
Zakir Gowani <zakirgowani@gmail.com>
2020-06-17 14:15:18 -0500

    fix: indexing gui request timeout

--- src/Indexing/Indexing.jsx
+++ src/Indexing/Indexing.jsx
@@ -96,12 +96,12 @@ class Indexing extends React.Component {
 
   putIndexFileToSignedURL = () => {
     const thisPointer = this;
-    return fetchWithCredsAndTimeout({
+    return fetchWithCreds({
       path: thisPointer.state.urlToIndexedFile,
       method: 'PUT',
       customHeaders: { 'Content-Type': 'application/json' },
       body: thisPointer.state.uploadedFile,
-    }, 7000).then(() => {
+    }).then(() => {
       thisPointer.setState({
         indexingFilesPopupMessage: 'Preparing indexing job...',
       });
