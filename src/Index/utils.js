38c6c742be4ef3eee0c1bf1a011da6630ea202d3
Zakir Gowani <zakirgowani@gmail.com>
2020-06-21 18:28:42 -0500

    Update from master

--- src/Index/utils.js
+++ src/Index/utils.js
@@ -22,54 +22,51 @@ const updateRedux = async projectNodeCounts => getReduxStore().then(
 // Chunk into groups of 15
 // Chunking is necessary to avoid hitting Postgres limits
 export const getChunkedPeregrineRequestUrls = (nodesToRequest) => {
-  let requestUrls = [];
+  const requestUrls = [];
 
   let k = 0;
-  let chunkSize = 15;
-  let n = nodesToRequest.length;
+  const chunkSize = 15;
+  const n = nodesToRequest.length;
 
-  if(n == 0) {
+  if (n === 0) {
     return [`${datasetUrl}?nodes=`];
   }
-  
-  while(k < n) {
-    let listRightBound = Math.min(k + chunkSize, n)
-    let nodeChunk = nodesToRequest.slice(k, listRightBound);
-    let chunkRequestUrl = `${datasetUrl}?nodes=${nodeChunk.join(',')}`;
-    requestUrls.push(chunkRequestUrl);
-    k = k + chunkSize;
 
+  while (k < n) {
+    const listRightBound = Math.min(k + chunkSize, n);
+    const nodeChunk = nodesToRequest.slice(k, listRightBound);
+    const chunkRequestUrl = `${datasetUrl}?nodes=${nodeChunk.join(',')}`;
+    requestUrls.push(chunkRequestUrl);
+    k += chunkSize;
   }
 
   return requestUrls;
-}
+};
 
-const makePeregrineRequestForNode = async (url) => {
-  return fetchWithCreds({
-    path: url,
-  }).then((res) => {
-    if(res.status == 200) {
-      return [res.data, 200];
-    }
-    return [null, res.status];
-  })
-    .catch((err) => {
-      console.log(err);
-    });
-}
+const makePeregrineRequestForNode = async url => fetchWithCreds({
+  path: url,
+}).then((res) => {
+  if (res.status === 200) {
+    return [res.data, 200];
+  }
+  return [null, res.status];
+})
+  .catch((err) => {
+    console.log(err);
+  });
 
 export const mergeChunkedChartData = (chartDataArray) => {
-  let mergedChartData = {};
-  var projects;
-  var projectKeys;
+  const mergedChartData = {};
+  let projects;
+  let projectKeys;
   for (let y = 0; y < chartDataArray.length; y += 1) {
     projects = chartDataArray[y][0];
     projectKeys = Object.keys(projects);
 
     for (let z = 0; z < projectKeys.length; z += 1) {
       if (mergedChartData[projectKeys[z]]) {
-        let nodes = projects[projectKeys[z]];
-        let nodeNames = Object.keys(nodes);
+        const nodes = projects[projectKeys[z]];
+        const nodeNames = Object.keys(nodes);
         for (let w = 0; w < nodeNames.length; w += 1) {
           mergedChartData[projectKeys[z]][nodeNames[w]] = nodes[nodeNames[w]];
         }
@@ -80,7 +77,7 @@ export const mergeChunkedChartData = (chartDataArray) => {
   }
 
   return mergedChartData;
-}
+};
 
 // loadHomepageChartdataFromDatasets queries Peregrine's /datasets endpoint for
 // summary data (counts of projects, counts of subjects, etc).
@@ -97,40 +94,38 @@ export const loadHomepageChartDataFromDatasets = async (callback) => {
   const nodesToRequest = _.union(fileNodes, nodesForIndexChart);
   const requestUrls = getChunkedPeregrineRequestUrls(nodesToRequest);
 
-  let fullResult = [];
+  const fullResult = [];
   for (let k = 0; k < requestUrls.length; k += 1) {
-    let chunkResult = await makePeregrineRequestForNode(requestUrls[k]);
+    const chunkResult = await makePeregrineRequestForNode(requestUrls[k]);
     fullResult.push(chunkResult);
   }
 
-  const queryFailure = fullResult.some((element) => element[1] !== 200);
-  const query401 =  fullResult.some((element) => element[1] === 401);
-  const query403 =  fullResult.some((element) => element[1] === 403);
-  const query404 =  fullResult.some((element) => element[1] === 404);
-  
-  if(queryFailure) {
-    if(query404) {
+  const queryFailure = fullResult.some(element => element[1] !== 200);
+  const query401 = fullResult.some(element => element[1] === 401);
+  const query403 = fullResult.some(element => element[1] === 403);
+  const query404 = fullResult.some(element => element[1] === 404);
+
+  if (queryFailure) {
+    if (query404) {
       // Shouldn't happen, this means peregrine datasets endpoint not enabled
       console.error(`REST endpoint ${datasetUrl} not enabled in Peregrine yet.`);
     }
-    if(query401 || query403 || query404) {
+    if (query401 || query403 || query404) {
       resultStatus.needLogin = true;
       if (callback) {
         return callback(resultStatus);
       }
     }
   }
-  if(query401) {
+  if (query401) {
     return;
   }
 
-  let mergedChartData = mergeChunkedChartData(fullResult);
-  console.log(fullResult);
-  console.log(mergedChartData);
+  const mergedChartData = mergeChunkedChartData(fullResult);
 
   updateRedux(mergedChartData);
   if (callback) {
-    callback(resultStatus);
+    return callback(resultStatus);
   }
 };
 
--- src/Index/utils.test.js
+++ src/Index/utils.test.js
@@ -1,48 +1,47 @@
-import { 
-  loadHomepageChartDataFromDatasets, 
-  getChunkedPeregrineRequestUrls, 
-  mergeChunkedChartData 
+import {
+  getChunkedPeregrineRequestUrls,
+  mergeChunkedChartData,
 } from './utils';
-import { homepageChartNodes, datasetUrl } from '../localconf';
+import { datasetUrl } from '../localconf';
 
-var urlPrefix = datasetUrl + '?nodes=';
+const urlPrefix = `${datasetUrl}?nodes=`;
 
 describe('the load homepage chart data flow', () => {
   it('provides accurate non-chunked queries', () => {
-    let testList1 = [];
-    let testList2 = ['aliquot'];
-    let testList3 = ['aliquot', 'sample_snp_array'];
-    
+    const testList1 = [];
+    const testList2 = ['aliquot'];
+    const testList3 = ['aliquot', 'sample_snp_array'];
+
     expect(getChunkedPeregrineRequestUrls(testList1)).toEqual([urlPrefix]);
-    expect(getChunkedPeregrineRequestUrls(testList2)).toEqual([urlPrefix + 'aliquot']);
-    expect(getChunkedPeregrineRequestUrls(testList3)).toEqual([urlPrefix + 'aliquot,sample_snp_array']);
-  })
+    expect(getChunkedPeregrineRequestUrls(testList2)).toEqual([`${urlPrefix}aliquot`]);
+    expect(getChunkedPeregrineRequestUrls(testList3)).toEqual([`${urlPrefix}aliquot,sample_snp_array`]);
+  });
 
   it('provides accurate chunked queries', () => {
-    let testList4 = ['aggregated_snp_array', 'aligned_reads', 'allele_expression', 'copy_number_variation', 'exon_expression', 'gene_expression,imaging_file', 'reference_file', 'simple_germline_variation', 'snp_array_variation', 'submitted_aligned_reads', 'submitted_cnv_array', 'submitted_snp_array', 'submitted_unaligned_reads', 'transcript_expression', 'subject', 'study', 'aliquot']
-    let testList5 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35'];
-    
+    const testList4 = ['aggregated_snp_array', 'aligned_reads', 'allele_expression', 'copy_number_variation', 'exon_expression', 'gene_expression,imaging_file', 'reference_file', 'simple_germline_variation', 'snp_array_variation', 'submitted_aligned_reads', 'submitted_cnv_array', 'submitted_snp_array', 'submitted_unaligned_reads', 'transcript_expression', 'subject', 'study', 'aliquot'];
+    const testList5 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35'];
+
     // testList4 is of length 17; chunk size is 15
     expect(getChunkedPeregrineRequestUrls(testList4)).toEqual([
-      urlPrefix + 'aggregated_snp_array,aligned_reads,allele_expression,copy_number_variation,exon_expression,gene_expression,imaging_file,reference_file,simple_germline_variation,snp_array_variation,submitted_aligned_reads,submitted_cnv_array,submitted_snp_array,submitted_unaligned_reads,transcript_expression,subject',
-      urlPrefix + 'study,aliquot'
+      `${urlPrefix}aggregated_snp_array,aligned_reads,allele_expression,copy_number_variation,exon_expression,gene_expression,imaging_file,reference_file,simple_germline_variation,snp_array_variation,submitted_aligned_reads,submitted_cnv_array,submitted_snp_array,submitted_unaligned_reads,transcript_expression,subject`,
+      `${urlPrefix}study,aliquot`,
     ]);
 
     // testList5 is of length 35
     expect(getChunkedPeregrineRequestUrls(testList5)).toEqual([
-      urlPrefix + '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15',
-      urlPrefix + '16,17,18,19,20,21,22,23,24,25,26,27,28,29,30',
-      urlPrefix + '31,32,33,34,35'
+      `${urlPrefix}1,2,3,4,5,6,7,8,9,10,11,12,13,14,15`,
+      `${urlPrefix}16,17,18,19,20,21,22,23,24,25,26,27,28,29,30`,
+      `${urlPrefix}31,32,33,34,35`,
     ]);
-  })
+  });
 
   it('correctly merges chunked chart data results', () => {
-    let chunk1 = [{'project-a': {'node-1': 17, 'node-2': 9}, 'project-b': {'node-1': 2, 'node-2': 0}}];
-    let chunk2 = [{'project-a': {'node-3': 3, 'node-4': 13}, 'project-b': {'node-3': 6, 'node-4': 2}}];
-    
+    const chunk1 = [{ 'project-a': { 'node-1': 17, 'node-2': 9 }, 'project-b': { 'node-1': 2, 'node-2': 0 } }];
+    const chunk2 = [{ 'project-a': { 'node-3': 3, 'node-4': 13 }, 'project-b': { 'node-3': 6, 'node-4': 2 } }];
+
     expect(mergeChunkedChartData([chunk1, chunk2])).toEqual({
-      'project-a': {'node-1': 17, 'node-2': 9, 'node-3': 3, 'node-4': 13}, 
-      'project-b': {'node-1': 2, 'node-2': 0, 'node-3': 6, 'node-4': 2}
+      'project-a': { 'node-1': 17, 'node-2': 9, 'node-3': 3, 'node-4': 13 },
+      'project-b': { 'node-1': 2, 'node-2': 0, 'node-3': 6, 'node-4': 2 },
     });
-  })
+  });
 });
