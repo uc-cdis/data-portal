ec390f8d363592b1dedd39b8d564dd59b5cbd7eb
Michael Ingram <mpingram@uchicago.edu>
2020-06-23 12:14:43 -0500

    Update not found text

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -8,8 +8,8 @@ class NotFound extends React.Component {
       <div className='error-placeholder__error-msg'>
         <h1>{'Oops! We can\'t seem to find the page you\'re looking for...'}</h1>
         <p>
-        Either URL is not valid, or you do not have access.
-        Please contact administrator for more information.
+        Either this URL is not valid, or you do not have access.
+        Please contact your administrator for more information.
         </p>
         <NotFoundSVG />
       </div>
