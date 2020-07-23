c569934d0d1c519926894292835de6db6870a998
Bobae Kang <bkang.dev@gmail.com>
2020-07-23 11:26:50 -0500

    Remove DataExplorer portion from story

--- src/stories/explorer.jsx
+++ src/stories/explorer.jsx
@@ -1,30 +1,22 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
-import DataExplorer from '../DataExplorer/.';
 import GuppyDataExplorer from '../GuppyDataExplorer/GuppyDataExplorer';
 import config from './explorerConfig';
 
-storiesOf('Data Explorer', module)
-  .add('Data Explorer', () => {
-    library.add(faAngleDown, faAngleUp);
-    return <DataExplorer />;
-  })
-  .add('Guppy Data Explorer', () => {
-    const guppyServerPath = 'http://localhost:3000';
-    return (
-      <GuppyDataExplorer
-        nodeCountTitle={config.nodeCountTitle}
-        chartConfig={config.charts}
-        filterConfig={config.filters}
-        tableConfig={config.table}
-        guppyConfig={{
-          path: guppyServerPath,
-          ...config.guppyConfig,
-        }}
-        buttonConfig={{ buttons: config.buttons }}
-        history={{}}
-      />
-    );
-  });
+storiesOf('Data Explorer', module).add('Guppy Data Explorer', () => {
+  const guppyServerPath = 'http://localhost:3000';
+  return (
+    <GuppyDataExplorer
+      nodeCountTitle={config.nodeCountTitle}
+      chartConfig={config.charts}
+      filterConfig={config.filters}
+      tableConfig={config.table}
+      guppyConfig={{
+        path: guppyServerPath,
+        ...config.guppyConfig,
+      }}
+      buttonConfig={{ buttons: config.buttons }}
+      history={{}}
+    />
+  );
+});
