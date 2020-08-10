82d21dde18f4436e4c52d084ccfbda80d7d2edab
Mingfei Shao <mshao1@uchicago.edu>
2020-08-10 12:48:57 -0500

    rearrange

--- src/StudyViewer/StudyViewer.jsx
+++ src/StudyViewer/StudyViewer.jsx
@@ -9,6 +9,34 @@ import StudyCard from './StudyCard';
 
 export const data = [
   {
+    title: 'The Adaptive COVID-19 Treatment Trial (ATCC)',
+    description: 'This study is an adaptive, randomized, double-blind, placebo-controlled trial to evaluate the safety and efficacy of novel therapeutic agents in hospitalized adults diagnosed with COVID-19. The study is a multicenter trial that will be conducted in up to approximately 100 sites globally. The study will compare different investigational therapeutic agents to a control arm. There will be interim monitoring to introduce new arms and allow early stopping for futility, efficacy, or safety.',
+    url: '/ATCC',
+    meta: {
+      condition: 'COVID-19',
+      study_design: 'Interventional (Clinical Trial), Randomized Allocation',
+      sponsor: 'National Institute of Allergy and Infectious Diseases (NIAID)',
+      study_dates: 'February 21, 2020 - May 21, 2020',
+      data_available: 'Patient-level data',
+      trial_website: <a href='https://clinicaltrials.gov/ct2/show/NCT04280705'>Visit NIH.gov</a>,
+    },
+    document: [
+      {
+        name: 'Data Dictionary',
+        format: 'PDF',
+        link: '/',
+        size: 763843,
+      },
+      {
+        name: 'Protocol',
+        format: 'PDF',
+        link: '/',
+        size: 7638643,
+      },
+    ],
+    hasAccess: false,
+  },
+  {
     title: 'COVID-19-associated Lymphopenia Pathogenesis Study in Blood (CALYPSO)',
     description: 'This study is an adaptive, randomized, double-blind, placebo-controlled trial to evaluate the safety and efficacy of novel therapeutic agents in hospitalized adults diagnosed with COVID-19. The study is a multicenter trial that will be conducted in up to approximately 100 sites globally. The study will compare different investigational therapeutic agents to a control arm. There will be interim monitoring to introduce new arms and allow early stopping for futility, efficacy, or safety.',
     url: '/CALYPSO',
@@ -47,34 +75,6 @@ export const data = [
       },
     ],
     hasAccess: true,
-  },
-  {
-    title: 'The Adaptive COVID-19 Treatment Trial (ATCC)',
-    description: 'This study is an adaptive, randomized, double-blind, placebo-controlled trial to evaluate the safety and efficacy of novel therapeutic agents in hospitalized adults diagnosed with COVID-19. The study is a multicenter trial that will be conducted in up to approximately 100 sites globally. The study will compare different investigational therapeutic agents to a control arm. There will be interim monitoring to introduce new arms and allow early stopping for futility, efficacy, or safety.',
-    url: '/ATCC',
-    meta: {
-      condition: 'COVID-19',
-      study_design: 'Interventional (Clinical Trial), Randomized Allocation',
-      sponsor: 'National Institute of Allergy and Infectious Diseases (NIAID)',
-      study_dates: 'February 21, 2020 - May 21, 2020',
-      data_available: 'Patient-level data',
-      trial_website: <a href='https://clinicaltrials.gov/ct2/show/NCT04280705'>Visit NIH.gov</a>,
-    },
-    document: [
-      {
-        name: 'Data Dictionary',
-        format: 'PDF',
-        link: '/',
-        size: 763843,
-      },
-      {
-        name: 'Protocol',
-        format: 'PDF',
-        link: '/',
-        size: 7638643,
-      },
-    ],
-    hasAccess: false,
   },
   {
     title: 'Longitudinal Study of COVID-19 Sequelae and Immunity (RECON_19)',
