4a8407884c21a421c4145fdecd212a59f86f4538
Mingfei Shao <2475897+mfshao@users.noreply.github.com>
2020-08-21 10:08:03 -0500

    PXP-6516 Fix/layout adjust (#722)
    * replace mui with antd
    
    * feat: submission components
    
    * fix warning in console
    
    * fix style
    
    * npm audit
    
    * text
    
    * fix style
    
    * cleanup
    
    * css
    
    * Update src/components/tables/ProjectTable.test.jsx
    
    Co-authored-by: Michael Ingram <mpingram@uchicago.edu>
    
    * clean
    
    * fix css
    
    Co-authored-by: Michael Ingram <mpingram@uchicago.edu>

--- src/Submission/SubmitNodeForm.jsx
+++ src/Submission/SubmitNodeForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Divider, Form } from 'antd';
 import OneOfInput from './OneOfInput';
 import EnumInput from './EnumInput';
 import AnyOfInput from './AnyOfInput';
@@ -18,82 +19,94 @@ const SubmitNodeForm = ({
   handleSubmit,
 }) => (
   <div>
-    <form onSubmit={handleSubmit}>{properties.map((property) => {
-      let description = ('description' in node.properties[property]) ? node.properties[property].description : '';
-      if (description === '') {
-        description = ('term' in node.properties[property]) ? node.properties[property].term.description : '';
-      }
-      const required = (requireds.indexOf(property) > -1);
+    <Form onFinish={handleSubmit}>
+      {properties.map((property, index) => {
+        let description = ('description' in node.properties[property]) ? node.properties[property].description : '';
+        if (description === '') {
+          description = ('term' in node.properties[property]) ? node.properties[property].term.description : '';
+        }
+        const required = (requireds.indexOf(property) > -1);
 
-      if (property === 'type') {
-        return null;
-      } else if ('enum' in node.properties[property]) {
+        let inputComponent;
+        if (property === 'type') {
+          inputComponent = null;
+        } else if ('enum' in node.properties[property]) {
+          inputComponent = (
+            <EnumInput
+              key={property}
+              name={property}
+              options={node.properties[property].enum}
+              onChange={onChangeEnum}
+              required={required}
+              onUpdateFormSchema={onUpdateFormSchema}
+              propertyType='string'
+              description={description}
+            />);
+        } else if ('oneOf' in node.properties[property]) {
+          inputComponent = (
+            <OneOfInput
+              key={property}
+              value={form[property]}
+              property={node.properties[property].oneOf}
+              name={property}
+              required={required}
+              description={description}
+              onChange={onChange}
+              onChangeEnum={onChangeEnum}
+              onUpdateFormSchema={onUpdateFormSchema}
+            />);
+        } else if ('anyOf' in node.properties[property]) {
+          inputComponent = (
+            <AnyOfInput
+              key={property}
+              values={form[property]}
+              name={property}
+              node={node.properties[property].anyOf[0].items}
+              properties={Object.keys(node.properties[property].anyOf[0].items.properties)}
+              required={required}
+              requireds={requireds}
+              onChange={onChangeAnyOf}
+            />
+          );
+        } else {
+          let propertyType = node.properties[property].type;
+          if (typeof (propertyType) === 'object') {
+            /* just use the first type if it allows multiple types */
+            propertyType = propertyType[0];
+          }
+          inputComponent = (
+            <TextInput
+              id={property}
+              key={property}
+              name={property}
+              onUpdateFormSchema={onUpdateFormSchema}
+              propertyType={propertyType}
+              value={form[property]}
+              required={required}
+              description={description}
+              onChange={onChange}
+            />);
+        }
         return (
-          <EnumInput
-            key={property}
-            name={property}
-            options={node.properties[property].enum}
-            onChange={onChangeEnum}
-            required={required}
-            onUpdateFormSchema={onUpdateFormSchema}
-            propertyType='string'
-            description={description}
-          />);
-      } else if ('oneOf' in node.properties[property]) {
-        return (
-          <OneOfInput
-            key={property}
-            value={form[property]}
-            property={node.properties[property].oneOf}
-            name={property}
-            required={required}
-            description={description}
-            onChange={onChange}
-            onChangeEnum={onChangeEnum}
-            onUpdateFormSchema={onUpdateFormSchema}
-          />);
-      } else if ('anyOf' in node.properties[property]) {
-        return (
-          <AnyOfInput
-            key={property}
-            values={form[property]}
-            name={property}
-            node={node.properties[property].anyOf[0].items}
-            properties={Object.keys(node.properties[property].anyOf[0].items.properties)}
-            required={required}
-            requireds={requireds}
-            onChange={onChangeAnyOf}
-          />
+          <React.Fragment key={index}>
+            {inputComponent}
+            {(index !== properties.length - 1) ? <Divider dashed /> : null}
+          </React.Fragment>
         );
-      }
-      let propertyType = node.properties[property].type;
-      if (typeof (propertyType) === 'object') {
-        /* just use the first type if it allows multiple types */
-        propertyType = propertyType[0];
-      }
-      return (
-        <TextInput
-          key={property}
-          name={property}
-          onUpdateFormSchema={onUpdateFormSchema}
-          propertyType={propertyType}
-          value={form[property]}
-          required={required}
-          description={description}
-          onChange={onChange}
-        />);
-    })}
-    <button type='submit' value='Submit' className='button-primary-white submit-node-form__upload-form-button'>
-        Upload submission json from form
-    </button>
-    </form>
+      })}
+      <Form.Item>
+        <button type='submit' value='Submit' className='button-primary-white submit-node-form__upload-form-button'>
+        Generate submission JSON from form
+        </button>
+      </Form.Item>
+    </Form>
   </div>
 );
 
 SubmitNodeForm.propTypes = {
   node: PropTypes.any.isRequired,
   form: PropTypes.object.isRequired,
-  properties: PropTypes.object.isRequired,
+  properties: PropTypes.array.isRequired,
   requireds: PropTypes.array,
   onChange: PropTypes.func.isRequired,
   onChangeEnum: PropTypes.func.isRequired,
--- src/components/tables/ProjectTable.test.jsx
+++ src/components/tables/ProjectTable.test.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { StaticRouter } from 'react-router-dom';
 
 import ProjectTable from './ProjectTable';
@@ -13,13 +12,10 @@ test('Project tables renders', () => {
   }];
   const summaryCounts = [5, 20, 30, 200];
 
-  // Material-UI components require the Mui theme ...
   const table = mount(
-    <MuiThemeProvider>
-      <StaticRouter location={{ pathname: '/identity' }} context={{}}>
-        <ProjectTable projectList={projectList} summaryCounts={summaryCounts} />
-      </StaticRouter>
-    </MuiThemeProvider>,
+    <StaticRouter location={{ pathname: '/identity' }} context={{}}>
+      <ProjectTable projectList={projectList} summaryCounts={summaryCounts} />
+    </StaticRouter>,
   );
   // 2 == 1 data row + 1 summary totals row
   expect(table.find('tbody tr').length).toBe(1);
