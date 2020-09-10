905e664c3856b6bf940ee0e3c49e046ae8134aca
Bobae Kang <bkang.dev@gmail.com>
2020-09-10 11:18:23 -0500

    Add external link buttons to TopBar PEDS-99
    Links to relevant page on https://commons.cri.uchicago.edu:
    - About PCDC -> /pcdc_consortium
    - Our Sponsors -> /sponsors

--- src/components/layout/TopBar.jsx
+++ src/components/layout/TopBar.jsx
@@ -17,72 +17,96 @@ class TopBar extends Component {
       <div className='top-bar'>
         <header className='top-bar__header'>
           <nav className='top-bar__nav'>
-            {this.props.topItems.map((item) => {
-              let buttonText = item.name;
-              if (item.name === 'Submit Data' && useArboristUI) {
-                if (
-                  userHasMethodOnAnyProject(
-                    'create',
-                    this.props.userAuthMapping
-                  )
-                ) {
-                  buttonText = 'Submit/Browse Data';
-                } else {
-                  buttonText = 'Browse Data';
+            <div className='top-bar__nav--hidden-lg-and-down'>
+              <a
+                className='top-bar__link'
+                href='https://commons.cri.uchicago.edu/pcdc-consortium/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <TopIconButton icon='external-link' name='About PCDC' />
+              </a>
+              <a
+                className='top-bar__link'
+                href='https://commons.cri.uchicago.edu/sponsors/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <TopIconButton icon='external-link' name='Our Sponsors' />
+              </a>
+            </div>
+            <div className='top-bar__nav--flex-center'>
+              {this.props.topItems.map((item) => {
+                let buttonText = item.name;
+                if (item.name === 'Submit Data' && useArboristUI) {
+                  if (
+                    userHasMethodOnAnyProject(
+                      'create',
+                      this.props.userAuthMapping
+                    )
+                  ) {
+                    buttonText = 'Submit/Browse Data';
+                  } else {
+                    buttonText = 'Browse Data';
+                  }
                 }
-              }
-              return item.link.startsWith('http') ? (
-                <a
-                  className='top-bar__link'
-                  key={item.link}
-                  href={item.link}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <TopIconButton
-                    name={buttonText}
-                    icon={item.icon}
-                    isActive={this.isActive(item.link)}
-                    onActiveTab={() => this.props.onActiveTab(item.link)}
-                  />
-                </a>
-              ) : (
-                <Link className='top-bar__link' key={item.link} to={item.link}>
-                  <TopIconButton
-                    name={buttonText}
-                    icon={item.icon}
-                    isActive={this.isActive(item.link)}
-                    onActiveTab={() => this.props.onActiveTab(item.link)}
-                  />
-                </Link>
-              );
-            })}
-            {this.props.user.username !== undefined && (
-              <React.Fragment>
-                <Link className='top-bar__link' to='/identity'>
-                  <TopIconButton
-                    icon='user-circle'
-                    name={this.props.user.username}
-                    isActive={this.isActive('/identity')}
-                    onActiveTab={() => this.props.onActiveTab('/identity')}
-                  />
-                </Link>
-                <Link
-                  className='top-bar__link'
-                  to='#'
-                  onClick={this.props.onLogoutClick}
-                >
-                  <TopIconButton icon='exit' name='Logout' />
-                </Link>
-              </React.Fragment>
-            )}
-            {typeof this.props.user.username === 'undefined' && (
-              <React.Fragment>
-                <Link className='top-bar__link' to='/login'>
-                  <TopIconButton icon='exit' name='Login' />
-                </Link>
-              </React.Fragment>
-            )}
+                return item.link.startsWith('http') ? (
+                  <a
+                    className='top-bar__link'
+                    key={item.link}
+                    href={item.link}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    <TopIconButton
+                      name={buttonText}
+                      icon={item.icon}
+                      isActive={this.isActive(item.link)}
+                      onActiveTab={() => this.props.onActiveTab(item.link)}
+                    />
+                  </a>
+                ) : (
+                  <Link
+                    className='top-bar__link'
+                    key={item.link}
+                    to={item.link}
+                  >
+                    <TopIconButton
+                      name={buttonText}
+                      icon={item.icon}
+                      isActive={this.isActive(item.link)}
+                      onActiveTab={() => this.props.onActiveTab(item.link)}
+                    />
+                  </Link>
+                );
+              })}
+              {this.props.user.username !== undefined && (
+                <React.Fragment>
+                  <Link className='top-bar__link' to='/identity'>
+                    <TopIconButton
+                      icon='user-circle'
+                      name={this.props.user.username}
+                      isActive={this.isActive('/identity')}
+                      onActiveTab={() => this.props.onActiveTab('/identity')}
+                    />
+                  </Link>
+                  <Link
+                    className='top-bar__link'
+                    to='#'
+                    onClick={this.props.onLogoutClick}
+                  >
+                    <TopIconButton icon='exit' name='Logout' />
+                  </Link>
+                </React.Fragment>
+              )}
+              {typeof this.props.user.username === 'undefined' && (
+                <React.Fragment>
+                  <Link className='top-bar__link' to='/login'>
+                    <TopIconButton icon='exit' name='Login' />
+                  </Link>
+                </React.Fragment>
+              )}
+            </div>
           </nav>
         </header>
       </div>
