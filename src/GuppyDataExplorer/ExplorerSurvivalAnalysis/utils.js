bcd3a4a88a1940ec57bb15bc909150db79d05e9e
Bobae Kang <bkang.dev@gmail.com>
2020-09-29 16:21:10 -0500

    Fix maxTime logic in getXAxisTicks

--- src/GuppyDataExplorer/ExplorerSurvivalAnalysis/utils.js
+++ src/GuppyDataExplorer/ExplorerSurvivalAnalysis/utils.js
@@ -31,7 +31,7 @@ export const getFactors = (aggsData) => {
 export const getXAxisTicks = (data, step = 2) => {
   const times = data.flatMap(({ data }) => data).map(({ time }) => time);
   const minTime = Math.floor(Math.min(...times));
-  const maxTime = Math.floor(Math.max(...times)) + 1;
+  const maxTime = Math.ceil(Math.max(...times));
 
   const ticks = [];
   for (let tick = minTime; tick <= maxTime; tick += step) ticks.push(tick);
