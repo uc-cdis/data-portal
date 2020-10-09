1a6c5556d9569067db3b4917fa959d217b4e90e4
Bobae Kang <bkang.dev@gmail.com>
2020-10-09 16:52:33 -0500

    Add a reducer for action type RECEIVE_INDEX_PAGE_COUNTS

--- src/Index/reducers.js
+++ src/Index/reducers.js
@@ -1,5 +1,23 @@
 const index = (state = {}, action) => {
   switch (action.type) {
+    case 'RECEIVE_INDEX_PAGE_COUNTS': {
+      const { names, count_total, ...counts } = action.data;
+
+      const projectsByName = {};
+      for (const [key, count] of Object.entries(counts)) {
+        const index = parseInt(key.split('_')[1]);
+        const name = names[index];
+        projectsByName[name] = { code: name, counts: [count], name };
+      }
+
+      return {
+        ...state,
+        projectsByName,
+        summaryCounts: { 0: count_total },
+        lastestListUpdating: Date.now(),
+        countNames: ['Subjects'],
+      };
+    }
     case 'RECEIVE_INDEX_PAGE_CHART_DATASETS': {
       const { projectNodeCounts, homepageChartNodes } = action;
       const nodesForIndexChart = homepageChartNodes.map((item) => item.node);
