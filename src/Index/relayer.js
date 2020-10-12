4ca9081aa1fe20eff3bef35d25895fe217609c3c
Bobae Kang <bkang.dev@gmail.com>
2020-10-12 11:09:22 -0500

    Integrate helper funcs into getIndexPageChartData
    And remove unused funcs

--- src/Index/relayer.js
+++ src/Index/relayer.js
@@ -4,101 +4,18 @@ import { GQLHelper } from '../gqlHelper';
 import getReduxStore from '../reduxStore';
 
 const gqlHelper = GQLHelper.getGQLHelper();
+const dataContributorIdList = GQLHelper.getDataContributorIdList();
 
-const updateRedux = async ({ projectList, summaryCounts }) =>
-  getReduxStore().then(
-    (store) => {
-      const indexState = store.getState().index || {};
-      if (!indexState.projectsByName) {
-        store.dispatch({
-          type: 'RECEIVE_INDEX_PAGE_CHART_PROJECT_LIST',
-          data: { projectList, summaryCounts },
-        });
-        return 'dispatch';
-      }
-      return 'NOOP';
+export const getIndexPageChartData = () => {
+  checkIndexState('lastestListUpdating').then(
+    (res) => {
+      if (res === 'OLD')
+        fetchQuery(environment, gqlHelper.indexPageCountsQuery, {}).then(
+          updateRedux
+        );
     },
-    (err) => {
-      console.error('WARNING: failed to load redux store', err);
-      return 'ERR';
-    }
+    (error) => updateReduxError(error)
   );
-
-const updateReduxError = async (error) =>
-  getReduxStore().then((store) => {
-    store.dispatch({ type: 'RECEIVE_RELAY_FAIL', data: error });
-  });
-
-/**
- * Translate relay properties to {summaryCounts, projectList} structure
- * that is friendly to underlying components.
- *
- * @param data
- * @return {projectList, summaryCounts}
- */
-const transformRelayProps = (data) => {
-  const nodeCount = Object.keys(data).length - 1;
-  const projectList = (data.projectList || []).map((proj) =>
-    // fill in missing properties
-    Object.assign(
-      { name: 'unknown', counts: new Array(nodeCount).fill(0), charts: [0, 0] },
-      proj
-    )
-  );
-  const summaryCounts = Object.keys(data)
-    .filter((key) => key.indexOf('count') === 0)
-    .map((key) => key)
-    .sort()
-    .map((key) => data[key]);
-  return {
-    projectList,
-    summaryCounts,
-  };
-};
-
-const extractCounts = (data) => {
-  let counts = Object.keys(data)
-    .filter((key) => key.indexOf('count') === 0)
-    .map((key) => key)
-    .sort()
-    .map((key) => data[key]);
-  return counts;
-};
-
-const extractCharts = (data) =>
-  Object.keys(data)
-    .filter((key) => key.indexOf('chart') === 0)
-    .map((key) => key)
-    .sort()
-    .map((key) => data[key]);
-
-const updateProjectDetailToRedux = (projInfo) => {
-  getReduxStore()
-    .then((store) => {
-      store.dispatch({
-        type: 'RECEIVE_INDEX_PAGE_CHART_PROJECT_DETAIL',
-        data: projInfo,
-      });
-    })
-    .catch((err) => {
-      /* eslint no-console: ["error", { allow: ["error"] }] */
-      console.error('WARNING: failed to load redux store', err);
-    });
-};
-
-const getProjectDetail = (projectList) => {
-  projectList.forEach((project) => {
-    fetchQuery(environment, gqlHelper.projectDetailQuery, {
-      name: project.name,
-    }).then((data) => {
-      const projInfo = {
-        ...data.project[0],
-        counts: extractCounts(data),
-        charts: extractCharts(data),
-      };
-      updateProjectDetailToRedux(projInfo);
-    });
-  });
 };
 
 const checkIndexState = (stateName) =>
@@ -122,47 +39,26 @@ const checkIndexState = (stateName) =>
     }
   );
 
-const getHomepageChartProjectsList = () => {
-  checkIndexState('lastestListUpdating').then(
-    (res) => {
-      if (res === 'OLD') {
-        fetchQuery(environment, gqlHelper.indexPageQuery, {}).then(
-          (data) => {
-            const { projectList, summaryCounts } = transformRelayProps(data);
-            updateRedux({ projectList, summaryCounts }).then(() =>
-              getProjectDetail(projectList)
-            );
-          },
-          (error) => {
-            updateReduxError(error);
-          }
-        );
+const updateRedux = (data) =>
+  getReduxStore().then(
+    (store) => {
+      const indexState = store.getState().index || {};
+      if (!indexState.projectsByName) {
+        store.dispatch({
+          type: 'RECEIVE_INDEX_PAGE_COUNTS',
+          data: { ...data, names: dataContributorIdList },
+        });
+        return 'dispatch';
       }
+      return 'NOOP';
     },
-    (error) => {
-      updateReduxError(error);
+    (err) => {
+      console.error('WARNING: failed to load redux store', err);
+      return 'ERR';
     }
   );
-};
 
-export const getIndexPageChartData = () => {
-  fetchQuery(environment, gqlHelper.indexPageCountsQuery, {}).then(
-    (data) =>
-      getReduxStore().then(
-        (store) => {
-          store.dispatch({
-            type: 'RECEIVE_INDEX_PAGE_COUNTS',
-            data: { ...data, names: GQLHelper.getDataContributorIdList() },
-          });
-          return 'dispatch';
-        },
-        (err) => {
-          console.error('WARNING: failed to load redux store', err);
-          return 'ERR';
-        }
-      ),
-    (error) => console.error(error)
+const updateReduxError = (error) =>
+  getReduxStore().then((store) =>
+    store.dispatch({ type: 'RECEIVE_RELAY_FAIL', data: error })
   );
-};
-
-export default getHomepageChartProjectsList;
