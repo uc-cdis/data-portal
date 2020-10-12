a7ba9e33c35c0a4ccf06c42d831f0cfef3c658b3
Bobae Kang <bkang.dev@gmail.com>
2020-10-12 11:20:18 -0500

    Remove unused reducers

--- src/Index/reducers.js
+++ src/Index/reducers.js
@@ -18,67 +18,6 @@ const index = (state = {}, action) => {
         countNames: ['Subjects'],
       };
     }
-    case 'RECEIVE_INDEX_PAGE_CHART_DATASETS': {
-      const { projectNodeCounts, homepageChartNodes } = action;
-      const nodesForIndexChart = homepageChartNodes.map((item) => item.node);
-
-      // constructing projct counts for index bar chart
-      const projectsByName = {};
-      Object.keys(projectNodeCounts).forEach((proj) => {
-        let code = proj;
-        const projCodeIndex = proj.indexOf('-');
-        if (projCodeIndex !== -1) {
-          code = proj.substring(projCodeIndex + 1);
-        }
-        let counts = 0;
-        if (projectNodeCounts[proj]) {
-          counts = nodesForIndexChart.map(
-            (node) => projectNodeCounts[proj][node]
-          );
-        }
-
-        projectsByName[proj] = {
-          code,
-          counts,
-          name: proj,
-        };
-      });
-
-      const countNames = homepageChartNodes.map((item) => item.name);
-
-      return { ...state, projectsByName, countNames };
-    }
-    case 'RECEIVE_INDEX_PAGE_CHART_PROJECT_LIST': {
-      //
-      // Note - save projectsByName, b/c we acquire more data for individual tables
-      // over time
-      //
-      const projectsByName = Object.assign({}, state.projectsByName || {});
-      action.data.projectList.forEach((proj) => {
-        const old = projectsByName[proj.name] || {};
-        projectsByName[proj.name] = Object.assign(old, proj);
-      });
-      const summaryCounts = Object.assign(
-        {},
-        state.summaryCounts || {},
-        action.data.summaryCounts
-      );
-      const lastestListUpdating = Date.now();
-      // const { error, ...state } = state;
-      return {
-        ...state,
-        projectsByName,
-        summaryCounts,
-        lastestListUpdating,
-        countNames: ['Persons', 'Subjects'],
-      };
-    }
-    case 'RECEIVE_INDEX_PAGE_CHART_PROJECT_DETAIL': {
-      const projectsByName = Object.assign({}, state.projectsByName || {});
-      projectsByName[action.data.name] = action.data;
-      const lastestDetailsUpdating = Date.now();
-      return { ...state, projectsByName, lastestDetailsUpdating };
-    }
     default:
       return state;
   }
