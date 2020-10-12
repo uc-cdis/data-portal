17b5fc724ec09407872415cceb4a229f8bfee090
Bobae Kang <bkang.dev@gmail.com>
2020-10-12 11:18:43 -0500

    BREAKING CHANGE: assume index page is never public

--- src/Index/page.jsx
+++ src/Index/page.jsx
@@ -10,27 +10,12 @@ import {
 import { getIndexPageChartData } from './relayer';
 import dictIcons from '../img/icons';
 import { components } from '../params';
-import { loadHomepageChartDataFromDatasets } from './utils';
-import { breakpoints, indexPublic } from '../localconf';
+import { breakpoints } from '../localconf';
 import './page.less';
 
 class IndexPageComponent extends React.Component {
   componentDidMount() {
-    // If the index page is publicly available, we will attempt to show the
-    // homepage charts on the landing page to logged-out users as well as logged-in users.
-    // To do this we will need to load data from Peregrine's datasets endpoint, which
-    // can be configured to be publicly accessible to logged-out users
-    // (with global.public_datasets: true in manifest.json)
-    if (indexPublic) {
-      loadHomepageChartDataFromDatasets((res) => {
-        // If Peregrine returns unauthorized, need to redirect to `/login` page.
-        if (res.needLogin) {
-          this.props.history.push('/login');
-        }
-      });
-    } else {
-      getIndexPageChartData();
-    }
+    getIndexPageChartData();
   }
 
   render() {
