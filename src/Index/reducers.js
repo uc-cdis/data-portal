94ea2057b5136ef6a7aee871074611733d2a80f5
Bobae Kang <bkang.dev@gmail.com>
2020-10-14 15:41:58 -0500

    Add a reducer for action type RECEIVE_INDEX_PAGE_OVERVIEW_COUNTS

--- src/Index/reducers.js
+++ src/Index/reducers.js
@@ -18,6 +18,35 @@ const index = (state = {}, action) => {
         countNames: ['Subjects'],
       };
     }
+    case 'RECEIVE_INDEX_PAGE_OVERVIEW_COUNTS': {
+      const { names, counts_total, ...counts } = action.data;
+
+      const total = {};
+      for (const val of counts_total)
+        for (const count of Object.keys(val)) {
+          if (total.hasOwnProperty(count)) total[count] += val[count];
+          else total[count] = val[count];
+        }
+
+      const byConsortium = {};
+      for (const [key, vals] of Object.entries(counts)) {
+        const index = parseInt(key.split('_')[1]) - 1;
+        const name = names[index];
+
+        byConsortium[name] = {};
+        for (const val of vals)
+          for (const count of Object.keys(val)) {
+            if (byConsortium[name].hasOwnProperty(count))
+              byConsortium[name][count] += val[count];
+            else byConsortium[name][count] = val[count];
+          }
+      }
+
+      return {
+        ...state,
+        overviewCounts: { ...byConsortium, total, updatedAt: Date.now() },
+      };
+    }
     default:
       return state;
   }
