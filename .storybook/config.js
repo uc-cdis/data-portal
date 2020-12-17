285cee1409aab51f102bb49c767ddfba88ffb071
Bobae Kang <bkang.dev@gmail.com>
2020-12-17 15:03:29 -0600

    Stop loading non-existing story files

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,11 +5,8 @@ import { faAngleUp, faAngleDown } from '@fortawesome/free-solid-svg-icons';
 function loadStories() {
   require('../src/stories/index.jsx');
   require('../src/stories/buttons.jsx');
-  require('../src/stories/charts.jsx');
-  require('../src/stories/tables.jsx');
   require('../src/stories/cards.jsx');
   require('../src/stories/explorer.jsx');
-  require('../src/stories/filters.jsx');
   require('../src/stories/user-agreement.jsx');
   library.add(faAngleUp, faAngleDown);
 }
