baa9d4e7ff83e726199b9f027c69e4172b48a6b5
ocshawn <shawnoconnor@uchicago.edu>
2020-12-17 17:09:34 -0600

    refactor/comma-numbers: removed numberWithCommas function made everything use built in js function

--- src/Covid19Dashboard/WorldMapChart/index.jsx
+++ src/Covid19Dashboard/WorldMapChart/index.jsx
@@ -5,7 +5,6 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 import { mapboxAPIToken } from '../../localconf';
 import ControlPanel from '../ControlPanel';
-import { numberWithCommas } from '../dataUtils.js';
 import worldData from '../data/world_50m'; // from https://geojson-maps.ash.ms
 import stateData from '../data/us_states_20m.json'; // from https://eric.clst.org/tech/usgeojson/
 import countyData from '../data/us_counties';
@@ -143,11 +142,11 @@ class WorldMapChart extends React.Component {
         return;
       }
       let confirmed = feature.properties.confirmed;
-      confirmed = confirmed && confirmed !== 'null' ? confirmed : 0;
+      confirmed = confirmed && confirmed !== 'null' ? Number(confirmed).toLocaleString() : 0;
       let deaths = feature.properties.deaths;
-      deaths = deaths && deaths !== 'null' ? deaths : 0;
+      deaths = deaths && deaths !== 'null' ? Number(deaths).toLocaleString() : 0;
       let recovered = feature.properties.recovered;
-      recovered = recovered && recovered !== 'null' ? recovered : 0;
+      recovered = recovered && recovered !== 'null' ? Number(recovered).toLocaleString() : 0;
 
       const state = feature.properties.province_state;
       const county = feature.properties.county;
@@ -165,12 +164,12 @@ class WorldMapChart extends React.Component {
         // choropleth map data contains FIPS.
         FIPS: `${feature.properties.FIPS || feature.properties.fips}`,
         values: {
-          'confirmed cases': numberWithCommas(confirmed),
-          deaths: numberWithCommas(deaths),
+          'confirmed cases': confirmed,
+          deaths: deaths,
         },
       };
       if (recovered) {
-        hoverInfo.values.recovered = numberWithCommas(recovered);
+        hoverInfo.values.recovered = recovered;
       }
     });
 
