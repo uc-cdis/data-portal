855efbbb2b46004e174a72b983654f8f0e6abcc4
Bobae Kang <bkang.dev@gmail.com>
2020-12-21 16:13:03 -0600

    Ensure no redux devtool for production build

--- src/reduxStore.js
+++ src/reduxStore.js
@@ -24,7 +24,9 @@ const preloadedState =
     : { user: {}, status: {} };
 
 const composeEnhancers =
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  process.env.NODE_ENV !== 'proudction' &&
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     : compose;
 const enhancer = composeEnhancers(applyMiddleware(thunk));
