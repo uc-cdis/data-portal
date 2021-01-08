8870b88c115c3731faeae99938305e8fb056a5be
Michael Ingram <mpingram@uchicago.edu>
2021-01-08 15:52:15 -0600

    [WIP] Write HTML/CSS skeleton of modal

--- src/DiscoveryBeta/index.tsx
+++ src/DiscoveryBeta/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Input, Table, Tag, Radio, Checkbox, Button } from 'antd';
+import { Input, Table, Tag, Radio, Checkbox, Button, Space } from 'antd';
 import { LockOutlined, LockFilled, LinkOutlined, UnlockOutlined, SearchOutlined, StarOutlined, StarFilled, StarTwoTone, CloseOutlined } from '@ant-design/icons';
 
 import './DiscoveryBeta.css';
@@ -102,111 +102,156 @@ const data = [
   },
 ];
 
-class DiscoveryBeta extends React.PureComponent {
-  render() {
-    return (<div className='discovery-container'>
-      <h1 className='discovery-page-title'>DISCOVERY</h1>
-      <div className='discovery-header'>
-        <div className='discovery-header__stats-container'>
-          <div className='discovery-header__stat'>
-            <div className='discovery-header__stat-number'>
-              137
-            </div>
-            <div className='discovery-header__stat-label'>
-              Studies
-            </div>
+const DiscoveryBeta: React.FunctionComponent = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  return (<div className='discovery-container'>
+    <h1 className='discovery-page-title'>DISCOVERY</h1>
+    <div className='discovery-header'>
+      <div className='discovery-header__stats-container'>
+        <div className='discovery-header__stat'>
+          <div className='discovery-header__stat-number'>
+            137
           </div>
-          <div className='discovery-header__stat'>
-            <div className='discovery-header__stat-number'>
-              306,583
-            </div>
-            <div className='discovery-header__stat-label'>
-              Files
-            </div>
-          </div>
-          <div className='discovery-header__stat'>
-            <div className='discovery-header__stat-number'>
-              406,853
-            </div>
-            <div className='discovery-header__stat-label'>
-              Subjects
-            </div>
-          </div>
-          <div className='discovery-header__stat'>
-            <div className='discovery-header__stat-number'>
-              3.03 PB
-            </div>
-            <div className='discovery-header__stat-label'>
-              Data
-            </div>
+          <div className='discovery-header__stat-label'>
+            Studies
           </div>
         </div>
-        <div className='discovery-header__tags-container' >
-          <h3 className='discovery-header__tags-header'>ASSOCIATED TAGS BY CATEGORY</h3>
-          <div className='discovery-header__tags'>
-            <div className='discovery-header__tag-group'>
-              <h5>Program</h5>
-              <Tag className='discovery-header__tag' color={'rgba(129, 211, 248, 0.5)'}>TOPMed</Tag>
-              <Tag className='discovery-header__tag' color={'rgba(129, 211, 248)'}>COVID 19</Tag>
-              <Tag className='discovery-header__tag' color={'rgba(129, 211, 248)'}>Parent</Tag>
-            </div>
-            <div className='discovery-header__tag-group'>
-              <h5>Disease</h5>
-              <Tag className='discovery-header__tag' color={'rgba(236, 128, 141)'}>Blood Disease</Tag>
-              <Tag className='discovery-header__tag' color={'rgba(236, 128, 141)'}>Lung Disease</Tag>
-              <Tag className='discovery-header__tag' color={'rgba(236, 128, 141)'}>Heart Disease</Tag>
-            </div>
-            <div className='discovery-header__tag-group'>
-              <h5>Data Freeze</h5>
-              <Tag className='discovery-header__tag' color={'rgba(112, 182, 3)'}>Freeze 5</Tag>
-              <Tag className='discovery-header__tag' color={'rgba(112, 182, 3)'}>Freeze 8</Tag>
-              <Tag className='discovery-header__tag' color={'rgba(112, 182, 3)'}>Freeze 9</Tag>
-            </div>
+        <div className='discovery-header__stat'>
+          <div className='discovery-header__stat-number'>
+            306,583
+          </div>
+          <div className='discovery-header__stat-label'>
+            Files
+          </div>
+        </div>
+        <div className='discovery-header__stat'>
+          <div className='discovery-header__stat-number'>
+            406,853
+          </div>
+          <div className='discovery-header__stat-label'>
+            Subjects
+          </div>
+        </div>
+        <div className='discovery-header__stat'>
+          <div className='discovery-header__stat-number'>
+            3.03 PB
+          </div>
+          <div className='discovery-header__stat-label'>
+            Data
           </div>
         </div>
       </div>
-      <div className='discovery-table-container'>
-        <div className='discovery-table__header'>
-          <Input className='discovery-table__search' prefix={<SearchOutlined />} />
-          <div className='disvovery-table__controls'>
-            <Checkbox className='discovery-table__show-favorites'>Show Favorites</Checkbox>
-            <Radio.Group
-              className='discovery-table__access-button'
-              defaultValue='both'
-              buttonStyle='solid'
-            >
-              <Radio.Button value='both'>Both</Radio.Button>
-              <Radio.Button value='access'><LockFilled /></Radio.Button>
-              <Radio.Button value='no-access'><UnlockOutlined /></Radio.Button>
-            </Radio.Group>
+      <div className='discovery-header__tags-container' >
+        <h3 className='discovery-header__tags-header'>ASSOCIATED TAGS BY CATEGORY</h3>
+        <div className='discovery-header__tags'>
+          <div className='discovery-header__tag-group'>
+            <h5>Program</h5>
+            <Tag className='discovery-header__tag' color={'rgba(129, 211, 248, 0.5)'}>TOPMed</Tag>
+            <Tag className='discovery-header__tag' color={'rgba(129, 211, 248)'}>COVID 19</Tag>
+            <Tag className='discovery-header__tag' color={'rgba(129, 211, 248)'}>Parent</Tag>
+          </div>
+          <div className='discovery-header__tag-group'>
+            <h5>Disease</h5>
+            <Tag className='discovery-header__tag' color={'rgba(236, 128, 141)'}>Blood Disease</Tag>
+            <Tag className='discovery-header__tag' color={'rgba(236, 128, 141)'}>Lung Disease</Tag>
+            <Tag className='discovery-header__tag' color={'rgba(236, 128, 141)'}>Heart Disease</Tag>
+          </div>
+          <div className='discovery-header__tag-group'>
+            <h5>Data Freeze</h5>
+            <Tag className='discovery-header__tag' color={'rgba(112, 182, 3)'}>Freeze 5</Tag>
+            <Tag className='discovery-header__tag' color={'rgba(112, 182, 3)'}>Freeze 8</Tag>
+            <Tag className='discovery-header__tag' color={'rgba(112, 182, 3)'}>Freeze 9</Tag>
           </div>
         </div>
-        <Table
-          columns={columns}
-          dataSource={data}
-          expandable={{
-            defaultExpandAllRows: true,
-            expandedRowClassName: () => 'discovery-table__expanded-row',
-            expandedRowRender: record => record.description,
-            expandIconColumnIndex: -1, // don't render expand icon
-          }}
-        />
       </div>
-      <Modal
-        modalRender={() => (
-          <div className='discovery-modal'>
-            <div className='discovery-modal__header'>
-              <h3>topmed-SAPPHIRE_asthma_DS-ASTHMA-IRB-COL</h3>
+    </div>
+    <div className='discovery-table-container'>
+      <div className='discovery-table__header'>
+        <Input className='discovery-table__search' prefix={<SearchOutlined />} />
+        <div className='disvovery-table__controls'>
+          <Checkbox className='discovery-table__show-favorites'>Show Favorites</Checkbox>
+          <Radio.Group
+            className='discovery-table__access-button'
+            defaultValue='both'
+            buttonStyle='solid'
+          >
+            <Radio.Button value='both'>Both</Radio.Button>
+            <Radio.Button value='access'><LockFilled /></Radio.Button>
+            <Radio.Button value='no-access'><UnlockOutlined /></Radio.Button>
+          </Radio.Group>
+        </div>
+      </div>
+      <Table
+        columns={columns}
+        onRow={() => ({
+          onClick: () => {
+            setModalVisible(true);
+          }
+        })}
+        dataSource={data}
+        expandable={{
+          defaultExpandAllRows: true,
+          expandedRowClassName: () => 'discovery-table__expanded-row',
+          expandedRowRender: record => record.description,
+          expandIconColumnIndex: -1, // don't render expand icon
+        }}
+      />
+    </div>
+    <Modal
+      width='80vw'
+      visible={modalVisible}
+      onCancel={() => setModalVisible(false)}
+      modalRender={() => (
+        <div className='discovery-modal'>
+
+          <div className='discovery-modal__header'>
+            <Space align='baseline'>
+              <h3 className='discovery-modal__header-text'>topmed-SAPPHIRE_asthma_DS-ASTHMA-IRB-COL</h3>
               <StarOutlined />
               <LinkOutlined />
-              <Button shape='circle' icon={<CloseOutlined />} />
-            </div>
+            </Space>
+            <Button className='discovery-modal__header-close-btn' shape='circle' icon={<CloseOutlined />} />
           </div>
-        )}
-        visible
-      />
-    </div>);
-  }
+
+          <Space direction='vertical' size='large'>
+            <Space direction='vertical' className='discovery-modal__attribute-group'>
+              <Space className='discovery-modal__attribute'>
+                <span className='discovery-modal__attribute-name'>PI</span>
+                <span className='discovery-modal__attribute-value'>Jackie Slivka</span>
+              </Space>
+              <Space className='discovery-modal__attribute'>
+                <span className='discovery-modal__attribute-name'>Author</span>
+                <span className='discovery-modal__attribute-value'>CTDS</span>
+              </Space>
+            </Space>
+
+            <Space direction='vertical' className='discovery-modal__attribute-group'>
+              <Space className='discovery-modal__attribute'>
+                <span className='discovery-modal__attribute-name'>Number of Subjects</span>
+                <span className='discovery-modal__attribute-value'>100</span>
+              </Space>
+              <Space className='discovery-modal__attribute'>
+                <span className='discovery-modal__attribute-name'>Target Population</span>
+                <span className='discovery-modal__attribute-value'>Vegetarians</span>
+              </Space>
+              <Space className='discovery-modal__attribute'>
+                <span className='discovery-modal__attribute-name'>Setting</span>
+                <span className='discovery-modal__attribute-value'>At home</span>
+              </Space>
+              <Space className='discovery-modal__attribute'>
+                <span className='discovery-modal__attribute-name'>Geography</span>
+                <span className='discovery-modal__attribute-value'>Northwest</span>
+              </Space>
+            </Space>
+          </Space>
+
+
+        </div>
+      )}
+
+    />
+  </div>);
 }
 
 export default DiscoveryBeta;
