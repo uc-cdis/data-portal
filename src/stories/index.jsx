d3764c3e2d4f203b52cc7b6d7191dbead33c6872
Bobae Kang <bkang.dev@gmail.com>
2021-01-11 15:13:09 -0600

    Use fontsource font

--- src/stories/index.jsx
+++ src/stories/index.jsx
@@ -1,4 +1,5 @@
 import '@babel/polyfill';
+import '@fontsource/source-sans-pro';
 import '../gen3-ui-component/css/base.css';
 import '../gen3-ui-component/css/icon.css';
 import '../gen3-ui-component/css/base.less';
