c8ba699ba60e25460fd5e8d4eada008c2c5fbe68
Bobae Kang <bkang.dev@gmail.com>
2021-01-12 11:06:29 -0600

    Use webpack serve to run webpack-dev-server

--- runWebpack.sh
+++ runWebpack.sh
@@ -157,8 +157,8 @@ export REACT_APP_DISABLE_SOCKET=true
 # run webpack to generate a static bundle.js
 #
 if [[ "$NODE_ENV" == "dev" || "$NODE_ENV" == "auto" ]]; then
-  echo ./node_modules/.bin/webpack-dev-server
-  ./node_modules/.bin/webpack-dev-server
+  echo npx webpack serve
+  npx webpack serve
 else
   export NODE_ENV="production"
   echo ./node_modules/.bin/webpack --bail
