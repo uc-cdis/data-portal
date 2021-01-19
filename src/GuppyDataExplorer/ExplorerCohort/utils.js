52b278dfd549494f6c78c7717cb537c6f62953f1
Bobae Kang <bkang.dev@gmail.com>
2021-01-19 12:20:41 -0600

    Add cohort helper functions to utils
    Helpr functions to connect to amanuensis for cohort data

--- src/GuppyDataExplorer/ExplorerCohort/utils.js
+++ src/GuppyDataExplorer/ExplorerCohort/utils.js
@@ -1,4 +1,61 @@
+import { fetchWithCreds } from '../../actions';
 import './typedef';
+
+const COHORT_URL = '/amanuensis/cohort';
+
+/**
+ * @returns {Promise<ExplorerCohort[]>}
+ */
+export function fetchCohorts() {
+  return fetchWithCreds({
+    path: COHORT_URL,
+    method: 'GET',
+  }).then(({ response, data, status }) => {
+    if (status !== 200) throw response.statusText;
+    return data.searches;
+  });
+}
+
+/**
+ * @param {ExplorerCohort} cohort
+ * @returns {Promise<ExplorerCohort>}
+ */
+export function saveCohort(cohort) {
+  return fetchWithCreds({
+    path: COHORT_URL,
+    method: 'POST',
+    body: JSON.stringify(cohort),
+  }).then(({ response, data, status }) => {
+    if (status !== 200) throw response.statusText;
+    return data;
+  });
+}
+
+/**
+ * @param {ExplorerCohort} cohort
+ */
+export function updateCohort(cohort) {
+  const { id, ...requestBody } = cohort;
+  return fetchWithCreds({
+    path: `${COHORT_URL}/${id}`,
+    method: 'PUT',
+    body: JSON.stringify(requestBody),
+  }).then(({ response, status }) => {
+    if (status !== 200) throw response.statusText;
+  });
+}
+
+/**
+ * @param {ExplorerCohort} cohort
+ */
+export function deleteCohort(cohort) {
+  return fetchWithCreds({
+    path: `${COHORT_URL}/${cohort.id}`,
+    method: 'DELETE',
+  }).then(({ response, status }) => {
+    if (status !== 200) throw response.statusText;
+  });
+}
 
 /**
  * @return {ExplorerCohort}
