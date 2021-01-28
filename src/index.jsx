23185f229a636a3804b409f8ac134425f464f940
Bobae Kang <bkang.dev@gmail.com>
2021-01-28 17:12:01 -0600

    Stop unnecessarily fetching resources at init
    - fetchUserAccess is called by fetchUser, called in ProtectedContent
    - fetchUserAuthMapping is for Arborist UI, not used for PCDC portal

--- src/index.jsx
+++ src/index.jsx
@@ -15,7 +15,6 @@ import {
   faTrashAlt,
 } from '@fortawesome/free-solid-svg-icons';
 import ReactGA from 'react-ga';
-import { fetchUserAccess, fetchUserAuthMapping } from './actions';
 import getReduxStore from './reduxStore';
 import { gaTracking } from './params';
 import App from './App';
@@ -49,14 +48,6 @@ library.add(
 // render the app after the store is configured
 async function init() {
   const store = await getReduxStore();
-
-  // asyncSetInterval(() => store.dispatch(fetchUser), 60000);
-  await Promise.all([
-    // resources can be open to anonymous users, so fetch access:
-    store.dispatch(fetchUserAccess),
-    store.dispatch(fetchUserAuthMapping),
-  ]);
-
   render(<App store={store} />, document.getElementById('root'));
 }
 
