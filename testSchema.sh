77b4dbac4e4735e7e3ea31369bc0efc9f7439a81
Mingfei Shao <mshao1@uchicago.edu>
2021-01-28 17:39:19 -0600

    fix: remove kfdcfinterop

--- testSchema.sh
+++ testSchema.sh
@@ -11,7 +11,6 @@ gtex "dcp.bionimbus.org"
 dev "qa.planx-pla.net"
 edc "portal.occ-data.org"
 genomel "genomel.bionimbus.org"
-kfDcfInterop "dcf-interop.kidsfirstdrc.org"
 )
 index=0
 while [[ $index -lt ${#testCases[@]} ]]; do
