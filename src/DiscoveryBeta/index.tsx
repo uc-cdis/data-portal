a7745a7f312c01893795d79bba4e71d9c6d4f97b
Michael Ingram <mpingram@uchicago.edu>
2021-02-11 08:25:04 -0600

    Restore MDS data loading code

--- src/DiscoveryBeta/index.tsx
+++ src/DiscoveryBeta/index.tsx
@@ -4,10 +4,7 @@ import { connect } from 'react-redux';
 import Discovery from './Discovery';
 import { DiscoveryConfig } from './DiscoveryConfig';
 import { userHasMethodForServiceOnResource } from '../authMappingUtils';
-import { discoveryConfig, useArboristUI } from '../localconf';
-
-// DEV ONLY
-import mockResources from './__mocks__/mock_mds_studies.json';
+import { hostname, discoveryConfig, useArboristUI } from '../localconf';
 
 if (!discoveryConfig) {
   throw new Error('Could not find configuration for Discovery page. Check the portal config.');
@@ -17,11 +14,27 @@ if (!useArboristUI) {
   throw new Error('Arborist UI must be enabled for the Discovery page to work. Set `useArboristUI: true` in the portal config.');
 }
 
-const loadStudiesFromMDS = () =>
-  // DEV ONLY
-  Promise.resolve(mockResources)
-  // DEV ONLY
-;
+// loads studies from metadataservice.
+const loadStudiesFromMDS = async (): Promise<any[]> => {
+  // Why `_guid_type='discovery_metadata'? We need to distinguish the discovery page studies in MDS
+  // from other data in MDS. So all MDS records with `_guid_type='discovery_metadata'` should be
+  // the full list of studies for this commons.
+  const GUID_TYPE = 'discovery_metadata';
+  const LIMIT = 10000; // required or else mds defaults to returning 10 records
+  const MDS_STUDY_DATA_URL = `mds/metadata?_guid_type=${GUID_TYPE}&data=True&limit=${LIMIT}`;
+  const url = hostname + MDS_STUDY_DATA_URL;
+  try {
+    const res = await fetch(url);
+    if (res.status !== 200) {
+      throw new Error(`Request for study data at ${url} failed. Response: ${JSON.stringify(res, null, 2)}`);
+    }
+    const jsonResponse = await res.json();
+    const studies = Object.values(jsonResponse).map((entry: any) => entry.gen3_discovery);
+    return studies;
+  } catch (err) {
+    throw new Error(`Request for study data at ${url} failed: ${err}`);
+  }
+};
 
 const DiscoveryWithMDSBackend: React.FC<{
   userAuthMapping: any,
