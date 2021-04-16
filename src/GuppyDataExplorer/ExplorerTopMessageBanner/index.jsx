1fc55420178737956c8db565af4ebf7858f0217a
Bobae Kang <bkang.dev@gmail.com>
2021-04-16 14:18:14 -0500

    Bring hideBanner conditional to the top

--- src/GuppyDataExplorer/ExplorerTopMessageBanner/index.jsx
+++ src/GuppyDataExplorer/ExplorerTopMessageBanner/index.jsx
@@ -6,37 +6,35 @@ import { GuppyConfigType } from '../configTypeDef';
 
 class ExplorerTopMessageBanner extends React.Component {
   render() {
-    return (
+    return this.props.hideBanner ? null : (
       <div className={this.props.className}>
-        {this.props.hideBanner ? null : (
-          <div className='top-message-banner'>
-            <div className='top-message-banner__space-column' />
-            <div className='top-message-banner__text-column'>
-              <div className='top-message-banner__button-wrapper'>
-                {this.props.hideGetAccessButton ? null : (
-                  <Button
-                    label='Get Access'
-                    className='top-message-banner__button'
-                    buttonType='default'
-                    enabled={!!this.props.getAccessButtonLink}
-                    tooltipEnabled={!this.props.getAccessButtonLink}
-                    tooltipText='Coming soon'
-                    onClick={
-                      this.props.getAccessButtonLink &&
-                      window.open(this.props.getAccessButtonLink)
-                    }
-                  />
-                )}
-              </div>
-              <div className='top-message-banner__text-wrapper'>
-                <span className='top-message-banner__normal-text'>
-                  You do not have permissions to view line-level data. To
-                  request access please reach out to the PCDC team.
-                </span>
-              </div>
+        <div className='top-message-banner'>
+          <div className='top-message-banner__space-column' />
+          <div className='top-message-banner__text-column'>
+            <div className='top-message-banner__button-wrapper'>
+              {this.props.hideGetAccessButton ? null : (
+                <Button
+                  label='Get Access'
+                  className='top-message-banner__button'
+                  buttonType='default'
+                  enabled={!!this.props.getAccessButtonLink}
+                  tooltipEnabled={!this.props.getAccessButtonLink}
+                  tooltipText='Coming soon'
+                  onClick={
+                    this.props.getAccessButtonLink &&
+                    window.open(this.props.getAccessButtonLink)
+                  }
+                />
+              )}
+            </div>
+            <div className='top-message-banner__text-wrapper'>
+              <span className='top-message-banner__normal-text'>
+                You do not have permissions to view line-level data. To request
+                access please reach out to the PCDC team.
+              </span>
             </div>
           </div>
-        )}
+        </div>
       </div>
     );
   }
