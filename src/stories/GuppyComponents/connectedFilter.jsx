80d2ebde6819e3682bc12727705ef5b0aef9f809
Bobae Kang <bkang.dev@gmail.com>
2021-04-21 16:57:55 -0500

    Remove now obsolete guppy components
    i.e. AccessibleFilter & UnaccessibleFilter

--- src/stories/GuppyComponents/connectedFilter.jsx
+++ src/stories/GuppyComponents/connectedFilter.jsx
@@ -3,49 +3,18 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { filterConfig, guppyConfig, fieldMapping } from './conf';
 import ConnectedFilter from '../../GuppyComponents/ConnectedFilter';
-import AccessibleFilter from '../../GuppyComponents/ConnectedFilter/AccessibleFilter';
-import UnaccessibleFilter from '../../GuppyComponents/ConnectedFilter/UnaccessibleFilter';
-import SwitchableFilterExample from './SwitchableFilterExample';
 import './guppyWrapper.css';
 
-storiesOf('ConnectedFilter', module)
-  .add('Filter', () => {
-    const processFilterAggsData = (aggsData) => aggsData;
-    return (
-      <ConnectedFilter
-        filterConfig={filterConfig}
-        guppyConfig={guppyConfig}
-        onFilterChange={action('filter change')}
-        fieldMapping={fieldMapping}
-        onProcessFilterAggsData={processFilterAggsData}
-        tierAccessLimit={guppyConfig.tierAccessLimit}
-      />
-    );
-  })
-  .add('Accessible Filter', () => {
-    const processFilterAggsData = (aggsData) => aggsData;
-    return (
-      <AccessibleFilter
-        filterConfig={filterConfig}
-        guppyConfig={guppyConfig}
-        onFilterChange={action('filter change')}
-        fieldMapping={fieldMapping}
-        onProcessFilterAggsData={processFilterAggsData}
-        tierAccessLimit={guppyConfig.tierAccessLimit}
-      />
-    );
-  })
-  .add('Unaccessible Filter', () => {
-    const processFilterAggsData = (aggsData) => aggsData;
-    return (
-      <UnaccessibleFilter
-        filterConfig={filterConfig}
-        guppyConfig={guppyConfig}
-        onFilterChange={action('filter change')}
-        fieldMapping={fieldMapping}
-        onProcessFilterAggsData={processFilterAggsData}
-        tierAccessLimit={guppyConfig.tierAccessLimit}
-      />
-    );
-  })
-  .add('SwitchableFilterExample', () => <SwitchableFilterExample />);
+storiesOf('ConnectedFilter', module).add('Filter', () => {
+  const processFilterAggsData = (aggsData) => aggsData;
+  return (
+    <ConnectedFilter
+      filterConfig={filterConfig}
+      guppyConfig={guppyConfig}
+      onFilterChange={action('filter change')}
+      fieldMapping={fieldMapping}
+      onProcessFilterAggsData={processFilterAggsData}
+      tierAccessLimit={guppyConfig.tierAccessLimit}
+    />
+  );
+});
