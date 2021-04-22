828754d680d49d9c9794b5918eb183570774699d
Bobae Kang <bkang.dev@gmail.com>
2021-04-22 09:52:10 -0500

    Add stories for guppy components to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
   stories: [
     '../src/stories/*.jsx',
     '../src/stories/gen3-ui-component/index.js',
+    '../src/stories/GuppyComponents/index.stories.jsx',
   ],
   addons: ['@storybook/addon-actions'],
   webpackFinal: (config) => {
