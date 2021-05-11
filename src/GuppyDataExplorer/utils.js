uc-cdis/data-portal

commit 43266258cfac1f53f76882206730001bb379e67f

Improve validateFilter util function

src/GuppyDataExplorer/utils.js
@@ -63,6 +63,28 @@ export const humanizeNumber = (number, fixedPoint = 2) => {
   return Number.parseFloat(number).toExponential(fixedPoint);
 };
 
+function isPlainObject(value) {
+  return typeof value === 'object' && value !== null && !Array.isArray(value);
+}
+
+function isTextFilter(value) {
+  return (
+    Object.keys(value).length === 1 &&
+    Array.isArray(value?.selectedValues) &&
+    value?.selectedValues.length > 0 &&
+    typeof value?.selectedValues[0] === 'string'
+  );
+}
+
+function isRangeFilter(value) {
+  return (
+    Object.keys(value).length === 2 &&
+    typeof value?.lowerBound === 'number' &&
+    typeof value?.upperBound === 'number' &&
+    value?.lowerBound < value?.upperBound
+  );
+}
+
 /**
  * Validates the provide filter value based on configuration.
  * Performs the following checks:
@@ -72,11 +94,17 @@ export const humanizeNumber = (number, fixedPoint = 2) => {
  * @param {{ tabs: { fields: string[] }[] }} filterConfig
  */
 export function validateFilter(value, filterConfig) {
-  if (typeof value !== 'object' || value === null || Array.isArray(value))
-    return false;
+  if (!isPlainObject(value)) return false;
 
   const allFields = filterConfig.tabs.flatMap(({ fields }) => fields);
   const testFieldSet = new Set(allFields);
-  for (const key of Object.keys(value)) testFieldSet.add(key);
+  for (const filterContent of Object.values(value))
+    if (
+      isPlainObject(filterContent) &&
+      (isTextFilter(filterContent) || isRangeFilter(filterContent))
+    )
+      testFieldSet.add(key);
+    else return false;
+
   return allFields.length === testFieldSet.size;
 }

commit 0ff4fe9ffeb3cf3f5e86d1e4461a4462dddfd26d

Fix error in validateFilter
Introduced in 43266258cfac1f53f76882206730001bb379e67f

src/GuppyDataExplorer/utils.js
@@ -98,12 +98,12 @@ export function validateFilter(value, filterConfig) {
 
   const allFields = filterConfig.tabs.flatMap(({ fields }) => fields);
   const testFieldSet = new Set(allFields);
-  for (const filterContent of Object.values(value))
+  for (const [field, filterContent] of Object.entries(value))
     if (
       isPlainObject(filterContent) &&
       (isTextFilter(filterContent) || isRangeFilter(filterContent))
     )
-      testFieldSet.add(key);
+      testFieldSet.add(field);
     else return false;
 
   return allFields.length === testFieldSet.size;