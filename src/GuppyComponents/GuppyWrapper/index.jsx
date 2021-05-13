5c7bf921ce8b8b4c5a446732a7cca18e88c41630
Bobae Kang <bkang.dev@gmail.com>
2021-05-13 15:54:14 -0500

    Stop passing guppyConfig prop to child

--- src/GuppyComponents/GuppyWrapper/index.jsx
+++ src/GuppyComponents/GuppyWrapper/index.jsx
@@ -361,7 +361,6 @@ class GuppyWrapper extends React.Component {
 
         // below are just for ConnectedFilter component
         onFilterChange: this.handleFilterChange.bind(this),
-        guppyConfig: this.props.guppyConfig,
         adminAppliedPreFilters: this.props.adminAppliedPreFilters,
         initialAppliedFilters: this.props.initialAppliedFilters,
         patientIds: this.props.patientIds,
