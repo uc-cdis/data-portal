94e07f8c386b476c9c16827a646ff90d4e9a1f16
Bobae Kang <bkang.dev@gmail.com>
2021-05-19 09:56:04 -0500

    Convert Introduction to a function compoent

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,40 +1,37 @@
+import React from 'react';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
 import IconicLink from './buttons/IconicLink';
 import './Introduction.less';
 
-class Introduction extends Component {
-  static propTypes = {
-    data: PropTypes.object.isRequired,
-    dictIcons: PropTypes.object.isRequired,
-  };
-
-  render() {
-    const buttonText = 'Submit Data';
-    return (
-      <div className='introduction'>
-        <div className='h1-typo introduction__title'>
-          {this.props.data.heading}
-        </div>
-        <div className='high-light introduction__text'>
-          {this.props.data.text}
-        </div>
-        {this.props.isAdminUser && (
-          <IconicLink
-            link={this.props.data.link}
-            dictIcons={this.props.dictIcons}
-            className='introduction__icon'
-            icon='upload'
-            iconColor='#'
-            caption={buttonText}
-          />
-        )}
-      </div>
-    );
-  }
+/**
+ * @param {Object} props
+ * @param {{ heading:string; text:string; link: string }} props.data
+ * @param {{ [iconName: string]: (height: string, svgStyles: Object) => SVGElement }} props.dictIcons
+ * @param {boolean} [props.isAdminUser]
+ */
+function Introduction({ data, dictIcons, isAdminUser }) {
+  const buttonText = 'Submit Data';
+  return (
+    <div className='introduction'>
+      <div className='h1-typo introduction__title'>{data.heading}</div>
+      <div className='high-light introduction__text'>{data.text}</div>
+      {isAdminUser && (
+        <IconicLink
+          link={data.link}
+          dictIcons={dictIcons}
+          className='introduction__icon'
+          icon='upload'
+          iconColor='#'
+          caption={buttonText}
+        />
+      )}
+    </div>
+  );
 }
 
 Introduction.propTypes = {
+  data: PropTypes.object.isRequired,
+  dictIcons: PropTypes.object.isRequired,
   isAdminUser: PropTypes.bool,
 };
 
