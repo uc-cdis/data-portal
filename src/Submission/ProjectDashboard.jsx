3bf92a727e324549d86b34a62fd2cd2a9421b1c7
Bobae Kang <bkang.dev@gmail.com>
2021-05-19 11:10:00 -0500

    Make ProjectDashboard a function component
    + add jsdoc

--- src/Submission/ProjectDashboard.jsx
+++ src/Submission/ProjectDashboard.jsx
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import ReduxSubmissionHeader from './ReduxSubmissionHeader';
 import ProjectTable from '../components/tables/ProjectTable';
 import './ProjectDashboard.less';
 
-class ProjectDashboard extends Component {
-  render() {
-    return (
-      <div className='project-dashboard'>
-        <div className='h2-typo project-dashboard__title'>Data Submission</div>
-        <ReduxSubmissionHeader />
-        <ProjectTable
-          projectList={this.props.projectList}
-          summaries={this.props.details}
-        />
-      </div>
-    );
-  }
+/**
+ * @param {Object} props
+ * @param {{ label: string; value: number; }[]} props.details
+ * @param {{ name: string; counts: number[]; }[]} props.projectList
+ */
+function ProjectDashboard({ details, projectList }) {
+  return (
+    <div className='project-dashboard'>
+      <div className='h2-typo project-dashboard__title'>Data Submission</div>
+      <ReduxSubmissionHeader />
+      <ProjectTable projectList={projectList} summaries={details} />
+    </div>
+  );
 }
 
 ProjectDashboard.propTypes = {
