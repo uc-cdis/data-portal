b0f864d7330554f091671a21126aee3275e28714
Bobae Kang <bkang.dev@gmail.com>
2021-05-19 10:46:31 -0500

    Fix typo

--- src/Layout/reduxer.js
+++ src/Layout/reduxer.js
@@ -19,11 +19,11 @@ export const ReduxNavBar = (() => {
 
 export const ReduxTopBar = (() => {
   const resourcePath = '/services/sheepdog/submission/project';
-  
+
   const mapStateToProps = (state) => ({
     topItems: components.topBar.items,
-    username = state.user.username,
-    isAdminUser = state.user.authz?.[resourcePath]?.[0].method === '*',
+    username: state.user.username,
+    isAdminUser: state.user.authz?.[resourcePath]?.[0].method === '*',
   });
 
   // Bar chart does not dispatch anything
