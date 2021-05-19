6f26384d656eb5396f35d729c6569187baf43216
Bobae Kang <bkang.dev@gmail.com>
2021-05-19 12:27:56 -0500

    Stop passing unused props to TransactionLogTable

--- src/Submission/ReduxTransaction.js
+++ src/Submission/ReduxTransaction.js
@@ -3,20 +3,9 @@ import TransactionLogTable from '../components/tables/TransactionLogTable';
 
 const ReduxTransaction = (() => {
   const mapStateToProps = (state) => {
-    if (state.submission && state.submission.transactions) {
-      return {
-        log: state.submission.transactions,
-        userAuthMapping: state.userAuthMapping,
-      };
-    }
-
-    return { log: [], userAuthMapping: state.userAuthMapping };
+    return { log: state.submission?.transactions ?? [] };
   };
-
-  // Table does not dispatch anything
-  const mapDispatchToProps = null;
-
-  return connect(mapStateToProps, mapDispatchToProps)(TransactionLogTable);
+  return connect(mapStateToProps)(TransactionLogTable);
 })();
 
 export default ReduxTransaction;
