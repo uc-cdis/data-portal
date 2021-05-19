9009db1bf68ae9c8c460e6f1e261c1e0af022ea8
Bobae Kang <bkang.dev@gmail.com>
2021-05-19 10:56:27 -0500

    Stop passing unused props to ProjectTable

--- src/Submission/ProjectDashboard.jsx
+++ src/Submission/ProjectDashboard.jsx
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import ReduxProjectTable from '../components/tables/reduxer';
 import ReduxSubmissionHeader from './ReduxSubmissionHeader';
+import ProjectTable from '../components/tables/ProjectTable';
 import './ProjectDashboard.less';
 
 class ProjectDashboard extends Component {
   render() {
-    const projectList = this.props.projectList || [];
     return (
       <div className='project-dashboard'>
         <div className='h2-typo project-dashboard__title'>Data Submission</div>
         <ReduxSubmissionHeader />
-        <ReduxProjectTable
-          projectList={projectList}
+        <ProjectTable
+          projectList={this.props.projectList}
           summaries={this.props.details}
-          {...this.props}
         />
       </div>
     );
@@ -24,7 +22,6 @@ class ProjectDashboard extends Component {
 ProjectDashboard.propTypes = {
   details: PropTypes.arrayOf(PropTypes.object).isRequired,
   projectList: PropTypes.array.isRequired,
-  history: PropTypes.object.isRequired,
 };
 
 export default ProjectDashboard;
