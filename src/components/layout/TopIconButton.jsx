ef894338bbd88f497a9eac55a00782641e5beba9
Bobae Kang <bkang.dev@gmail.com>
2021-05-21 15:31:09 -0500

    Refactor TopIconButton & add jsdoc & fix prop types

--- src/components/layout/TopIconButton.jsx
+++ src/components/layout/TopIconButton.jsx
@@ -2,32 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './TopIconButton.less';
 
-const TopIconButton = ({ name, icon, isActive = false }) => (
-  <div
-    className={
-      isActive
-        ? 'top-icon-button button-top-active body-typo'
-        : 'top-icon-button body-typo'
-    }
-  >
-    {name}&nbsp;
-    {icon ? (
-      <i className={`g3-icon g3-icon--${icon} top-icon-button__icon`} />
-    ) : (
-      ''
-    )}
-  </div>
-);
+/**
+ * @param {Object} props
+ * @param {string} props.name
+ * @param {string} [props.icon]
+ * @param {boolean} [props.isActive]
+ */
+function TopIconButton({ name, icon, isActive = false }) {
+  return (
+    <div
+      className={
+        'top-icon-button body-typo' + isActive ? ' button-top-active' : ''
+      }
+    >
+      {name}&nbsp;
+      {icon && (
+        <i className={`g3-icon g3-icon--${icon} top-icon-button__icon`} />
+      )}
+    </div>
+  );
+}
 
 TopIconButton.propTypes = {
   name: PropTypes.string.isRequired,
   icon: PropTypes.string,
-  isActive: PropTypes.bool.isRequired,
-};
-
-TopIconButton.defaultProps = {
-  icon: null,
-  isActive: false,
+  isActive: PropTypes.bool,
 };
 
 export default TopIconButton;
