70168b2a4930821fcf8584e4179e5de9c760b666
Bobae Kang <bkang.dev@gmail.com>
2021-05-21 16:28:33 -0500

    Integrate link element into TopIconButton

--- src/components/layout/TopBar.jsx
+++ src/components/layout/TopBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import TopIconButton from './TopIconButton';
 import './TopBar.less';
@@ -19,67 +19,47 @@ function TopBar({ topItems, username, isAdminUser, onLogoutClick }) {
     <header className='top-bar'>
       <nav className='top-bar__nav'>
         <div className='top-bar__nav--hidden-lg-and-down'>
-          <a
-            className='top-bar__link'
-            href='https://commons.cri.uchicago.edu/pcdc-consortium/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <TopIconButton icon='external-link' name='About PCDC' />
-          </a>
-          <a
-            className='top-bar__link'
-            href='https://commons.cri.uchicago.edu/sponsors/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <TopIconButton icon='external-link' name='Our Sponsors' />
-          </a>
+          <TopIconButton
+            icon='external-link'
+            name='About PCDC'
+            to='https://commons.cri.uchicago.edu/pcdc-consortium/'
+          />
+          <TopIconButton
+            icon='external-link'
+            name='Our Sponsors'
+            to='https://commons.cri.uchicago.edu/sponsors/'
+          />
         </div>
         <div className='top-bar__nav--flex-center'>
-          {topItems.map((item) =>
-            (item.link !== '/submission' || isAdminUser) &&
-            item.link.startsWith('http') ? (
-              <a
-                className='top-bar__link'
-                key={item.link}
-                href={item.link}
-                target='_blank'
-                rel='noopener noreferrer'
-              >
+          {topItems.map(
+            (item) =>
+              (item.link !== '/submission' || isAdminUser) && (
                 <TopIconButton
+                  key={item.link}
                   name={item.name}
                   icon={item.icon}
                   isActive={location.pathname === item.link}
+                  to={item.link}
                 />
-              </a>
-            ) : (
-              <Link className='top-bar__link' key={item.link} to={item.link}>
-                <TopIconButton
-                  name={item.name}
-                  icon={item.icon}
-                  isActive={location.pathname === item.link}
-                />
-              </Link>
-            )
+              )
           )}
           {username !== undefined ? (
             <>
-              <Link className='top-bar__link' to='/identity'>
-                <TopIconButton
-                  icon='user-circle'
-                  name={username}
-                  isActive={location.pathname === '/identity'}
-                />
-              </Link>
-              <Link className='top-bar__link' to='#' onClick={onLogoutClick}>
-                <TopIconButton icon='exit' name='Logout' />
-              </Link>
+              <TopIconButton
+                icon='user-circle'
+                name={username}
+                isActive={location.pathname === '/identity'}
+                to='/identity'
+              />
+              <TopIconButton
+                icon='exit'
+                name='Logout'
+                to='#'
+                onClick={onLogoutClick}
+              />
             </>
           ) : (
-            <Link className='top-bar__link' to='/login'>
-              <TopIconButton icon='exit' name='Login' />
-            </Link>
+            <TopIconButton icon='exit' name='Login' to='/login' />
           )}
         </div>
       </nav>
--- src/components/layout/TopIconButton.jsx
+++ src/components/layout/TopIconButton.jsx
@@ -1,30 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import './TopIconButton.less';
 
 /**
  * @param {Object} props
  * @param {string} props.name
+ * @param {string} props.to
  * @param {string} [props.icon]
  * @param {boolean} [props.isActive]
  */
-function TopIconButton({ name, icon, isActive = false }) {
-  return (
-    <div
-      className={
-        'top-icon-button body-typo' + isActive ? ' button-top-active' : ''
-      }
-    >
+function TopIconButton({ name, to, icon, isActive = false }) {
+  const buttonClassName = isActive
+    ? 'top-icon-button button-top-active'
+    : 'top-icon-button';
+  const buttonContent = (
+    <span className='top-icon-button__content body-typo'>
       {name}&nbsp;
       {icon && (
         <i className={`g3-icon g3-icon--${icon} top-icon-button__icon`} />
       )}
-    </div>
+    </span>
+  );
+
+  return to.startsWith('http') ? (
+    <a
+      className={buttonClassName}
+      target='_blank'
+      rel='noopener noreferrer'
+      href={to}
+    >
+      {buttonContent}
+    </a>
+  ) : (
+    <Link className={buttonClassName} to={to}>
+      {buttonContent}
+    </Link>
   );
 }
 
 TopIconButton.propTypes = {
   name: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
   icon: PropTypes.string,
   isActive: PropTypes.bool,
 };
