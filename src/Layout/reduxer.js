56dca5aa182d703611119f31c759a4e201cff657
Bobae Kang <bkang.dev@gmail.com>
2021-05-24 13:28:11 -0500

    Use a Layout component
    - Use connect to pass redux states only
    - Take conditional rendering outside of Footer copmonent
    - Put app main contents inside <main>

--- src/Layout/reduxer.js
+++ src/Layout/reduxer.js
@@ -1,16 +1,10 @@
 import { connect } from 'react-redux';
 import NavBar from '../components/layout/NavBar';
 import TopBar from '../components/layout/TopBar';
-import Footer from '../components/layout/Footer';
-import dictIcons from '../img/icons/index';
 import { logoutAPI } from '../actions';
-import { components } from '../params';
 
 export const ReduxNavBar = (() => {
   const mapStateToProps = (state) => ({
-    navTitle: components.navigation.title,
-    navItems: components.navigation.items,
-    dictIcons,
     userAccess: state.userAccess.access,
   });
 
@@ -21,7 +15,6 @@ export const ReduxTopBar = (() => {
   const resourcePath = '/services/sheepdog/submission/project';
 
   const mapStateToProps = (state) => ({
-    topItems: components.topBar.items,
     username: state.user.username,
     isAdminUser: state.user.authz?.[resourcePath]?.[0].method === '*',
   });
@@ -32,12 +25,4 @@ export const ReduxTopBar = (() => {
   });
 
   return connect(mapStateToProps, mapDispatchToProps)(TopBar);
-})();
-
-export const ReduxFooter = (() => {
-  const mapStateToProps = (state) => ({
-    links: components.footer ? components.footer.links : [],
-  });
-
-  return connect(mapStateToProps)(Footer);
 })();
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useRouteMatch } from 'react-router-dom';
 import './Footer.css';
 
 /**
@@ -9,60 +8,56 @@ import './Footer.css';
  * @param {{ alt: string; height: number; href: string; src: string; }[]} props.logos
  * @param {{ footerHref: string; text: string; }} [props.privacyPolicy]
  */
-function Footer({ links = [], logos, privacyPolicy }) {
-  const isFooterHidden = useRouteMatch('/dd')?.isExact;
-
+function Footer({ links, logos, privacyPolicy }) {
   return (
-    isFooterHidden || (
-      <footer>
-        <nav className='footer__nav' aria-label='Footer Navigation'>
-          <div className='footer__spacer-area' />
-          {privacyPolicy?.text && (
-            <div className='footer__privacy-policy-area'>
-              <a
-                className='h4-typo footer__privacy-policy'
-                href={privacyPolicy.footerHref}
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                {privacyPolicy.text}
-              </a>
-            </div>
-          )}
-          <div className='footer__logo-area'>
-            {logos.map(({ alt, height, href, src }, i) => (
-              <a
-                key={i}
-                target='_blank'
-                href={href}
-                className='footer__icon'
-                rel='noopener noreferrer'
-              >
-                <img
-                  className='footer__img'
-                  src={src}
-                  alt={alt}
-                  style={{ height: height ?? 60 }}
-                />
-              </a>
-            ))}
+    <footer>
+      <nav className='footer__nav' aria-label='Footer Navigation'>
+        <div className='footer__spacer-area' />
+        {privacyPolicy?.text && (
+          <div className='footer__privacy-policy-area'>
+            <a
+              className='h4-typo footer__privacy-policy'
+              href={privacyPolicy.footerHref}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              {privacyPolicy.text}
+            </a>
           </div>
-          <div className='footer__link-area'>
-            {links.map(({ href, text }) => (
-              <a
-                key={href}
-                href={href}
-                className='footer__link'
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                {text || href}
-              </a>
-            ))}
-          </div>
-        </nav>
-      </footer>
-    )
+        )}
+        <div className='footer__logo-area'>
+          {logos.map(({ alt, height, href, src }, i) => (
+            <a
+              key={i}
+              target='_blank'
+              href={href}
+              className='footer__icon'
+              rel='noopener noreferrer'
+            >
+              <img
+                className='footer__img'
+                src={src}
+                alt={alt}
+                style={{ height: height ?? 60 }}
+              />
+            </a>
+          ))}
+        </div>
+        <div className='footer__link-area'>
+          {links?.map(({ href, text }) => (
+            <a
+              key={href}
+              href={href}
+              className='footer__link'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              {text || href}
+            </a>
+          ))}
+        </div>
+      </nav>
+    </footer>
   );
 }
 
