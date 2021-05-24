30baf7b9601e6876aefece5bbd6d7f47291b18c3
Bobae Kang <bkang.dev@gmail.com>
2021-05-24 11:49:52 -0500

    Convert less files to css
    No LESS-specific syntax is used

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useRouteMatch } from 'react-router-dom';
-import './Footer.less';
+import './Footer.css';
 
 /**
  * @param {Object} props
