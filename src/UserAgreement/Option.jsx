4dd271c43cc1283ce61441b446932af13978cadf
Bobae Kang <bkang.dev@gmail.com>
2021-05-27 13:12:42 -0500

    Fix simple lint errors at /src/UserAgreement

--- src/UserAgreement/Option.jsx
+++ src/UserAgreement/Option.jsx
@@ -1,5 +1,5 @@
+import React from 'react';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
 import './Option.less';
 
 function getCharFromA(i) {
@@ -9,47 +9,51 @@ function getCharFromA(i) {
 /**
  * Little question component - properties: option, idx, isCorrectAnswer, onChange, selected
  */
-class Option extends Component {
-  static propTypes = {
-    option: PropTypes.string.isRequired,
-    idx: PropTypes.number.isRequired,
-    isCorrectAnswer: PropTypes.bool.isRequired,
-    onChange: PropTypes.func.isRequired,
-    selected: PropTypes.bool.isRequired,
-    hasCorrectAnswers: PropTypes.bool.isRequired,
-  };
-
-  render() {
-    let frameClassModifier = '';
-    let bulletClassModifier = '';
-    if (this.props.selected) {
-      frameClassModifier = this.props.isCorrectAnswer
-        ? 'option__frame--correct'
-        : 'option__frame--incorrect';
-      bulletClassModifier = this.props.isCorrectAnswer
-        ? 'option__bullet--correct'
-        : 'option__bullet--incorrect';
-    }
-    return (
-      <div
-        className={`option__frame ${frameClassModifier}`}
-        role='button'
-        onClick={() => this.props.onChange(this.props.idx)}
-        onKeyDown={this.handleKeyDown}
-        tabIndex={-10}
-      >
-        {this.props.hasCorrectAnswers ? (
-          <div
-            className={`option__bullet body ${bulletClassModifier}`}
-            key={this.props.option}
-          >
-            {getCharFromA(this.props.idx)}
-          </div>
-        ) : null}
-        <div className='option__content body'>{this.props.option}</div>
-      </div>
-    );
+function Option({
+  option,
+  idx,
+  isCorrectAnswer,
+  onChange,
+  selected,
+  hasCorrectAnswers,
+}) {
+  let frameClassModifier = '';
+  let bulletClassModifier = '';
+  if (selected) {
+    frameClassModifier = isCorrectAnswer
+      ? 'option__frame--correct'
+      : 'option__frame--incorrect';
+    bulletClassModifier = isCorrectAnswer
+      ? 'option__bullet--correct'
+      : 'option__bullet--incorrect';
   }
+  return (
+    <div
+      className={`option__frame ${frameClassModifier}`}
+      role='button'
+      onClick={() => onChange(idx)}
+      tabIndex={-10}
+    >
+      {hasCorrectAnswers ? (
+        <div
+          className={`option__bullet body ${bulletClassModifier}`}
+          key={option}
+        >
+          {getCharFromA(idx)}
+        </div>
+      ) : null}
+      <div className='option__content body'>{option}</div>
+    </div>
+  );
 }
+
+Option.propTypes = {
+  option: PropTypes.string.isRequired,
+  idx: PropTypes.number.isRequired,
+  isCorrectAnswer: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired,
+  selected: PropTypes.bool.isRequired,
+  hasCorrectAnswers: PropTypes.bool.isRequired,
+};
 
 export default Option;
