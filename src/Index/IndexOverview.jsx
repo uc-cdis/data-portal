uc-cdis/data-portal

commit f258ec38f34623ac8573f115e36228f5a90b8fca

Ensure inputs are associated with their labels

src/Index/IndexOverview.jsx
@@ -75,9 +75,9 @@ function IndexOverview({ overviewCounts }) {
         <h2>Overview</h2>
         <div className='index-overview__actions'>
           <div className='index-overview__select'>
-            <label>Consortium</label>
+            <label htmlFor='index-overview-select'>Consortium</label>
             <Select
-              id='index-overview-select'
+              indexId='index-overview-select'
               options={consortiumOptions}
               value={consortium}
               onChange={setConsortium}

src/Submission/SubmitForm.jsx
@@ -110,9 +110,12 @@ class SubmitForm extends Component {
     return (
       <div>
         <form>
-          <label style={{ display: 'block' }}>
+          <label style={{ display: 'block' }} htmlFor='form-submission-switch'>
             Use Form Submission
-            <Switch onChange={this.onFormToggle} />
+            <Switch
+              inputProps={{ role: 'switch', id: 'form-submission-switch' }}
+              onChange={this.onFormToggle}
+            />
           </label>
 
           {this.state.fill_form && (

commit 639226fbde98e26328329120bb603b8e597b1903

Fix IndexOverview select style & attr
- bug caused by f258ec38f34623ac8573f115e36228f5a90b8fca
- keep original id to Select for proper style
- fix typo: indexId -> inputId

src/Index/IndexOverview.jsx
@@ -75,9 +75,10 @@ function IndexOverview({ overviewCounts }) {
         <h2>Overview</h2>
         <div className='index-overview__actions'>
           <div className='index-overview__select'>
-            <label htmlFor='index-overview-select'>Consortium</label>
+            <label htmlFor='index-overview-select-input'>Consortium</label>
             <Select
-              indexId='index-overview-select'
+              id='index-overview-select'
+              inputId='index-overview-select-input'
               options={consortiumOptions}
               value={consortium}
               onChange={setConsortium}