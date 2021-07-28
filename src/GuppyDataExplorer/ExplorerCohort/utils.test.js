ff402f3adf794af8eaae049c62368a22ea22670f
Bobae Kang <bkang.dev@gmail.com>
2021-07-28 11:16:02 -0500

    Add tests for anchor filter

--- src/GuppyDataExplorer/ExplorerCohort/utils.test.js
+++ src/GuppyDataExplorer/ExplorerCohort/utils.test.js
@@ -33,4 +33,43 @@ describe('stringifyFilters', () => {
     const expected = "* A\n\t- 'foo'\n\t- 'bar'\n* B\n\t- from: 0\n\t- to: 1\n";
     expect(stringifyFilters(filterState)).toEqual(expected);
   });
+  it('displays multiple filters with an anchor', () => {
+    const filterState = {
+      'x:y': {
+        filter: {
+          a: {
+            selectedValues: ['foo', 'bar'],
+          },
+          b: {
+            lowerBound: 0,
+            upperBound: 1,
+          },
+        },
+      },
+    };
+    const expected =
+      "* X: 'Y'\n\t* A\n\t\t- 'foo'\n\t\t- 'bar'\n\t* B\n\t\t- from: 0\n\t\t- to: 1\n";
+    expect(stringifyFilters(filterState)).toEqual(expected);
+  });
+  it('displays multiple filters', () => {
+    const filterState = {
+      a: {
+        selectedValues: ['foo', 'bar'],
+      },
+      'x:y': {
+        filter: {
+          a: {
+            selectedValues: ['foo', 'bar'],
+          },
+          b: {
+            lowerBound: 0,
+            upperBound: 1,
+          },
+        },
+      },
+    };
+    const expected =
+      "* A\n\t- 'foo'\n\t- 'bar'\n* X: 'Y'\n\t* A\n\t\t- 'foo'\n\t\t- 'bar'\n\t* B\n\t\t- from: 0\n\t\t- to: 1\n";
+    expect(stringifyFilters(filterState)).toEqual(expected);
+  });
 });
