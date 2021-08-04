5f9206084bcacc694e930d3cf0cd287617714007
Bobae Kang <bkang.dev@gmail.com>
2021-08-04 13:48:36 -0500

    Create typedef file for filter group component

---
+++ src/gen3-ui-component/components/filters/FilterGroup/typedef.js
@@ -0,0 +1,7 @@
+/// <reference path="../typedef.js" />
+
+/** @typedef {{ [option: string]: boolean }} OptionFilterStatus */
+
+/** @typedef {[lowerBound: number, upperBound: number]} RangeFilterStatus */
+
+/** @typedef {OptionFilterStatus | RangeFilterStatus} FilterSectionStatus */
