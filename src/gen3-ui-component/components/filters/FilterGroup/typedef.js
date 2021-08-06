0f94617cd1038ada6eadd5db314fd2cadb191e91
Bobae Kang <bkang.dev@gmail.com>
2021-08-06 12:01:18 -0500

    Create a new jsdoc type for anchor config

--- src/gen3-ui-component/components/filters/FilterGroup/typedef.js
+++ src/gen3-ui-component/components/filters/FilterGroup/typedef.js
@@ -5,3 +5,10 @@
 /** @typedef {[lowerBound: number, upperBound: number]} RangeFilterStatus */
 
 /** @typedef {OptionFilterStatus | RangeFilterStatus} FilterSectionStatus */
+
+/**
+ * @typedef {Object} AnchorConfig
+ * @property {string} fieldName
+ * @property {string[]} options
+ * @property {string[]} tabs
+ */
