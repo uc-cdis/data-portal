ea549a4acd2279a9d33b310d9a7442d26ee4f2da
Bobae Kang <bkang.dev@gmail.com>
2021-08-13 13:21:25 -0500

    Refactor ConnectedFilter to function component
    also update jsdoc and prop types

--- src/GuppyComponents/ConnectedFilter/index.jsx
+++ src/GuppyComponents/ConnectedFilter/index.jsx
@@ -1,5 +1,5 @@
 /* eslint react/forbid-prop-types: 0 */
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import FilterGroup from '../../gen3-ui-component/components/filters/FilterGroup';
 import FilterList from '../../gen3-ui-component/components/filters/FilterList';
@@ -14,124 +14,106 @@ import '../typedef';
 
 /**
  * @typedef {Object} ConnectedFilterProps
+ * @property {{ [x: string]: OptionFilter }} [adminAppliedPreFilters]
+ * @property {string} className
  * @property {FilterState} filter
  * @property {FilterConfig} filterConfig
  * @property {GuppyConfig} guppyConfig
- * @property {(x: FilterState) => void} onFilterChange
- * @property {(x: string[]) => void} onPatientIdsChange
- * @property {string} className
- * @property {number} tierAccessLimit
- * @property {(x: AggsData) => AggsData} onProcessFilterAggsData
- * @property {{ [x: string]: OptionFilter }} adminAppliedPreFilters
- * @property {string[]} patientIds
- * @property {FilterState} initialAppliedFilters
+ * @property {boolean} [hidden]
+ * @property {boolean} [hideZero]
+ * @property {FilterState} [initialAppliedFilters]
  * @property {SimpleAggsData} initialTabsOptions
+ * @property {(x: FilterState) => void} onFilterChange
+ * @property {(x: string[]) => void} [onPatientIdsChange]
+ * @property {(x: AggsData) => AggsData} [onProcessFilterAggsData]
+ * @property {string[]} [patientIds]
  * @property {AggsData} receivedAggsData
- * @property {boolean} hideZero
- * @property {boolean} hidden
+ * @property {number} [tierAccessLimit]
  */
 
-/**
- * @typedef {Object} ConnectedFilterState
- * @property {FilterState} filter
- */
+/** @param {ConnectedFilterProps} props */
+function ConnectedFilter({
+  adminAppliedPreFilters = {},
+  className = '',
+  filter,
+  filterConfig,
+  guppyConfig,
+  hidden = false,
+  hideZero = false,
+  initialAppliedFilters = {},
+  initialTabsOptions,
+  onFilterChange,
+  onPatientIdsChange,
+  onProcessFilterAggsData = (data) => data,
+  patientIds,
+  receivedAggsData,
+  tierAccessLimit,
+}) {
+  if (
+    hidden ||
+    filterConfig.tabs === undefined ||
+    filterConfig.tabs.length === 0
+  )
+    return null;
 
-/** @augments {React.Component<ConnectedFilterProps, ConnectedFilterState>} */
-class ConnectedFilter extends React.Component {
-  /** @param {ConnectedFilterProps} props */
-  constructor(props) {
-    super(props);
-
-    /** @type {string[][]} */
-    this.arrayFields = [];
+  const tabsOptions = unnestAggsData(onProcessFilterAggsData(receivedAggsData));
+  const processedTabsOptions = sortTabsOptions(
+    updateCountsInInitialTabsOptions(initialTabsOptions, tabsOptions, filter)
+  );
+  if (Object.keys(processedTabsOptions).length === 0) {
+    return null;
   }
 
-  componentDidMount() {
-    // get array types info from guppy
-    queryGuppyForStatus(this.props.guppyConfig.path).then((res) => {
-      for (const { arrayFields } of Object.values(res.indices))
-        if (arrayFields?.length > 0)
-          this.arrayFields = this.arrayFields.concat(arrayFields);
+  const arrayFields = useRef([]);
+  useEffect(() => {
+    queryGuppyForStatus(guppyConfig.path).then((res) => {
+      for (const { fields } of Object.values(res.indices))
+        if (fields?.length > 0) arrayFields.current.concat(fields);
     });
-  }
+  }, []);
 
-  /**
-   * This function contains partial rendering logic for filter components.
-   * It transfers aggregation data (`this.props.receivedAggsData`) to items inside filters.
-   * But before that, the function first calls `this.props.onProcessFilterAggsData`, which is
-   * a callback function passed by `ConnectedFilter`'s parent component, so that the parent
-   * component could do some pre-processing modification about filter.
-   */
-  getFilterTabs() {
-    if (this.props.hidden) return null;
-
-    const tabsOptions = unnestAggsData(
-      this.props.onProcessFilterAggsData(this.props.receivedAggsData)
-    );
-
-    const processedTabsOptions = sortTabsOptions(
-      updateCountsInInitialTabsOptions(
-        this.props.initialTabsOptions,
-        tabsOptions,
-        this.props.filter
-      )
-    );
-    if (Object.keys(processedTabsOptions).length === 0) return null;
-
-    const lockedTooltipMessage = `You may only view summary information for this project. You do not have ${this.props.guppyConfig.dataType}-level access.`;
-    const disabledTooltipMessage = `This resource is currently disabled because you are exploring restricted data. When exploring restricted data you are limited to exploring cohorts of ${
-      this.props.tierAccessLimit
-    } ${
-      this.props.guppyConfig.nodeCountTitle.toLowerCase() ||
-      this.props.guppyConfig.dataType
-    } or more.`;
-
-    return this.props.filterConfig.tabs.map(
-      ({ fields, searchFields }, index) => (
-        <FilterList
-          key={index}
-          sections={getFilterSections(
-            fields,
-            searchFields,
-            this.props.guppyConfig.fieldMapping,
-            processedTabsOptions,
-            this.props.initialTabsOptions,
-            this.props.adminAppliedPreFilters,
-            this.props.guppyConfig,
-            this.arrayFields
-          )}
-          tierAccessLimit={this.props.tierAccessLimit}
-          lockedTooltipMessage={lockedTooltipMessage}
-          disabledTooltipMessage={disabledTooltipMessage}
-          arrayFields={this.arrayFields}
-        />
-      )
-    );
-  }
-
-  render() {
-    if (this.props.hidden) return null;
-
-    const filterTabs = this.getFilterTabs();
-    if (!filterTabs || filterTabs.length === 0) return null;
-
-    return (
-      <FilterGroup
-        className={this.props.className}
-        tabs={filterTabs}
-        filterConfig={this.props.filterConfig}
-        onFilterChange={this.props.onFilterChange}
-        onPatientIdsChange={this.props.onPatientIdsChange}
-        patientIds={this.props.patientIds}
-        hideZero={this.props.hideZero}
-        initialAppliedFilters={this.props.initialAppliedFilters}
+  const filterTabs = filterConfig.tabs.map(
+    ({ fields, searchFields }, index) => (
+      <FilterList
+        key={index}
+        sections={getFilterSections(
+          fields,
+          searchFields,
+          guppyConfig.fieldMapping,
+          processedTabsOptions,
+          initialTabsOptions,
+          adminAppliedPreFilters,
+          guppyConfig,
+          arrayFields.current
+        )}
+        tierAccessLimit={tierAccessLimit}
+        lockedTooltipMessage={`You may only view summary information for this project. You do not have ${guppyConfig.dataType}-level access.`}
+        disabledTooltipMessage={`This resource is currently disabled because you are exploring restricted data. When exploring restricted data you are limited to exploring cohorts of ${tierAccessLimit} ${
+          guppyConfig.nodeCountTitle?.toLowerCase() || guppyConfig.dataType
+        } or more.`}
+        arrayFields={arrayFields.current}
       />
-    );
-  }
+    )
+  );
+
+  return (
+    <FilterGroup
+      className={className}
+      tabs={filterTabs}
+      filterConfig={filterConfig}
+      onFilterChange={onFilterChange}
+      onPatientIdsChange={onPatientIdsChange}
+      patientIds={patientIds}
+      hideZero={hideZero}
+      initialAppliedFilters={initialAppliedFilters}
+    />
+  );
 }
 
 ConnectedFilter.propTypes = {
-  filter: PropTypes.object,
+  adminAppliedPreFilters: PropTypes.object,
+  className: PropTypes.string,
+  filter: PropTypes.object.isRequired,
   filterConfig: PropTypes.shape({
     tabs: PropTypes.arrayOf(
       PropTypes.shape({
@@ -152,33 +134,16 @@ ConnectedFilter.propTypes = {
     ),
     nodeCountTitle: PropTypes.string,
   }).isRequired,
-  onFilterChange: PropTypes.func,
-  onPatientIdsChange: PropTypes.func,
-  className: PropTypes.string,
-  tierAccessLimit: PropTypes.number,
-  onProcessFilterAggsData: PropTypes.func,
-  adminAppliedPreFilters: PropTypes.object,
-  patientIds: PropTypes.arrayOf(PropTypes.string),
-  initialAppliedFilters: PropTypes.object,
-  initialTabsOptions: PropTypes.object,
-  receivedAggsData: PropTypes.object,
-  hideZero: PropTypes.bool,
   hidden: PropTypes.bool,
-};
-
-ConnectedFilter.defaultProps = {
-  filter: {},
-  onFilterChange: () => {},
-  onPatientIdsChange: () => {},
-  className: '',
-  tierAccessLimit: undefined,
-  onProcessFilterAggsData: (data) => data,
-  adminAppliedPreFilters: {},
-  initialAppliedFilters: {},
-  initialTabsOptions: {},
-  receivedAggsData: {},
-  hideZero: false,
-  hidden: false,
+  hideZero: PropTypes.bool,
+  initialAppliedFilters: PropTypes.object,
+  initialTabsOptions: PropTypes.object.isRequired,
+  onFilterChange: PropTypes.func.isRequired,
+  onPatientIdsChange: PropTypes.func,
+  onProcessFilterAggsData: PropTypes.func,
+  patientIds: PropTypes.arrayOf(PropTypes.string),
+  receivedAggsData: PropTypes.object.isRequired,
+  tierAccessLimit: PropTypes.number,
 };
 
 export default ConnectedFilter;
