a542fa2b020ef3fbcc40e121a16357528942de3a
Bobae Kang <bkang.dev@gmail.com>
2021-08-16 14:22:31 -0500

    Update FilterSection tests
    Removed tests can be done interactively via storybook

--- src/gen3-ui-component/components/filters/FilterSection/FilterSection.test.jsx
+++ src/gen3-ui-component/components/filters/FilterSection/FilterSection.test.jsx
@@ -26,63 +26,16 @@ describe('FilterSection', () => {
   });
 
   it('renders', () => {
-    expect(component.find(FilterSection).length).toBe(1);
+    expect(component.exists(FilterSection)).toBe(true);
   });
 
   it('toggles expand on click', () => {
-    expect(component.instance().state.isExpanded).toBe(true);
-    expect(component.find('.g3-filter-section__header').length).toBe(1);
+    expect(
+      component.exists(`[aria-label="Collapse filter: Section Title"]`)
+    ).toBe(true);
     component.find('.g3-filter-section__title').simulate('click');
-    expect(component.instance().state.isExpanded).toBe(false);
-  });
-
-  it('shows the number of currently selected filters', () => {
-    // expect the filterChip to not be shown
     expect(
-      component.find('.g3-filter-section__selected-count-chip').length
-    ).toBe(0);
-
-    // select two options
-    const option1 = singleSelectOptions[0];
-    const option2 = singleSelectOptions[1];
-    component.instance().handleSelectSingleSelectFilter(option1.text);
-    component.instance().handleSelectSingleSelectFilter(option2.text);
-
-    // expect the filterChip to appear
-    component.update();
-    expect(
-      component.find('.g3-filter-section__selected-count-chip').length
-    ).toBe(1);
-    // expect the filterChip to display that 2 filters are selected
-    const filterChip = component
-      .find('.g3-filter-section__selected-count-chip')
-      .first();
-    expect(
-      filterChip
-        .find('.g3-filter-section__selected-count-chip-text-emphasis')
-        .first()
-        .instance().text === '2'
-    );
-  });
-
-  it('clears all selected filters on clear button click', () => {
-    // select two options
-    const option1 = singleSelectOptions[0];
-    const option2 = singleSelectOptions[1];
-    component.instance().handleSelectSingleSelectFilter(option1.text);
-    component.instance().handleSelectSingleSelectFilter(option2.text);
-
-    // expect options to be selected
-    expect(component.state('filterStatus')).toEqual({
-      [option1.text]: true,
-      [option2.text]: true,
-    });
-
-    // click the clear button
-    const mockEvent = { stopPropagation: () => {} };
-    component.instance().handleClearButtonClick(mockEvent);
-
-    // expect all options to be unselected
-    expect(component.state('filterStatus')).toEqual({});
+      component.exists(`[aria-label="Expand filter: Section Title"]`)
+    ).toBe(true);
   });
 });
