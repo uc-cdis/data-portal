1bae024f7d166d859468fb48e55fbbe3455efc47
Bobae Kang <bkang.dev@gmail.com>
2021-09-08 13:26:45 -0500

    Pass flat model schema to GqlEditor

--- src/GraphQLEditor/ReduxGqlEditor.js
+++ src/GraphQLEditor/ReduxGqlEditor.js
@@ -6,6 +6,7 @@ const mapStateToProps = (state, ownProps) => {
 
   return {
     schema: state.graphiql.schema,
+    schemaFlat: state.graphiql.schemaFlat,
     endpointIndex: searchParams.has('endpoint')
       ? parseInt(searchParams.get('endpoint'), 10)
       : null,
