958f890e7bf88b0f86ca867fcb748b0543cd0b1e
Bobae Kang <bkang.dev@gmail.com>
2021-09-08 13:24:52 -0500

    Add a new reducer for flat model schema

--- src/GraphQLEditor/reducers.js
+++ src/GraphQLEditor/reducers.js
@@ -2,6 +2,8 @@ const graphiql = (state = {}, action) => {
   switch (action.type) {
     case 'RECEIVE_SCHEMA':
       return { ...state, schema: action.schema };
+    case 'RECEIVE_SCHEMA_FLAT':
+      return { ...state, schemaFlat: action.data };
     default:
       return state;
   }
