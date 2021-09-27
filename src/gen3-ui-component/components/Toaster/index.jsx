246feb78f9590752d2e918c3e16052ff7f77574e
Bobae Kang <bkang.dev@gmail.com>
2021-09-27 11:08:16 -0500

    Improve typing & minor refactoring
    - src/gen3-ui-component/components/Toaster

--- src/gen3-ui-component/components/Toaster/index.jsx
+++ src/gen3-ui-component/components/Toaster/index.jsx
@@ -2,23 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Toaster.css';
 
-function Toaster({ isEnabled, className, children }) {
+/**
+ * @typedef {Object} ToasterProps
+ * @property {React.ReactNode} children
+ * @property {string} [className]
+ * @property {boolean} isEnabled
+ */
+
+/** @param {ToasterProps} props */
+function Toaster({ children, className = '', isEnabled }) {
   return isEnabled ? (
-    <div className={`${className} toaster__div`}>{children}</div>
+    <div className={`${className} toaster__div`.trim()}>{children}</div>
   ) : null;
 }
 
 Toaster.propTypes = {
-  isEnabled: PropTypes.bool.isRequired,
-  className: PropTypes.string,
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
   ]).isRequired,
-};
-
-Toaster.defaultProps = {
-  className: '',
+  className: PropTypes.string,
+  isEnabled: PropTypes.bool.isRequired,
 };
 
 export default Toaster;
