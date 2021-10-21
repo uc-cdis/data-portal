4a457f49cb4c3e445633929127a7233d2b9360da
Bobae Kang <bkang.dev@gmail.com>
2021-10-21 14:42:05 -0500

    Add endTime param to explicitly set max time value

--- src/GuppyDataExplorer/ExplorerSurvivalAnalysis/utils.js
+++ src/GuppyDataExplorer/ExplorerSurvivalAnalysis/utils.js
@@ -51,11 +51,12 @@ export const getFactors = (aggsData, fieldMapping, enumFilterList) => {
  * Builds x-axis ticks array to use in plots
  * @param {{ data: { time: number; }[]}[]} data
  * @param {number} step
+ * @param {number} endtime
  */
-export const getXAxisTicks = (data, step = 2) => {
+export const getXAxisTicks = (data, step = 2, endtime = undefined) => {
   const times = data.flatMap(({ data }) => data).map(({ time }) => time);
   const minTime = Math.floor(Math.min(...times));
-  const maxTime = Math.ceil(Math.max(...times));
+  const maxTime = endtime ?? Math.ceil(Math.max(...times));
 
   const ticks = [];
   for (let tick = minTime; tick <= maxTime; tick += step) ticks.push(tick);
