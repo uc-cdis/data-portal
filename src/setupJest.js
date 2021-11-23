6ea665eb5bb2cadadb13d391c4baa0232a061c05
Bobae Kang <bkang.dev@gmail.com>
2021-11-23 13:30:11 -0600

    Add icon to use

--- src/setupJest.js
+++ src/setupJest.js
@@ -1,12 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faAngleUp, faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { faAngleUp, faAngleDown, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 import jestFetchMock from 'jest-fetch-mock'
 import '@testing-library/jest-dom'
 
 jestFetchMock.enableMocks()
 
-library.add(faAngleUp, faAngleDown);
+library.add(faAngleUp, faAngleDown, faExclamationTriangle);
 
 /* eslint-disable no-console */
 // Tests will fail if there are any warning messages
