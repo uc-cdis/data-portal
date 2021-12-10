8ec0165b65d50364f379e2fcd4ec11b5dcf7ebe3
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2021-12-10 13:20:57 -0600

    PXP-9144 Allow charts for non-filter fields (#954)

--- src/GuppyDataExplorer/ExplorerVisualization/index.jsx
+++ src/GuppyDataExplorer/ExplorerVisualization/index.jsx
@@ -35,6 +35,7 @@ class ExplorerVisualization extends React.Component {
       value: this.props.totalCount,
     });
     Object.keys(chartConfig).forEach((field) => {
+      // use `${field}` to handle nested fields, which contain '.'
       if (!aggsData || !aggsData[`${field}`] || !aggsData[`${field}`].histogram) return;
       const { histogram } = aggsData[`${field}`];
       switch (chartConfig[`${field}`].chartType) {
