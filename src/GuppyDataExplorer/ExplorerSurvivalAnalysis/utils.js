09728ff9708669ae3b77446174b37e5a89cdae19
Bobae Kang <bkang.dev@gmail.com>
2021-12-13 15:22:27 -0600

    Remove unused getFactor() util function

--- src/GuppyDataExplorer/ExplorerSurvivalAnalysis/utils.js
+++ src/GuppyDataExplorer/ExplorerSurvivalAnalysis/utils.js
@@ -2,52 +2,6 @@
 import './typedef';
 
 /**
- * Get factor variables to use for survival analysis
- * @param {SimpleAggsData} aggsData
- * @param {{ field: string; name: string }[]} fieldMapping
- * @param {string[]} enumFilterList
- */
-export const getFactors = (aggsData, fieldMapping, enumFilterList) => {
-  const factors = [];
-  const exceptions = ['project_id', 'data_contributor_id'];
-
-  /** @type {{ [key: string]: string }} */
-  const fieldNameMap = {};
-  for (const { field, name } of fieldMapping) fieldNameMap[field] = name;
-
-  const fields = Object.keys(aggsData);
-  for (const field of fields)
-    if (
-      enumFilterList.includes(field) &&
-      !exceptions.includes(field) &&
-      !(
-        aggsData[field].histogram.length === 1 &&
-        aggsData[field].histogram[0].key === 'no data'
-      )
-    )
-      factors.push({
-        label:
-          fieldNameMap[field] !== undefined
-            ? fieldNameMap[field]
-            : field
-                .toLowerCase()
-                .replace(/_|\./gi, ' ')
-                .replace(/\b\w/g, (c) => c.toUpperCase())
-                .trim(),
-        value: field,
-      });
-
-  return factors.sort((a, b) => {
-    const labelA = a.label.toUpperCase();
-    const labalB = b.label.toUpperCase();
-
-    if (labelA < labalB) return -1;
-    if (labelA > labalB) return 1;
-    return 0;
-  });
-};
-
-/**
  * Builds x-axis ticks array to use in plots
  * @param {{ data: { time: number; }[]}[]} data
  * @param {number} step
