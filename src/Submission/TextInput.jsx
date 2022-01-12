5ada19499995b5e1e645fc498bcf8efd4e442ed7
Bobae Kang <bkang.dev@gmail.com>
2022-01-12 11:30:49 -0600

    Refactor TextInput into function component

--- src/Submission/TextInput.jsx
+++ src/Submission/TextInput.jsx
@@ -1,61 +1,64 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './TextInput.css';
 
-class TextInput extends Component {
-  // eslint-disable-next-line camelcase
-  UNSAFE_componentWillMount() {
-    if (this.props.onUpdateFormSchema !== undefined) {
-      this.props.onUpdateFormSchema({
-        [this.props.name]: this.props.propertyType,
-      });
-    }
-  }
+/**
+ * @param {Object} props
+ * @param {string} props.description
+ * @param {string} props.name
+ * @param {React.ChangeEventHandler} props.onChange
+ * @param {(formSchema: any) => void} [props.onUpdateFormSchema]
+ * @param {string} [props.propertyType]
+ * @param {boolean} props.required
+ * @param {string} [props.value]
+ */
+function TextInput({
+  description,
+  name,
+  onChange,
+  onUpdateFormSchema,
+  propertyType = null,
+  required,
+  value,
+}) {
+  useEffect(() => {
+    onUpdateFormSchema?.({ [name]: propertyType });
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <label className='text-input__label' htmlFor={this.props.name}>
-          {' '}
-          {this.props.name}:{' '}
-        </label>
-        {this.props.description !== '' && (
-          <span className='text-input__input-description'>
-            {this.props.description}
-          </span>
-        )}
-        <br />
-        <input
-          id={this.props.name}
-          className='text-input__input'
-          type='text'
-          name={this.props.name}
-          value={this.props.value || ''}
-          required={this.props.required}
-          onChange={this.props.onChange}
-        />
-        {this.props.required && (
-          <span className='text-input__required-notification'> {'*'} </span>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <label className='text-input__label' htmlFor={name}>
+        {' '}
+        {name}:{' '}
+      </label>
+      {description !== '' && (
+        <span className='text-input__input-description'>{description}</span>
+      )}
+      <br />
+      <input
+        id={name}
+        className='text-input__input'
+        type='text'
+        name={name}
+        value={value || ''}
+        required={required}
+        onChange={onChange}
+      />
+      {required && (
+        <span className='text-input__required-notification'> {'*'} </span>
+      )}
+    </div>
+  );
 }
 
 TextInput.propTypes = {
-  name: PropTypes.string.isRequired,
-  value: PropTypes.string,
-  required: PropTypes.bool.isRequired,
   description: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   onUpdateFormSchema: PropTypes.func,
   propertyType: PropTypes.string,
-};
-
-TextInput.defaultProps = {
-  onUpdateFormSchema: () => {},
-  propertyType: undefined,
-  value: undefined,
+  required: PropTypes.bool.isRequired,
+  value: PropTypes.string,
 };
 
 export default TextInput;
