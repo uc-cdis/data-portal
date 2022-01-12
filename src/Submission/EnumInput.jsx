801a0d929be5cd2dd6e687ecd33985db1f650e96
Bobae Kang <bkang.dev@gmail.com>
2022-01-12 11:40:58 -0600

    Refactor EnumInput into function component

--- src/Submission/EnumInput.jsx
+++ src/Submission/EnumInput.jsx
@@ -1,82 +1,76 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Select from 'react-select';
 import { overrideSelectTheme } from '../utils';
 import './EnumInput.css';
 
-class EnumInput extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      chosenEnum: '',
-    };
-  }
+/**
+ * @param {Object} props
+ * @param {string} props.description
+ * @param {string} props.name
+ * @param {(name: string, newEnum: { value: string }) => void} props.onChange
+ * @param {(formSchema: any) => void} [props.onUpdateFormSchema]
+ * @param {string[]} props.options
+ * @param {string} [props.propertyType]
+ * @param {boolean} props.required
+ */
+function EnumInput({
+  description,
+  name,
+  onChange,
+  onUpdateFormSchema,
+  options,
+  propertyType = null,
+  required,
+}) {
+  useEffect(() => {
+    onUpdateFormSchema?.({ [name]: propertyType });
+  }, []);
 
-  // eslint-disable-next-line camelcase
-  UNSAFE_componentWillMount() {
-    if (this.props.onUpdateFormSchema !== undefined) {
-      this.props.onUpdateFormSchema({
-        [this.props.name]: this.props.propertyType,
-      });
-    }
-  }
+  const [currentEnum, setCurrentEnum] = useState({ label: '', value: '' });
+  const enumOptions = options.map((option) => ({
+    label: option,
+    value: option,
+  }));
 
-  render() {
-    const options = this.props.options.map((option) => ({
-      label: option,
-      value: option,
-    }));
-
-    const onChangeEnumWrapper = (newValue) => {
-      this.setState({
-        chosenEnum: newValue,
-      });
-      this.props.onChange(this.props.name, newValue);
-    };
-    return (
-      <div>
-        <label className='enum-input__label' htmlFor={this.props.name}>
-          {' '}
-          {this.props.name}:{' '}
-        </label>
-        {this.props.description !== '' && (
-          <span className='enum-input__input-description'>
-            {this.props.description}
-          </span>
-        )}
-        <br />
-        <Select
-          inputId={this.props.name}
-          name={this.props.name}
-          options={options}
-          required={this.props.required}
-          value={this.state.chosenEnum}
-          onChange={onChangeEnumWrapper}
-          className='enum-input__select'
-          theme={overrideSelectTheme}
-        />
-        {this.props.required && (
-          <span className='enum-input__required-notification'> {'*'} </span>
-        )}
-        <br />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <label className='enum-input__label' htmlFor={name}>
+        {' '}
+        {name}:{' '}
+      </label>
+      {description !== '' && (
+        <span className='enum-input__input-description'>{description}</span>
+      )}
+      <br />
+      <Select
+        inputId={name}
+        name={name}
+        options={enumOptions}
+        value={currentEnum}
+        onChange={(newEnum) => {
+          setCurrentEnum(newEnum);
+          onChange(name, newEnum);
+        }}
+        className='enum-input__select'
+        theme={overrideSelectTheme}
+      />
+      {required && (
+        <span className='enum-input__required-notification'> {'*'} </span>
+      )}
+      <br />
+    </div>
+  );
 }
 
 EnumInput.propTypes = {
-  name: PropTypes.string.isRequired,
-  options: PropTypes.array.isRequired,
-  required: PropTypes.bool.isRequired,
   description: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   onUpdateFormSchema: PropTypes.func,
+  options: PropTypes.array.isRequired,
   propertyType: PropTypes.string,
-};
-
-EnumInput.defaultProps = {
-  onUpdateFormSchema: () => {},
-  propertyType: null,
+  required: PropTypes.bool.isRequired,
 };
 
 export default EnumInput;
