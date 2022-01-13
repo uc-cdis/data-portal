93a6704622418f1c7cc69d2d27feda0f0d349107
Bobae Kang <bkang.dev@gmail.com>
2022-01-13 11:25:47 -0600

    Improve type hint for fetchUnmappedFiles

--- src/Submission/ReduxMapFiles.js
+++ src/Submission/ReduxMapFiles.js
@@ -4,43 +4,51 @@ import { fetchWithCreds } from '../actions';
 import { STARTING_DID, FETCH_LIMIT } from './utils';
 import { indexdPath, useIndexdAuthz } from '../localconf';
 
-const fetchUnmappedFiles = (user, total, start) => (dispatch) => {
-  const unmappedFilesCheck = useIndexdAuthz ? 'authz=null' : 'acl=null';
-  return fetchWithCreds({
-    path: `${indexdPath}index?${unmappedFilesCheck}&uploader=${user}&start=${start}&limit=${FETCH_LIMIT}`,
-    method: 'GET',
-  })
-    .then(
-      ({ status, data }) => {
-        switch (status) {
-          case 200:
-            total = total.concat(data.records ?? []);
-            if (data.records?.length === FETCH_LIMIT) {
-              return dispatch(
-                fetchUnmappedFiles(
-                  user,
-                  total,
-                  data.records[FETCH_LIMIT - 1].did
-                )
-              );
-            }
-            return {
-              type: 'RECEIVE_UNMAPPED_FILES',
-              data: total,
-            };
-          default:
-            return {
-              type: 'FETCH_ERROR',
-              error: data.records,
-            };
-        }
-      },
-      (err) => ({ type: 'FETCH_ERROR', error: err })
-    )
-    .then((msg) => {
-      if (msg) dispatch(msg);
-    });
-};
+/**
+ * @param {{ username: string }} user
+ * @param {Array} total
+ * @param {string} start
+ */
+const fetchUnmappedFiles =
+  ({ username }, total, start) =>
+  /** @param {import('redux-thunk').ThunkDispatch} dispatch */
+  (dispatch) => {
+    const unmappedFilesCheck = useIndexdAuthz ? 'authz=null' : 'acl=null';
+    return fetchWithCreds({
+      path: `${indexdPath}index?${unmappedFilesCheck}&uploader=${username}&start=${start}&limit=${FETCH_LIMIT}`,
+      method: 'GET',
+    })
+      .then(
+        ({ status, data }) => {
+          switch (status) {
+            case 200:
+              total = total.concat(data.records ?? []);
+              if (data.records?.length === FETCH_LIMIT) {
+                return dispatch(
+                  fetchUnmappedFiles(
+                    { username },
+                    total,
+                    data.records[FETCH_LIMIT - 1].did
+                  )
+                );
+              }
+              return {
+                type: 'RECEIVE_UNMAPPED_FILES',
+                data: total,
+              };
+            default:
+              return {
+                type: 'FETCH_ERROR',
+                error: data.records,
+              };
+          }
+        },
+        (err) => ({ type: 'FETCH_ERROR', error: err })
+      )
+      .then((msg) => {
+        if (msg) dispatch(msg);
+      });
+  };
 
 const mapSelectedFiles = (files) => ({
   type: 'RECEIVE_FILES_TO_MAP',
