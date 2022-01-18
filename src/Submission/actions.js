44d18e82525c0db12cd09e48ed1ccbb20c352eb7
Bobae Kang <bkang.dev@gmail.com>
2022-01-18 14:57:55 -0600

    Add/imporve types for redux actions

--- src/Submission/actions.js
+++ src/Submission/actions.js
@@ -1,18 +1,32 @@
 import { predictFileType } from '../utils';
 
-export const uploadTSV = (value, type) => (dispatch) => {
-  dispatch({ type: 'REQUEST_UPLOAD', file: value, file_type: type });
-};
+/** @typedef {import('redux').Dispatch} Dispatch */
+/** @typedef {import('./types').SubmissionState} SubmissionState */
 
+/**
+ * @param {SubmissionState['file']} value
+ * @param {SubmissionState['file_type']} type
+ */
+export const uploadTSV =
+  (value, type) => (/** @type {Dispatch} */ dispatch) => {
+    dispatch({ type: 'REQUEST_UPLOAD', file: value, file_type: type });
+  };
+
+/** @param {SubmissionState['formSchema']} formSchema */
 export const updateFormSchema = (formSchema) => ({
   type: 'UPDATE_FORM_SCHEMA',
   formSchema,
 });
 
-export const updateFileContent = (value, fileType) => (dispatch) => {
-  dispatch({
-    type: 'UPDATE_FILE',
-    file: value,
-    file_type: predictFileType(value, fileType),
-  });
-};
+/**
+ * @param {SubmissionState['file']} value
+ * @param {string} [fileType]
+ */
+export const updateFileContent =
+  (value, fileType) => (/** @type {Dispatch} */ dispatch) => {
+    dispatch({
+      type: 'UPDATE_FILE',
+      file: value,
+      file_type: predictFileType(value, fileType),
+    });
+  };
