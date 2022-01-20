8a725fd7c2d0d5445a661b41a514483538a3933d
Bobae Kang <bkang.dev@gmail.com>
2022-01-20 15:06:42 -0600

    Remove unused import

--- src/Index/page.jsx
+++ src/Index/page.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import MediaQuery from 'react-responsive';
 import {
   ReduxIndexButtonBar,
