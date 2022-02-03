da688d137191ea3363f11cbc678b2a66b3e1a5ab
Bobae Kang <bkang.dev@gmail.com>
2022-02-03 11:48:26 -0600

    Remove duplicated css class

--- src/gen3-ui-component/components/filters/FilterSection/index.jsx
+++ src/gen3-ui-component/components/filters/FilterSection/index.jsx
@@ -488,7 +488,7 @@ function FilterSection({
                 reset
               </div>
               <div className='g3-filter-section__range-filter-clear-btn-icon'>
-                <i className='g3-icon g3-icon--sm g3-icon-color__lightgray g3-icon--sm g3-icon--undo' />
+                <i className='g3-icon g3-icon--sm g3-icon-color__lightgray g3-icon--undo' />
               </div>
             </div>
           </div>
