afea0a6c014c815201e979202e8e4e12ab759a7a
Bobae Kang <bkang.dev@gmail.com>
2022-02-04 16:10:54 -0600

    Remove unused chart helper functions & improve jsdoc
    Also update tests

--- src/gen3-ui-component/components/charts/helper.js
+++ src/gen3-ui-component/components/charts/helper.js
@@ -1,8 +1,13 @@
+/** @param {boolean} showPercentage */
 const percentageFormatter = (showPercentage) => (v) =>
   showPercentage ? `${v}%` : v;
 
-const addPercentage = (v) => percentageFormatter(true)(v);
+const addPercentage = (v) => `${v}%`;
 
+/**
+ * @param {{ name: string; value: number }[]} data
+ * @param {number} percentageFixedPoint
+ */
 const calculateChartData = (data, percentageFixedPoint) => {
   const sum = data.reduce((a, entry) => a + entry.value, 0);
   const max = data.reduce((a, entry) => Math.max(a, entry.value), -Infinity);
@@ -15,6 +20,10 @@ const calculateChartData = (data, percentageFixedPoint) => {
   });
 };
 
+/**
+ * @param {{ name: string; value: number }[]} chartData
+ * @param {number} percentageFixedPoint
+ */
 const getPercentageData = (chartData, percentageFixedPoint) => {
   const result = {};
   const sum = chartData.reduce((a, entry) => a + entry.value, 0);
@@ -26,9 +35,7 @@ const getPercentageData = (chartData, percentageFixedPoint) => {
     } else {
       percentage = percentRemaining;
     }
-    percentage = Number(
-      Number.parseFloat(percentage).toFixed(percentageFixedPoint)
-    );
+    percentage = Number.parseFloat(percentage.toFixed(percentageFixedPoint));
     percentRemaining -= percentage;
     result[entry.name] = percentage;
   });
@@ -46,136 +53,42 @@ const categoricalColors = [
   '#ef8523',
   '#26d9b1',
 ];
+
+/** @param {number} index */
 const getCategoryColor = (index) =>
   categoricalColors[index % categoricalColors.length];
 
+/** @param {number} index */
 const getCategoryColorFrom2Colors = (index) => {
   const colors = ['#3283c8', '#e7e7e7'];
   return colors[index % colors.length];
 };
 
+/** @param {boolean} showPercentage */
 const getDataKey = (showPercentage) =>
   showPercentage ? 'percentage' : 'value';
 
-const prettifyValueName = (name) => {
-  if (name === '__missing__') {
-    return 'No Data';
-  }
-  return name;
-};
-
-const transformArrangerDataToChart = (field, sqonValues) => {
-  const chartData = [];
-  field.buckets
-    .filter((bucket) => sqonValues === null || sqonValues.includes(bucket.key))
-    .forEach((bucket) =>
-      chartData.push({
-        name: prettifyValueName(bucket.key),
-        value: bucket.doc_count,
-      })
-    );
-  return chartData;
-};
-
-const transformArrangerDataToSummary = (
-  field,
-  chartType,
-  title,
-  sqonValues
-) => ({
-  type: chartType,
-  title,
-  data: transformArrangerDataToChart(field, sqonValues),
-});
-
-const transformDataToCount = (field, label, sqonValues) => ({
-  label,
-  value: sqonValues
-    ? Math.min(field.buckets.length, sqonValues.length)
-    : field.buckets.length,
-});
-
-/**
- * Return an array of selected values in a given field
- * If no value selected, return null
- */
-const getSQONValues = (sqon, field) => {
-  if (!sqon || !sqon.content) return null;
-  const sqonItems = sqon.content.filter((item) => item.content.field === field);
-  if (!sqonItems || sqonItems.length !== 1) return null;
-  const sqonValues = sqonItems[0].content.value;
-  return sqonValues;
-};
-
-const getCharts = (data, singleExplorerConfig, sqon) => {
-  const countItems = [];
-  const summaries = [];
-  const stackedBarCharts = [];
-  const { arrangerConfig } = singleExplorerConfig;
-
-  if (data && data[arrangerConfig.graphqlField].aggregations) {
-    const fields = data[arrangerConfig.graphqlField].aggregations;
-    Object.keys(fields).forEach((field) => {
-      const fieldConfig = singleExplorerConfig.charts[field];
-      const sqonValues = getSQONValues(sqon, field);
-      if (fieldConfig) {
-        switch (fieldConfig.chartType) {
-          case 'count':
-            countItems.push(
-              transformDataToCount(fields[field], fieldConfig.title, sqonValues)
-            );
-            break;
-          case 'pie':
-          case 'bar':
-            summaries.push(
-              transformArrangerDataToSummary(
-                fields[field],
-                fieldConfig.chartType,
-                fieldConfig.title,
-                sqonValues
-              )
-            );
-            break;
-          case 'stackedBar':
-            stackedBarCharts.push(
-              transformArrangerDataToSummary(
-                fields[field],
-                fieldConfig.chartType,
-                fieldConfig.title,
-                sqonValues
-              )
-            );
-            break;
-          default:
-            break;
-        }
-      }
-    });
-  }
-  return { summaries, countItems, stackedBarCharts };
-};
-
+/** @param {number | string} width */
 const parseParamWidth = (width) =>
   typeof width === 'number' ? `${width}px` : width;
 
+/**
+ * @param {{ name: string; value: number }[]} data
+ * @param {number} lockValue
+ */
 const shouldHideChart = (data, lockValue) =>
   data.find((item) => item.value === lockValue);
 
 const helper = {
-  percentageFormatter,
   addPercentage,
   calculateChartData,
+  categoricalColors,
   getPercentageData,
   getCategoryColor,
   getCategoryColorFrom2Colors,
   getDataKey,
-  transformDataToCount,
-  transformArrangerDataToChart,
-  transformArrangerDataToSummary,
-  getCharts,
-  getSQONValues,
   parseParamWidth,
-  categoricalColors,
+  percentageFormatter,
   shouldHideChart,
 };
 
