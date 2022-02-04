b3bbe2b78f4c1eb60d0c3040573de5bba99621c4
Bobae Kang <bkang.dev@gmail.com>
2022-02-04 13:43:12 -0600

    Refactor toaster story to function component

--- src/stories/gen3-ui-component/toaster.jsx
+++ src/stories/gen3-ui-component/toaster.jsx
@@ -1,47 +1,29 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { storiesOf } from '@storybook/react';
 import Toaster from '../../gen3-ui-component/components/Toaster';
 import Button from '../../gen3-ui-component/components/Button';
 
-class ToasterWrapper extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      toasterEnabled: false,
-    };
-  }
-
-  enableToaster = () => {
-    this.setState({ toasterEnabled: true });
-  };
-
-  disableToaster = () => {
-    this.setState({ toasterEnabled: false });
-  };
-
-  render() {
-    return (
-      <div>
+storiesOf('Toaster', module).add('Basic', () => {
+  const [show, setShow] = useState(false);
+  return (
+    <div>
+      <Button
+        buttonType='primary'
+        label='Show Toaster'
+        onClick={() => setShow(true)}
+      />
+      <Button
+        buttonType='primary'
+        label='Close Toaster'
+        onClick={() => setShow(false)}
+      />
+      <Toaster isEnabled={show}>
         <Button
-          buttonType='primary'
-          label='Show Toaster'
-          onClick={this.enableToaster}
+          buttonType='secondary'
+          label='Close'
+          onClick={() => setShow(false)}
         />
-        <Button
-          buttonType='primary'
-          label='Close Toaster'
-          onClick={this.disableToaster}
-        />
-        <Toaster isEnabled={this.state.toasterEnabled}>
-          <Button
-            buttonType='secondary'
-            label='Close'
-            onClick={this.disableToaster}
-          />
-        </Toaster>
-      </div>
-    );
-  }
-}
-
-storiesOf('Toaster', module).add('Basic', () => <ToasterWrapper />);
+      </Toaster>
+    </div>
+  );
+});
