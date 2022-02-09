6b295a1f961baf19062bf93b0761fa7b5840c705
Mingfei Shao <mshao1@uchicago.edu>
2022-02-09 14:24:35 -0600

    comment

--- webpack.config.js
+++ webpack.config.js
@@ -254,7 +254,7 @@ module.exports = {
     },
     {
       test: /\.svg$/,
-      // loaders: ['babel-loader', 'react-svg-loader'],
+      // loaders: ['babel-loader', 'react-svg-loader'], // to address the `css-what` vulnerability issue, after updating to webpack 5 and latest `react-svg-loader` we can switch back to this
       loader: 'svg-react-loader',
     },
     {
