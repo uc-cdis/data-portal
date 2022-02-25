f9800348c46ab17fd814104ce9a3a518d6312f1f
Bobae Kang <bkang.dev@gmail.com>
2022-02-25 15:38:42 -0600

    Refactor DataDictionary to function component

--- src/DataDictionary/DataDictionary.jsx
+++ src/DataDictionary/DataDictionary.jsx
@@ -1,4 +1,4 @@
-import { Component, createRef } from 'react';
+import { useRef } from 'react';
 import PropTypes from 'prop-types';
 import Dashboard from '../Layout/Dashboard';
 import ReduxDataDictionaryTable from './table/DataDictionaryTable';
@@ -8,118 +8,117 @@ import ReduxDictionarySearcher from './search/DictionarySearcher';
 import ReduxDictionarySearchHistory from './search/DictionarySearchHistory';
 import './DataDictionary.css';
 
-class DataDictionary extends Component {
-  constructor(props) {
-    super(props);
-    this.dictionarySearcherRef = createRef();
+/**
+ * @param {Object} props
+ * @param {string} props.dataVersion
+ * @param {boolean} props.isGraphView
+ * @param {(isGraphView: boolean) => void} props.onSetGraphView
+ * @param {string} props.portalVersion
+ */
+function DataDictionary({
+  dataVersion,
+  isGraphView,
+  onSetGraphView,
+  portalVersion,
+}) {
+  const dictionarySearcherRef = useRef(null);
+
+  function handleClickSearchHistoryItem(keyword) {
+    dictionarySearcherRef.current.launchSearchFromOutside(keyword);
   }
 
-  setGraphView = (isGraphView) => {
-    this.props.onSetGraphView(isGraphView);
-  };
+  function handleClearSearchResult() {
+    dictionarySearcherRef.current.launchClearSearchFromOutside();
+  }
 
-  handleClickSearchHistoryItem = (keyword) => {
-    this.dictionarySearcherRef.current.launchSearchFromOutside(keyword);
-  };
-
-  handleClearSearchResult = () => {
-    this.dictionarySearcherRef.current.launchClearSearchFromOutside();
-  };
-
-  render() {
-    return (
-      <Dashboard>
-        <Dashboard.Sidebar className='data-dictionary__sidebar'>
-          <div>
-            <div className='data-dictionary__switch'>
-              <span
-                className={`data-dictionary__switch-button ${
-                  !this.props.isGraphView
-                    ? ''
-                    : 'data-dictionary__switch-button--active'
-                }`}
-                onClick={() => {
-                  this.setGraphView(true);
-                }}
-                onKeyPress={(e) => {
-                  if (e.charCode === 13 || e.charCode === 32) {
-                    e.preventDefault();
-                    this.setGraphView(true);
-                  }
-                }}
-                role='button'
-                tabIndex={0}
-                aria-label='Graph view'
-              >
-                Graph View
-              </span>
-              <span
-                className={`data-dictionary__switch-button ${
-                  this.props.isGraphView
-                    ? ''
-                    : 'data-dictionary__switch-button--active'
-                }`}
-                onClick={() => {
-                  this.setGraphView(false);
-                }}
-                onKeyPress={(e) => {
-                  if (e.charCode === 13 || e.charCode === 32) {
-                    e.preventDefault();
-                    this.setGraphView(false);
-                  }
-                }}
-                role='button'
-                tabIndex={0}
-                aria-label='Dictionary view'
-              >
-                Table View
-              </span>
-            </div>
-            <ReduxDictionarySearcher ref={this.dictionarySearcherRef} />
-            <ReduxDataModelStructure />
-            <ReduxDictionarySearchHistory
-              onClickSearchHistoryItem={this.handleClickSearchHistoryItem}
+  return (
+    <Dashboard>
+      <Dashboard.Sidebar className='data-dictionary__sidebar'>
+        <div>
+          <div className='data-dictionary__switch'>
+            <span
+              className={`data-dictionary__switch-button ${
+                !isGraphView ? '' : 'data-dictionary__switch-button--active'
+              }`}
+              onClick={() => {
+                onSetGraphView(true);
+              }}
+              onKeyPress={(e) => {
+                if (e.charCode === 13 || e.charCode === 32) {
+                  e.preventDefault();
+                  onSetGraphView(true);
+                }
+              }}
+              role='button'
+              tabIndex={0}
+              aria-label='Graph view'
+            >
+              Graph View
+            </span>
+            <span
+              className={`data-dictionary__switch-button ${
+                isGraphView ? '' : 'data-dictionary__switch-button--active'
+              }`}
+              onClick={() => {
+                onSetGraphView(false);
+              }}
+              onKeyPress={(e) => {
+                if (e.charCode === 13 || e.charCode === 32) {
+                  e.preventDefault();
+                  onSetGraphView(false);
+                }
+              }}
+              role='button'
+              tabIndex={0}
+              aria-label='Dictionary view'
+            >
+              Table View
+            </span>
+          </div>
+          <ReduxDictionarySearcher ref={dictionarySearcherRef} />
+          <ReduxDataModelStructure />
+          <ReduxDictionarySearchHistory
+            onClickSearchHistoryItem={handleClickSearchHistoryItem}
+          />
+        </div>
+        <div className='data-dictionary__version-info-area'>
+          <div className='data-dictionary__version-info-list'>
+            {dataVersion !== '' && (
+              <div className='data-dictionary__version-info'>
+                <span>Data Release Version:</span> {dataVersion}
+              </div>
+            )}
+            {portalVersion !== '' && (
+              <div className='data-dictionary__version-info'>
+                <span>Portal Version:</span> {portalVersion}
+              </div>
+            )}
+          </div>
+        </div>
+      </Dashboard.Sidebar>
+      <Dashboard.Main className='data-dictionary__main'>
+        {isGraphView ? (
+          <div
+            className={`data-dictionary__graph ${
+              isGraphView ? '' : 'data-dictionary__graph--hidden'
+            }`}
+          >
+            <DataDictionaryGraph
+              onClearSearchResult={handleClearSearchResult}
             />
           </div>
-          <div className='data-dictionary__version-info-area'>
-            <div className='data-dictionary__version-info-list'>
-              {this.props.dataVersion !== '' && (
-                <div className='data-dictionary__version-info'>
-                  <span>Data Release Version:</span> {this.props.dataVersion}
-                </div>
-              )}
-              {this.props.portalVersion !== '' && (
-                <div className='data-dictionary__version-info'>
-                  <span>Portal Version:</span> {this.props.portalVersion}
-                </div>
-              )}
-            </div>
+        ) : (
+          <div
+            className={`data-dictionary__table ${
+              !isGraphView ? '' : 'data-dictionary__table--hidden'
+            }`}
+          >
+            <ReduxDataDictionaryTable />
           </div>
-        </Dashboard.Sidebar>
-        <Dashboard.Main className='data-dictionary__main'>
-          {this.props.isGraphView ? (
-            <div
-              className={`data-dictionary__graph ${
-                this.props.isGraphView ? '' : 'data-dictionary__graph--hidden'
-              }`}
-            >
-              <DataDictionaryGraph
-                onClearSearchResult={this.handleClearSearchResult}
-              />
-            </div>
-          ) : (
-            <div
-              className={`data-dictionary__table ${
-                !this.props.isGraphView ? '' : 'data-dictionary__table--hidden'
-              }`}
-            >
-              <ReduxDataDictionaryTable />
-            </div>
-          )}
-        </Dashboard.Main>
-      </Dashboard>
-    );
-  }
+        )}
+      </Dashboard.Main>
+    </Dashboard>
+  );
 }
 
 DataDictionary.propTypes = {
