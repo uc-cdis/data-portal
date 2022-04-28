4c4a93e51df8cd0e861c87bb1717c92c0753769d
Bobae Kang <bkang.dev@gmail.com>
2022-04-28 09:24:04 -0500

    Use handleFilterChange to clear filter state
    To keep searchParams in sync

--- src/GuppyDataExplorer/ExplorerStateContext.jsx
+++ src/GuppyDataExplorer/ExplorerStateContext.jsx
@@ -125,7 +125,7 @@ export function ExplorerStateProvider({ children }) {
   }
 
   function clearFilters() {
-    setFilters({});
+    handleFilterChange(undefined);
   }
 
   const value = useMemo(
