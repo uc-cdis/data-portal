uc-cdis/data-portal

commit e274f6778cf623445fc69627cc78765702005f25

Rename FilterSet type prop filters to filter
Also rename some related component props accordingly

src/GuppyDataExplorer/types.d.ts
@@ -100,7 +100,7 @@ export type AlteredExplorerConfig = {
 export type ExplorerFilterSet = {
   description: string;
   explorerId?: number;
-  filters: ExplorerFilter;
+  filter: ExplorerFilter;
   id?: number;
   name: string;
 };

src/GuppyDataExplorer/ExplorerExploreExternalButton/index.jsx
@@ -82,7 +82,7 @@ function ExplorerExploreExternalButton({ filter }) {
                   />
                 }
               />
-              <FilterSetQueryDisplay filters={filter} />
+              <FilterSetQueryDisplay filter={filter} />
             </form>
             <div>
               <Button

src/GuppyDataExplorer/ExplorerFilterSet/FilterSetActionComponents.jsx
@@ -105,7 +105,7 @@ function FilterSetOpenForm({
             />
           }
         />
-        <FilterSetQueryDisplay filters={selected.value.filters} />
+        <FilterSetQueryDisplay filter={selected.value.filter} />
       </form>
       <div>
         <FilterSetButton
@@ -145,7 +145,7 @@ FilterSetOpenForm.propTypes = {
 /**
  * @param {Object} prop
  * @param {ExplorerFilterSet} prop.currentFilterSet
- * @param {ExplorerFilter} prop.currentFilters
+ * @param {ExplorerFilter} prop.currentFilter
  * @param {ExplorerFilterSet[]} prop.filterSets
  * @param {boolean} prop.isFiltersChanged
  * @param {(created: ExplorerFilterSet) => void} prop.onAction
@@ -153,7 +153,7 @@ FilterSetOpenForm.propTypes = {
  */
 function FilterSetCreateForm({
   currentFilterSet,
-  currentFilters,
+  currentFilter,
   filterSets,
   isFiltersChanged,
   onAction,
@@ -217,7 +217,7 @@ function FilterSetCreateForm({
             />
           }
         />
-        <FilterSetQueryDisplay filters={currentFilters} />
+        <FilterSetQueryDisplay filter={currentFilter} />
       </form>
       <div>
         <FilterSetButton
@@ -228,7 +228,7 @@ function FilterSetCreateForm({
         <FilterSetButton
           enabled={filterSet.name !== currentFilterSet.name && !error.isError}
           label='Save Filter Set'
-          onClick={() => onAction({ ...filterSet, filters: currentFilters })}
+          onClick={() => onAction({ ...filterSet, filter: currentFilter })}
         />
       </div>
     </div>
@@ -242,7 +242,7 @@ FilterSetCreateForm.propTypes = {
     filters: PropTypes.object,
     id: PropTypes.number,
   }),
-  currentFilters: PropTypes.object,
+  currentFilter: PropTypes.object,
   filterSets: PropTypes.arrayOf(
     PropTypes.shape({
       name: PropTypes.string,
@@ -259,7 +259,7 @@ FilterSetCreateForm.propTypes = {
 /**
  * @param {Object} prop
  * @param {ExplorerFilterSet} prop.currentFilterSet
- * @param {ExplorerFilter} prop.currentFilters
+ * @param {ExplorerFilter} prop.currentFilter
  * @param {ExplorerFilterSet[]} prop.filterSets
  * @param {boolean} prop.isFiltersChanged
  * @param {(updated: ExplorerFilterSet) => void} prop.onAction
@@ -267,7 +267,7 @@ FilterSetCreateForm.propTypes = {
  */
 function FilterSetUpdateForm({
   currentFilterSet,
-  currentFilters,
+  currentFilter,
   filterSets,
   isFiltersChanged,
   onAction,
@@ -333,10 +333,10 @@ function FilterSetUpdateForm({
             />
           }
         />
-        <FilterSetQueryDisplay filters={filterSet.filters} />
+        <FilterSetQueryDisplay filter={filterSet.filter} />
         {isFiltersChanged && (
           <FilterSetQueryDisplay
-            filters={currentFilters}
+            filter={currentFilter}
             title='Filters (changed)'
           />
         )}
@@ -355,7 +355,7 @@ function FilterSetUpdateForm({
               filterSet.description !== currentFilterSet.description) &&
             !error.isError
           }
-          onClick={() => onAction({ ...filterSet, filters: currentFilters })}
+          onClick={() => onAction({ ...filterSet, filter: currentFilter })}
         />
       </div>
     </div>
@@ -369,7 +369,7 @@ FilterSetUpdateForm.propTypes = {
     filters: PropTypes.object,
     id: PropTypes.number,
   }),
-  currentFilters: PropTypes.object,
+  currentFilter: PropTypes.object,
   filterSets: PropTypes.arrayOf(
     PropTypes.shape({
       name: PropTypes.string,
@@ -423,7 +423,7 @@ FilterSetDeleteForm.propTypes = {
  * @param {Object} prop
  * @param {ExplorerFilterSetActionType} prop.actionType
  * @param {ExplorerFilterSet} prop.currentFilterSet
- * @param {ExplorerFilter} prop.currentFilters
+ * @param {ExplorerFilter} prop.currentFilter
  * @param {ExplorerFilterSet[]} prop.filterSets
  * @param {object} prop.handlers
  * @param {(opened: ExplorerFilterSet) => void} prop.handlers.handleOpen
@@ -436,7 +436,7 @@ FilterSetDeleteForm.propTypes = {
 export function FilterSetActionForm({
   actionType,
   currentFilterSet,
-  currentFilters,
+  currentFilter,
   filterSets,
   handlers,
   isFiltersChanged,
@@ -458,7 +458,7 @@ export function FilterSetActionForm({
       return currentFilterSet.name === '' ? (
         <FilterSetCreateForm
           currentFilterSet={currentFilterSet}
-          currentFilters={currentFilters}
+          currentFilter={currentFilter}
           filterSets={filterSets}
           isFiltersChanged={isFiltersChanged}
           onAction={handleCreate}
@@ -467,7 +467,7 @@ export function FilterSetActionForm({
       ) : (
         <FilterSetUpdateForm
           currentFilterSet={currentFilterSet}
-          currentFilters={currentFilters}
+          currentFilter={currentFilter}
           filterSets={filterSets}
           isFiltersChanged={isFiltersChanged}
           onAction={handleUpdate}
@@ -478,7 +478,7 @@ export function FilterSetActionForm({
       return (
         <FilterSetCreateForm
           currentFilterSet={currentFilterSet}
-          currentFilters={currentFilters}
+          currentFilter={currentFilter}
           filterSets={filterSets}
           isFiltersChanged={isFiltersChanged}
           onAction={handleCreate}
@@ -506,7 +506,7 @@ FilterSetActionForm.propTypes = {
     filters: PropTypes.object,
     id: PropTypes.number,
   }),
-  currentFilters: PropTypes.object,
+  currentFilter: PropTypes.object,
   filterSets: PropTypes.arrayOf(
     PropTypes.shape({
       name: PropTypes.string,

src/GuppyDataExplorer/ExplorerFilterSet/FilterSetQueryDisplay.jsx
@@ -5,18 +5,18 @@ import './FilterSetQueryDisplay.css';
 
 /**
  * @param {Object} props
- * @param {import('../types').ExplorerFilter} props.filters
+ * @param {import('../types').ExplorerFilterSet['filter']} props.filter
  * @param {string} [props.title]
  */
-function FilterSetQueryDisplay({ filters, title = 'Filters' }) {
+function FilterSetQueryDisplay({ filter, title = 'Filters' }) {
   const filterInfo = useExplorerConfig().current.filterConfig.info;
   return (
     <div className='filter-set-query-display'>
-      {Object.keys(filters).length > 0 ? (
+      {Object.keys(filter).length > 0 ? (
         <>
           <header>{title}</header>
           <main>
-            <QueryDisplay filter={filters} filterInfo={filterInfo} />
+            <QueryDisplay filter={filter} filterInfo={filterInfo} />
           </main>
         </>
       ) : (
@@ -29,7 +29,7 @@ function FilterSetQueryDisplay({ filters, title = 'Filters' }) {
 }
 
 FilterSetQueryDisplay.propTypes = {
-  filters: PropTypes.any,
+  filter: PropTypes.any,
   title: PropTypes.string,
 };
 

src/GuppyDataExplorer/ExplorerFilterSet/index.jsx
@@ -57,7 +57,7 @@ function ExplorerFilterSet({ className, filter }) {
   }
   function handleOpen(/** @type {ExplorerFilterSet} */ opened) {
     setFilterSet(cloneDeep(opened));
-    handleFilterChange(cloneDeep(opened.filters));
+    handleFilterChange(cloneDeep(opened.filter));
     closeActionForm();
   }
   async function handleCreate(/** @type {ExplorerFilterSet} */ created) {
@@ -103,7 +103,7 @@ function ExplorerFilterSet({ className, filter }) {
   }
 
   const isFiltersChanged =
-    JSON.stringify(filter) !== JSON.stringify(filterSet.filters);
+    JSON.stringify(filter) !== JSON.stringify(filterSet.filter);
 
   return (
     <div className={className}>
@@ -134,12 +134,12 @@ function ExplorerFilterSet({ className, filter }) {
                 >
                   <span
                     onClick={() =>
-                      handleFilterChange(cloneDeep(filterSet.filters))
+                      handleFilterChange(cloneDeep(filterSet.filter))
                     }
                     onKeyPress={(e) => {
                       if (e.charCode === 13 || e.charCode === 32) {
                         e.preventDefault();
-                        handleFilterChange(cloneDeep(filterSet.filters));
+                        handleFilterChange(cloneDeep(filterSet.filter));
                       }
                     }}
                     role='button'
@@ -186,7 +186,7 @@ function ExplorerFilterSet({ className, filter }) {
           <FilterSetActionForm
             actionType={actionType}
             currentFilterSet={filterSet}
-            currentFilters={filter}
+            currentFilter={filter}
             filterSets={filterSets}
             handlers={{
               handleOpen,

src/GuppyDataExplorer/ExplorerFilterSet/utils.js
@@ -10,7 +10,7 @@ export function createEmptyFilterSet() {
   return {
     name: '',
     description: '',
-    filters: {},
+    filter: {},
   };
 }
 

src/GuppyDataExplorer/ExplorerSurvivalAnalysis/ControlForm.jsx
@@ -59,7 +59,7 @@ const emptyFilterSets = [];
 export const defaultFilterSet = {
   name: '*** All Subjects ***',
   description: '',
-  filters: {},
+  filter: {},
   id: -1,
 };
 

src/GuppyDataExplorer/ExplorerSurvivalAnalysis/FilterSetCard.jsx
@@ -62,7 +62,7 @@ export default function FilterSetCard({ count, filterSet, label, onClose }) {
               />
             }
           />
-          <FilterSetQueryDisplay filters={filterSet.filters} />
+          <FilterSetQueryDisplay filter={filterSet.filter} />
         </>
       ) : null}
     </div>

commit 1fe69c1ac7dcc2203e963a66b82dacb5e57c4f64

Fix invalid body for request to filter set service
Error caused by e274f6778cf623445fc69627cc78765702005f25

src/GuppyDataExplorer/ExplorerFilterSetsContext.jsx
@@ -4,8 +4,25 @@ import { fetchWithCreds } from '../actions';
 import { useExplorerConfig } from './ExplorerConfigContext';
 
 /** @typedef {import('./types').ExplorerFilterSet} ExplorerFilterSet */
+/** @typedef {import('./types').ExplorerFilterSetDTO} ExplorerFilterSetDTO */
 
 const FILTER_SET_URL = '/amanuensis/filter-sets';
+
+/**
+ * @param {ExplorerFilterSet} filterSet
+ * @returns {ExplorerFilterSetDTO}
+ */
+function convertToFilterSetDTO({ filter: filters, ...rest }) {
+  return { filters, ...rest };
+}
+
+/**
+ * @param {ExplorerFilterSetDTO} filterSetDTO
+ * @returns {ExplorerFilterSet}
+ */
+function convertFromFilterSetDTO({ filters: filter, ...rest }) {
+  return { filter, ...rest };
+}
 
 /**
  * @param {number} explorerId
@@ -24,7 +41,7 @@ function fetchFilterSets(explorerId) {
       !Array.isArray(data.filter_sets)
     )
       throw new Error('Error: Incorrect Response Data');
-    return data.filter_sets;
+    return data.filter_sets.map(convertFromFilterSetDTO);
   });
 }
 
@@ -37,7 +54,7 @@ export function createFilterSet(explorerId, filterSet) {
   return fetchWithCreds({
     path: `${FILTER_SET_URL}?explorerId=${explorerId}`,
     method: 'POST',
-    body: JSON.stringify(filterSet),
+    body: JSON.stringify(convertToFilterSetDTO(filterSet)),
   }).then(({ response, data, status }) => {
     if (status !== 200) throw response.statusText;
     return data;
@@ -66,7 +83,7 @@ export function updateFilterSet(explorerId, filterSet) {
   return fetchWithCreds({
     path: `${FILTER_SET_URL}/${id}?explorerId=${explorerId}`,
     method: 'PUT',
-    body: JSON.stringify(requestBody),
+    body: JSON.stringify(convertToFilterSetDTO(requestBody)),
   }).then(({ response, status }) => {
     if (status !== 200) throw response.statusText;
   });

src/GuppyDataExplorer/types.d.ts
@@ -104,3 +104,11 @@ export type ExplorerFilterSet = {
   id?: number;
   name: string;
 };
+
+export type ExplorerFilterSetDTO = {
+  description: string;
+  explorerId?: number;
+  filters: ExplorerFilter;
+  id?: number;
+  name: string;
+};