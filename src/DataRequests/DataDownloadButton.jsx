bc9060257474f94ffeeb1df6cf45c7b97742c4b5
Bobae Kang <bkang.dev@gmail.com>
2022-05-11 15:08:24 -0500

    Handle download button loading state

--- src/DataRequests/DataDownloadButton.jsx
+++ src/DataRequests/DataDownloadButton.jsx
@@ -1,21 +1,27 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import Button from '../gen3-ui-component/components/Button';
 
 /** @param {{ project: import('./index.jsx').DataRequestProject }} props */
 function DataDownloadButton({ project }) {
+  const [isLoading, setIsLoading] = useState(false);
+  function handleClick() {
+    setIsLoading(true);
+    fetch(`/amanuensis/download-urls/${project.id}`)
+      .then((res) => res.json())
+      .then((data) =>
+        window.open(data.download_url, '_blank', 'noopener, noreferrer')
+      )
+      .finally(() => setIsLoading(false));
+  }
   return (
     <Button
       buttonType='primary'
       enabled={project.status === 'Data Delivered' && project.has_access}
-      onClick={() =>
-        fetch(`/amanuensis/download-urls/${project.id}`)
-          .then((res) => res.json())
-          .then((data) =>
-            window.open(data.download_url, '_blank', 'noopener, noreferrer')
-          )
-      }
+      onClick={handleClick}
       label='Download Data'
       rightIcon='download'
+      isPending={isLoading}
     />
   );
 }
