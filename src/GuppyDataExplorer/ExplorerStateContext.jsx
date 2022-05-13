5f0ca86f5a92aa7ba0ca7266c046adefd3dc81cb
Bobae Kang <bkang.dev@gmail.com>
2022-05-13 15:42:54 -0500

    Stop syncing explorer state with URL

--- src/GuppyDataExplorer/ExplorerStateContext.jsx
+++ src/GuppyDataExplorer/ExplorerStateContext.jsx
@@ -1,16 +1,6 @@
-import {
-  createContext,
-  useContext,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from 'react';
-import ReactDOM from 'react-dom';
+import { createContext, useContext, useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
-import { useNavigate, useSearchParams } from 'react-router-dom';
 import { useExplorerConfig } from './ExplorerConfigContext';
-import { extractExplorerStateFromURL } from './utils';
 
 /** @typedef {import('./types').ExplorerFilter} ExplorerFilter */
 
@@ -28,71 +18,17 @@ import { extractExplorerStateFromURL } from './utils';
 const ExplorerStateContext = createContext(null);
 
 export function ExplorerStateProvider({ children }) {
-  const navigate = useNavigate();
-  const [searchParams] = useSearchParams();
   const {
     current: { filterConfig, patientIdsConfig },
-    shouldUpdateState,
-    setShouldUpdateState,
   } = useExplorerConfig();
 
-  const initialState = useMemo(
-    () =>
-      extractExplorerStateFromURL(searchParams, filterConfig, patientIdsConfig),
-    []
-  );
-  const [explorerFilter, setExplorerFilter] = useState(
-    initialState.explorerFilter
-  );
-  const [patientIds, setPatientIds] = useState(initialState.patientIds);
-  useEffect(() => {
-    if (shouldUpdateState) {
-      const newState = extractExplorerStateFromURL(
-        searchParams,
-        filterConfig,
-        patientIdsConfig
-      );
-      setExplorerFilter(newState.explorerFilter);
-      setPatientIds(newState.patientIds);
-      setShouldUpdateState(false);
-    }
-  }, [shouldUpdateState]);
+  /** @type {ExplorerFilter} */
+  const initialExplorerFilter = {};
+  const [explorerFilter, setExplorerFilter] = useState(initialExplorerFilter);
 
-  const isBrowserNavigation = useRef(false);
-  useEffect(() => {
-    if (isBrowserNavigation.current) {
-      isBrowserNavigation.current = false;
-      return;
-    }
-    const newSearchParams = new URLSearchParams(searchParams.toString());
-    newSearchParams.delete('filter');
-    newSearchParams.delete('patientIds');
-
-    if (explorerFilter && Object.keys(explorerFilter).length > 0)
-      newSearchParams.set('filter', JSON.stringify(explorerFilter));
-
-    if (patientIds?.length > 0)
-      newSearchParams.set('patientIds', patientIds.join(','));
-
-    if (searchParams.toString() !== newSearchParams.toString())
-      navigate(`?${decodeURIComponent(newSearchParams.toString())}`, {
-        state: { scrollY: window.scrollY },
-      });
-  }, [explorerFilter, patientIds]);
-
-  function handleBrowserNavigationForState() {
-    isBrowserNavigation.current = true;
-    const newState = extractExplorerStateFromURL(
-      new URL(document.URL).searchParams,
-      filterConfig,
-      patientIdsConfig
-    );
-    // batch to avoid double-triggering useEffect() above
-    ReactDOM.unstable_batchedUpdates(() => {
-      setExplorerFilter(newState.explorerFilter);
-      setPatientIds(newState.patientIds);
-    });
-  }
+  /** @type {string[]} */
+  const initielPatientIds = patientIdsConfig?.filter ? [] : undefined;
+  const [patientIds, setPatientIds] = useState(initielPatientIds);
 
   function handleFilterChange(/** @type {ExplorerFilter} */ filter) {
     let newFilter = /** @type {ExplorerFilter} */ ({});
@@ -136,7 +72,7 @@ export function ExplorerStateProvider({ children }) {
     () => ({
       explorerFilter,
       patientIds,
-      handleBrowserNavigationForState,
+      handleBrowserNavigationForState() {},
       handleFilterChange,
       handleFilterClear,
       handlePatientIdsChange,
