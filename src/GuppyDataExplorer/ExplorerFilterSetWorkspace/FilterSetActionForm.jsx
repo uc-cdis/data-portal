ca4b3bc0debd81a66c03905d3d017fe130322bf6
Bobae Kang <bkang.dev@gmail.com>
2022-05-25 15:47:45 -0500

    Support save action in generic action form

--- src/GuppyDataExplorer/ExplorerFilterSetWorkspace/FilterSetActionForm.jsx
+++ src/GuppyDataExplorer/ExplorerFilterSetWorkspace/FilterSetActionForm.jsx
@@ -1,7 +1,10 @@
 import PropTypes from 'prop-types';
 import { createEmptyFilterSet } from '../ExplorerFilterSet/utils';
+import FilterSetCreateForm from '../ExplorerFilterSetForms/FilterSetCreateForm';
 import FilterSetOpenForm from '../ExplorerFilterSetForms/FilterSetOpenForm';
+import FilterSetUpdateForm from '../ExplorerFilterSetForms/FilterSetUpdateForm';
 import { useExplorerFilterSets } from '../ExplorerFilterSetsContext';
+import useFilterSetWorkspace from './useFilterSetWorkspace';
 
 /** @typedef {import('../types').ExplorerFilterSet} ExplorerFilterSet */
 /** @typedef {import('./types').FilterSetWorkspaceAction} FilterSetWorkspaceAction */
@@ -14,9 +17,11 @@ const emptyFilterSet = createEmptyFilterSet();
  * @param {object} prop.handlers
  * @param {() => void} prop.handlers.close
  * @param {(loaded: ExplorerFilterSet) => void} prop.handlers.load
+ * @param {(saved: ExplorerFilterSet) => void} prop.handlers.save
  */
 function FilterSetActionForm({ actionType, handlers }) {
   const filterSets = useExplorerFilterSets();
+  const workspace = useFilterSetWorkspace();
   switch (actionType) {
     case 'LOAD':
       return (
@@ -27,16 +32,37 @@ function FilterSetActionForm({ actionType, handlers }) {
           onClose={handlers.close}
         />
       );
+    case 'SAVE':
+      return filterSets.active === undefined ? (
+        <FilterSetCreateForm
+          currentFilter={workspace.active.filterSet.filter}
+          currentFilterSet={emptyFilterSet}
+          filterSets={filterSets.all}
+          onAction={handlers.save}
+          onClose={handlers.close}
+          isFiltersChanged={false}
+        />
+      ) : (
+        <FilterSetUpdateForm
+          currentFilter={workspace.active.filterSet.filter}
+          currentFilterSet={filterSets.active}
+          filterSets={filterSets.all}
+          onAction={handlers.save}
+          onClose={handlers.close}
+          isFiltersChanged={false}
+        />
+      );
     default:
       return null;
   }
 }
 
 FilterSetActionForm.propTypes = {
-  actionType: PropTypes.oneOf(['LOAD']),
+  actionType: PropTypes.oneOf(['LOAD', 'SAVE']),
   handlers: PropTypes.shape({
     close: PropTypes.func,
     load: PropTypes.func,
+    save: PropTypes.func,
   }),
 };
 
