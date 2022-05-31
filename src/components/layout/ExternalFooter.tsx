e4c7423491551e6503de63c0b53c91b14f5f3776
Matthew Cannalte <mcannalte@uchicago.edu>
2022-05-31 10:49:20 -0500

    feat(external-footer): iframe title

--- src/components/layout/ExternalFooter.tsx
+++ src/components/layout/ExternalFooter.tsx
@@ -1,23 +1,24 @@
-import React, {useState} from "react";
+import React, { useState } from 'react';
 
 interface ExternalFooterProps {
     url: string
 }
 
 const ExternalFoooter: React.FC<ExternalFooterProps> = (props: ExternalFooterProps) => {
-    const [elementHeight, setElementHeight] = useState("178px");
-    return (
-        <iframe
-            id="externalFooter"
-            frameBorder={0}
-            src={props.url}
-            style={{width: "100%", height: elementHeight}}
-            onLoad={
-                () => setElementHeight(
-                    `${document.getElementById("externalFooter").clientHeight}px`
-                )
-            }
-        />
-    )
-}
+  const [elementHeight, setElementHeight] = useState('178px');
+  return (
+    <iframe
+      title='externalFooter'
+      id='externalFooter'
+      frameBorder={0}
+      src={props.url}
+      style={{ width: '100%', height: elementHeight }}
+      onLoad={
+        () => setElementHeight(
+          `${document.getElementById('externalFooter').clientHeight}px`,
+        )
+      }
+    />
+  );
+};
 export default ExternalFoooter;
