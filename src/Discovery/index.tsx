c31af5f2922e5b3bd2b7918378a9861f485c339f
Mingfei Shao <mshao1@uchicago.edu>
2022-06-09 09:33:58 -0500

    fix

--- src/Discovery/index.tsx
+++ src/Discovery/index.tsx
@@ -8,7 +8,7 @@ import { userHasMethodForServiceOnResource } from '../authMappingUtils';
 import { discoveryConfig, studyRegistrationConfig, useArboristUI } from '../localconf';
 import isEnabled from '../helpers/featureFlags';
 import loadStudiesFromAggMDS from './aggMDSUtils';
-import { loadStudiesFromMDS } from './MDSUtils';
+import loadStudiesFromMDS from './MDSUtils';
 
 const DiscoveryWithMDSBackend: React.FC<{
     userAuthMapping: any,
