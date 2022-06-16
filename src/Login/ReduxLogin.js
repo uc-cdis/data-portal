2d8942784a1a07f97eee9144257d841d078df104
Bobae Kang <bkang.dev@gmail.com>
2022-06-16 11:31:41 -0500

    Put fetchLogin in actions.thunk file

--- src/Login/ReduxLogin.js
+++ src/Login/ReduxLogin.js
@@ -1,46 +1,8 @@
 import { connect } from 'react-redux';
-
 import Login from './Login';
-import { fetchWithCreds } from '../actions.thunk';
-import { loginPath } from '../localconf';
 import { components } from '../params';
-import { loginEndpointErrored, receiveLoginEndpoint } from './actions';
 
-/** @typedef {import('./types').LoginProvider} LoginProvider */
 /** @typedef {import('./types').LoginState} LoginState */
-
-/**
- * @param {{ providers: LoginProvider[]; error: any }} data
- * @param {number} status
- * @returns {import('redux').AnyAction}
- */
-function getLoginAction(data, status) {
-  switch (status) {
-    case 200:
-      return receiveLoginEndpoint(data.providers);
-    case 404:
-      return receiveLoginEndpoint([
-        {
-          idp: 'google',
-          name: 'Google OAuth',
-          urls: [
-            {
-              name: 'Google OAuth',
-              url: `${loginPath}google/`,
-            },
-          ],
-        },
-      ]);
-    default:
-      return loginEndpointErrored(data.error);
-  }
-}
-
-export const fetchLogin =
-  () => (/** @type {import('redux').Dispatch} */ dispatch) =>
-    fetchWithCreds({ path: loginPath, dispatch }).then(({ data, status }) => {
-      dispatch(getLoginAction(data, status));
-    });
 
 /** @param {{ login: LoginState; user: import('../types').UserState }} state */
 const mapStateToProps = (state) => ({
