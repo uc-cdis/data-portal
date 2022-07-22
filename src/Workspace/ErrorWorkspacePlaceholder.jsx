a9eea75404178950c7c4f72dc3f9147084e7b369
Mingfei Shao <2475897+mfshao@users.noreply.github.com>
2022-07-22 12:08:51 -0500

    Chore/workspace error (#1036)
    * update text
    
    * update text

--- src/Workspace/ErrorWorkspacePlaceholder.jsx
+++ src/Workspace/ErrorWorkspacePlaceholder.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './Workspace.less';
 import NotFoundSVG from '../img/not-found.svg';
+import { components } from '../params';
 
 class ErrorWorkspacePlaceholder extends React.Component {
   render() {
+    const supportEmail = components.login?.email || 'support@datacommons.io';
     return (
       <div className='error-workspace-placeholder__error-msg'>
         <h1>Error opening workspace...</h1>
         <p>
-        Workspace is not enabled, or you do not have access.
-        Please contact administrator for more information.
+        Workspace access requires authorization. Please contact <a href={`mailto:${supportEmail}`}>{supportEmail}</a> for more information.
         </p>
         <NotFoundSVG />
       </div>
