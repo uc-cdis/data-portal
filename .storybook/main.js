78aa1eb6b45ee144cd39ff18e393dfdbbaa3670a
Bobae Kang <bkang.dev@gmail.com>
2022-07-27 11:00:16 -0500

    Stop using custom webpack module rules
    This somehow ignores stories that import components

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,17 @@
 const path = require('path');
-const customWebpackConfig = require('../webpack.config.js');
 
 module.exports = {
   stories: ['./stories/*.jsx', './stories/gen3-ui-component/index.js'],
   addons: ['@storybook/addon-actions'],
-  webpackFinal: (config) => {
-    return {
-      ...config,
-      module: { ...config.module, rules: customWebpackConfig.module.rules },
-      resolve: {
-        ...config.resolve,
-        alias: {
-          '@src': path.resolve(__dirname, '../src'),
-        },
+  webpackFinal: (config) => ({
+    ...config,
+    resolve: {
+      ...config.resolve,
+      alias: {
+        '@src': path.resolve(__dirname, '../src'),
       },
-    };
-  },
+    },
+  }),
   core: {
     builder: 'webpack5',
   },
