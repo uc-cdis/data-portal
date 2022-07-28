7bb6dbac66a889e5f985550bce593819be1e7f69
Bobae Kang <bkang.dev@gmail.com>
2022-07-28 14:39:57 -0500

    Mock crypto.randomUUID() web API

--- src/setupJest.js
+++ src/setupJest.js
@@ -3,6 +3,7 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faAngleUp, faAngleDown, faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
 import jestFetchMock from 'jest-fetch-mock'
 import '@testing-library/jest-dom'
+import crypto from 'crypto'
 
 jestFetchMock.enableMocks()
 
@@ -13,3 +14,8 @@ library.add(faAngleUp, faAngleDown, faTriangleExclamation);
 console.warn = jest.fn((warn) => {
   throw new Error(warn);
 });
+
+// Tests for explorer requires crypto.randomUUID() web API
+Object.defineProperty(global, 'crypto', {
+  value: { randomUUID: crypto.randomUUID }
+})
