8112a7e5375ea53199a19dde78147d2e3409cfb3
Bobae Kang <bkang.dev@gmail.com>
2022-07-29 16:37:49 -0500

    Use test data for filter set form

--- .storybook/stories/FilterSetForms.jsx
+++ .storybook/stories/FilterSetForms.jsx
@@ -1,69 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
 import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
 import { Provider } from 'react-redux';
-import { FILTER_TYPE } from '@src/GuppyComponents/Utils/const';
 import FilterSetCreateForm from '@src/GuppyDataExplorer/ExplorerFilterSetForms/FilterSetCreateForm';
 import FilterSetDeleteForm from '@src/GuppyDataExplorer/ExplorerFilterSetForms/FilterSetDeleteForm';
 import FilterSetOpenForm from '@src/GuppyDataExplorer/ExplorerFilterSetForms/FilterSetOpenForm';
 import FilterSetUpdateForm from '@src/GuppyDataExplorer/ExplorerFilterSetForms/FilterSetUpdateForm';
-import explorerReducer from '@src/redux/explorer/slice';
-
-/** @type {import('@src/GuppyDataExplorer/types').ExplorerFilterSet[]} */
-const filterSets = [
-  {
-    name: 'Simple filter set',
-    description: '',
-    filter: {
-      __combineMode: 'AND',
-      __type: FILTER_TYPE.STANDARD,
-      value: {
-        foo: { __type: FILTER_TYPE.OPTION, selectedValues: ['x', 'y'] },
-        bar: { __type: FILTER_TYPE.RANGE, lowerBound: 0, upperBound: 1 },
-      },
-    },
-  },
-  {
-    name: 'Complex filter set',
-    description: 'A filter set with complex filter.',
-    filter: {
-      __combineMode: 'OR',
-      __type: FILTER_TYPE.STANDARD,
-      value: {
-        foo: { __type: FILTER_TYPE.OPTION, selectedValues: ['x', 'y'] },
-        'lorem:ipsum': {
-          __type: FILTER_TYPE.ANCHORED,
-          value: {
-            bar: { __type: FILTER_TYPE.RANGE, lowerBound: 0, upperBound: 1 },
-            baz: {
-              __type: FILTER_TYPE.OPTION,
-              selectedValues: ['hello', 'world'],
-            },
-          },
-        },
-      },
-    },
-  },
-];
-
-const store = configureStore({
-  reducer: { explorer: explorerReducer },
-  preloadedState: {
-    explorer: {
-      config: {
-        // @ts-ignore
-        filterConfig: {
-          info: {
-            foo: { label: 'Foo' },
-            bar: { label: 'Bar' },
-            baz: { label: 'Baz' },
-            lorem: { label: 'Lorem' },
-          },
-        },
-      },
-    },
-  },
-});
+import {
+  testFilterSets,
+  testReduxStore,
+} from '@src/GuppyDataExplorer/ExplorerFilterSetForms/testData';
 
 const style = {
   backgroundColor: 'white',
@@ -76,7 +21,7 @@ const style = {
 
 function Wrapper({ children }) {
   return (
-    <Provider store={store}>
+    <Provider store={testReduxStore}>
       <div style={style}>{children}</div>
     </Provider>
   );
@@ -86,13 +31,13 @@ storiesOf('FilterSetForms', module)
   .add('Create form', () => (
     <Wrapper>
       <FilterSetCreateForm
-        currentFilter={filterSets[0].filter}
+        currentFilter={testFilterSets[0].filter}
         currentFilterSet={{
           name: '',
           description: '',
-          filter: filterSets[0].filter,
+          filter: testFilterSets[0].filter,
         }}
-        filterSets={filterSets}
+        filterSets={testFilterSets}
         onAction={action('open')}
         onClose={action('close')}
         isFiltersChanged={false}
@@ -102,7 +47,7 @@ storiesOf('FilterSetForms', module)
   .add('Delet form', () => (
     <Wrapper>
       <FilterSetDeleteForm
-        currentFilterSet={filterSets[0]}
+        currentFilterSet={testFilterSets[0]}
         onAction={action('delete')}
         onClose={action('close')}
       />
@@ -112,7 +57,7 @@ storiesOf('FilterSetForms', module)
     <Wrapper>
       <FilterSetOpenForm
         currentFilterSet={{ name: '', description: '', filter: {} }}
-        filterSets={filterSets}
+        filterSets={testFilterSets}
         onAction={action('open')}
         onClose={action('close')}
       />
@@ -121,9 +66,9 @@ storiesOf('FilterSetForms', module)
   .add('Update form', () => (
     <Wrapper>
       <FilterSetUpdateForm
-        currentFilterSet={filterSets[0]}
-        currentFilter={filterSets[0].filter}
-        filterSets={filterSets}
+        currentFilterSet={testFilterSets[0]}
+        currentFilter={testFilterSets[0].filter}
+        filterSets={testFilterSets}
         isFiltersChanged={false}
         onAction={action('open')}
         onClose={action('close')}
