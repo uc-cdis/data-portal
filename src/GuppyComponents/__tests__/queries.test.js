eecd91b6890a3df6cf104106f6dfa85b9295a04e
Bobae Kang <bkang.dev@gmail.com>
2022-08-16 10:48:08 -0500

    Add getGQLFilter tests for composed filter state support

--- src/GuppyComponents/__tests__/queries.test.js
+++ src/GuppyComponents/__tests__/queries.test.js
@@ -277,6 +277,85 @@ describe('Get GQL filter from filter object from', () => {
     };
     expect(getGQLFilter(filterState)).toEqual(gqlFilter);
   });
+  test('a simple composed filter', () => {
+    const filterState = {
+      __combineMode: /** @type {CombineMode} */ ('AND'),
+      __type: FILTER_TYPE.COMPOASED,
+      value: [
+        {
+          __combineMode: /** @type {CombineMode} */ ('AND'),
+          __type: FILTER_TYPE.STANDARD,
+          value: {
+            a: { __type: FILTER_TYPE.OPTION, selectedValues: ['foo', 'bar'] },
+          },
+        },
+        {
+          __combineMode: /** @type {CombineMode} */ ('OR'),
+          __type: FILTER_TYPE.STANDARD,
+          value: {
+            b: { __type: FILTER_TYPE.RANGE, lowerBound: 0, upperBound: 1 },
+          },
+        },
+      ],
+    };
+    const gqlFilter = {
+      AND: [
+        {
+          AND: [{ IN: { a: ['foo', 'bar'] } }],
+        },
+        {
+          OR: [{ AND: [{ GTE: { b: 0 } }, { LTE: { b: 1 } }] }],
+        },
+      ],
+    };
+    expect(getGQLFilter(filterState)).toEqual(gqlFilter);
+  });
+  test('a higher-order composed filter', () => {
+    const filterState = {
+      __combineMode: /** @type {CombineMode} */ ('AND'),
+      __type: FILTER_TYPE.COMPOASED,
+      value: [
+        {
+          __combineMode: /** @type {CombineMode} */ ('OR'),
+          __type: FILTER_TYPE.COMPOASED,
+          value: [
+            {
+              __combineMode: /** @type {CombineMode} */ ('AND'),
+              __type: FILTER_TYPE.STANDARD,
+              value: {
+                a: {
+                  __type: FILTER_TYPE.OPTION,
+                  selectedValues: ['foo', 'bar'],
+                },
+              },
+            },
+          ],
+        },
+        {
+          __combineMode: /** @type {CombineMode} */ ('OR'),
+          __type: FILTER_TYPE.STANDARD,
+          value: {
+            b: { __type: FILTER_TYPE.RANGE, lowerBound: 0, upperBound: 1 },
+          },
+        },
+      ],
+    };
+    const gqlFilter = {
+      AND: [
+        {
+          OR: [
+            {
+              AND: [{ IN: { a: ['foo', 'bar'] } }],
+            },
+          ],
+        },
+        {
+          OR: [{ AND: [{ GTE: { b: 0 } }, { LTE: { b: 1 } }] }],
+        },
+      ],
+    };
+    expect(getGQLFilter(filterState)).toEqual(gqlFilter);
+  });
 });
 
 describe('Get query info objects for aggregation options data', () => {
