be2beb70ac3b1fec0773d3d11d222c8a7618605d
Jarvis Raymond <jarvisraymond@uchicago.edu>
2022-10-11 13:44:20 -0500

    feat(CombineGWASWorkflowView1) Resolved ESLINT issues with GWASContainer.jsx and cohortMiddlewareApi

--- src/Analysis/GWASV2/GWASContainer.jsx
+++ src/Analysis/GWASV2/GWASContainer.jsx
@@ -1,104 +1,109 @@
-import React, { useState, useEffect } from "react";
-import { PropTypes } from "prop-types";
-import { Space, Button, Steps, Step, Popconfirm } from "antd";
-import SelectStudyPopulation from "./SelectStudyPopulation/SelectStudyPopulation";
-import './GWASV2.css'
+import React, { useState } from 'react';
+import {
+  Space, Button, Steps, Step, Popconfirm,
+} from 'antd';
+import SelectStudyPopulation from './SelectStudyPopulation/SelectStudyPopulation';
+import './GWASV2.css';
 
 const GWASContainer = () => {
-    const [current, setCurrent] = useState(0);
-    const [selectedCaseCohort, setSelectedCaseCohort] = useState(undefined);
-    const gwasSteps = [
-        {
-            title: 'Step 1',
-            description: 'Select Study Population',
-        },
-        {
-            title: 'Step 2',
-            description: 'Select Outcome Phenotypes',
-        },
-        {
-            title: 'Step 3',
-            description: 'Select Covariate Phenotype',
-        },
-        {
-            title: 'Step 4',
-            description: 'Configure GWAS',
-        }
-    ]
+  const [current, setCurrent] = useState(0);
+  const [selectedCaseCohort, setSelectedCaseCohort] = useState(undefined);
+  const gwasSteps = [
+    {
+      title: 'Step 1',
+      description: 'Select Study Population',
+    },
+    {
+      title: 'Step 2',
+      description: 'Select Outcome Phenotypes',
+    },
+    {
+      title: 'Step 3',
+      description: 'Select Covariate Phenotype',
+    },
+    {
+      title: 'Step 4',
+      description: 'Configure GWAS',
+    },
+  ];
 
-    const generateStep = () => {
-        // steps 2 & 3 very similar
-        switch (current) {
-            case 0:
-                // select study population
-                return <SelectStudyPopulation selectedCaseCohort={selectedCaseCohort}
-                        setSelectedCaseCohort={setSelectedCaseCohort} current={current}/>
-            case 1:
-                // outcome (customdichotomous or not)
-                return <>step 2</>
-            case 2:
-                // covariates (customdichtomous or not)
-                return <>step 3</>
-            case 3:
-                // all other input (mafs, imputation, etc), review, and submit
-                return <>step 4</>
-        }
+  const generateStep = () => {
+    // steps 2 & 3 very similar
+    switch (current) {
+    case 0:
+      // select study population
+      return (
+        <SelectStudyPopulation
+          selectedCaseCohort={selectedCaseCohort}
+          setSelectedCaseCohort={setSelectedCaseCohort}
+          current={current}
+        />
+      );
+    case 1:
+      // outcome (customdichotomous or not)
+      return <React.Fragment>step 2</React.Fragment>;
+    case 2:
+      // covariates (customdichtomous or not)
+      return <React.Fragment>step 3</React.Fragment>;
+    case 3:
+      // all other input (mafs, imputation, etc), review, and submit
+      return <React.Fragment>step 4</React.Fragment>;
+    default:
+      // required for eslint
+      return null;
     }
-    return (
-        <React.Fragment>
-            <React.Fragment>
-                <Space direction={'vertical'} style={{ width: '100%' }}>
-                    <Steps current={current}>
-                        {gwasSteps.map((item, idx) => {
-                            <Step key={idx} title={item.title} description={item.description} />
-                        })}
-                    </Steps>
-                    <div className='steps-content'>
-                        <Space direction={'vertical'} align={'center'} style={{ width: '100%' }}>
-                            {generateStep(current)}
-                        </Space>
-                    </div>
-                    <div className='steps-action'>
-                        <Button
-                            className='GWASUI-navBtn GWASUI-navBtn__next'
-                            type='primary'
-                            onClick={() => {
-                                setCurrent(current - 1);
-                            }}
-                        >
+  };
+  return (
+    <React.Fragment>
+      <React.Fragment>
+        <Space direction={'vertical'} style={{ width: '100%' }}>
+          <Steps current={current}>
+            {gwasSteps.map((item, idx) => (
+              <Step key={idx} title={item.title} description={item.description} />
+            ))}
+          </Steps>
+          <div className='steps-content'>
+            <Space direction={'vertical'} align={'center'} style={{ width: '100%' }}>
+              {generateStep(current)}
+            </Space>
+          </div>
+          <div className='steps-action'>
+            <Button
+              className='GWASUI-navBtn GWASUI-navBtn__next'
+              type='primary'
+              onClick={() => {
+                setCurrent(current - 1);
+              }}
+            >
                             Previous
-                        </Button>
-                        <Popconfirm
-                            title='Are you sure you want to leave this page?'
-                            //   onConfirm={() => resetGWASType()}
-                            okText='Yes'
-                            cancelText='No'
-                        >
-                            <Button type='link' size='medium' ghost>Select Different GWAS Type</Button>
-                        </Popconfirm>
-                        {current < gwasSteps.length - 1 && (
-                            <Button
-                                data-tour='next-button'
-                                className='GWASUI-navBtn GWASUI-navBtn__next'
-                                type='primary'
-                                onClick={() => {
-                                    setCurrent(current + 1);
-                                }}
-                            // disabled={!nextButtonEnabled}
-                            >
+            </Button>
+            <Popconfirm
+              title='Are you sure you want to leave this page?'
+              //   onConfirm={() => resetGWASType()}
+              okText='Yes'
+              cancelText='No'
+            >
+              <Button type='link' size='medium' ghost>Select Different GWAS Type</Button>
+            </Popconfirm>
+            {current < gwasSteps.length - 1 && (
+              <Button
+                data-tour='next-button'
+                className='GWASUI-navBtn GWASUI-navBtn__next'
+                type='primary'
+                onClick={() => {
+                  setCurrent(current + 1);
+                }}
+                // disabled={!nextButtonEnabled}
+              >
                                 Next
-                            </Button>
-                        )}
-                        {current === gwasSteps.length - 1 && (<div className='GWASUI-navBtn' />)}
-                    </div>
-                </Space>
-            </React.Fragment>
-        </React.Fragment>
-    )
-}
-
-GWASContainer.propTypes = {
-    refreshWorkflows: PropTypes.func.isRequired
-}
+              </Button>
+            )}
+            {current === gwasSteps.length - 1 && (<div className='GWASUI-navBtn' />)}
+          </div>
+        </Space>
+      </React.Fragment>
+    </React.Fragment>
+  );
+};
 
 export default GWASContainer;
--- src/Analysis/GWASV2/Shared/wizardEndpoints/cohortMiddlewareApi.js
+++ src/Analysis/GWASV2/Shared/wizardEndpoints/cohortMiddlewareApi.js
@@ -1,29 +1,30 @@
 /* eslint-disable camelcase */
-import { useState, useEffect } from "react";
-import { cohortMiddlewarePath, wtsPath } from "../../../../localconf";
-import { fetchWithCreds } from "../../../../actions";
-import { headers } from "../../../../configs";
+import { useState, useEffect } from 'react';
+import { cohortMiddlewarePath, wtsPath } from '../../../../localconf';
+import { fetchWithCreds } from '../../../../actions';
+import { headers } from '../../../../configs';
+
+const hareConceptId = 2000007027;
 
 export const fetchConceptStatsByHare = async (
   cohortDefinitionId,
   selectedCovariates,
   selectedDichotomousCovariates,
-  sourceId
+  sourceId,
 ) => {
-  const hareConceptId = 2000007027;
   const variablesPayload = {
     variables: [
       ...selectedDichotomousCovariates.map(({ uuid, ...withNoId }) => withNoId),
       ...selectedCovariates.map((c) => ({
-        variable_type: "concept",
+        variable_type: 'concept',
         concept_id: c.concept_id,
       })),
     ],
   };
   const conceptStatsEndPoint = `${cohortMiddlewarePath}concept-stats/by-source-id/${sourceId}/by-cohort-definition-id/${cohortDefinitionId}/breakdown-by-concept-id/${hareConceptId}`;
   const reqBody = {
-    method: "POST",
-    credentials: "include",
+    method: 'POST',
+    credentials: 'include',
     headers,
     body: JSON.stringify(variablesPayload),
   };
@@ -37,21 +38,21 @@ export const fetchOverlapInfo = async (
   controlCohortDefinitionId,
   selectedHare,
   selectedCovariates,
-  selectedDichotomousCovariates
+  selectedDichotomousCovariates,
 ) => {
   const variablesPayload = {
     variables: [
       ...selectedDichotomousCovariates.map(({ uuid, ...withNoId }) => withNoId),
       ...selectedCovariates.map((c) => ({
-        variable_type: "concept",
+        variable_type: 'concept',
         concept_id: c.concept_id,
       })),
     ],
   };
   const statsEndPoint = `${cohortMiddlewarePath}cohort-stats/check-overlap/by-source-id/${sourceId}/by-case-control-cohort-definition-ids/${caseCohortDefinitionId}/${controlCohortDefinitionId}/filter-by-concept-id-and-value/${hareConceptId}/${selectedHare.concept_value_as_concept_id}`;
   const reqBody = {
-    method: "POST",
-    credentials: "include",
+    method: 'POST',
+    credentials: 'include',
     headers,
     body: JSON.stringify(variablesPayload),
   };
@@ -62,7 +63,7 @@ export const fetchOverlapInfo = async (
 export const filterSubsetCovariates = (subsetCovariates) => {
   const filteredSubsets = [];
   subsetCovariates.forEach((covariate) => {
-    if (covariate.variable_type === "custom_dichotomous") {
+    if (covariate.variable_type === 'custom_dichotomous') {
       filteredSubsets.push({
         cohort_ids: covariate.cohort_ids,
         provided_name: covariate.provided_name,
@@ -70,7 +71,7 @@ export const filterSubsetCovariates = (subsetCovariates) => {
       });
     } else {
       filteredSubsets.push({
-        variable_type: "concept",
+        variable_type: 'concept',
         concept_id: covariate.concept_id,
       });
     }
@@ -81,15 +82,15 @@ export const filterSubsetCovariates = (subsetCovariates) => {
 export const fetchConceptStatsByHareSubset = async (
   cohortDefinitionId,
   subsetCovariates,
-  sourceId
+  sourceId,
 ) => {
   const variablesPayload = {
     variables: [...filterSubsetCovariates(subsetCovariates)],
   };
   const conceptStatsEndPoint = `${cohortMiddlewarePath}concept-stats/by-source-id/${sourceId}/by-cohort-definition-id/${cohortDefinitionId}/breakdown-by-concept-id/${hareConceptId}`;
   const reqBody = {
-    method: "POST",
-    credentials: "include",
+    method: 'POST',
+    credentials: 'include',
     headers,
     body: JSON.stringify(variablesPayload),
   };
@@ -103,10 +104,10 @@ export const addCDFilter = (cohortId, otherCohortId, covariateArr) => {
   // [cohortId, otherCohortId] also gets filtered out:
   const covariateRequest = [...covariateArr];
   const cdFilter = {
-    variable_type: "custom_dichotomous",
+    variable_type: 'custom_dichotomous',
     cohort_ids: [cohortId, otherCohortId],
     provided_name:
-      "auto_generated_extra_item_to_filter_out_case_control_overlap",
+      'auto_generated_extra_item_to_filter_out_case_control_overlap',
   };
   covariateRequest.push(cdFilter);
   return covariateRequest;
@@ -116,40 +117,38 @@ export const fetchConceptStatsByHareSubsetCC = async (
   cohortDefinitionId,
   otherCohortDefinitionId,
   covariateSubset,
-  sourceId
-) =>
-  fetchConceptStatsByHareSubset(
-    cohortDefinitionId,
-    addCDFilter(cohortDefinitionId, otherCohortDefinitionId, covariateSubset),
-    sourceId
-  );
+  sourceId,
+) => fetchConceptStatsByHareSubset(
+  cohortDefinitionId,
+  addCDFilter(cohortDefinitionId, otherCohortDefinitionId, covariateSubset),
+  sourceId,
+);
 
 export const fetchConceptStatsByHareForCaseControl = async (
   queriedCohortDefinitionId,
   otherCohortDefinitionId,
   selectedCovariates,
   selectedDichotomousCovariates,
-  sourceId
-) =>
-  fetchConceptStatsByHareSubset(
-    queriedCohortDefinitionId,
-    addCDFilter(queriedCohortDefinitionId, otherCohortDefinitionId, [
-      ...selectedCovariates,
-      ...selectedDichotomousCovariates,
-    ]),
-    sourceId
-  );
+  sourceId,
+) => fetchConceptStatsByHareSubset(
+  queriedCohortDefinitionId,
+  addCDFilter(queriedCohortDefinitionId, otherCohortDefinitionId, [
+    ...selectedCovariates,
+    ...selectedDichotomousCovariates,
+  ]),
+  sourceId,
+);
 
 export const fetchCovariateStats = async (
   cohortDefinitionId,
   selectedCovariateIds,
-  sourceId
+  sourceId,
 ) => {
   const covariateIds = { ConceptIds: selectedCovariateIds };
   const conceptStatsEndpoint = `${cohortMiddlewarePath}concept-stats/by-source-id/${sourceId}/by-cohort-definition-id/${cohortDefinitionId}`;
   const reqBody = {
-    method: "POST",
-    credentials: "include",
+    method: 'POST',
+    credentials: 'include',
     headers,
     body: JSON.stringify(covariateIds),
   };
@@ -165,12 +164,12 @@ export const fetchCohortDefinitions = async (sourceId) => {
 
 export const fetchCovariates = async (sourceId) => {
   const allowedConceptTypes = {
-    ConceptTypes: ["MVP Continuous"],
+    ConceptTypes: ['MVP Continuous'],
   };
   const conceptEndpoint = `${cohortMiddlewarePath}concept/by-source-id/${sourceId}/by-type`;
   const reqBody = {
-    method: "POST",
-    credentials: "include",
+    method: 'POST',
+    credentials: 'include',
     headers,
     body: JSON.stringify(allowedConceptTypes),
   };
@@ -191,7 +190,7 @@ export const useSourceFetch = () => {
     // do wts login and fetch sources on initialization
     fetchWithCreds({
       path: `${wtsPath}connected`,
-      method: "GET",
+      method: 'GET',
     }).then((res) => {
       if (res.status !== 200) {
         window.location.href = `${wtsPath}authorization_url?redirect=${window.location.pathname}`;
@@ -218,16 +217,15 @@ export const queryConfig = {
 export const getAllHareItems = (
   concept_value,
   allCaseHares,
-  allControlHares
+  allControlHares,
 ) => {
   const caseHareBreakdown = allCaseHares.find(
-    (hare) => hare.concept_value === concept_value
+    (hare) => hare.concept_value === concept_value,
   );
   const controlHareBreakdown = allControlHares.find(
-    (hare) => hare.concept_value === concept_value
+    (hare) => hare.concept_value === concept_value,
   );
   return [caseHareBreakdown, controlHareBreakdown];
 };
 
-export const atlasDomain = () =>
-  cohortMiddlewarePath.replace("cohort-middleware", "analysis/OHDSI%20Atlas");
+export const atlasDomain = () => cohortMiddlewarePath.replace('cohort-middleware', 'analysis/OHDSI%20Atlas');
