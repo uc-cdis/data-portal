b204886cd17ec542535d36b84b526f990037ce95
Bobae Kang <bkang.dev@gmail.com>
2022-10-11 19:51:46 -0500

    Display raw count values in tooltip

--- src/components/charts/TooltipCDIS/index.jsx
+++ src/components/charts/TooltipCDIS/index.jsx
@@ -5,7 +5,8 @@ import './TooltipCDIS.css';
  * @typedef {Object} TooltipCDISProps
  * @property {boolean} [active]
  * @property {string} [label]
- * @property {{ fill: string; name: string; value: number }[]} [payload]
+ * @property {{ fill: string; name: string; payload: { allCounts: number[] } }[]} [payload]
+ * @property {any} [testProp]
  */
 
 /** @param {TooltipCDISProps} props */
@@ -13,16 +14,15 @@ function TooltipCDIS({ active = false, label = '', payload = [] }) {
   if (!active) return null;
 
   const txts = label.split('#');
-  const number = parseInt(txts[0], 10);
   return (
     <div className='cdis-tooltip'>
       <h2>{`${txts[1]}`}</h2>
-      {payload.map((item) => (
+      {payload.map((item, i) => (
         <div
           key={item.name}
           style={{ color: `${item.fill}` }}
           className='body-typo'
-        >{`${item.name} : ${Math.round((item.value / 100) * number)}`}</div>
+        >{`${item.name} : ${item.payload.allCounts[i]}`}</div>
       ))}
     </div>
   );
@@ -35,7 +35,9 @@ TooltipCDIS.propTypes = {
     PropTypes.shape({
       fill: PropTypes.string,
       name: PropTypes.string,
-      value: PropTypes.string,
+      payload: PropTypes.shape({
+        allCounts: PropTypes.arrayOf(PropTypes.number),
+      }),
     })
   ),
 };
