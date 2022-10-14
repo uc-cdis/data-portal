2f36e85351f7b9a9af2a65cb626138af43e33ff9
Jarvis <113449836+jarvisraymond-uchicago@users.noreply.github.com>
2022-10-14 08:42:11 -0500

    Feat/combine gwas workflow view1 (#1117)
    * feat(CombineGWASWorkflowView1): Added bolerplate files
    
    * feat(CombineGWASWorkflowView1): created working prototype
    
    * feat(CombineGWASWorkflowView1): updated layout to better match design
    
    * feat(CombineGWASWorkflowView1): updated code organization to use utils and shared folder
    
    * feat(CombineGWASWorkflowView1): moved util files to utils folder in SelectStudyPopulation and updated refs
    
    * feat(CombineGWASWorkflowView1): formatted CSS and removed unneeded JSX space element
    
    * feat(CombineGWASWorkflowView1): Cleaned up CSS
    
    * feat(CombineGWASWorkflowView1): Updated text in button and moved state to parent
    
    * feat(CombineGWASWorkflowView1): updated child to use state from parent
    
    * feat(CombineGWASWorkflowView1): Fixed Eslint issues on AddCohortButton
    
    * feat(CombineGWASWorkflowView1): Fixed Eslint issues on CohortDefinitions
    
    * feat(CombineGWASWorkflowView1): Fixed Eslint issues on CohortDefinations
    
    * feat(CombineGWASWorkflowView1) Resolved ESLINT issues with SelectStudyPopulation
    
    * feat(CombineGWASWorkflowView1) Resolved ESLINT issues with GWASContainer.jsx and cohortMiddlewareApi
    
    * feat(CombineGWASWorkflowView1) Resolved ESLINT issues with GWASContainer.jsx props
    
    * feat(CombineGWASWorkflowView1) Removed unneeded steps component and logic from GWASContainer
    
    * feat(CombineGWASWorkflowView1) Added workflow specific wrapping CSS class so style changes do not impact other workflows
    
    * feat(CombineGWASWorkflowView1): created a story book to mock CohortSelect
    
    * feat(CombineGWASWorkflowView1): removed unneeded imports from GWASContainer.jsx
    
    * feat(CombineGWASWorkflowView1): set button styles so they render for story book and app
    
    * feat(CombineGWASWorkflowView1): Formatted code for clarity in CohortSelect.stories
    
    * feat(CombineGWASWorkflowView1): Added clarifying comment to explain eslint-disable-next-line in CohortSelect.jsx
    
    * feat(CombineGWASWorkflowView1): organized CSS
    
    * feat(CombineGWASWorkflowView1): ran stylelint auto fix for css files
    
    * feat(CombineGWASWorkflowView1): ensure form field and button are perfectly aligned
    
    * feat(CombineGWASWorkflowView1): updated state name to selectedStudyPopulationCohort per PR review recommendation
    
    * feat(CombineGWASWorkflowView1): removed unneeded var and prop otherCohortSelected
    
    * feat(CombineGWASWorkflowView1): removed unneeded var and prop otherCohortSelected from CohortDefinations.jsx
    
    * feat(CombineGWASWorkflowView1): removed unneeded var and prop otherCohortSelected from CohortSelect.jsx
    
    * feat(CombineGWASWorkflowView1): updated var name from fields to field in SearchBar.jsx
    
    * feat(CombineGWASWorkflowView1): updated var name from fields to field in CohortSelect.jsx

--- src/Analysis/GWASV2/GWASContainer.jsx
+++ src/Analysis/GWASV2/GWASContainer.jsx
@@ -1,100 +1,116 @@
-import React, { useState, useEffect } from "react";
-import { PropTypes } from "prop-types";
-import { Space, Button, Steps, Step, Popconfirm } from "antd";
+import React, { useState } from "react";
+import { Space, Button, Popconfirm } from "antd";
+import SelectStudyPopulation from "./SelectStudyPopulation/SelectStudyPopulation";
+import "./GWASV2.css";
 
 const GWASContainer = () => {
-    const [current, setCurrent] = useState(0);
-    const gwasSteps = [
-        {
-            title: 'Step 1',
-            description: 'Select Study Population',
-        },
-        {
-            title: 'Step 2',
-            description: 'Select Outcome Phenotypes',
-        },
-        {
-            title: 'Step 3',
-            description: 'Select Covariate Phenotype',
-        },
-        {
-            title: 'Step 4',
-            description: 'Configure GWAS',
-        }
-    ]
+  const [current, setCurrent] = useState(0);
+  const [
+    selectedStudyPopulationCohort,
+    setSelectedStudyPopulationCohort,
+  ] = useState({});
+  const gwasSteps = [
+    {
+      title: "Step 1",
+      description: "Select Study Population",
+    },
+    {
+      title: "Step 2",
+      description: "Select Outcome Phenotypes",
+    },
+    {
+      title: "Step 3",
+      description: "Select Covariate Phenotype",
+    },
+    {
+      title: "Step 4",
+      description: "Configure GWAS",
+    },
+  ];
 
-    const generateStep = () => {
-        // steps 2 & 3 very similar
-        switch (current) {
-            case 0:
-                // select study population
-                return <>step 1</>
-            case 1:
-                // outcome (customdichotomous or not)
-                return <>step 2</>
-            case 2:
-                // covariates (customdichtomous or not)
-                return <>step 3</>
-            case 3:
-                // all other input (mafs, imputation, etc), review, and submit
-                return <>step 4</>
-        }
+  const generateStep = () => {
+    // steps 2 & 3 very similar
+    switch (current) {
+      case 0:
+        // select study population
+        return (
+          <SelectStudyPopulation
+            selectedStudyPopulationCohort={selectedStudyPopulationCohort}
+            setSelectedStudyPopulationCohort={setSelectedStudyPopulationCohort}
+            current={current}
+          />
+        );
+      case 1:
+        // outcome (customdichotomous or not)
+        return <React.Fragment>step 2</React.Fragment>;
+      case 2:
+        // covariates (customdichtomous or not)
+        return <React.Fragment>step 3</React.Fragment>;
+      case 3:
+        // all other input (mafs, imputation, etc), review, and submit
+        return <React.Fragment>step 4</React.Fragment>;
+      default:
+        // required for eslint
+        return null;
     }
-    return (
-        <React.Fragment>
-            <React.Fragment>
-                <Space direction={'vertical'} style={{ width: '100%' }}>
-                    <Steps current={current}>
-                        {gwasSteps.map((item, idx) => {
-                            <Step key={idx} title={item.title} description={item.description} />
-                        })}
-                    </Steps>
-                    <div className='steps-content'>
-                        <Space direction={'vertical'} align={'center'} style={{ width: '100%' }}>
-                            {generateStep(current)}
-                        </Space>
-                    </div>
-                    <div className='steps-action'>
-                        <Button
-                            className='GWASUI-navBtn GWASUI-navBtn__next'
-                            type='primary'
-                            onClick={() => {
-                                setCurrent(current - 1);
-                            }}
-                        >
-                            Previous
-                        </Button>
-                        <Popconfirm
-                            title='Are you sure you want to leave this page?'
-                            //   onConfirm={() => resetGWASType()}
-                            okText='Yes'
-                            cancelText='No'
-                        >
-                            <Button type='link' size='medium' ghost>Select Different GWAS Type</Button>
-                        </Popconfirm>
-                        {current < gwasSteps.length - 1 && (
-                            <Button
-                                data-tour='next-button'
-                                className='GWASUI-navBtn GWASUI-navBtn__next'
-                                type='primary'
-                                onClick={() => {
-                                    setCurrent(current + 1);
-                                }}
-                            // disabled={!nextButtonEnabled}
-                            >
-                                Next
-                            </Button>
-                        )}
-                        {current === gwasSteps.length - 1 && (<div className='GWASUI-navBtn' />)}
-                    </div>
-                </Space>
-            </React.Fragment>
-        </React.Fragment>
-    )
-}
-
-GWASContainer.propTypes = {
-    refreshWorkflows: PropTypes.func.isRequired
-}
+  };
+  return (
+    <React.Fragment>
+      {/* Inline style block needed so centering rule doesn't impact other workflows */}
+      <style>{`.analysis-app__actions > div:nth-child(1) {margin: 0 auto; }`}</style>
+      <div className="GWASV2">
+        <Space direction={"vertical"} style={{ width: "100%" }}>
+          <div className="steps-content">
+            <Space
+              direction={"vertical"}
+              align={"center"}
+              style={{ width: "100%" }}
+            >
+              {generateStep(current)}
+            </Space>
+          </div>
+          <div className="steps-action">
+            <Button
+              className="GWASUI-navBtn GWASUI-navBtn__next"
+              type="primary"
+              onClick={() => {
+                setCurrent(current - 1);
+              }}
+              disabled={current < 1 ? true : false}
+            >
+              Previous
+            </Button>
+            <Popconfirm
+              title="Are you sure you want to leave this page?"
+              //   onConfirm={() => resetGWASType()}
+              okText="Yes"
+              cancelText="No"
+            >
+              <Button type="link" size="medium">
+                Select Different GWAS Type
+              </Button>
+            </Popconfirm>
+            {current < gwasSteps.length - 1 && (
+              <Button
+                data-tour="next-button"
+                className="GWASUI-navBtn GWASUI-navBtn__next"
+                type="primary"
+                onClick={() => {
+                  setCurrent(current + 1);
+                }}
+                // disabled={!nextButtonEnabled}
+              >
+                Next
+              </Button>
+            )}
+            {current === gwasSteps.length - 1 && (
+              <div className="GWASUI-navBtn" />
+            )}
+          </div>
+        </Space>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default GWASContainer;
