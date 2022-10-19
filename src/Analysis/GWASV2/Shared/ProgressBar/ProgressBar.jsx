f7523f7b15700cbfa108503a5835a046d76bbc08
Jarvis Raymond <jarvisraymond@uchicago.edu>
2022-10-19 13:45:11 -0500

    feat(CombineGWASWorkflowProgressBar): ran autoformatters eslint and stylelint

--- src/Analysis/GWASV2/Shared/ProgressBar/ProgressBar.jsx
+++ src/Analysis/GWASV2/Shared/ProgressBar/ProgressBar.jsx
@@ -1,31 +1,37 @@
-import React from "react";
-import { Button, Steps } from "antd";
-import "./ProgressBar.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Steps } from 'antd';
+import './ProgressBar.css';
 
 const { Step } = Steps;
-const ProgressBar = ({ current }) => {
-  return (
-    <div className="progress-bar">
-      <div className="progress-bar__steps">
-        <Steps current={current}>
-          <Step
-            icon={<>1</>}
-            title={`${current <= 0 ? "Select" : "Edit"} Study Population`}
-          />
-          <Step
-            icon={<>2</>}
-            title={`${current <= 1 ? "Select" : "Edit"} Outcome Phenotype`}
-          />
-          <Step
-            icon={<>3</>}
-            title={`${current <= 2 ? "Select" : "Edit"} Covariate Phenotype`}
-          />
-          <Step icon={<>4</>} title={`Configure GWAS`} />
-        </Steps>
-      </div>
-      <Button>New to GWAS? Get started here</Button>
+const ProgressBar = ({ current }) => (
+  <div className='progress-bar'>
+    <div className='progress-bar__steps'>
+      <Steps current={current}>
+        <Step
+          icon={<React.Fragment>1</React.Fragment>}
+          title={`${current <= 0 ? 'Select' : 'Edit'} Study Population`}
+        />
+        <Step
+          icon={<React.Fragment>2</React.Fragment>}
+          title={`${current <= 1 ? 'Select' : 'Edit'} Outcome Phenotype`}
+        />
+        <Step
+          icon={<React.Fragment>3</React.Fragment>}
+          title={`${current <= 2 ? 'Select' : 'Edit'} Covariate Phenotype`}
+        />
+        <Step
+          icon={<React.Fragment>4</React.Fragment>}
+          title={'Configure GWAS'}
+        />
+      </Steps>
     </div>
-  );
+    <Button>New to GWAS? Get started here</Button>
+  </div>
+);
+
+ProgressBar.propTypes = {
+  current: PropTypes.number.isRequired,
 };
 
 export default ProgressBar;
