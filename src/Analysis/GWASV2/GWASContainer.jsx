0d3d5e5ad8a97dff30c31da994df959a809079e6
Jarvis Raymond <jarvisraymond@uchicago.edu>
2022-10-20 14:30:13 -0500

    feat(CombineGWASWorkflowProgressBar): Updated GWASContainer.jsx to use nextButtonEnabled boolean for disabling or enabling next button

--- src/Analysis/GWASV2/GWASContainer.jsx
+++ src/Analysis/GWASV2/GWASContainer.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { Space, Button, Popconfirm } from 'antd';
-import SelectStudyPopulation from './SelectStudyPopulation/SelectStudyPopulation';
-import ProgressBar from './Shared/ProgressBar/ProgressBar';
-import { gwasV2Steps } from './Shared/constants';
-import './GWASV2.css';
+import React, { useState } from "react";
+import { Space, Button, Popconfirm } from "antd";
+import SelectStudyPopulation from "./SelectStudyPopulation/SelectStudyPopulation";
+import ProgressBar from "./Shared/ProgressBar/ProgressBar";
+import { gwasV2Steps } from "./Shared/constants";
+import "./GWASV2.css";
 
 const GWASContainer = () => {
   const [current, setCurrent] = useState(0);
@@ -15,51 +15,60 @@ const GWASContainer = () => {
   const generateStep = () => {
     // steps 2 & 3 very similar
     switch (current) {
-    case 0:
-      // select study population
-      return (
-        <SelectStudyPopulation
-          selectedStudyPopulationCohort={selectedStudyPopulationCohort}
-          setSelectedStudyPopulationCohort={setSelectedStudyPopulationCohort}
-          current={current}
-        />
-      );
-    case 1:
-      // outcome (customdichotomous or not)
-      return <React.Fragment>step 2</React.Fragment>;
-    case 2:
-      // covariates (customdichtomous or not)
-      return <React.Fragment>step 3</React.Fragment>;
-    case 3:
-      // all other input (mafs, imputation, etc), review, and submit
-      return <React.Fragment>step 4</React.Fragment>;
-    default:
-      // required for eslint
-      return null;
+      case 0:
+        // select study population
+        return (
+          <SelectStudyPopulation
+            selectedStudyPopulationCohort={selectedStudyPopulationCohort}
+            setSelectedStudyPopulationCohort={setSelectedStudyPopulationCohort}
+            current={current}
+          />
+        );
+      case 1:
+        // outcome (customdichotomous or not)
+        return <React.Fragment>step 2</React.Fragment>;
+      case 2:
+        // covariates (customdichtomous or not)
+        return <React.Fragment>step 3</React.Fragment>;
+      case 3:
+        // all other input (mafs, imputation, etc), review, and submit
+        return <React.Fragment>step 4</React.Fragment>;
+      default:
+        // required for eslint
+        return null;
     }
   };
+
+  let nextButtonEnabled = true;
+  if (
+    current === 0 &&
+    Object.keys(selectedStudyPopulationCohort).length === 0
+  ) {
+    nextButtonEnabled = false;
+  }
+
   return (
     <React.Fragment>
       <ProgressBar current={current} />
       {/* Inline style block needed so centering rule doesn't impact other workflows */}
       <style>
-        {'.analysis-app__actions > div:nth-child(1) { width: 100%; }'}
+        {".analysis-app__actions > div:nth-child(1) { width: 100%; }"}
       </style>
-      <div className='GWASV2'>
-        <Space direction={'vertical'} style={{ width: '100%' }}>
-          <div className='steps-content'>
+      <div className="GWASV2">
+        <Space direction={"vertical"} style={{ width: "100%" }}>
+          <div className="steps-content">
             <Space
-              direction={'vertical'}
-              align={'center'}
-              style={{ width: '100%' }}
+              direction={"vertical"}
+              align={"center"}
+              style={{ width: "100%" }}
             >
               {generateStep(current)}
             </Space>
           </div>
-          <div className='steps-action'>
+          <div className="steps-action">
             <Button
-              className='GWASUI-navBtn GWASUI-navBtn__next'
-              type='primary'
+              className="GWASUI-navBtn GWASUI-navBtn__next"
+              type="primary"
               onClick={() => {
                 setCurrent(current - 1);
               }}
@@ -68,30 +77,30 @@ const GWASContainer = () => {
               Previous
             </Button>
             <Popconfirm
-              title='Are you sure you want to leave this page?'
+              title="Are you sure you want to leave this page?"
               //   onConfirm={() => resetGWASType()}
-              okText='Yes'
-              cancelText='No'
+              okText="Yes"
+              cancelText="No"
             >
-              <Button type='link' size='medium'>
+              <Button type="link" size="medium">
                 Select Different GWAS Type
               </Button>
             </Popconfirm>
             {current < gwasV2Steps.length - 1 && (
               <Button
-                data-tour='next-button'
-                className='GWASUI-navBtn GWASUI-navBtn__next'
-                type='primary'
+                data-tour="next-button"
+                className="GWASUI-navBtn GWASUI-navBtn__next"
+                type="primary"
                 onClick={() => {
                   setCurrent(current + 1);
                 }}
-                // disabled={!nextButtonEnabled}
+                disabled={!nextButtonEnabled}
               >
                 Next
               </Button>
             )}
             {current === gwasV2Steps.length - 1 && (
-              <div className='GWASUI-navBtn' />
+              <div className="GWASUI-navBtn" />
             )}
           </div>
         </Space>
