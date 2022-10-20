768cace1374b844444536060c5b804b6c06c8509
Jarvis Raymond <jarvisraymond@uchicago.edu>
2022-10-20 14:14:34 -0500

    feat(CombineGWASWorkflowProgressBar): Updated JEST test to use for loops

--- src/Analysis/GWASV2/Shared/ProgressBar/ProgressBar.test.jsx
+++ src/Analysis/GWASV2/Shared/ProgressBar/ProgressBar.test.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import Enzyme, { render, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import ProgressBar from './ProgressBar';
+import React from "react";
+import Enzyme, { render, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import ProgressBar from "./ProgressBar";
+import { gwasV2Steps } from "../../Shared/constants";
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -9,8 +10,9 @@ Enzyme.configure({ adapter: new Adapter() });
   Code to aid in Jest Mocking, see:
   https://stackoverflow.com/questions/39830580/jest-test-fails-typeerror-window-matchmedia-is-not-a-function
 */
-window.matchMedia = window.matchMedia
-  || function () {
+window.matchMedia =
+  window.matchMedia ||
+  function() {
     return {
       matches: false,
       addListener() {},
@@ -30,7 +32,7 @@ const testElementClass = (wrapper, elNum, className) => {
     Enzyme has problems using Selectors, work around from:
     https://stackoverflow.com/questions/56145868/how-to-test-all-children-from-a-selector-except-the-first-child-in-jest
   */
-  wrapper.find('div.ant-steps-item').forEach((item, index) => {
+  wrapper.find("div.ant-steps-item").forEach((item, index) => {
     if (index === elNum - 1) {
       expect(item.hasClass(className)).toEqual(true);
     } else {
@@ -41,95 +43,39 @@ const testElementClass = (wrapper, elNum, className) => {
 
 /* TESTS */
 /* Test active step class */
-describe('Test that active step class renders with active class when current is zero', () => {
-  const wrapper = mount(<ProgressBar current={0} />);
-  it('should render first step with active class when current is zero', () => {
-    testElementClass(wrapper, 1, 'ant-steps-item-active');
-  });
-});
-
-describe('Test that active step class renders with active class when current is one', () => {
-  const wrapper = mount(<ProgressBar current={1} />);
-  it('should render second step with active class when current is one', () => {
-    testElementClass(wrapper, 2, 'ant-steps-item-active');
-  });
-});
-
-describe('Test that active step class renders with active class when current is two', () => {
-  const wrapper = mount(<ProgressBar current={2} />);
-  it('should render third step with active class when current is two', () => {
-    testElementClass(wrapper, 3, 'ant-steps-item-active');
-  });
-});
-
-describe('Test that active step class renders with active class when current is three', () => {
-  const wrapper = mount(<ProgressBar current={3} />);
-  it('should render fourth step with active class when current is three', () => {
-    testElementClass(wrapper, 4, 'ant-steps-item-active');
-  });
+describe("Test that active step class renders with active class when current is between 0 and 3", () => {
+  for (let i = 0; i < 4; i = i + 1) {
+    const wrapper = mount(<ProgressBar current={i} />);
+    it(`should render step ${i +
+      1} with active class when current is ${i}`, () => {
+      testElementClass(wrapper, i + 1, "ant-steps-item-active");
+    });
+  }
 });
 
 /* Test Dynamic Text for Steps */
-describe('Test that each step renders with correct text when current is zero', () => {
-  const wrapper = render(<ProgressBar current={0} />);
-  it('should render first step', () => {
-    testElementText(wrapper, 1, '1Select Study Population');
-  });
-  it('should render second step', () => {
-    testElementText(wrapper, 2, '2Select Outcome Phenotype');
-  });
-  it('should render third step', () => {
-    testElementText(wrapper, 3, '3Select Covariate Phenotype');
-  });
-  it('should render fourth step', () => {
-    testElementText(wrapper, 4, '4Configure GWAS');
-  });
-});
-
-describe('Test that each step renders with correct text when current is one', () => {
-  const wrapper = render(<ProgressBar current={1} />);
-  it('should render first step', () => {
-    testElementText(wrapper, 1, '1Edit Study Population');
-  });
-  it('should render second step', () => {
-    testElementText(wrapper, 2, '2Select Outcome Phenotype');
-  });
-  it('should render third step', () => {
-    testElementText(wrapper, 3, '3Select Covariate Phenotype');
-  });
-  it('should render fourth step', () => {
-    testElementText(wrapper, 4, '4Configure GWAS');
-  });
-});
-
-describe('Test that each step renders with correct text when current is two', () => {
-  const wrapper = render(<ProgressBar current={2} />);
-  it('should render first step', () => {
-    testElementText(wrapper, 1, '1Edit Study Population');
-  });
-  it('should render second step', () => {
-    testElementText(wrapper, 2, '2Edit Outcome Phenotype');
-  });
-  it('should render third step', () => {
-    testElementText(wrapper, 3, '3Select Covariate Phenotype');
-  });
-  it('should render fourth step', () => {
-    testElementText(wrapper, 4, '4Configure GWAS');
-  });
-});
-
-describe('Test that each step renders with correct text when current is three', () => {
-  const wrapper = render(<ProgressBar current={3} />);
-  it('should render first step', () => {
-    testElementText(wrapper, 1, '1Edit Study Population');
-  });
-  it('should render second step', () => {
-    testElementText(wrapper, 2, '2Edit Outcome Phenotype');
-  });
-  it('should render third step', () => {
-    testElementText(wrapper, 3, '3Edit Covariate Phenotype');
-  });
-  it('should render fourth step', () => {
-    testElementText(wrapper, 4, '4Configure GWAS');
-  });
+describe("Test that each step renders with correct text when current is between 0 and 3", () => {
+  for (let i = 0; i < 4; i = i + 1) {
+    const wrapper = render(<ProgressBar current={i} />);
+    const correctTextStep1 =
+      i === 0 ? gwasV2Steps[0].title : gwasV2Steps[0].secondaryTitle;
+    const correctTextStep2 =
+      i <= 1 ? gwasV2Steps[1].title : gwasV2Steps[1].secondaryTitle;
+    const correctTextStep3 =
+      i <= 2 ? gwasV2Steps[2].title : gwasV2Steps[2].secondaryTitle;
+    const correctTextStep4 =
+      i <= 3 ? gwasV2Steps[3].title : gwasV2Steps[3].secondaryTitle;
+    it(`should render first step with correct text: 1${correctTextStep1} when current is ${i}`, () => {
+      testElementText(wrapper, 1, `1${correctTextStep1}`);
+    });
+    it(`should render second step with correct text: 2${correctTextStep2} when current is ${i}`, () => {
+      testElementText(wrapper, 2, `2${correctTextStep2}`);
+    });
+    it(`should render third step with correct text: 3${correctTextStep3} when current is ${i}`, () => {
+      testElementText(wrapper, 3, `3${correctTextStep3}`);
+    });
+    it(`should render fourth step with correct text: 4${correctTextStep4} when current is ${i}`, () => {
+      testElementText(wrapper, 4, `4${correctTextStep4}`);
+    });
+  }
 });
