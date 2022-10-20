23e4f811f669f4287f4e9e5c367875cbd1cb7671
pieterlukasse <pieterlukasse@gmail.com>
2022-10-20 21:15:13 +0200

    add debounce for WTS refresh call
    to avoid multiple unnecessary repeated calls to this service
    in a short time interval. This is based on a similar solution
    found in `checkLoginStatus` (in `ProtectedContent.jsx`).

--- src/Workspace/WorkspaceRefreshToken.js
+++ src/Workspace/WorkspaceRefreshToken.js
@@ -1,20 +1,27 @@
 import { fetchWithCreds } from '../actions';
 import { wtsPath } from '../localconf';
 
+let lastRefreshMs = 0;
+const debounceMs = 60000;
+
 // start workspace session for WTS, call optional connectedCallBack if initialized/connected
 export const initWorkspaceRefreshToken = (connectedCallBack) => {
-    console.log('init/renew WTS refresh token...');
-    fetchWithCreds({
-      path: `${wtsPath}connected`,
-      method: 'GET',
-    })
-      .then(
-        ({ status }) => {
-          if (status !== 200) {
-            window.location.href = `${wtsPath}/authorization_url?redirect=${window.location.pathname}`;
-          } else if (connectedCallBack) {
-              connectedCallBack();
-          }
-        },
-      );
-  }
+    const nowMs = Date.now();
+    if (nowMs - lastRefreshMs > debounceMs) {
+      console.log('init/renew WTS refresh token...');
+      fetchWithCreds({
+        path: `${wtsPath}connected`,
+        method: 'GET',
+      })
+        .then(
+          ({ status }) => {
+            if (status !== 200) {
+              window.location.href = `${wtsPath}/authorization_url?redirect=${window.location.pathname}`;
+            } else if (connectedCallBack) {
+                connectedCallBack();
+            }
+          },
+        );
+        lastRefreshMs = Date.now();
+    }
+}
