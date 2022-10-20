83d47d0550f6f0fb13820a4d185986f0346c35bd
Jarvis <113449836+jarvisraymond-uchicago@users.noreply.github.com>
2022-10-20 15:26:57 -0500

    Feat/combine gwas workflow progress bar (#1125)
    * Update node js and fix failing unit tests (#1119)
    
    * fixed unhandled promise rejection
    
    ...which fixes locally failing jest test
    
    * fix failing unit test for UserProfile
    
    * udpate node js and npm for CI tests
    
    * make test a bit more specific
    
    * set node js to 16.x
    
    * update node js to 16.x and npm to 8.15 in Dockerfile
    
    * revert npm version change
    
    * feat(CombineGWASWorkflowProgressBar): Initial commit
    
    * feat(CombineGWASWorkflowProgressBar): created partially working prototype
    
    * feat(CombineGWASWorkflowProgressBar): added logic for displaying select or edit for each step
    
    * feat(CombineGWASWorkflowProgressBar): created CSS style sheet for new component and began overriding AntD styles to match design
    
    * feat(CombineGWASWorkflowProgressBar): fixed colors to match design for numeric icons
    
    * feat(CombineGWASWorkflowProgressBar): set width to container to 100%
    
    * feat(CombineGWASWorkflowProgressBar): added animation
    
    * feat(CombineGWASWorkflowProgressBar): added styles for Get Started button
    
    * feat(CombineGWASWorkflowProgressBar): positioned get started button
    
    * feat(CombineGWASWorkflowProgressBar): removed inline styles from ProgressBar.jsx
    
    * feat(CombineGWASWorkflowProgressBar): formatted GWASContainer.jsx
    
    * feat(CombineGWASWorkflowProgressBar): changed class name to progress-bar
    
    * feat(CombineGWASWorkflowProgressBar): began authoring JEST test
    
    * feat(CombineGWASWorkflowProgressBar): Added clarifying comment to jest test
    
    * feat(CombineGWASWorkflowProgressBar): Formatted test code
    
    * feat(CombineGWASWorkflowProgressBar): Added Story Book for ProgressBar
    
    * feat(CombineGWASWorkflowProgressBar): ran eslint on ProgressBar.test.jsx
    
    * feat(CombineGWASWorkflowProgressBar): ran autoformatters eslint and stylelint
    
    * feat(CombineGWASWorkflowProgressBar): Resolved style lint error in ProgressBar.css
    
    * feat(CombineGWASWorkflowProgressBar): Resolved remaining style lint errors in ProgressBar.css
    
    * feat(CombineGWASWorkflowProgressBar): reverted unintentionally changed file
    
    * feat(CombineGWASWorkflowProgressBar): reverted unintentionally changed file
    
    * feat(CombineGWASWorkflowProgressBar): reverted unintentionally changed file
    
    * feat(CombineGWASWorkflowProgressBar): reverted unintentionally changed file Dockerfile
    
    * feat(CombineGWASWorkflowProgressBar): reverted unintentionally changed file Submission/MapDataModel.jsx
    
    * feat(CombineGWASWorkflowProgressBar): reverted unintentionally changed file UserProfile/UserProfile.test.jsx
    
    * feat(CombineGWASWorkflowProgressBar): reverted unintentionally changed file again  UserProfile/UserProfile.test.jsx
    
    * feat(CombineGWASWorkflowProgressBar): Ran ESLINT fix
    
    * feat(CombineGWASWorkflowProgressBar): Refactored code to use a constants file to make code more DRY
    
    * feat(CombineGWASWorkflowProgressBar): Ran ESLINT on recently updated files
    
    * feat(CombineGWASWorkflowProgressBar): Ran ESLINT on recently updated files
    
    * feat(CombineGWASWorkflowProgressBar): Added media query so that button will not overlap progress bar on smaller screens
    
    * feat(CombineGWASWorkflowProgressBar): Formatted CSS with line break
    
    * feat(CombineGWASWorkflowProgressBar): Updated JEST test to use for loops
    
    * feat(CombineGWASWorkflowProgressBar): Updated GWASContainer.jsx to use nextButtonEnabled boolean for disabling or enabling next button
    
    * feat(CombineGWASWorkflowProgressBar): Ran eslint on  GWASContainer.jsx
    
    Co-authored-by: pieterlukasse <pieterlukasse@gmail.com>

---
+++ src/Analysis/GWASV2/Shared/constants.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/prefer-default-export */
+export const gwasV2Steps = [
+  {
+    title: 'Select Study Population',
+    secondaryTitle: 'Edit Study Population',
+  },
+  {
+    title: 'Select Outcome Phenotype',
+    secondaryTitle: 'Edit Outcome Phenotype',
+  },
+  {
+    title: 'Select Covariate Phenotype',
+    secondaryTitle: 'Edit Covariate Phenotype',
+  },
+  {
+    title: 'Configure GWAS',
+    secondaryTitle: 'Configure GWAS',
+  },
+];
