b912f9b2f7fe9c05f1c1c978b970f5f33bf7d293
Jarvis Raymond <jarvisraymond@uchicago.edu>
2022-10-20 09:07:27 -0500

    feat(CombineGWASWorkflowProgressBar): Ran ESLINT on recently updated files

--- src/Analysis/GWASV2/Shared/ProgressBar/ProgressBar.jsx
+++ src/Analysis/GWASV2/Shared/ProgressBar/ProgressBar.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Button, Steps } from "antd";
-import { gwasV2Steps } from "../constants";
-import "./ProgressBar.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Steps } from 'antd';
+import { gwasV2Steps } from '../constants';
+import './ProgressBar.css';
 
 const { Step } = Steps;
 const ProgressBar = ({ current }) => (
-  <div className="progress-bar">
-    <div className="progress-bar__steps">
+  <div className='progress-bar'>
+    <div className='progress-bar__steps'>
       <Steps current={current}>
         {gwasV2Steps.map((item, index) => (
           <Step
