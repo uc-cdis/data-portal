cec426b0ed385d60d98e3cbd2fb436818b497480
Mingfei Shao <mshao1@uchicago.edu>
2022-10-25 16:54:22 -0500

    mode change

--- src/components/GoogleAnalytics.jsx
+++ src/components/GoogleAnalytics.jsx
@@ -2,16 +2,16 @@ import React, { useEffect } from 'react';
 import { Route, useLocation } from 'react-router-dom';
 import ReactGA from 'react-ga4';
 
-export const GAInit = (trackingId, gaDebug, options = {}) => {
+export const GAInit = (trackingId, gaDebug) => {
   const isGAEnabled = (trackingId?.startsWith('UA-') || trackingId?.startsWith('G-'));
   if (isGAEnabled) {
     // needs to explicitly exclude the debug parameter to turn off debug mode
     // see https://support.google.com/analytics/answer/7201382?hl=en#zippy=%2Cgoogle-tag-websites
     console.log(gaDebug);
     if (gaDebug) {
-      ReactGA.initialize(trackingId, { debug: gaDebug, ...options });
+      ReactGA.initialize(trackingId, { gtagOptions: { debug_mode: true } });
     } else {
-      ReactGA.initialize(trackingId, { ...options });
+      ReactGA.initialize(trackingId);
     }
   }
   return isGAEnabled;
