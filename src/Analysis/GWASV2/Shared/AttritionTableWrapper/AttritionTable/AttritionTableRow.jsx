fe72e4877d8469f53178ce8ad37e1adafdabf413
Jarvis Raymond <jarvisraymond@uchicago.edu>
2022-11-16 09:40:04 -0600

    feat(CombineGWASWorkflowAttritionTableLogic): Ran eslint-new on changed files from previous commit

--- src/Analysis/GWASV2/Shared/AttritionTableWrapper/AttritionTable/AttritionTableRow.jsx
+++ src/Analysis/GWASV2/Shared/AttritionTableWrapper/AttritionTable/AttritionTableRow.jsx
@@ -35,13 +35,12 @@ const AttritionTableRow = ({
     ],
     // if there are not two cohorts selected, then quantitative
     // Otherwise if there are two cohorts selected, case control
-    () =>
-      fetchConceptStatsByHareSubset(
-        cohortDefinitionId,
-        currentCovariateAndCovariatesFromPrecedingRows,
-        outcome,
-        sourceId
-      ),
+    () => fetchConceptStatsByHareSubset(
+      cohortDefinitionId,
+      currentCovariateAndCovariatesFromPrecedingRows,
+      outcome,
+      sourceId,
+    ),
     /* TODO For Case Control
         : fetchConceptStatsByHareSubsetCC(
             cohortDefinitionId,
@@ -50,7 +49,7 @@ const AttritionTableRow = ({
             sourceId
           ),
           */
-    queryConfig
+    queryConfig,
   );
 
   const { breakdown } = { breakdown: data?.concept_breakdown };
@@ -59,13 +58,13 @@ const AttritionTableRow = ({
     if (breakdown?.length) {
       const filteredBreakdown = breakdown.filter(
         // eslint-disable-next-line camelcase
-        ({ concept_value }) => concept_value !== 'OTH'
+        ({ concept_value }) => concept_value !== 'OTH',
       );
       setBreakdownSize(
         filteredBreakdown.reduce(
           (acc, curr) => acc + curr.persons_in_cohort_with_value,
-          0
-        )
+          0,
+        ),
       );
       setBreakdownColumns(filteredBreakdown);
     } else {
@@ -83,7 +82,7 @@ const AttritionTableRow = ({
     const getSizeByColumn = (hare) => {
       const hareIndex = breakdownColumns.findIndex(
         // eslint-disable-next-line camelcase
-        ({ concept_value }) => concept_value === hare
+        ({ concept_value }) => concept_value === hare,
       );
       return hareIndex > -1
         ? breakdownColumns[hareIndex].persons_in_cohort_with_value
