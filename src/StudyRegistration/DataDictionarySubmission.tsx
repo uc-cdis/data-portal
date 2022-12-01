9bdba01b01ff94322685a3d112d113277130d659
Mingfei Shao <mshao1@uchicago.edu>
2022-12-01 15:41:03 -0600

    fixup

--- src/StudyRegistration/DataDictionarySubmission.tsx
+++ src/StudyRegistration/DataDictionarySubmission.tsx
@@ -57,7 +57,7 @@ const validateMessages = {
 /* eslint-enable no-template-curly-in-string */
 
 interface LocationState {
-  studyUID?: string|Number;
+  studyUID?: string | Number;
   studyNumber?: string;
   studyName?: string;
   studyRegistrationAuthZ?: string;
@@ -69,11 +69,11 @@ const DataDictionarySubmission: React.FunctionComponent<StudyRegistrationProps> 
   const location = useLocation();
 
   const [formSubmissionStatus, setFormSubmissionStatus] = useState<FormSubmissionState | null>(null);
-  const [studyNumber, setStudyNumber] = useState<string|undefined|null>(null);
-  const [studyName, setStudyName] = useState<string|undefined|null>(null);
-  const [studyUID, setStudyUID] = useState<string|Number|undefined|null>(null);
-  const [studyRegistrationAuthZ, setStudyRegistrationAuthZ] = useState<string|undefined|null>(null);
-  const [existingDataDictionaryName, setExistingDataDictionaryName] = useState<Array<string>>([]);
+  const [studyNumber, setStudyNumber] = useState<string | undefined | null>(null);
+  const [studyName, setStudyName] = useState<string | undefined | null>(null);
+  const [studyUID, setStudyUID] = useState<string | Number | undefined | null>(null);
+  const [studyRegistrationAuthZ, setStudyRegistrationAuthZ] = useState<string | undefined | null>(null);
+  const [existingDataDictionaryName, setExistingDataDictionaryName] = useState<Array<string> | undefined>([]);
   const [uploadProgress, setUploadProgress] = useState<number>(100);
   const [uploading, setUploading] = useState<boolean>(false);
 
@@ -83,8 +83,7 @@ const DataDictionarySubmission: React.FunctionComponent<StudyRegistrationProps> 
     setStudyNumber(locationStateData.studyNumber);
     setStudyName(locationStateData.studyName);
     setStudyRegistrationAuthZ(locationStateData.studyRegistrationAuthZ);
-    // TODO: change this back
-    setExistingDataDictionaryName(['baseline', 'followup']);
+    setExistingDataDictionaryName(locationStateData.existingDataDictionaryName);
   }, [location.state]);
 
   useEffect(() => form.resetFields(), [studyNumber, studyName, form]);
@@ -94,11 +93,11 @@ const DataDictionarySubmission: React.FunctionComponent<StudyRegistrationProps> 
       return true;
     }
     return (userHasDataUpload(props.userAuthMapping)
-    && userHasMethodForServiceOnResource('access', 'study_registration', studyRegistrationAuthZ, props.userAuthMapping)
+      && userHasMethodForServiceOnResource('access', 'study_registration', studyRegistrationAuthZ, props.userAuthMapping)
     );
   };
 
-  const uploadToS3 = (s3URL, file, progressCallback):Promise<any> => new Promise((resolve, reject) => {
+  const uploadToS3 = (s3URL, file, progressCallback): Promise<any> => new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = () => {
@@ -126,7 +125,7 @@ const DataDictionarySubmission: React.FunctionComponent<StudyRegistrationProps> 
 
   const handleUpload = (formValues) => {
     setFormSubmissionStatus({ status: 'info', text: 'Preparing for upload' });
-    const fileInfo = formValues.fileList[0];
+    const fileInfo = formValues.fileList_doNotInclude[0];
     if (!fileInfo?.name) {
       setFormSubmissionStatus({ status: 'error', text: 'Invalid file info received' });
       return;
@@ -145,7 +144,6 @@ const DataDictionarySubmission: React.FunctionComponent<StudyRegistrationProps> 
             }
             const fullName = `${formValues['First Name']} ${formValues['Last Name']}`;
             const email = formValues['E-mail Address'];
-            const dataDictionaryName = formValues['Data Dictionary Name'];
             let contents = `Grant Number: ${studyNumber}\nStudy Name: ${studyName}\nEnvironment: ${hostname}\nStudy UID: ${studyUID}\nData Dictionary GUID: ${guid}`;
             Object.entries(formValues).filter(([key]) => !key.includes('_doNotInclude')).forEach((entry) => {
               const [key, value] = entry;
@@ -177,55 +175,55 @@ const DataDictionarySubmission: React.FunctionComponent<StudyRegistrationProps> 
 
   if (formSubmissionStatus) {
     switch (formSubmissionStatus.status) {
-    case 'success':
-      return (
-        <div className='study-reg-container'>
-          <div className='study-reg-form-container'>
-            <Result
-              status={formSubmissionStatus.status}
-              title='Your Data Dictionary has been submitted!'
-              subTitle='Please allow up to 48 hours before this data dictionary shows up in Discovery page'
-              extra={[
-                <Link key='discovery' to={'/discovery'}>
-                  <Button>Go To Discovery Page</Button>
-                </Link>,
-              ]}
-            />
+      case 'success':
+        return (
+          <div className='study-reg-container'>
+            <div className='study-reg-form-container'>
+              <Result
+                status={formSubmissionStatus.status}
+                title='Your Data Dictionary has been submitted!'
+                subTitle='Please allow up to 48 hours before this data dictionary shows up in Discovery page'
+                extra={[
+                  <Link key='discovery' to={'/discovery'}>
+                    <Button>Go To Discovery Page</Button>
+                  </Link>,
+                ]}
+              />
+            </div>
           </div>
-        </div>
-      );
-    case 'info':
-      return (
-        <div className='study-reg-container'>
-          <div className='study-reg-form-container'>
-            <Result
-              status={formSubmissionStatus.status}
-              title='Submitting data dictionary, please do not close this page or navigate away'
-              subTitle={formSubmissionStatus.text}
-              extra={<Progress percent={uploadProgress} showInfo={false} status='active' />}
-            />
+        );
+      case 'info':
+        return (
+          <div className='study-reg-container'>
+            <div className='study-reg-form-container'>
+              <Result
+                status={formSubmissionStatus.status}
+                title='Submitting data dictionary, please do not close this page or navigate away'
+                subTitle={formSubmissionStatus.text}
+                extra={<Progress percent={uploadProgress} showInfo={false} status='active' />}
+              />
+            </div>
           </div>
-        </div>
-      );
-    case 'error':
-      return (
-        <div className='study-reg-container'>
-          <div className='study-reg-form-container'>
-            <Result
-              status={formSubmissionStatus.status}
-              title='A problem has occurred during submission!'
-              subTitle={formSubmissionStatus.text}
-              extra={[
-                <Button type='primary' key='close' onClick={() => { setFormSubmissionStatus(null); }}>
-                  Close
-                </Button>,
-              ]}
-            />
+        );
+      case 'error':
+        return (
+          <div className='study-reg-container'>
+            <div className='study-reg-form-container'>
+              <Result
+                status={formSubmissionStatus.status}
+                title='A problem has occurred during submission!'
+                subTitle={formSubmissionStatus.text}
+                extra={[
+                  <Button type='primary' key='close' onClick={() => { setFormSubmissionStatus(null); }}>
+                    Close
+                  </Button>,
+                ]}
+              />
+            </div>
           </div>
-        </div>
-      );
-    default:
-      return null;
+        );
+      default:
+        return null;
     }
   }
 
@@ -268,18 +266,20 @@ const DataDictionarySubmission: React.FunctionComponent<StudyRegistrationProps> 
             rules={[{ required: true }]}
           >
             <Input />
-            { (existingDataDictionaryName.length)
-              && (
+          </Form.Item>
+          {(existingDataDictionaryName?.length) ?
+            (
+              <Form.Item {...tailLayout}>
                 <Space direction='vertical'>
                   <Typography>This study is already linked to data dictionaries with the following names:</Typography>
-                  <div>{existingDataDictionaryName.map(name => <Tag>{name}</Tag>)}</div>
+                  <div>{existingDataDictionaryName.map(name => <Tag key={name}>{name}</Tag>)}</div>
                   <Typography>Using an existing name will overwrite the existing link.</Typography>
                 </Space>
-              )}
-          </Form.Item>
-          <Divider plain>Administrative Fields <Tooltip title='We need these information so we can send you updates regarding your data dictionary submission. We do not save these information on the platform'>
-                <QuestionCircleOutlined className='study-reg-form-item__middle-icon' />
-              </Tooltip></Divider>
+              </Form.Item>
+            ) : null}
+          <Divider plain>Administrative Fields<Tooltip title='We need these information so we can send you updates regarding your data dictionary submission. We do not save these information on the platform'>
+            <QuestionCircleOutlined className='study-reg-form-item__middle-icon' />
+          </Tooltip></Divider>
           <Form.Item
             name='First Name'
             label='Submitter First Name'
