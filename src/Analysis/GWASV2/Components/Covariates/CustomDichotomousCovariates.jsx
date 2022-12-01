257ed0425c8c395e2f23bef4c64561395e13d4cc
Jarvis <113449836+jarvisraymond-uchicago@users.noreply.github.com>
2022-12-01 13:36:51 -0600

    Feat/gwas unified flow file organization (#1148)
    * feat(gwasUnifiedFlowFileOrganization): Created Steps directory and selectStudyPopulation step
    
    * feat(gwasUnifiedFlowFileOrganization): Removed unneeded import from GWASContainer
    
    * feat(gwasUnifiedFlowFileOrganization): Updated code to resolve selection error in SelectStudyPopulation from prior commit
    
    * feat(gwasUnifiedFlowFileOrganization): Fixed ESLINT issues
    
    * feat(gwasUnifiedFlowFileOrganization): Rearranged files into Steps folder
    
    * feat(gwasUnifiedFlowFileOrganization): Renamed CohortSelect to SelectCohort to match 'SelectOutcome'
    
    * feat(gwasUnifiedFlowFileOrganization): Updated Refs to file locations
    
    * feat(gwasUnifiedFlowFileOrganization): Created Components directory
    
    * feat(gwasUnifiedFlowFileOrganization): Removed console statement from reducer.js
    
    * feat(gwasUnifiedFlowFileOrganization): Updated import in ProgressBar.test.jsx and ran npm run eslint-new
    
    * feat(gwasUnifiedFlowFileOrganization): Renamed charts directory to Diagrams
    
    * feat(gwasUnifiedFlowFileOrganization): Fixed imports in storybook file
    
    * feat(gwasUnifiedFlowFileOrganization): Fixed import order in SelectCohort.stories.jsx
    
    * feat(gwasUnifiedFlowFileOrganization): Updated cohortMiddlewareApi location and Searchbar location
    
    * feat(gwasUnifiedFlowFileOrganization): Ran ESLINT-new
    
    * feat(gwasUnifiedFlowFileOrganization): Reverted unintentionally changed file
    
    * feat(gwasUnifiedFlowFileOrganization): Reverted unintentionally changed file (again)
    
    * feat(gwasUnifiedFlowFileOrganization): Reverted unintentionally changed file (third times the charm)

---
+++ src/Analysis/GWASV2/Components/Covariates/CustomDichotomousCovariates.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import CohortSelect from '../SelectCohort/SelectCohort';
+import ACTIONS from '../../Shared/StateManagement/Actions';
+import '../../../GWASUIApp/GWASUIApp.css';
+
+const CustomDichotomousCovariates = ({ dispatch, setMode, type }) => {
+  const [firstPopulation, setFirstPopulation] = useState(undefined);
+  const [secondPopulation, setSecondPopulation] = useState(undefined);
+  const [providedName, setProvidedName] = useState('');
+
+  const handleDichotomousSubmit = () => {
+    const dichotomous = {
+      variable_type: 'custom_dichotomous',
+      cohort_ids: [
+        firstPopulation.cohort_definition_id,
+        secondPopulation.cohort_definition_id,
+      ],
+      provided_name: providedName,
+    };
+    dispatch(
+      type === 'outcome'
+        ? { type: ACTIONS.SET_OUTCOME, payload: dichotomous }
+        : { type: ACTIONS.ADD_COVARIATE, payload: dichotomous },
+    );
+    setMode('');
+  };
+
+  const customDichotomousValidation = providedName.length === 0
+    || firstPopulation === undefined
+    || secondPopulation === undefined;
+
+  return (
+    <div>
+      <div className='GWASUI-flexRow' data-tour='name'>
+        <input
+          type='text'
+          className={'GWASUI-providedName'}
+          onChange={(e) => setProvidedName(e.target.value)}
+          value={providedName}
+          placeholder='Provide a name...'
+          style={{
+            width: '50%',
+            textSize: 'small',
+            paddingLeft: 5,
+            height: 45,
+            borderRadius: 5,
+            marginTop: 5,
+          }}
+        />
+        <button
+          type='button'
+          className={'GWASUI-dichBtn'}
+          onClick={() => setMode(undefined)}
+        >
+          cancel
+        </button>
+        <div data-tour='add-button'>
+          <button
+            type='button'
+            disabled={customDichotomousValidation}
+            className={`${
+              !customDichotomousValidation ? 'GWASUI-btnEnable' : ''
+            } GWASUI-dichBtn`}
+            onClick={() => handleDichotomousSubmit()}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+      <React.Fragment>
+        <div data-tour='choosing-dichotomous'>
+          <div className='GWASUI-flexRow' data-tour='table-repeat'>
+            <div>
+              <h3>Select NO Cohort</h3>
+              <CohortSelect
+                selectedCohort={firstPopulation}
+                handleCohortSelect={setFirstPopulation}
+              />
+            </div>
+            <div>
+              <h3>Select YES Cohort</h3>
+              <CohortSelect
+                selectedCohort={secondPopulation}
+                handleCohortSelect={setSecondPopulation}
+              />
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+      <div />
+    </div>
+  );
+};
+
+CustomDichotomousCovariates.propTypes = {
+  setMode: PropTypes.func.isRequired,
+  dispatch: PropTypes.func.isRequired,
+  type: PropTypes.string.isRequired,
+};
+
+export default CustomDichotomousCovariates;
---
+++ src/Analysis/GWASV2/Steps/SelectOutcome/SelectOutcome.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { PropTypes } from 'prop-types';
+import ContinuousCovariates from '../../Components/Covariates/ContinuousCovariates';
+import CustomDichotomousCovariates from '../../Components/Covariates/CustomDichotomousCovariates';
+
+const SelectOutcome = ({ covariates, dispatch, outcome }) => {
+  const [mode, setMode] = useState('');
+  const [selectedOutcome, setSelectedOutcome] = useState({});
+
+  const determineSelectOutcomeJsx = () => {
+    if (mode === 'continuous') {
+      return (
+        <ContinuousCovariates
+          setMode={setMode}
+          selected={selectedOutcome}
+          dispatch={dispatch}
+          handleSelect={setSelectedOutcome}
+          covariates={covariates}
+          outcome={outcome}
+          type={'outcome'}
+        />
+      );
+    }
+    if (mode === 'dichotomous') {
+      return (
+        <CustomDichotomousCovariates
+          setMode={setMode}
+          dispatch={dispatch}
+          covariates={covariates}
+          outcome={outcome}
+          type={'outcome'}
+        />
+      );
+    }
+    return (
+      <div>
+        <button
+          type='button'
+          style={{ height: 60, marginRight: 5 }}
+          onClick={() => setMode('continuous')}
+        >
+          Add Continuous Outcome Phenotype
+        </button>
+        <button
+          type='button'
+          style={{ height: 60, marginRight: 5 }}
+          onClick={() => setMode('dichotomous')}
+        >
+          Add Dichotomous Outcome Phenotype
+        </button>
+      </div>
+    );
+  };
+
+  // Outputs the JSX for the component:
+  return <div>{determineSelectOutcomeJsx()}</div>;
+};
+
+SelectOutcome.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  covariates: PropTypes.array.isRequired,
+  outcome: PropTypes.object,
+};
+
+SelectOutcome.defaultProps = {
+  outcome: null,
+};
+
+export default SelectOutcome;
