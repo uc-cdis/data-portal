da262056448feb83e24d25f76d7a60ed5318d747
Jarvis Raymond <jarvisraymond@uchicago.edu>
2022-12-08 11:15:59 -0600

    feat(gwasUnifiedFlowTableScroll): Initial commit

--- src/Analysis/GWASV2/Components/AttritionTableWrapper/AttritionTable/AttritionTable.stories.jsx
+++ src/Analysis/GWASV2/Components/AttritionTableWrapper/AttritionTable/AttritionTable.stories.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { rest } from 'msw';
 import AttritionTable from './AttritionTable';
+import { SourceContextProvider } from '../../../Shared/Source';
 import '../../../GWASV2.css';
 
 export default {
@@ -16,75 +17,214 @@ const mockedQueryClient = new QueryClient({
 });
 
 const MockTemplate = () => {
-  const [selectedOutcome, setSelectedOutcome] = useState({
+  const selectedCohort = {
+    cohort_definition_id: 123,
+    cohort_name: 'cohort name abc',
+  };
+  const [outcome] = useState({
     variable_type: 'concept',
     concept_id: 'id',
     concept_name: 'concept name',
   });
-
-  const [selectedCohort, setSelectedCohort] = useState({
-    cohort_definition_id: 123,
-    cohort_name: 'cohort name abc',
-  });
-  const [otherSelectedCohort, otherSetSelectedCohort] = useState({
-    cohort_definition_id: 456,
-    cohort_name: 'cohort name def',
-  });
-  const [covariates] = useState([
+  const tableType = '';
+  const covariatesLong = [
+    {
+      variable_type: 'custom_dichotomous',
+      provided_name: 'providednamebyuser',
+      cohort_ids: [12, 32],
+    },
     {
       variable_type: 'custom_dichotomous',
       cohort_ids: [1, 2],
       provided_name: 'dichotomous test1',
-      uuid: '12345',
-    },
-    {
-      variable_type: 'custom_dichotomous',
-      cohort_ids: [3, 4],
-      provided_name: 'dichotomous test2',
-      uuid: '123456',
     },
     {
       variable_type: 'concept',
+      concept_id: 'id',
+      concept_name: 'concept name',
+    },
+    {
       concept_id: 2000006886,
       concept_name: 'Attribute1',
-      concept_code: '',
-      concept_type: 'MVP Continuous',
+      variable_type: 'concept',
     },
     {
+      concept_id: 2000006880,
+      concept_name: 'Attribute0',
       variable_type: 'concept',
+    },
+    {
+      concept_id: 2000006881,
+      concept_name: 'Attribute1',
+      variable_type: 'concept',
+    },
+    {
+      concept_id: 2000006882,
+      concept_name: 'Attribute2',
+      variable_type: 'concept',
+    },
+    {
+      concept_id: 2000006883,
+      concept_name: 'Attribute3',
+      variable_type: 'concept',
+    },
+    {
+      concept_id: 2000006884,
+      concept_name: 'Attribute4',
+      variable_type: 'concept',
+    },
+    {
       concept_id: 2000006885,
-      concept_name: 'Attribute10',
-      concept_code: '',
-      concept_type: 'MVP Continuous',
+      concept_name: 'Attribute5',
+      variable_type: 'concept',
     },
     {
+      concept_id: 2000006886,
+      concept_name: 'Attribute6',
       variable_type: 'concept',
-      concept_id: 2000000708,
-      concept_name: 'Attribute11',
-      concept_code: '',
-      concept_type: 'MVP Continuous',
     },
-  ]);
+
+    {
+      concept_id: 2000006887,
+      concept_name: 'Attribute7',
+      variable_type: 'concept',
+    },
+
+    {
+      concept_id: 2000006888,
+      concept_name: 'Attribute8',
+      variable_type: 'concept',
+    },
+
+    {
+      concept_id: 2000006889,
+      concept_name: 'Attribute9',
+      variable_type: 'concept',
+    },
+
+    {
+      concept_id: 20000068810,
+      concept_name: 'Attribute10',
+      variable_type: 'concept',
+    },
+
+    {
+      concept_id: 20000068811,
+      concept_name: 'Attribute11',
+      variable_type: 'concept',
+    },
+
+    {
+      concept_id: 20000068812,
+      concept_name: 'Attribute12',
+      variable_type: 'concept',
+    },
+
+    {
+      concept_id: 20000068813,
+      concept_name: 'Attribute13',
+      variable_type: 'concept',
+    },
+
+    {
+      concept_id: 20000068814,
+      concept_name: 'Attribute14',
+      variable_type: 'concept',
+    },
+
+    {
+      concept_id: 20000068815,
+      concept_name: 'Attribute15',
+      variable_type: 'concept',
+    },
+
+    {
+      concept_id: 20000068816,
+      concept_name: 'Attribute16',
+      variable_type: 'concept',
+    },
+
+    {
+      concept_id: 20000068817,
+      concept_name: 'Attribute17',
+      variable_type: 'concept',
+    },
+
+    {
+      concept_id: 20000068818,
+      concept_name: 'Attribute18',
+      variable_type: 'concept',
+    },
+
+    {
+      concept_id: 20000068819,
+      concept_name: 'Attribute19',
+      variable_type: 'concept',
+    },
+
+    {
+      concept_id: 20000068820,
+      concept_name: 'Attribute20',
+      variable_type: 'concept',
+    },
+
+    {
+      concept_id: 20000068821,
+      concept_name: 'Attribute21',
+      variable_type: 'concept',
+    },
+
+    {
+      concept_id: 20000068822,
+      concept_name: 'Attribute22',
+      variable_type: 'concept',
+    },
+
+    {
+      concept_id: 20000068823,
+      concept_name: 'Attribute23',
+      variable_type: 'concept',
+    },
+
+    {
+      concept_id: 20000068824,
+      concept_name: 'Attribute24',
+      variable_type: 'concept',
+    },
+    {
+      concept_id: 20000068825,
+      concept_name: 'Attribute25',
+      variable_type: 'concept',
+    },
+  ];
+  const covariatesShort = [
+    {
+      variable_type: 'custom_dichotomous',
+      provided_name: 'providednamebyuser',
+      cohort_ids: [12, 32],
+    },
+  ];
 
   return (
     <QueryClientProvider client={mockedQueryClient}>
-      <AttritionTable
-        // quantitative:
-        sourceId={1}
-        selectedCohort={selectedCohort}
-        outcome={selectedOutcome}
-        covariates={covariates}
-        tableHeader={'Quantitative Attrition Table'}
-      />
-      <AttritionTable
-        // case-control:
-        sourceId={1}
-        selectedCohort={selectedCohort}
-        otherSelectedCohort={otherSelectedCohort}
-        outcome={selectedOutcome}
-        covariates={covariates}
-        tableHeader={'Case Cohort Attrition Table (1 of 2)'}
-      />
+      <SourceContextProvider>
+        <AttritionTable
+          selectedCohort={selectedCohort}
+          outcome={{
+            variable_type: 'custom_dichotomous',
+            cohort_ids: [1, 2],
+            provided_name: 'dichotomous test1',
+          }}
+          covariates={covariatesLong}
+          tableType={'Case Cohort'}
+        />
+        <AttritionTable
+          selectedCohort={selectedCohort}
+          outcome={outcome}
+          covariates={covariatesShort}
+          tableType={'Control'}
+        />
+      </SourceContextProvider>
     </QueryClientProvider>
   );
 };
