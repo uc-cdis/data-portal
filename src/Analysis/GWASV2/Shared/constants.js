b364eb2e1d6d36623e81977626ce75d939011d7a
alilahrime <alahrime@uchicago.edu>
2022-12-12 21:37:25 +0100

    refactor(VADC): parametrize common styles

--- src/Analysis/GWASV2/Shared/constants.js
+++ src/Analysis/GWASV2/Shared/constants.js
@@ -17,3 +17,63 @@ export const gwasV2Steps = [
     secondaryTitle: 'Configure GWAS',
   },
 ];
+
+/* eslint-disable import/prefer-default-export */
+export const pseudoTw = {
+  flex: {
+    direction: (dir) => {
+      return {
+        display: "flex",
+        flexDirection: dir
+      }
+  }},
+  text: {
+    align: (pos) => {
+      return {
+        textAlign: pos
+      }
+    },
+    color: (c) => {
+      return {
+        color : c
+      }
+    }
+  },
+  width: {
+    size: (w) => {
+      return {
+        width: w
+      }
+    }
+  },
+  height: {
+    size: (h) => {
+      return {
+        height: h
+      }
+    }
+  },
+  margin: {
+    auto: (...args) => {
+      if (args[0] === "default") return { margin: "auto" }
+      let { length } = args;
+      return length === 1 ? {
+        margin: args[0] === "x" ? "0 auto" : "auto 0"
+      }
+      : {
+          margin: args[0] === "x" ?
+          `${args[1]} auto` : `auto ${args[1]}`
+       }
+    }
+  },
+  bg: {
+    color: (c) => {
+      return {
+        backgroundColor: c
+      }
+    }
+  },
+  justifyBetween: {
+    justifyContent: "space-between"
+  },
+}
