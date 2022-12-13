5a6d99113c5ff97bb46228036b2b704c67373060
Jarvis Raymond <jarvisraymond@uchicago.edu>
2022-12-13 15:42:09 -0600

    feat(hare_and_submit_modal): Ran ESLINT-new

--- src/Analysis/GWASV2/Steps/ConfigureGWAS/ConfigureGWAS.jsx
+++ src/Analysis/GWASV2/Steps/ConfigureGWAS/ConfigureGWAS.jsx
@@ -59,18 +59,17 @@ const ConfigureGWAS = ({
   }, [showModal]);
 
   const submitJob = useMutation(
-    () =>
-      jobSubmission(
-        sourceId,
-        numOfPCs,
-        covariates,
-        outcome,
-        selectedHare,
-        mafThreshold,
-        imputationScore,
-        selectedCohort,
-        jobName
-      ),
+    () => jobSubmission(
+      sourceId,
+      numOfPCs,
+      covariates,
+      outcome,
+      selectedHare,
+      mafThreshold,
+      imputationScore,
+      selectedCohort,
+      jobName,
+    ),
     {
       onSuccess: (data) => {
         if (data?.status === 200) {
@@ -94,7 +93,7 @@ const ConfigureGWAS = ({
           });
         }
       },
-    }
+    },
   );
 
   const handleSubmit = () => {
@@ -168,9 +167,7 @@ const ConfigureGWAS = ({
               value={numOfPCs}
               min={1}
               max={10}
-              onChange={(e) =>
-                dispatch({ type: ACTIONS.UPDATE_NUM_PCS, payload: Number(e) })
-              }
+              onChange={(e) => dispatch({ type: ACTIONS.UPDATE_NUM_PCS, payload: Number(e) })}
             />
           </div>
           <div className='GWASUI-column'>
@@ -184,12 +181,10 @@ const ConfigureGWAS = ({
             <InputNumber
               id='input-maf'
               value={mafThreshold}
-              onChange={(e) =>
-                dispatch({
-                  type: ACTIONS.UPDATE_MAF_THRESHOLD,
-                  payload: Number(e),
-                })
-              }
+              onChange={(e) => dispatch({
+                type: ACTIONS.UPDATE_MAF_THRESHOLD,
+                payload: Number(e),
+              })}
               stringMode
               step='0.01'
               min={'0'}
@@ -218,12 +213,10 @@ const ConfigureGWAS = ({
             <InputNumber
               id='input-imputation'
               value={imputationScore}
-              onChange={(e) =>
-                dispatch({
-                  type: ACTIONS.UPDATE_IMPUTATION_SCORE,
-                  payload: Number(e),
-                })
-              }
+              onChange={(e) => dispatch({
+                type: ACTIONS.UPDATE_IMPUTATION_SCORE,
+                payload: Number(e),
+              })}
               stringMode
               step='0.1'
               min={'0'}
@@ -240,11 +233,11 @@ const ConfigureGWAS = ({
           okButtonProps={{ disabled: jobName === '' }}
           onOk={() => handleSubmit()}
           onCancel={() => setOpen(false)}
-          title={
+          title={(
             <div style={{ ...flexRow, ...{ justifyContent: 'space-between' } }}>
               <div>Review Details</div>
             </div>
-          }
+          )}
         >
           <Input placeholder='Enter Job Name' onChange={handleEnterJobName} />
           <div style={flexCol}>
