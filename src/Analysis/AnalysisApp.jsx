35392068226c0948180fdd866d787cbc84c6792b
Jarvis Raymond <jarvisraymond@uchicago.edu>
2022-12-19 15:43:39 -0600

    feat(gwasUnifiedFlowProgressBarText): Removed autoformating except from eslint-new from AnalysisApp.jsx

--- src/Analysis/AnalysisApp.jsx
+++ src/Analysis/AnalysisApp.jsx
@@ -40,9 +40,8 @@ class AnalysisApp extends React.Component {
 
   static getDerivedStateFromProps(props) {
     if (props.job && props.job.status === 'Completed') {
-      this.fetchJobResult().then((res) => ({
-        results: `${res.data.output}`.split('\n'),
-      }));
+      this.fetchJobResult()
+        .then((res) => ({ results: `${res.data.output}`.split('\n') }));
     }
     return null;
   }
@@ -54,11 +53,9 @@ class AnalysisApp extends React.Component {
   onSubmitJob = (e) => {
     e.preventDefault();
     this.clearResult();
-    this.props.submitJob({
-      organ: this.state.jobInput ? this.state.jobInput.value : null,
-    });
+    this.props.submitJob({ organ: this.state.jobInput ? this.state.jobInput.value : null });
     this.props.checkJobStatus();
-  };
+  }
 
   processAppMessages = (event) => {
     const pathArray = this.state.app.applicationUrl.split('/');
@@ -68,14 +65,12 @@ class AnalysisApp extends React.Component {
 
     // ONLY process messages coming from the same domain as the app AND
     // which contain the message "refresh token!":
-    if (
-      event.origin === applicationBaseUrl
-      && event.data === 'refresh token!'
-    ) {
+    if (event.origin === applicationBaseUrl
+      && event.data === 'refresh token!') {
       // Call function to refresh session:
       sessionMonitor.updateUserActivity();
     }
-  };
+  }
 
   getAppContent = (app) => {
     switch (app) {
@@ -88,31 +83,18 @@ class AnalysisApp extends React.Component {
             options={analysisApps[app].options}
             onChange={this.selectChange}
           />
-          <Button
-            label='Run Analysis'
-            buttonType='primary'
-            onClick={this.onSubmitJob}
-            isPending={this.isJobRunning()}
-          />
+          <Button label='Run Analysis' buttonType='primary' onClick={this.onSubmitJob} isPending={this.isJobRunning()} />
         </React.Fragment>
       );
     case 'ndhHIV':
-      return <HIVCohortFilter />;
+      return (
+        <HIVCohortFilter />
+      );
     case 'ndhVirus':
       return (
         <React.Fragment>
-          <input
-            className='text-input'
-            type='text'
-            placeholder='input data'
-            name='input'
-          />
-          <Button
-            label='Run'
-            buttonType='primary'
-            onClick={this.onSubmitJob}
-            isPending={this.isJobRunning()}
-          />
+          <input className='text-input' type='text' placeholder='input data' name='input' />
+          <Button label='Run' buttonType='primary' onClick={this.onSubmitJob} isPending={this.isJobRunning()} />
         </React.Fragment>
       );
     case 'GWASUIApp':
@@ -164,48 +146,45 @@ class AnalysisApp extends React.Component {
         </React.Fragment>
       );
     }
-  };
+  }
 
   isJobRunning = () => this.props.job && this.props.job.status === 'Running';
 
   selectChange = (option) => {
-    this.setState(
-      {
-        jobInput: option,
-        results: null,
-      },
-      () => {
-        if (option === null || this.props.job) {
-          this.props.resetJobState();
-        }
-      },
-    );
-  };
+    this.setState({
+      jobInput: option,
+      results: null,
+    }, () => {
+      if (option === null || this.props.job) {
+        this.props.resetJobState();
+      }
+    });
+  }
 
   fetchJobResult = async () => this.props.fetchJobResult(this.props.job.uid);
 
   clearResult = () => {
     this.setState({ results: null });
-  };
+  }
 
   updateApp = async () => {
     this.setState({
       app: analysisApps[decodeURIComponent(this.props.params.app)],
       loaded: true,
     });
-  };
+  }
 
   handleFullscreenButtonClick = () => {
     this.setState((prevState) => ({
       analysisIsFullscreen: !prevState.analysisIsFullscreen,
     }));
-  };
+  }
 
   handleIframeApp = () => {
     this.setState({
       isIframeApp: true,
     });
-  };
+  }
 
   render() {
     const { job, params } = this.props;
@@ -218,11 +197,7 @@ class AnalysisApp extends React.Component {
       <Button
         className='analysis-app__button'
         onClick={this.handleFullscreenButtonClick}
-        label={
-          this.state.analysisIsFullscreen
-            ? 'Exit Fullscreen'
-            : 'Make Fullscreen'
-        }
+        label={this.state.analysisIsFullscreen ? 'Exit Fullscreen' : 'Make Fullscreen'}
         buttonType='secondary'
         rightIcon={this.state.analysisIsFullscreen ? 'back' : 'external-link'}
       />
@@ -236,42 +211,39 @@ class AnalysisApp extends React.Component {
     return (
       <div className='analysis-app-wrapper'>
         <BackLink url='/analysis' label='Back to Apps' />
-        {loaded ? (
-          <div className='analysis-app'>
-            <h2 className='analysis-app__title'>{app.title}</h2>
-            <p className='analysis-app__description'>{app.description}</p>
-            <div
-              className={`${
-                this.state.analysisIsFullscreen
-                  ? 'analysis-app__fullscreen'
-                  : ''
-              }`}
-            >
-              <div className='analysis-app__actions'>
-                <QueryClientProvider client={queryClient} contextSharing>
-                  {appContent}
-                </QueryClientProvider>
-              </div>
-              {this.state.isIframeApp ? (
-                <div className='analysis-app__buttongroup'>
-                  {fullscreenButton}
+        {
+          loaded
+            ? (
+              <div className='analysis-app'>
+                <h2 className='analysis-app__title'>{app.title}</h2>
+                <p className='analysis-app__description'>{app.description}</p>
+                <div className={`${this.state.analysisIsFullscreen ? 'analysis-app__fullscreen' : ''}`}>
+                  <div className='analysis-app__actions'>
+                    <QueryClientProvider client={queryClient} contextSharing>
+                      {appContent}
+                    </QueryClientProvider>
+                  </div>
+                  {this.state.isIframeApp
+                    ? (
+                      <div className='analysis-app__buttongroup'>
+                        {fullscreenButton}
+                      </div>
+                    ) : null}
                 </div>
-              ) : null}
-            </div>
-            {showJobStatus ? (
-              <div className='analysis-app__job-status'>
-                {this.isJobRunning() ? (
-                  <Spin size='large' tip='Job in progress...' />
-                ) : null}
-                {job?.status === 'Completed' ? <h3>Job Completed</h3> : null}
-                {job?.status === 'Failed' ? <h3>Job Failed</h3> : null}
-                {results
-                  ? results.map((line, i) => <p key={i}>{line}</p>)
+                {(showJobStatus)
+                  ? (
+                    <div className='analysis-app__job-status'>
+                      {this.isJobRunning() ? <Spin size='large' tip='Job in progress...' /> : null}
+                      {job?.status === 'Completed' ? <h3>Job Completed</h3> : null}
+                      {job?.status === 'Failed' ? <h3>Job Failed</h3> : null}
+                      {results ? results.map((line, i) => <p key={i}>{line}</p>) : null}
+                    </div>
+                  )
                   : null}
               </div>
-            ) : null}
-          </div>
-        ) : null}
+            )
+            : null
+        }
       </div>
     );
   }
