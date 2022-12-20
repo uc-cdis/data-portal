e8627b180db42594330ff8b4483bd900774d93b5
Jarvis Raymond <jarvisraymond@uchicago.edu>
2022-12-20 11:15:03 -0600

    chore(gwasUnifiedFlowConfigureGwasRefactor): Moved Congratulations CSS to Congratulations.css

--- src/Analysis/GWASV2/Components/Congratulations/Congratulations.jsx
+++ src/Analysis/GWASV2/Components/Congratulations/Congratulations.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import DismissibleMessage from '../DismissibleMessage/DismissibleMessage';
 import ACTIONS from '../../Utils/StateManagement/Actions';
+import './Congratulations.css';
 
 const Congratulations = ({
   dispatch,
