40358c6b4b4164422a2ef989865150e6c2ed27fb
Jarvis Raymond <jarvisraymond@uchicago.edu>
2022-12-22 11:28:27 -0600

    chore(gwasUnifiedFlowRemoveInlineStyles): Removed inline styles in Histogram.css

--- src/Analysis/GWASV2/Components/Diagrams/PhenotypeHistogram/Histogram.jsx
+++ src/Analysis/GWASV2/Components/Diagrams/PhenotypeHistogram/Histogram.jsx
@@ -4,11 +4,12 @@ import {
 } from 'recharts';
 import PropTypes from 'prop-types';
 import { formatNumber } from '../../../Utils/constants';
+import "./Histogram.css";
 
 const CustomTooltip = ({ active, payload, label }) => {
   if (active && payload && payload.length) {
     return (
-      <div style={{ background: 'lightgray' }}>
+      <div className="histogram-tooltip">
         <p>{`Number of persons: ${formatNumber(payload[0].value)}, for values starting at: ${formatNumber(label)}`}</p>
       </div>
     );
