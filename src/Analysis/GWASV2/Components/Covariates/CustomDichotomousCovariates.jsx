719651a3bad33ff1399ce7fbd05735a66d2b50be
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-01-27 10:39:55 -0600

    bugfix(gwasUnifiedFlowVirtualTourButtons): Ran eslint and stylelint

--- src/Analysis/GWASV2/Components/Covariates/CustomDichotomousCovariates.jsx
+++ src/Analysis/GWASV2/Components/Covariates/CustomDichotomousCovariates.jsx
@@ -31,10 +31,9 @@ const CustomDichotomousCovariates = ({
     handleClose();
   };
 
-  const customDichotomousValidation =
-    providedName.length === 0 ||
-    firstPopulation === undefined ||
-    secondPopulation === undefined;
+  const customDichotomousValidation = providedName.length === 0
+    || firstPopulation === undefined
+    || secondPopulation === undefined;
 
   return (
     <div className='custom-dichotomous-covariates'>
