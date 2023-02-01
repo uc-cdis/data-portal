9851cad82b7616f8c52dc46fe6d2ac6668cd6348
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-02-01 09:22:16 -0600

    test(addReactTestingLibrary): Refactored Histogram.test.jsx to use a single args obj

--- src/Analysis/GWASV2/Components/Diagrams/PhenotypeHistogram/Histogram.test.jsx
+++ src/Analysis/GWASV2/Components/Diagrams/PhenotypeHistogram/Histogram.test.jsx
@@ -16,53 +16,44 @@ window.matchMedia = window.matchMedia
     };
   };
 
+const args = {
+  data: [
+    {
+      start: 0,
+      end: 13.499253780913016,
+      nr_persons: 1483,
+    },
+    {
+      start: 472.47388233195556,
+      end: 485.9731361128686,
+      nr_persons: 1331,
+    },
+    {
+      start: 485.9731361128686,
+      end: 499.4723898937816,
+      nr_persons: 1488,
+    },
+  ],
+  xAxisDataKey: 'key',
+  barDataKey: 'value',
+};
+
 describe('Histogram component', () => {
   it('renders histogram with default props', () => {
-    render(<Histogram data={[]} xAxisDataKey='key' barDataKey='value' />);
+    render(<Histogram {...args} />);
     const histogramComponent = screen.getByTestId('histogram');
     expect(histogramComponent).toBeInTheDocument();
   });
 
   it('renders three bars when given three datums', () => {
-    const { container } = render(
-      <Histogram
-        data={[
-          {
-            start: 0,
-            end: 13.499253780913016,
-            nr_persons: 1483,
-          },
-          {
-            start: 472.47388233195556,
-            end: 485.9731361128686,
-            nr_persons: 1331,
-          },
-          {
-            start: 485.9731361128686,
-            end: 499.4723898937816,
-            nr_persons: 1488,
-          },
-        ]}
-        xAxisDataKey='key'
-        barDataKey='value'
-      />,
-    );
+    const { container } = render(<Histogram {...args} />);
     expect(
       container.getElementsByClassName('recharts-bar-rectangle').length,
     ).toBe(3);
   });
 
   it('renders with a tooltip container', () => {
-    const data = [];
-    const xAxisDataKey = 'otherValue';
-    const barDataKey = 'value';
-    const { container } = render(
-      <Histogram
-        data={data}
-        xAxisDataKey={xAxisDataKey}
-        barDataKey={barDataKey}
-      />,
-    );
+    const { container } = render(<Histogram {...args} />);
     expect(
       container.getElementsByClassName('recharts-tooltip-wrapper').length,
     ).toBe(1);
