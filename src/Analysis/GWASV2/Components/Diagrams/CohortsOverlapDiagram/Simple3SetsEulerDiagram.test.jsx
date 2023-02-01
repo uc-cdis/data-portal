a06e02fa98716597d9d9e3af998bd117b25556f4
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-02-01 08:29:29 -0600

    test(addReactTestingLibrary): Simplified Simple3SetsEulerDiagram.test.jsx

--- src/Analysis/GWASV2/Components/Diagrams/CohortsOverlapDiagram/Simple3SetsEulerDiagram.test.jsx
+++ src/Analysis/GWASV2/Components/Diagrams/CohortsOverlapDiagram/Simple3SetsEulerDiagram.test.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Simple3SetsEulerDiagram from './Simple3SetsEulerDiagram';
 
-afterEach(cleanup);
-
 describe('<Simple3SetsEulerDiagram />', () => {
   it('renders the component', () => {
-    const { getByTestId } = render(
+    render(
       <Simple3SetsEulerDiagram
         set1Size={100}
         set2Size={200}
@@ -16,16 +14,13 @@ describe('<Simple3SetsEulerDiagram />', () => {
         set13Size={60}
         set23Size={70}
         set123Size={10}
-      />,
+      />
     );
-    const diagram = getByTestId('euler-diagram');
-    expect(diagram).toBeInTheDocument();
+    expect(screen.getByTestId('euler-diagram')).toBeInTheDocument();
   });
 
   it('throws an error if invalid set sizes are passed', () => {
-    const consoleSpy = jest
-      .spyOn(console, 'error')
-      .mockImplementation(() => {});
+    jest.spyOn(console, 'error').mockImplementation(() => {});
     expect(() => {
       render(
         <Simple3SetsEulerDiagram
@@ -36,11 +31,10 @@ describe('<Simple3SetsEulerDiagram />', () => {
           set13Size={60}
           set23Size={70}
           set123Size={10}
-        />,
+        />
       );
     }).toThrowError(
-      'Error: invalid set sizes. A set overlap cannot be bigger than the set itself.',
+      'Error: invalid set sizes. A set overlap cannot be bigger than the set itself.'
     );
-    consoleSpy.mockRestore();
   });
 });
