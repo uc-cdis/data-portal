868320f49690e4a4ec8c30ac587829a8eac438a6
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-02-01 13:31:46 -0600

    test(addReactTestingLibrary): Removed comments from ProgressBar.test.jsx

--- src/Analysis/GWASV2/Components/ProgressBar/ProgressBar.test.jsx
+++ src/Analysis/GWASV2/Components/ProgressBar/ProgressBar.test.jsx
@@ -3,15 +3,13 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import ProgressBar from './ProgressBar';
 
-// import ProgressBar from './ProgressBar';
-// Uncomment the above lines once @reactour is on CI server
-
 /*
   Code to aid in Jest Mocking, see:
   https://stackoverflow.com/questions/39830580/jest-test-fails-typeerror-window-matchmedia-is-not-a-function
 */
-window.matchMedia = window.matchMedia
-  || function () {
+window.matchMedia =
+  window.matchMedia ||
+  function() {
     return {
       matches: false,
       addListener() {},
@@ -20,10 +18,7 @@ window.matchMedia = window.matchMedia
   };
 
 const testElementClass = (currentStep, className) => {
-  // Uncomment this line once the CI testing server is updated
-  // render(<ProgressBar currentStep={currentStep} selectionMode='continuous' />);
   render(<ProgressBar currentStep={currentStep} selectionMode='continuous' />);
-
   const stepElements = screen.getAllByTestId('progress-bar-step');
   stepElements.forEach((item, index) => {
     if (index === currentStep) {
@@ -34,14 +29,12 @@ const testElementClass = (currentStep, className) => {
   });
 };
 
-/* TESTS /
-/ Test active step class */
+/* TESTS */
+/* Test active step class */
 describe('Test that active step class renders with active class when current is between 0 and 3', () => {
   for (let i = 0; i < 4; i += 1) {
-    // Remove the skip once the CI server has been updated with @reactour
-    // it.skip(`should render step ${i +
-    it(`should render step ${i
-      + 1} with active class when currentStep is ${i}`, () => {
+    it(`should render step ${i +
+      1} with active class when currentStep is ${i}`, () => {
       testElementClass(i, 'ant-steps-item-active');
     });
   }
