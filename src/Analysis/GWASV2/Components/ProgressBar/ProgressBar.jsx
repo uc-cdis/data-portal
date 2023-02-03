d0bcf214c4c112ba1daa83b839615d07035e5d2f
Jarvis <113449836+jarvisraymond-uchicago@users.noreply.github.com>
2023-02-03 14:22:26 -0600

    Test/add react testing library (#1221)
    * test(addReactTestingLibrary): Installed working version of RTL. Began refactoring ProgressBar.test.jsx to use RTL instead of Enzyme
    
    * test(addReactTestingLibrary): Refactored ProgressBar.test.jsx to use RTL
    
    * test(addReactTestingLibrary): Autoformatted code for ProgressBar.test.jsx
    
    * test(addReactTestingLibrary): Created Simple3SetesEulerDiagram.test.jsx
    
    * test(addReactTestingLibrary): Wrote a unit test using RTL for Histrogram
    
    * test(addReactTestingLibrary):Updated test and removed unneeded test file
    
    * test(addReactTestingLibrary): Resolved ESLINT errors
    
    * test(addReactTestingLibrary): CI test for NPM is failing, commented out code causing issues
    
    * test(addReactTestingLibrary): reverted historgram file back, accidental inclusion
    
    * test(addReactTestingLibrary): Simplified Simple3SetsEulerDiagram.test.jsx
    
    * test(addReactTestingLibrary): Added selector for Histrogram back in Histogram.jsx
    
    * test(addReactTestingLibrary): Removed test header from progress bar
    
    * test(addReactTestingLibrary): Resolved ESLINT errors
    
    * test(addReactTestingLibrary): Refactored Histogram.test.jsx to use a single args obj
    
    * test(addReactTestingLibrary): Refactored ProgressBar.test.jsx, it is now back in a state that should trigger CI testing issue
    
    * test(addReactTestingLibrary): Updated ProgressBar.test.jsx
    
    * test(addReactTestingLibrary): Ran npm install @emotion/react
    
    * Update ci.yaml
    
    * test(addReactTestingLibrary): Removed comments from ProgressBar.test.jsx
    
    * test(addReactTestingLibrary): Ran eslint new
    
    ---------
    
    Co-authored-by: Hara Prasad <haraprasadj@uchicago.edu>

--- src/Analysis/GWASV2/Components/ProgressBar/ProgressBar.jsx
+++ src/Analysis/GWASV2/Components/ProgressBar/ProgressBar.jsx
@@ -13,6 +13,7 @@ const ProgressBar = ({ currentStep, selectionMode }) => (
         {gwasV2Steps.map((item, index) => (
           <Step
             key={item.title}
+            data-testid='progress-bar-step'
             icon={<React.Fragment>{index + 1}</React.Fragment>}
             title={item.title}
           />
