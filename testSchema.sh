17b21150308765f61ee1e7876dcdd9f339f165b5
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-02-06 11:48:22 -0600

    fix(badLineInTestSchemaScript): Remove line 12 per Hara suggestion

--- testSchema.sh
+++ testSchema.sh
@@ -9,7 +9,6 @@ bpa "data.bloodpac.org"
 dcf "nci-crdc.datacommons.io"
 gtex "gen3.biodatacatalyst.nhlbi.nih.gov"
 anvil "gen3.theanvil.io"
-dev "qa.planx-pla.net"
 genomel "genomel.bionimbus.org"
 )
 index=0
