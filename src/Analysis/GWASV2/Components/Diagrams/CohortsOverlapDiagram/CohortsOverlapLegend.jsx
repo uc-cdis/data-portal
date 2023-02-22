a8b45a4c5b6dec39b3b8cbbb0af2db7c09813697
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-02-22 13:59:11 -0600

    feat(eulerDiagramLegend): Removed unneeded line breaks from CohortsOverlapLegend

--- src/Analysis/GWASV2/Components/Diagrams/CohortsOverlapDiagram/CohortsOverlapLegend.jsx
+++ src/Analysis/GWASV2/Components/Diagrams/CohortsOverlapDiagram/CohortsOverlapLegend.jsx
@@ -7,12 +7,10 @@ const CohortsOverlapLegend = ({ cohort1Label, cohort2Label, cohort3Label }) => (
       <span className='legend-mark legend-mark-1' />
       <span className='legend-label'>{cohort1Label}</span>
     </div>
-
     <div className='legend-item'>
       <span className='legend-mark legend-mark-2' />
       <span className='legend-label'>{cohort2Label}</span>
     </div>
-
     <div className='legend-item' id='legend-set3'>
       <span className='legend-mark legend-mark-3' />
       <span className='legend-label'>{cohort3Label}</span>
