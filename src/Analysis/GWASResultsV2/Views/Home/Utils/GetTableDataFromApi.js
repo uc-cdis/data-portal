824e1a620a81e82c43d6183bb8bb9e80debc8dd1
Jarvis <113449836+jarvisraymond-uchicago@users.noreply.github.com>
2023-03-08 14:36:37 -0600

    Feat/results app setup fe (#1248)
    * feat(resultAppSetupFE): setup home page
    
    * feat(resultsAppSetupFE): added useContext hook to maintain state
    
    * feat(resultsAppSetupFE): Setup Results and Execution data to display using useContext
    
    * feat(resultsAppSetupFE): simplified rand num code
    
    * feat(resultsAppSetupFE): fixed linter issues
    
    * feat(resultsAppSetupFE): fixed table layout issues
    
    * feat(resultsAppSetupFE): fixed style formatting
    
    * feat(resultsAppSetupFE): Created placeholder file for stoybook
    
    * feat(resultsAppSetupFE): Fixed broken storybook file
    
    * feat(resultsAppSetupFE): added multiple storybooks
    
    * feat(resultsAppSetupFE): added spinner and updated storybook
    
    * feat(resultsAppSetupFE): updated storybook to use null
    
    * feat(resultsAppSetupFE): cleaned up code
    
    * feat(resultsAppSetupFE): removed ref to deleted CSS file in ReturnHomeButton
    
    * feat(resultsAppSetupFE): added explanatory comment in GWASResultsContainer.jsx
    
    * feat(resultsAppSetupFE): made buttons more like design
    
    * feat(resultsAppSetupFE): reset poll timer to 5 seconds
    
    * feat(resultsAppSetupFE): ran eslint-new
    
    * feat(resultsAppSetupFE): addressed PR reviews comments, added ENUM for storing Views
    
    * feat(resultsAppSetupFE): fixed linting issues with proptypes
    
    * feat(resultsAppSetupFE): formatted code in HomeTable.jsx

---
+++ src/Analysis/GWASResultsV2/Views/Home/Utils/GetTableDataFromApi.js
@@ -0,0 +1,25 @@
+const sixDigitRandomNumber = () => Math.floor(100000 + Math.random() * 900000);
+
+// this is a placeholder for the API call in ticket VADC 465
+const GetTableDataFromApi = () => [
+  {
+    RunId: sixDigitRandomNumber(),
+    WorkflowName: 'some workflow name',
+    DateTimeStarted: 'DateTimeStarted',
+    JobStatus: 'some job status',
+    DateTimeSubmitted: 'some date time submitted',
+    ExecutionData: 'some ExecutionData for item 1',
+    ResultsData: 'some resultsData for item 1',
+  },
+  {
+    RunId: sixDigitRandomNumber(),
+    WorkflowName: 'some workflow name',
+    DateTimeStarted: 'DateTimeStarted',
+    JobStatus: 'some job status',
+    DateTimeSubmitted: 'some date time submitted',
+    ExecutionData: 'some ExecutionData for item 2',
+    ResultsData: 'some resultsData for item 2',
+  },
+];
+
+export default GetTableDataFromApi;
