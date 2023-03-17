9e1fc2394ea8a6fdc04c4e5fff255cb01fc71c99
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-03-17 14:40:13 -0500

    feat(resultsTablePagination): Refactored to use AntD table

--- src/Analysis/GWASResultsV2/Views/Home/HomeTable/HomeTable.jsx
+++ src/Analysis/GWASResultsV2/Views/Home/HomeTable/HomeTable.jsx
@@ -1,64 +1,144 @@
-import { Button } from 'antd';
-import React, { useContext } from 'react';
+import { Button, Table, Space, Dropdown } from 'antd';
+import { EllipsisOutlined } from '@ant-design/icons';
+import React, { useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import SharedContext from '../../../Utils/SharedContext';
 import './HomeTable.css';
+import Completed from './icons/Completed';
+import Pending from './icons/Pending';
+import Running from './icons/Running';
+import Failed from './icons/Failed';
 
 const HomeTable = ({ data }) => {
+    const dropDownItems = [
+    {
+      key: '1',
+      label: (
+        <a target="_blank" rel="noopener noreferrer" href="#">
+          Download
+        </a>
+      ),
+      disabled: true,
+    },
+    {
+      key: '2',
+      label: (
+        <a target="_blank" rel="noopener noreferrer" href="#">
+          Rerun
+        </a>
+      ),
+      disabled: true,
+    },
+    {
+      key: '3',
+      label: (
+        <a target="_blank" rel="noopener noreferrer" href="#">
+          Archive Job
+        </a>
+      ),
+      disabled: true,
+    },
+  ];
+
   const { setCurrentView, setSelectedRowData } = useContext(SharedContext);
+  const columns = [
+    {
+      title: 'UID',
+      dataIndex: 'uid',
+      key: 'uid',
+      sorter: (a, b) => {return a.uid.localeCompare(b.uid)},
+    },
+    {
+      title: 'Workflow name',
+      dataIndex: 'name',
+      key: 'name',
+      sorter: (a, b) => {return a.name.localeCompare(b.name)},
+    },
+    {
+      title: 'Date/ Time Started',
+      dataIndex: 'startedAt',
+      key: 'startedAt',
+      sorter: (a, b) => {return a.startedAt.localeCompare(b.startedAt)},
+    },
+    {
+      title: 'Job status',
+      key: 'phase',
+      render: (record) => {
+        return (
+          <>
+            {record.phase === 'Succeeded' && <Completed />}
+            {record.phase === 'pending' && <Pending />}
+            {record.phase === 'running' && <Running />}
+            {record.phase === 'Failed' && <Failed />}
+
+            {record.phase}
+          </>
+        )
+      },
+      sorter: (a, b) => {return a.phase.localeCompare(b.phase)},
+    },
+    {
+      title: 'Date/ Time Submitted',
+      key: 'DateTimeSubmitted',
+      render: (record) => {
+        return (
+          `item.DateTimeSubmitted missing at ${new Date().toLocaleString()}`
+        )},
+    },
+    {
+      title: 'View Details',
+      key: 'viewDetails',
+      render: (record) => {
+        return (
+          <Space>
+            <Button
+            onClick={() => {
+              setSelectedRowData({
+                uid: record.uid,
+                name: record.name,
+              });
+              setCurrentView('execution');
+            }}
+          >
+            Execution
+          </Button>
+          <Button
+            onClick={() => {
+              setSelectedRowData({
+                uid: record.uid,
+                name: record.name,
+              });
+              setCurrentView('results');
+            }}
+          >
+            Results
+          </Button>
+        </Space>
+      )},
+    },
+    {
+      title: 'Actions',
+      key: 'actions',
+      render:  (record) => {
+        return (
+          /*<Dropdown menu={{ dropDownItems }}>
+            <a onClick={(e) => e.preventDefault()}>
+              <EllipsisOutlined />
+            </a>
+          </Dropdown>*/<>Fix Commented code, causes runtime err on click</>
+        )
+      }
+    },
+  ];
 
   return (
-    <table className='home-table'>
-      <tbody>
-        <tr>
-          <th>Run ID</th>
-          <th>Workflow Name</th>
-          <th>Date/ Time Started</th>
-          <th>Job Status</th>
-          <th>Date/ Time Submitted</th>
-          <th>View Details</th>
-          <th>Actions</th>
-        </tr>
-        {data
-          && data.map((item) => (
-            <tr key={item?.uid}>
-              <td>{item?.uid}</td>
-              <td>{item?.name}</td>
-              <td>{item?.startedAt}</td>
-              <td>{item?.phase}</td>
-              <td>
-                {item.DateTimeSubmitted
-                  || `item.DateTimeSubmiited missing at ${new Date().toLocaleString()}`}
-              </td>
-              <td>
-                <Button
-                  onClick={() => {
-                    setSelectedRowData({
-                      uid: item?.uid,
-                      name: item?.name,
-                    });
-                    setCurrentView('execution');
-                  }}
-                >
-                  Execution
-                </Button>
-                <Button
-                  onClick={() => {
-                    setSelectedRowData({
-                      uid: item?.uid,
-                      name: item?.name,
-                    });
-                    setCurrentView('results');
-                  }}
-                >
-                  Results
-                </Button>
-              </td>
-              <td>Actions</td>
-            </tr>
-          ))}
-      </tbody>
-    </table>
+    <div className='home-table'>
+      <Table
+        dataSource={[...data]}
+        columns={columns}
+        pagination={{ defaultPageSize: 10, showSizeChanger: true, pageSizeOptions: ['10', '20', '30']}}
+      />
+    </div>
   );
 };
 HomeTable.propTypes = {
