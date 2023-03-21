45330dbf59f095bf02b658dde67dbbbcd9039714
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-03-21 12:21:32 -0500

    feat(resultsTablePagination): Fixed failing test

--- src/Analysis/GWASResultsV2/Views/Home/HomeTable/HomeTable.test.jsx
+++ src/Analysis/GWASResultsV2/Views/Home/HomeTable/HomeTable.test.jsx
@@ -17,14 +17,14 @@ describe('HomeTable component', () => {
     render(
       <SharedContext.Provider value={mockContext}>
         <HomeTable data={data} />
-      </SharedContext.Provider>,
+      </SharedContext.Provider>
     );
 
-    // Check that each of the values from data appear once in the dom
+    // Check that each of the values from data appear  in the dom
     data.forEach((item) => {
       Object.values(item).forEach((value) => {
-        const textTestNode = screen.getByText(value);
-        expect(textTestNode).toBeInTheDocument();
+        const textTestNodes = screen.getAllByText(value);
+        expect(textTestNodes[0]).toBeInTheDocument();
       });
     });
 
