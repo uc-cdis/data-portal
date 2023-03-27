459decfcab92166b23623b26a7453336a90e666f
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-03-27 15:30:54 -0500

    feat(resultsAppExecConnect): Ran linter

--- src/Analysis/GWASResultsV2/Views/Execution/Execution.jsx
+++ src/Analysis/GWASResultsV2/Views/Execution/Execution.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { useQuery } from 'react-query';
 import { Spin } from 'antd';
 import { gwasWorkflowPath } from '../../../../localconf';
