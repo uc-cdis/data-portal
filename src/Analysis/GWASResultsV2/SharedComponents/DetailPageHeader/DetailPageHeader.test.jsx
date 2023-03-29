775b1bc4a116fc86e82e4dcf78f169b8085f0d1c
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-03-29 09:26:11 -0500

    feat(resultsAppExecConnect): Wrote unit test for DetailPageHeader

---
+++ src/Analysis/GWASResultsV2/SharedComponents/DetailPageHeader/DetailPageHeader.test.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import SharedContext from '../../Utils/SharedContext';
+import DetailPageHeader from './DetailPageHeader';
+import '@testing-library/jest-dom';
+
+const setCurrentView = (input) => {
+  alert(`setCurrentView called with ${input}`);
+};
+describe('DetailPageHeader', () => {
+  test('renders page title and a return button', () => {
+    const pageTitle = 'Execution Details';
+    render(
+      <SharedContext.Provider value={{ setCurrentView }}>
+        <DetailPageHeader pageTitle={pageTitle} />
+      </SharedContext.Provider>
+    );
+    const titleElement = screen.getByText(pageTitle);
+    expect(titleElement).toBeInTheDocument();
+    const returnButton = screen.getByRole('button');
+    expect(returnButton).toBeInTheDocument();
+  });
+});
