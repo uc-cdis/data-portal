3b2e9cedfd74d15fd41f544c2421585bcf2e0e1c
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-04-03 14:16:37 -0500

    feat(results403StoryBooks): Updated test to pass

--- src/Analysis/GWASResultsV2/SharedComponents/ErrorMessage/ErrorMessage.jsx
+++ src/Analysis/GWASResultsV2/SharedComponents/ErrorMessage/ErrorMessage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-const ErrorMessage = () => <h1>❌ Error loading data for table</h1>;
+const ErrorMessage = () => <h1 data-testid='error-message'>❌ Error loading data for table</h1>;
 
 export default ErrorMessage;
