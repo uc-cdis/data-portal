82561d1d7ac507bf585c6b14d991987bb3f611a6
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-04-05 14:53:13 -0500

    feat(resultsViewStyles): Reverted storybook, added logic to hide broke image and only show error message

--- src/Analysis/GWASResultsV2/Views/Results/Results.stories.jsx
+++ src/Analysis/GWASResultsV2/Views/Results/Results.stories.jsx
@@ -16,7 +16,6 @@ const selectedRowData2 = { name: 'Test_Name2', uid: '7891011' };
 const selectedRowData3 = { name: 'Test_Name3', uid: '999111' };
 const selectedRowData4 = { name: 'Test_Name4', uid: '999222' };
 
-
 const setCurrentView = (input) => {
   alert(`setCurrentView called with ${input}`);
 };
@@ -27,19 +26,18 @@ const mockedQueryClient = new QueryClient({
   },
 });
 
-const MockTemplate = (selectedRowData) =>
+const MockTemplate = (selectedRowData) => (
   <QueryClientProvider client={mockedQueryClient}>
     <SharedContext.Provider
-        value={{
-          selectedRowData: selectedRowData,
-          setCurrentView,
-        }}
-      >
-        <Results />
-      </SharedContext.Provider>
-    </QueryClientProvider>
-
-
+      value={{
+        selectedRowData: selectedRowData,
+        setCurrentView,
+      }}
+    >
+      <Results />
+    </SharedContext.Provider>
+  </QueryClientProvider>
+);
 
 export const MockedSuccess = MockTemplate.bind({});
 MockedSuccess.args = selectedRowData1;
@@ -52,10 +50,7 @@ MockedSuccess.parameters = {
           const { argowrapperpath, workflowname } = req.params;
           console.log(argowrapperpath);
           console.log(workflowname);
-          return res(
-            ctx.delay(50000),
-            ctx.json(WorkflowStatusResponse)
-          );
+          return res(ctx.delay(500), ctx.json(WorkflowStatusResponse));
         }
       ),
       rest.get(
@@ -65,14 +60,18 @@ MockedSuccess.parameters = {
           console.log(index_did);
           return res(
             ctx.delay(500),
-            ctx.json({"url": index_did === '999-8888-7777-aaaa123456-777777' ? imageFile : imageFile+'.zip'}) // note: the .zip here is fake and although its download will be initiated in this storybook, it won't really work or download any .zip file
+            ctx.json({
+              url:
+                index_did === '999-8888-7777-aaaa123456-777777'
+                  ? imageFile
+                  : imageFile + '.zip',
+            }) // note: the .zip here is fake and although its download will be initiated in this storybook, it won't really work or download any .zip file
           );
         }
       ),
     ],
   },
 };
-
 
 export const MockedError = MockTemplate.bind({});
 MockedError.args = selectedRowData2;
@@ -81,7 +80,7 @@ MockedError.parameters = {
     handlers: [
       rest.get(
         'http://:argowrapperpath/ga4gh/wes/v2/status/:workflowname',
-        (_, res, ctx) =>  res(ctx.delay(800), ctx.status(403))
+        (_, res, ctx) => res(ctx.delay(800), ctx.status(403))
       ),
     ],
   },
@@ -98,10 +97,7 @@ MockedError2.parameters = {
           const { argowrapperpath, workflowname } = req.params;
           console.log(argowrapperpath);
           console.log(workflowname);
-          return res(
-            ctx.delay(500),
-            ctx.json(WorkflowStatusResponse)
-          );
+          return res(ctx.delay(500), ctx.json(WorkflowStatusResponse));
         }
       ),
       rest.get(
@@ -109,10 +105,7 @@ MockedError2.parameters = {
         (req, res, ctx) => {
           const { manhattan_plot_index_did } = req.params;
           console.log(manhattan_plot_index_did);
-          return res(
-            ctx.delay(500),
-            ctx.json({"url": imageFile +".invalid"})
-          );
+          return res(ctx.delay(500), ctx.json({ url: imageFile + '.invalid' }));
         }
       ),
     ],
@@ -132,7 +125,7 @@ MockedError3.parameters = {
           console.log(workflowname);
           return res(
             ctx.delay(500),
-            ctx.json({ "some_dummy": "and-wrong-response-format",})
+            ctx.json({ some_dummy: 'and-wrong-response-format' })
           );
         }
       ),
