b9e3f40ded3197d8e2a96b0d6f9449d3b4e8a6b6
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-04-11 16:19:19 -0500

    feat(resultsStyleTableView): implemented revisions from PR

--- src/Analysis/GWASResultsV2/SharedComponents/DateForTable/DateForTable.jsx
+++ src/Analysis/GWASResultsV2/SharedComponents/DateForTable/DateForTable.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './DateForTable.css';
 
-const DateForTable = ({ unformattedDate }) => {
-  const date = new Date(unformattedDate);
+const DateForTable = ({ utcFormattedDate }) => {
+  const date = new Date(utcFormattedDate);
   date.setSeconds(0, 0);
   const formattedDate = date.toLocaleDateString();
   const formattedTime = date.toLocaleTimeString().replace(/:00 /, '');
-  const userTimeZone = new Date()
+  const userTimeZone = date
     .toLocaleTimeString('en-us', { timeZoneName: 'short' })
     .split(' ')[2];
 
@@ -22,7 +22,7 @@ const DateForTable = ({ unformattedDate }) => {
 };
 
 DateForTable.propTypes = {
-  unformattedDate: PropTypes.string.isRequired,
+  utcFormattedDate: PropTypes.string.isRequired,
 };
 
 export default DateForTable;
