31cea078b7270f04403e07765528a60bba60c40b
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-05-01 11:22:22 -0500

    feat(resultsHomeTableFiltering): removed unneeded code comment

--- src/Analysis/GWASResultsV2/Views/Home/HomeTable/HomeTable.jsx
+++ src/Analysis/GWASResultsV2/Views/Home/HomeTable/HomeTable.jsx
@@ -1,7 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import {
-  Button, Table, Space, Input, DatePicker, Select,
-} from 'antd';
+import { Button, Table, Space, Input, DatePicker, Select } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 import PropTypes from 'prop-types';
 import moment from 'moment';
@@ -31,8 +29,7 @@ const HomeTable = ({ data }) => {
         currentPage: pagination.current,
       });
     }
-    // User updates sorting or filtering, set page to first page
-    // Ask Pieter what he thinks about having sort move pagination back to page one
+    // When the user updates sorting or filtering, set page to first page
     return setHomeTableState({
       ...homeTableState,
       currentPage: 1,
@@ -102,7 +99,8 @@ const HomeTable = ({ data }) => {
   };
 
   const phaseOptions = [];
-  Object.values(PHASES).forEach((phase) => phaseOptions.push({ value: phase, label: phase }),
+  Object.values(PHASES).forEach((phase) =>
+    phaseOptions.push({ value: phase, label: phase })
   );
 
   const columns = [
@@ -112,8 +110,8 @@ const HomeTable = ({ data }) => {
       key: 'name',
       sorter: (a, b) => a.name.localeCompare(b.name),
       sortOrder:
-        homeTableState.sortInfo?.columnKey === 'name'
-        && homeTableState.sortInfo.order,
+        homeTableState.sortInfo?.columnKey === 'name' &&
+        homeTableState.sortInfo.order,
       children: [
         {
           title: (
@@ -134,8 +132,8 @@ const HomeTable = ({ data }) => {
       key: 'wf_name',
       sorter: (a, b) => a.wf_name.localeCompare(b.wf_name),
       sortOrder:
-        homeTableState.sortInfo?.columnKey === 'wf_name'
-        && homeTableState.sortInfo.order,
+        homeTableState.sortInfo?.columnKey === 'wf_name' &&
+        homeTableState.sortInfo.order,
       children: [
         {
           title: (
@@ -156,8 +154,8 @@ const HomeTable = ({ data }) => {
       key: 'submittedAt',
       sorter: (a, b) => a.submittedAt.localeCompare(b.submittedAt),
       sortOrder:
-        homeTableState.sortInfo?.columnKey === 'submittedAt'
-        && homeTableState.sortInfo.order,
+        homeTableState.sortInfo?.columnKey === 'submittedAt' &&
+        homeTableState.sortInfo.order,
       children: [
         {
           title: (
@@ -180,8 +178,8 @@ const HomeTable = ({ data }) => {
       dataIndex: 'phase',
       key: 'phase',
       sortOrder:
-        homeTableState.sortInfo?.columnKey === 'phase'
-        && homeTableState.sortInfo.order,
+        homeTableState.sortInfo?.columnKey === 'phase' &&
+        homeTableState.sortInfo.order,
       children: [
         {
           title: (
@@ -215,8 +213,8 @@ const HomeTable = ({ data }) => {
       key: 'finishedAt',
       sorter: (a, b) => a.finishedAt.localeCompare(b.finishedAt),
       sortOrder:
-        homeTableState.sortInfo?.columnKey === 'finishedAt'
-        && homeTableState.sortInfo.order,
+        homeTableState.sortInfo?.columnKey === 'finishedAt' &&
+        homeTableState.sortInfo.order,
       dataIndex: 'finishedAt',
       children: [
         {
