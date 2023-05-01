9153379ca36387459d0cf2ecbce73fba8729a7b4
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-05-01 13:45:50 -0500

    feat(resultsHomeTableFiltering): Added recent changes to spinner container text

--- src/Analysis/GWASResultsV2/Views/Home/Home.jsx
+++ src/Analysis/GWASResultsV2/Views/Home/Home.jsx
@@ -21,7 +21,9 @@ const Home = () => {
     return (
       <React.Fragment>
         <div className='spinner-container'>
-          <Spin />
+          <Spin /> Retrieving the list of workflows.
+          <br />
+          Please wait...
         </div>
       </React.Fragment>
     );
