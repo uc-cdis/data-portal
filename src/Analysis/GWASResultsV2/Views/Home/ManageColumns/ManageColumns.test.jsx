67b76fc74b37b74c1f010e1296b1d07cf54af5ec
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-05-10 14:51:14 -0500

    feat(homeTableManageColumns): Wrote unit tests

--- src/Analysis/GWASResultsV2/Views/Home/ManageColumns/ManageColumns.test.jsx
+++ src/Analysis/GWASResultsV2/Views/Home/ManageColumns/ManageColumns.test.jsx
@@ -4,6 +4,7 @@ import SharedContext from '../../../Utils/SharedContext';
 import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import ManageColumns from './ManageColumns';
 import InitialHomeTableState from '../../../Utils/InitialHomeTableState';
+import InitialColumnManagement from '../../../Utils/InitialColumnManagement';
 
 describe('ManageColumns', () => {
   const state = {
@@ -12,7 +13,7 @@ describe('ManageColumns', () => {
   };
 
   const rowNames = [
-    'Restore Defaults'
+    'Restore Defaults',
     'Run ID',
     'Workflow Name',
     'Date/Time Submitted',
@@ -21,7 +22,37 @@ describe('ManageColumns', () => {
     'View Details',
     'Actions',
   ];
-  it('renders all row with correct default values', async () => {
+  const expectedNumberOfSwitches = 7;
+
+  it('renders manage columns button and all row names with correct default values', async () => {
+    render(
+      <SharedContext.Provider value={state}>
+        <ManageColumns />
+      </SharedContext.Provider>
+    );
+    const button = screen.getByText('Manage columns');
+    fireEvent.click(button);
+    await waitFor(() => {
+      rowNames.forEach((rowName) => {
+        expect(screen.getByText(rowName)).toBeInTheDocument();
+      });
+    });
+  });
+
+  it('renders all switches initially as checked', async () => {
+    render(
+      <SharedContext.Provider value={state}>
+        <ManageColumns />
+      </SharedContext.Provider>
+    );
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+    const checkedSwitches = await screen.findAllByRole('switch', {
+      checked: true,
+    });
+    expect(checkedSwitches).toHaveLength(expectedNumberOfSwitches);
+  });
+  it('Calls setHomeTableState with correct data after toggling a switch', async () => {
     render(
       <SharedContext.Provider value={state}>
         <ManageColumns />
@@ -30,77 +61,38 @@ describe('ManageColumns', () => {
     const button = screen.getByRole('button');
     fireEvent.click(button);
     await waitFor(() => {
-      rowNames.forEach((rowName) => {
-        expect(screen.getByText(rowName)).toBeInTheDocument();
-      });
+      expect(screen.getByText('Run ID')).toBeInTheDocument();
     });
-
-    /*
-    expect(
-      container.querySelectorAll('.manage-columns-switch .ant-switch-checked')
-        .length
-    ).toBe(6);
-    */
-  });
-
-  // expect(container.getElementsByClassName('ant-switch-checked').length).toBe(6);
-  /*  expect(getByLabelText('Run ID')).toBeChecked();
-    expect(getByText('Workflow Name')).toBeInTheDocument();
-    expect(getByLabelText('Workflow Name')).toBeChecked();
-    expect(getByText('Date/Time Submitted')).toBeInTheDocument();
-    expect(getByLabelText('Date/Time Submitted')).toBeChecked();
-    expect(getByText('Job Status')).toBeInTheDocument();
-    expect(getByLabelText('Job Status')).toBeChecked();
-    expect(getByText('Date/Time Finished')).toBeInTheDocument();
-    expect(getByLabelText('Date/Time Finished')).toBeChecked();
-    expect(getByText('View Details')).toBeInTheDocument();
-    expect(getByLabelText('View Details')).toBeChecked();
-    expect(getByText('Actions')).toBeInTheDocument();
-    expect(getByLabelText('Actions')).toBeChecked();
-
-  it('toggles Run ID column when switch is clicked', () => {
-    const { getByLabelText } = render(
-      <SharedContext.Provider value={state}>
-        <ManageColumns />
-      </SharedContext.Provider>,
-    );
-
-    fireEvent.click(getByLabelText('Run ID'));
-
-    expect(state.homeTableState.setHomeTableState).toHaveBeenCalledWith({
-      ...state.homeTableState.homeTableState,
+    fireEvent.click(screen.getByText('Run ID'));
+    expect(state.setHomeTableState).toHaveBeenCalledWith({
+      ...state.homeTableState,
       sortInfo: {},
       currentPage: 1,
       nameSearchTerm: '',
       columnManagement: {
-        ...state.homeTableState.homeTableState.columnManagement,
+        ...state.homeTableState.columnManagement,
         runId: false,
       },
     });
   });
-
-  it('toggles Workflow Name column when switch is clicked', () => {
-    const { getByLabelText } = render(
+  it('Calls setHomeTableState with correct data after restore defaults is clicked', async () => {
+    render(
       <SharedContext.Provider value={state}>
         <ManageColumns />
-      </SharedContext.Provider>,
+      </SharedContext.Provider>
     );
-
-    fireEvent.click(getByLabelText('Workflow Name'));
-
-    expect(state.homeTableState.setHomeTableState).toHaveBeenCalledWith({
-      ...state.homeTableState.homeTableState,
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+    await waitFor(() => {
+      expect(screen.getByText('Restore Defaults')).toBeInTheDocument();
+    });
+    fireEvent.click(screen.getByText('Restore Defaults'));
+    expect(state.setHomeTableState).toHaveBeenCalledWith({
+      ...state.homeTableState,
       sortInfo: {},
       currentPage: 1,
-      wfNameSearchTerm: '',
-      columnManagement: {
-        ...state.homeTableState.homeTableState.columnManagement,
-        workflowName: false,
-      },
+      nameSearchTerm: '',
+      columnManagement: InitialColumnManagement,
     });
   });
-
-  // add more tests for other columns
-});
- */
 });
