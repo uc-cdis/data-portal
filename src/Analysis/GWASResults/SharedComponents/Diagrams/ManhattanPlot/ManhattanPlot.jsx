fc8ccec0282b0f89877da94d14b6286ef8157cf5
pieterlukasse <pieterlukasse@gmail.com>
2023-05-30 21:28:51 +0200

    fix: eslint fixes for PheWeb Manhattan

--- src/Analysis/GWASResults/SharedComponents/Diagrams/ManhattanPlot/ManhattanPlot.jsx
+++ src/Analysis/GWASResults/SharedComponents/Diagrams/ManhattanPlot/ManhattanPlot.jsx
@@ -1,30 +1,24 @@
 import React, { useEffect } from 'react';
-import {create_gwas_plot} from '../lib/pheweb_plots';
-
 import PropTypes from 'prop-types';
+import { create_gwas_plot } from '../lib/pheweb_plots';
+
+/* eslint camelcase: 0 */ // --> OFF
 
 const ManhattanPlot = ({
-  variant_bins, unbinned_variants
+  variant_bins, unbinned_variants,
 }) => {
-
   useEffect(() => {
     create_gwas_plot(variant_bins, unbinned_variants);
   }, [variant_bins, unbinned_variants]);
 
   return (
-    <div id='manhattan_plot_container' data-testid='manhattan_plot_container' style={{marginTop: "100px"}}/>
+    <div id='manhattan_plot_container' data-testid='manhattan_plot_container' style={{ marginTop: '100px' }} />
   );
 };
-
 
 ManhattanPlot.propTypes = {
   variant_bins: PropTypes.array.isRequired,
   unbinned_variants: PropTypes.array.isRequired,
-};
-
-ManhattanPlot.defaultProps = {
-  variant_bins: null,
-  unbinned_variants: null,
 };
 
 export default ManhattanPlot;
