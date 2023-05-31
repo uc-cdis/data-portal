b92edb569cae202ff89e649f4a84af0fd95c2a03
pieterlukasse <pieterlukasse@gmail.com>
2023-05-31 20:14:56 +0200

    feat: add downloadManhattanPlot for PheWeb viz

--- src/Analysis/GWASResults/Components/Diagrams/ManhattanPlot/ManhattanPlot.jsx
+++ src/Analysis/GWASResults/Components/Diagrams/ManhattanPlot/ManhattanPlot.jsx
@@ -5,20 +5,21 @@ import { create_gwas_plot } from '../lib/pheweb_plots';
 /* eslint camelcase: 0 */ // --> OFF
 
 const ManhattanPlot = ({
-  variant_bins, unbinned_variants,
+  variant_bins, unbinned_variants, manhattan_plot_container_id,
 }) => {
   useEffect(() => {
-    create_gwas_plot(variant_bins, unbinned_variants);
+    create_gwas_plot(variant_bins, unbinned_variants, manhattan_plot_container_id);
   }, [variant_bins, unbinned_variants]);
 
   return (
-    <div id='manhattan_plot_container' data-testid='manhattan_plot_container' style={{ marginTop: '100px' }} />
+    <div id={manhattan_plot_container_id} data-testid={manhattan_plot_container_id} style={{ marginTop: '100px' }} />
   );
 };
 
 ManhattanPlot.propTypes = {
   variant_bins: PropTypes.array.isRequired,
   unbinned_variants: PropTypes.array.isRequired,
+  manhattan_plot_container_id: PropTypes.string.isRequired,
 };
 
 export default ManhattanPlot;
