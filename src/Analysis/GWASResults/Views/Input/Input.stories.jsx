76833731631c0f7dcb9a0e01b70c39861585b866
Jarvis <113449836+jarvisraymond-uchicago@users.noreply.github.com>
2023-05-31 10:40:08 -0500

    Feat/input details view (#1306)
    * feat(no-access): add no access message (#1301)
    
    * feat(inputDetailsView): Initial commit
    
    * feat(inputDetailsView): Ran linter
    
    * Feat/home table column management menu (#1304)
    
    * feat(no-access): add no access message (#1301)
    
    * feat(homeTableColumnManagementMenu): Initial commit
    
    * feat(homeTableColumnManagementMenu): Fixed bug due to hasOnlyBoolValues not returning computational output
    
    * feat(homeTableColumnManagementMenu): Ran linter
    
    * feat(homeTableColumnManagementMenu): Formatted CSS and reverted unintentionally changed file
    
    * feat(homeTableColumnManagementMenu): Ran stylelint
    
    * feat(homeTableColumnManagementMenu): Ran eslint new
    
    * feat(homeTableColumnManagementMenu): Changed method name for clarity
    
    * feat(homeTableColumnManagementMenu): Refactored toggleSwitch functions into a single re-usable function
    
    * feat(homeTableColumnManagementMenu): Reverted changes: refactoring doesn't address filtering resets
    
    * feat(homeTableColumnManagementMenu): Refactored again to address dynamic filtering for toggleColumn
    
    * feat(homeTableColumnManagementMenu): Renamed variables for clarity in toggleColumn function
    
    * feat(homeTableColumnManagementMenu): Clarified comment
    
    * feat(homeTableColumnManagementMenu): ran linter
    
    * feat(homeTableColumnManagementMenu): Renamed folder to HomeTableState, created separate file to hold function UpdateColumnManagement.js
    
    * feat(homeTableColumnManagementMenu): changed ColumnManagement obj var names to sound more boolean
    
    * feat(homeTableColumnManagementMenu): Wrote method to generate switch JSX, fixed broken test, ran linter
    
    * feat(homeTableColumnManagementMenu): Ran linter, fixed broken storybook story
    
    * feat(homeTableColumnManagementMenu): updated test for Manage Columns to programmatically determined num of expected switches
    
    ---------
    
    Co-authored-by: Thanh Dang Nguyen <thanhnd@uchicago.edu>
    
    * feat(inputDetailsView): Added logic for row specific API call
    
    * feat(inputDetailsView): Added comment explaining how to output covariates, formatted code
    
    * feat(inputDetailsView): Added comment explaining how to output covariates, formatted code
    
    * feat(inputDetailsView): Wrote method to parse covariates and return JSX for each
    
    * feat(inputDetailsView): Added fall backs for missing data
    
    * feat(inputDetailsView): refactored for code clarity
    
    * feat(inputDetailsView): fixed bug due to missing conditional
    
    * feat(inputDetailsView): Ran ESLINT new
    
    * feat(inputDetailsView): Removed unneeded console statement
    
    * feat(inputDetailsView): Removed unneeded JSON logging to DOM
    
    * feat(inputDetailsView): Wrote storybook and input unit test
    
    * feat(inputDetailsView): Created separate data for the tests, made test pass with null data for mocked failure json
    
    * feat(inputDetailsViews): Began writing JobDetails.test.jsx
    
    * feat(inputDetailsView): Added JEST test for job details
    
    * feat(inputDetailsView): Formatted code
    
    * feat(inputDetailsView): Updated failure JSON test data to have all shown fields null
    
    * feat(inputDetailsView): cleaned up code
    
    * feat(inputDetailsView): made test approach consistent
    
    * feat(inputDetailsView): made test approach consistent and work
    
    * feat(inputDetailsView): Ran linters
    
    * feat(inputDetailsView): Removed unneeded storybook template
    
    * feat(inputDetailsView): Updated output string and console.error per PR review
    
    * feat(inputDetailsView): Simplified processing function
    
    * feat(inputDetailsView): Added corrections from PR
    
    * feat(inputDetailsView): Added corrections from PR for JobDetails.js
    
    * feat(inputDetailsView): Ran linter
    
    ---------
    
    Co-authored-by: Thanh Dang Nguyen <thanhnd@uchicago.edu>

---
+++ src/Analysis/GWASResults/Views/Input/Input.stories.jsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import SharedContext from '../../Utils/SharedContext';
+import Input from './Input';
+import { rest } from 'msw';
+import MockedSuccessJSON from '../../TestData/InputViewData/MockedSuccessJSON';
+import MockedFailureJSON from '../../TestData/InputViewData/MockedFailureJSON';
+import './../../GWASResultsContainer.css';
+
+export default {
+  title: 'Tests2/GWASResults/Views/Input',
+  component: 'Input',
+};
+
+const mockedQueryClient = new QueryClient({
+  defaultOptions: {
+    queries: { retry: false },
+  },
+});
+
+const setCurrentView = (input) => {
+  alert(`setCurrentView called with ${input}`);
+};
+
+const selectedRowData = {
+  name: 'gwas-workflow-787571537',
+  wf_name: 'user created name',
+  uid: '4b125c09-9712-486f-bacd-ec1451aae935',
+  startedAt: '2022-02-15T13:00:00Z',
+  finishedAt: '2022-02-15T14:00:00Z',
+  phase: 'Failed',
+  DateTimeSubmitted: '2022-02-15T13:30:00Z',
+};
+
+const { name, uid } = selectedRowData;
+
+const MockTemplate = () => {
+  return (
+    <QueryClientProvider client={mockedQueryClient}>
+      <SharedContext.Provider
+        value={{
+          selectedRowData: selectedRowData,
+          setCurrentView,
+        }}
+      >
+        <Input />
+      </SharedContext.Provider>
+    </QueryClientProvider>
+  );
+};
+
+export const MockedFailure = MockTemplate.bind({});
+MockedFailure.parameters = {
+  msw: {
+    handlers: [
+      rest.get(
+        `http://:argowrapperpath/ga4gh/wes/v2/status/${name}?uid=${uid}`,
+        (req, res, ctx) => {
+          const { argowrapperpath } = req.params;
+          return res(ctx.delay(100), ctx.json(MockedFailureJSON));
+        }
+      ),
+    ],
+  },
+};
+
+export const MockedSuccess = MockTemplate.bind({});
+MockedSuccess.parameters = {
+  msw: {
+    handlers: [
+      rest.get(
+        `http://:argowrapperpath/ga4gh/wes/v2/status/${name}?uid=${uid}`,
+        (req, res, ctx) => {
+          const { argowrapperpath } = req.params;
+          return res(ctx.delay(100), ctx.json(MockedSuccessJSON));
+        }
+      ),
+    ],
+  },
+};
+
+export const MockedError500Response = MockTemplate.bind({});
+MockedError500Response.parameters = {
+  msw: {
+    handlers: [
+      rest.get(
+        `http://:argowrapperpath/ga4gh/wes/v2/status/${name}?uid=${uid}`,
+        (req, res, ctx) => {
+          const { argowrapperpath } = req.params;
+          return res(
+            ctx.delay(100),
+            ctx.status(500),
+            // Some errroneous responses can return an error object
+            ctx.json({ error: 'Mocked Server error response' })
+          );
+        }
+      ),
+    ],
+  },
+};
+export const MockedError403Response = MockTemplate.bind({});
+MockedError403Response.parameters = {
+  msw: {
+    handlers: [
+      rest.get(
+        `http://:argowrapperpath/ga4gh/wes/v2/status/${name}?uid=${uid}`,
+        (req, res, ctx) => {
+          const { argowrapperpath } = req.params;
+          return res(ctx.delay(100), ctx.status(403));
+        }
+      ),
+    ],
+  },
+};
---
+++ src/Analysis/GWASResults/Views/Input/JobDetails.jsx
@@ -0,0 +1,122 @@
+import React, { useContext } from 'react';
+import { useQuery } from 'react-query';
+import { Spin } from 'antd';
+import { gwasWorkflowPath } from '../../../../localconf';
+import IsJsonString from '../../Utils/IsJsonString';
+import SharedContext from '../../Utils/SharedContext';
+import LoadingErrorMessage from '../../SharedComponents/LoadingErrorMessage/LoadingErrorMessage';
+
+const JobDetails = () => {
+  const { selectedRowData } = useContext(SharedContext);
+  const { name, uid } = selectedRowData;
+  const endpoint = `${gwasWorkflowPath}status/${name}?uid=${uid}`;
+
+  const fetchData = async () => {
+    const res = await fetch(endpoint);
+    return res.json();
+  };
+
+  const { data, status } = useQuery('jobDetails', fetchData);
+
+  if (status === 'error') {
+    return <LoadingErrorMessage message='Error getting job details' />;
+  }
+  if (status === 'loading') {
+    return (
+      <div className='spinner-container' data-testid='spinner'>
+        <Spin />
+      </div>
+    );
+  }
+
+  if (!data || data.length === 0 || data.error) {
+    return <LoadingErrorMessage message='Issue Loading Data for Job Details' />;
+  }
+
+  const getParameterData = (key) => {
+    const datum = data?.arguments?.parameters?.find((obj) => obj.name === key);
+    return datum?.value || 'Unexpected Error';
+  };
+
+  const getPhenotype = () => {
+    if (
+      getParameterData('outcome')
+      && IsJsonString(getParameterData('outcome'))
+    ) {
+      return (
+        JSON.parse(getParameterData('outcome'))?.concept_name
+        || JSON.parse(getParameterData('outcome'))?.provided_name
+      );
+    }
+    /* eslint-disable-next-line no-console */
+    console.error('Data not found or not in expected JSON format');
+    return 'Unexpected Error';
+  };
+
+  const processCovariates = () => {
+    const variablesData = getParameterData('variables');
+    if (IsJsonString(variablesData)) {
+      return JSON.parse(variablesData);
+    }
+    return false;
+  };
+
+  const displayCovariates = () => {
+    const covariates = processCovariates();
+    if (covariates && covariates.length > 0) {
+      return covariates.map((obj, index) => (
+        <React.Fragment key={index}>
+          <span className='covariate-item'>
+            {obj?.concept_name}
+            {obj?.provided_name}
+            {!obj?.concept_name && !obj?.provided_name && 'Unexpected Error'}
+          </span>
+          <br />
+        </React.Fragment>
+      ));
+    }
+    return 'No covariates';
+  };
+
+  return (
+    <section data-testid='job-details' className='job-details'>
+      <h2 className='job-details-title'>{data.wf_name}</h2>
+      <div className='GWASResults-flex-col job-details-table'>
+        <div className='GWASResults-flex-row'>
+          <div>Number of PCs</div>
+          <div>{getParameterData('n_pcs')}</div>
+        </div>
+        <div className='GWASResults-flex-row'>
+          <div>MAF Cutoff</div>
+          <div>{getParameterData('maf_threshold')}</div>
+        </div>
+        <div className='GWASResults-flex-row'>
+          <div>HARE Ancestry</div>
+          <div>{getParameterData('hare_population')}</div>
+        </div>
+        <div className='GWASResults-flex-row'>
+          <div>Imputation Score Cutoff</div>
+          <div>{getParameterData('imputation_score_cutoff')}</div>
+        </div>
+        <hr />
+        <div className='GWASResults-flex-row'>
+          <div>Cohort</div>
+          <div>{getParameterData('source_population_cohort')}</div>
+        </div>
+        <div className='GWASResults-flex-row'>
+          <div>Phenotype</div>
+          <div>{getPhenotype()}</div>
+        </div>
+        <div className='GWASResults-flex-row'>
+          <div>Final Size</div>
+          <div>TBD</div>
+        </div>
+        <div className='GWASResults-flex-row'>
+          <div>Covariates</div>
+          <div>{displayCovariates()}</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default JobDetails;
