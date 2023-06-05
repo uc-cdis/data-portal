879407601af85e70529db987b87f598a93298206
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-06-05 14:13:23 -0500

    feat(attritionTableInputDetails): Created app specific CSS file for attrition table

--- src/Analysis/GWASResults/Views/Input/AttritionTable/AttrtitionTable.jsx
+++ src/Analysis/GWASResults/Views/Input/AttritionTable/AttrtitionTable.jsx
@@ -7,6 +7,8 @@ import { getDataForWorkflowArtifact } from '../../../Utils/gwasWorkflowApi';
 import { queryConfig } from '../../../Utils/gwasWorkflowApi';
 import SharedContext from '../../../Utils/SharedContext';
 import LoadingErrorMessage from '../../../Components/LoadingErrorMessage/LoadingErrorMessage';
+import './AttritionTable.css';
+
 const { Panel } = Collapse;
 const AttritionTable = () => {
   const { selectedRowData } = useContext(SharedContext);
@@ -20,6 +22,7 @@ const AttritionTable = () => {
   if (status === 'error') {
     return <LoadingErrorMessage message='Error getting attrition table data' />;
   }
+
   if (status === 'loading') {
     return (
       <div className='spinner-container' data-testid='spinner'>
@@ -36,11 +39,7 @@ const AttritionTable = () => {
 
   return (
     <section data-testid='attrition-table' className='attrition-table'>
-      <h2>
-        <b>Attrition table</b>
-      </h2>
-      {JSON.stringify(data)}
-      <div className='gwasv2-attrition-table'>
+      <div className='attrition-table'>
         <Collapse
           defaultActiveKey={['1']}
           onClick={(event) => event.stopPropagation()}
@@ -49,51 +48,50 @@ const AttritionTable = () => {
             <table>
               <thead>
                 <tr>
-                  <th className='gwasv2-attrition-table--leftpad gwasv2-attrition-table--w15'>
+                  <th className='attrition-table--leftpad attrition-table--w15'>
                     Type
                   </th>
-
-                  <th className='gwasv2-attrition-table--w15'>Name</th>
+                  <th className='attrition-table--w15'>Name</th>
                   <th
-                    className='gwasv2-attrition-table--rightborder
-                gwasv2-attrition-table--w5'
+                    className='attrition-table--rightborder
+                attrition-table--w5'
                   >
                     Size
                   </th>
                   <th
-                    className='gwasv2-attrition-table--w15
-                gwasv2-attrition-table--leftpad'
+                    className='attrition-table--w15
+                attrition-table--leftpad'
                   >
                     Non-Hispanic Black
                   </th>
-                  <th className='gwasv2-attrition-table--w15'>
-                    Non-Hispanic Asian
-                  </th>
-                  <th className='gwasv2-attrition-table--w15'>
-                    Non-Hispanic White
-                  </th>
-                  <th className='gwasv2-attrition-table--w15'>Hispanic</th>
+                  <th className='attrition-table--w15'>Non-Hispanic Asian</th>
+                  <th className='attrition-table--w15'>Non-Hispanic White</th>
+                  <th className='attrition-table--w15'>Hispanic</th>
                 </tr>
               </thead>
               <tbody>
                 {data[0].rows.map((row, index) => (
                   <tr key={index}>
-                    <td>{row?.type}</td>
-                    <td>{row?.name}</td>
-                    <td className='gwasv2-attrition-table--rightborder'>
-                      {row?.size}
+                    <td>{row?.type || <h3>❌</h3>}</td>
+                    <td>{row?.name || <h3>❌</h3>}</td>
+                    <td className='attrition-table--rightborder'>
+                      {row?.size || <h3>❌</h3>}
                     </td>
                     <td>
-                      {row?.concept_breakdown[0]?.persons_in_cohort_with_value}
+                      {row?.concept_breakdown[0]
+                        ?.persons_in_cohort_with_value || <h3>❌</h3>}
                     </td>
                     <td>
-                      {row?.concept_breakdown[1]?.persons_in_cohort_with_value}
+                      {row?.concept_breakdown[1]
+                        ?.persons_in_cohort_with_value || <h3>❌</h3>}
                     </td>
                     <td>
-                      {row?.concept_breakdown[2]?.persons_in_cohort_with_value}
+                      {row?.concept_breakdown[2]
+                        ?.persons_in_cohort_with_value || <h3>❌</h3>}
                     </td>
                     <td>
-                      {row?.concept_breakdown[3]?.persons_in_cohort_with_value}
+                      {row?.concept_breakdown[3]
+                        ?.persons_in_cohort_with_value || <h3>❌</h3>}
                     </td>
                   </tr>
                 ))}
