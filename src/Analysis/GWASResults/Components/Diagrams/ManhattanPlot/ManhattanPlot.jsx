b21b65c5ad22d106c8f94b176a0a2bd7f2312bf2
pieterlukasse <pieterlukasse@gmail.com>
2023-06-05 17:33:15 +0200

    fix: fixed exception handling for ManhattanPlot

--- src/Analysis/GWASResults/Components/Diagrams/ManhattanPlot/ManhattanPlot.jsx
+++ src/Analysis/GWASResults/Components/Diagrams/ManhattanPlot/ManhattanPlot.jsx
@@ -1,15 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { create_gwas_plot } from '../lib/pheweb_plots';
+import LoadingErrorMessage from '../../LoadingErrorMessage/LoadingErrorMessage';
 
 /* eslint camelcase: 0 */ // --> OFF
 
 const ManhattanPlot = ({
   variant_bins, unbinned_variants, manhattan_plot_container_id,
 }) => {
+  const [pheWebFailed, setPheWebFailed] = useState(false);
+  const [pheWebFailure, setPheWebFailure] = useState('');
+
   useEffect(() => {
-    create_gwas_plot(variant_bins, unbinned_variants, manhattan_plot_container_id);
+    try {
+      create_gwas_plot(variant_bins, unbinned_variants, manhattan_plot_container_id);
+    } catch (error) {
+      setPheWebFailed(true);
+      setPheWebFailure(error);
+    }
+
   }, [variant_bins, unbinned_variants, manhattan_plot_container_id]);
+
+  if (pheWebFailed) {
+    return (
+      <LoadingErrorMessage message={`Error while trying to render Manhattan plot: ${pheWebFailure}`} />
+    );
+  }
 
   return (
     <div id={manhattan_plot_container_id} data-testid={manhattan_plot_container_id} style={{ marginTop: '100px' }} />
