0a0a81e816b3d33b2450b067b32bab95c28c3d2e
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-06-05 13:56:42 -0500

    feat(attritionTableInputDetails): Created table that successfully prints to screen

--- src/Analysis/GWASResults/Views/Input/AttritionTable/AttrtitionTable.jsx
+++ src/Analysis/GWASResults/Views/Input/AttritionTable/AttrtitionTable.jsx
@@ -1,13 +1,13 @@
 import React, { useContext } from 'react';
 import { useQuery } from 'react-query';
-import { Spin } from 'antd';
+import { Spin, Collapse } from 'antd';
 import { gwasWorkflowPath } from '../../../../../localconf';
 import IsJsonString from '../../../Utils/IsJsonString';
 import { getDataForWorkflowArtifact } from '../../../Utils/gwasWorkflowApi';
 import { queryConfig } from '../../../Utils/gwasWorkflowApi';
 import SharedContext from '../../../Utils/SharedContext';
 import LoadingErrorMessage from '../../../Components/LoadingErrorMessage/LoadingErrorMessage';
-
+const { Panel } = Collapse;
 const AttritionTable = () => {
   const { selectedRowData } = useContext(SharedContext);
   const { name, uid } = selectedRowData;
@@ -36,8 +36,72 @@ const AttritionTable = () => {
 
   return (
     <section data-testid='attrition-table' className='attrition-table'>
-      <h2> Attrition table</h2>
+      <h2>
+        <b>Attrition table</b>
+      </h2>
       {JSON.stringify(data)}
+      <div className='gwasv2-attrition-table'>
+        <Collapse
+          defaultActiveKey={['1']}
+          onClick={(event) => event.stopPropagation()}
+        >
+          <Panel key='1' header='Attrition Table'>
+            <table>
+              <thead>
+                <tr>
+                  <th className='gwasv2-attrition-table--leftpad gwasv2-attrition-table--w15'>
+                    Type
+                  </th>
+
+                  <th className='gwasv2-attrition-table--w15'>Name</th>
+                  <th
+                    className='gwasv2-attrition-table--rightborder
+                gwasv2-attrition-table--w5'
+                  >
+                    Size
+                  </th>
+                  <th
+                    className='gwasv2-attrition-table--w15
+                gwasv2-attrition-table--leftpad'
+                  >
+                    Non-Hispanic Black
+                  </th>
+                  <th className='gwasv2-attrition-table--w15'>
+                    Non-Hispanic Asian
+                  </th>
+                  <th className='gwasv2-attrition-table--w15'>
+                    Non-Hispanic White
+                  </th>
+                  <th className='gwasv2-attrition-table--w15'>Hispanic</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data[0].rows.map((row, index) => (
+                  <tr key={index}>
+                    <td>{row?.type}</td>
+                    <td>{row?.name}</td>
+                    <td className='gwasv2-attrition-table--rightborder'>
+                      {row?.size}
+                    </td>
+                    <td>
+                      {row?.concept_breakdown[0]?.persons_in_cohort_with_value}
+                    </td>
+                    <td>
+                      {row?.concept_breakdown[1]?.persons_in_cohort_with_value}
+                    </td>
+                    <td>
+                      {row?.concept_breakdown[2]?.persons_in_cohort_with_value}
+                    </td>
+                    <td>
+                      {row?.concept_breakdown[3]?.persons_in_cohort_with_value}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </Panel>
+        </Collapse>
+      </div>
     </section>
   );
 };
