21f9d53f4a5fc2cd40db79c80067378f32e86f5b
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-06-06 14:59:24 -0500

    feat(attritionTableInputDetails): Added working storybook logic for Attrition Table

--- src/Analysis/GWASResults/Views/Input/AttritionTable/AttritionTable.test.jsx
+++ src/Analysis/GWASResults/Views/Input/AttritionTable/AttritionTable.test.jsx
@@ -25,7 +25,7 @@ describe('Attrition Table', () => {
     render(
       <SharedContext.Provider value={{ selectedRowData }}>
         <AttritionTable />
-      </SharedContext.Provider>
+      </SharedContext.Provider>,
     );
     expect(screen.getByTestId('loading-error-message')).toBeInTheDocument();
   });
@@ -39,7 +39,7 @@ describe('Attrition Table', () => {
     render(
       <SharedContext.Provider value={{ selectedRowData }}>
         <AttritionTable />
-      </SharedContext.Provider>
+      </SharedContext.Provider>,
     );
     expect(screen.getByTestId('spinner')).toBeInTheDocument();
   });
@@ -53,11 +53,10 @@ describe('Attrition Table', () => {
     render(
       <SharedContext.Provider value={{ selectedRowData }}>
         <AttritionTable />
-      </SharedContext.Provider>
+      </SharedContext.Provider>,
     );
 
-    await waitFor(() =>
-      expect(screen.getByTestId('loading-error-message')).toBeInTheDocument()
+    await waitFor(() => expect(screen.getByTestId('loading-error-message')).toBeInTheDocument(),
     );
   });
 
@@ -69,7 +68,7 @@ describe('Attrition Table', () => {
     render(
       <SharedContext.Provider value={{ selectedRowData }}>
         <AttritionTable />
-      </SharedContext.Provider>
+      </SharedContext.Provider>,
     );
 
     await waitFor(() => {
