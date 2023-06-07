2c3b4567ba15053cf647e25a48b51d72a142fce5
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-06-07 11:09:25 -0500

    feat(attritionTableInputDetails): Reverted file to sprint10 version

--- src/Analysis/GWASResults/Utils/gwasWorkflowApi.js
+++ src/Analysis/GWASResults/Utils/gwasWorkflowApi.js
@@ -2,36 +2,26 @@ import { headers } from '../../../configs';
 import { gwasWorkflowPath } from '../../../localconf';
 import { getPresignedUrl } from '../../AnalysisJob';
 
-export const fetchPresignedUrlForWorkflowArtifact = async (
+export const getWorkflowDetails = async (
   workflowName,
   workflowUid,
-  artifactName,
 ) => {
   // query argo-wrapper endpoint to get the list of artifacts produced by the workflow:
   const endPoint = `${gwasWorkflowPath}status/${workflowName}?uid=${workflowUid}`;
+  const errorMessage = 'An error has occured while fetching workflow details';
   const response = await fetch(endPoint, { headers })
     .then((res) => res.json())
-    .then((data) => data);
+    .then((data) => data)
+    .catch((error) => { throw new Error(`${errorMessage}: ${error}`); });
   if (!response) {
-    throw new Error('Error while querying workflow artifacts');
+    throw new Error(`${errorMessage}: empty response`);
   }
-  if (!response.outputs.parameters) {
-    throw new Error('Found no artifacts for workflow');
-  }
-  // filter the list to find the artifact matching the given name:
-  const results = response.outputs.parameters.filter(
-    (entry) => entry.name === artifactName,
-  );
-  if (results.length !== 1) {
-    throw new Error(
-      `Expected 1 artifact with name ${artifactName}, found: ${results.length}`,
-    );
-  }
-  // return a pre-signed "download ready" URL to the artifact:
-  return getPresignedUrl(JSON.parse(results[0].value).did, 'download');
+  return response;
 };
 
-const _getDataFromUrl = async (url) => {
+const getDataFromUrl = async (
+  url,
+) => {
   const response = await fetch(url, { headers });
   if (!response.ok) {
     const message = `An error has occured: ${response.status}`;
@@ -40,17 +30,33 @@ const _getDataFromUrl = async (url) => {
   return response.json();
 };
 
+export const fetchPresignedUrlForWorkflowArtifact = async (
+  workflowName,
+  workflowUid,
+  artifactName,
+) => {
+  const response = await getWorkflowDetails(workflowName, workflowUid);
+  if (!response.outputs.parameters) {
+    throw new Error('Found no artifacts for workflow');
+  }
+  // filter the list to find the artifact matching the given name:
+  const results = response.outputs.parameters.filter((entry) => entry.name === artifactName);
+  if (results.length !== 1) {
+    throw new Error(`Expected 1 artifact with name ${artifactName}, found: ${results.length}`);
+  }
+  // return a pre-signed "download ready" URL to the artifact:
+  return getPresignedUrl(JSON.parse(results[0].value).did, 'download');
+};
+
 export const getDataForWorkflowArtifact = async (
   workflowName,
   workflowUid,
   artifactName,
 ) => {
-  const url = await fetchPresignedUrlForWorkflowArtifact(
-    workflowName,
+  const url = await fetchPresignedUrlForWorkflowArtifact(workflowName,
     workflowUid,
-    artifactName,
-  );
-  const result = await _getDataFromUrl(url);
+    artifactName);
+  const result = await getDataFromUrl(url);
   return result;
 };
 
