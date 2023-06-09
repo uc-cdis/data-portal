8fc94cd3f8485e1d1106132445fcac5d09bcd40f
pieterlukasse <pieterlukasse@gmail.com>
2023-06-09 17:42:12 +0200

    feat: updated JEST test for ActionsDropdown

--- src/Analysis/GWASResults/Views/Home/HomeTable/ActionsDropdown/ActionsDropdown.test.jsx
+++ src/Analysis/GWASResults/Views/Home/HomeTable/ActionsDropdown/ActionsDropdown.test.jsx
@@ -2,14 +2,29 @@ import React from 'react';
 import { render, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import ActionsDropdown from './ActionsDropdown';
+import PHASES from '../../../../Utils/PhasesEnumeration';
 
 describe('ActionsDropdown', () => {
-  it('should open the dropdown menu when the button is clicked', () => {
-    const { getByRole, getByText } = render(<ActionsDropdown />);
+  it('should open the dropdown menu when the button is clicked and Retry option should be disabled for the Phase "running"', () => {
+    const record = {phase: PHASES.Running};
+    const { getByRole, getByText } = render(<ActionsDropdown record={record}/>);
     const dropdownButton = getByRole('button');
     waitFor(() => {
       fireEvent.click(dropdownButton);
     });
     expect(getByText('Download')).toBeInTheDocument();
+    expect(getByText('Retry')).toBeInTheDocument();
+    expect(getByText('Retry').parentElement.parentElement).toHaveClass('ant-dropdown-menu-item-disabled');
+  });
+  it('should open the dropdown menu when the button is clicked and Retry option should be enabled for the Phase "failed"', () => {
+    const record = {phase: PHASES.Failed};
+    const { getByRole, getByText } = render(<ActionsDropdown record={record}/>);
+    const dropdownButton = getByRole('button');
+    waitFor(() => {
+      fireEvent.click(dropdownButton);
+    });
+    expect(getByText('Download')).toBeInTheDocument();
+    expect(getByText('Retry')).toBeInTheDocument();
+    expect(getByText('Retry').parentElement.parentElement).not.toHaveClass('ant-dropdown-menu-item-disabled');
   });
 });
