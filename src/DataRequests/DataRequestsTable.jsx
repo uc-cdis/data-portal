7a8c8504b0cc3640199bd6a84f1b154e707dcc4d
Spencer Claxton <saclaxton@gmail.com>
2023-07-06 11:24:23 -0700

    Table ui improvements

--- src/DataRequests/DataRequestsTable.jsx
+++ src/DataRequests/DataRequestsTable.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Table from '../components/tables/base/Table';
 import Button from '../gen3-ui-component/components/Button';
+import Popup from '../components/Popup';
 import { useAppSelector } from '../redux/hooks';
 import { formatLocalTime } from '../utils';
 import DataDownloadButton from './DataDownloadButton';
@@ -44,6 +45,17 @@ function parseResearcherInfo(researcher) {
 function parseTableData({ projects, showApprovedOnly, userId }) {
   return projects
     ?.filter((project) => !showApprovedOnly || project.status === 'Approved')
+    .sort((a, b) => {
+      let dateA = Date.parse(a.submitted_at);
+      let dateB = Date.parse(b.submitted_at);
+      if (dateA < dateB) {
+        return 1;
+      }
+      if (dateA > dateB) {
+        return -1
+      }
+      return 0;
+    })
     .map((project) => [
       project.id,
       project.name,
@@ -80,6 +92,7 @@ function DataRequestsTable({ className = '', projects, isAdmin, onToggleAdmin, i
     [projects, showApprovedOnly, userId]
   );
   let [isAdminActive, setAdminActive] = useState(false);
+  let [isMoreActionsPopupOpen, setMoreActionsPopupOpen] = useState(false);
 
   return (
     <div className={className}>
@@ -88,26 +101,43 @@ function DataRequestsTable({ className = '', projects, isAdmin, onToggleAdmin, i
             {isAdminActive ? "All Requests" : "List of My Requests"}
           </h2>
           <div className='data-requests__table-actions'>
-            {isAdmin && 
-              <span className="data-requests__admin-toggle">
-                <label htmlFor="data-request-admin-toggle">View Admin</label>
-                <input
-                  id="data-request-admin-toggle"
-                  type='checkbox'
-                  checked={isAdminActive}
-                  onChange={() => { setAdminActive(!isAdminActive);onToggleAdmin(); }} />
-              </span>
-            }
             <Button
               label={'Create Request'}
               enabled={isAdmin}
               onClick={() => transitionTo('/requests/create')}
             />
-            <Button
-              label={showApprovedOnly ? 'Show All' : 'Show Approved Only'}
-              onClick={() => setShowApprovedOnly((s) => !s)}
-            />
+            <button
+              type='button'
+              className='data-request__table-view-options-trigger'
+              aria-label='Table view options'
+              onClick={() => { setMoreActionsPopupOpen(true) }}
+            >
+              <i className='data-request__table-view-options-trigger-icon' />
+            </button>
           </div>
+          {isMoreActionsPopupOpen &&
+            <Popup title='Requests View Options' onClose={() => { setMoreActionsPopupOpen(false) }}>
+              <div className="data-requests__more-actions-container">
+                <div className="data-requests__checkbox">
+                    <label htmlFor="data-requests-approved-only-toggle">Approved Only</label>
+                    <input
+                      id="data-requests-approved-only-toggle"
+                      type='checkbox'
+                      checked={showApprovedOnly}
+                      onChange={() => setShowApprovedOnly((s) => !s)} />
+                  </div>
+                  <div className="data-requests__checkbox">
+                    <label htmlFor="data-request-admin-toggle">View All (Admin)</label>
+                    <input
+                      disabled={!isAdmin}
+                      id="data-request-admin-toggle"
+                      type='checkbox'
+                      checked={isAdminActive}
+                      onChange={() => { setAdminActive(!isAdminActive);onToggleAdmin(); }} />
+                  </div>
+              </div>
+            </Popup>
+          }
         </div>
         {isLoading ? (
           <Spinner />
