94c4f41c9f459701fd5a6d8fd9836b28aaf8c6e3
Jarvis <113449836+jarvisraymond-uchicago@users.noreply.github.com>
2023-07-18 10:39:43 -0500

    Feat/job details table update (#1361)
    * feat(no-access): add no access message (#1301)
    
    * feat(jobDetailsTableUpdate): Wrote totalSizes calculator
    
    * feat(jobDetailsTableUpdate): Formatted code
    
    * feat(jobDetailsTableUpdate): Added notes from discussion with Pieter as comments and commented example code
    
    * feat(jobDetailsTableUpdate): refactored to use null instead of sentinel values and conditionally render table rows in JobDetails
    
    * feat(jobDetailsTableUpdate): formatted code and updated teset
    
    * feat(jobDetailsTableUpdate): Updated tests to test new functionality better
    
    * feat(jobDetailsTableUpdate): Updated tests
    
    * feat(jobDetailsTableUpdate): Reverted file to sprint version
    
    * feat(jobDetailsTableUpdate): Began working on logic to removeOutcomeFromVariablesData function
    
    * feat(jobDetailsTableUpdate): Wrote logic to remove outcome from covariates row
    
    * feat(jobDetailsTableUpdate): Renamed final size to total size
    
    * feat(jobDetailsTableUpdate): Renamed final size to total size
    
    * feat(jobDetailsTableUpdate): formatted code
    
    * feat(jobDetailsTableUpdate): Refactored to use new architecture, working prototype
    
    * feat(jobDetailsTableUpdate): Ran linter
    
    * feat(jobDetailsTableUpdate): Refactored tests so they pass
    
    * feat(jobDetailsTableUpdate): Fixed linting issues
    
    * feat(jobDetailsTableUpdate): Removed unneeded object from JobDetails test
    
    * feat(jobDetailsTableUpdate): Ran linter on test code
    
    * feat(jobDetailsTableUpdate): Ran linter and removed unneeded console log
    
    * feat(jobDetailsTableUpdate): Ran linter
    
    ---------
    
    Co-authored-by: Thanh Dang Nguyen <thanhnd@uchicago.edu>

--- src/Analysis/GWASResults/Views/Input/AttritionTable/AttrtitionTableWrapper.jsx
+++ src/Analysis/GWASResults/Views/Input/AttritionTable/AttrtitionTableWrapper.jsx
@@ -1,53 +1,26 @@
-import React, { useContext } from 'react';
-import { useQuery } from 'react-query';
-import { Spin } from 'antd';
-import {
-  getDataForWorkflowArtifact,
-  queryConfig,
-} from '../../../Utils/gwasWorkflowApi';
-import SharedContext from '../../../Utils/SharedContext';
-import LoadingErrorMessage from '../../../Components/LoadingErrorMessage/LoadingErrorMessage';
+import React from 'react';
+import PropTypes from 'prop-types';
+
 import './AttritionTable.css';
 import AttritionTable from './AttrtitionTable';
 
-const AttritionTableWrapper = () => {
-  const { selectedRowData } = useContext(SharedContext);
-  const { name, uid } = selectedRowData;
-  const { data, status } = useQuery(
-    [`getDataForWorkflowArtifact${name}`, name, uid, 'attrition_json_index'],
-    () => getDataForWorkflowArtifact(name, uid, 'attrition_json_index'),
-    queryConfig,
-  );
-
-  if (status === 'error') {
-    return (
-      <LoadingErrorMessage
-        data-testid='loading-error-message'
-        message='Error getting attrition table data'
+const AttritionTableWrapper = ({ data }) => (
+  <section
+    data-testid='attrition-table-wrapper'
+    className='attrition-table-wrapper'
+  >
+    <AttritionTable tableData={data[0]} title='Case Cohort Attrition Table' />
+    {data[1]?.table_type === 'control' && (
+      <AttritionTable
+        tableData={data[1]}
+        title='Control Cohort Attrition Table'
       />
-    );
-  }
+    )}
+  </section>
+);
 
-  if (status === 'loading') {
-    return (
-      <div className='spinner-container' data-testid='spinner'>
-        <Spin />
-      </div>
-    );
-  }
-
-  if (!data || data.length === 0 || data[0].table_type !== 'case' || data.error) {
-    return (
-      <LoadingErrorMessage message='Error Getting Attrition Table Data' />
-    );
-  }
-
-  return (
-    <section data-testid='attrition-table-wrapper' className='attrition-table-wrapper'>
-      <AttritionTable tableData={data[0]} title='Case Cohort Attrition Table' />
-      {data[1]?.table_type === 'control'
-      && <AttritionTable tableData={data[1]} title='Control Cohort Attrition Table' />}
-    </section>
-  );
+AttritionTableWrapper.propTypes = {
+  data: PropTypes.array.isRequired,
 };
+
 export default AttritionTableWrapper;
