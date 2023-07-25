829fa5de62b5ac42fa698c46d5e8d4d5a8e25b43
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-07-25 14:50:44 -0500

    fix(resultsAppHomeTableFix): Added code to prevent application crashing in case of user sorting when finishedAt is null

--- src/Analysis/GWASResults/Views/Home/HomeTable/HomeTable.jsx
+++ src/Analysis/GWASResults/Views/Home/HomeTable/HomeTable.jsx
@@ -1,7 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import {
-  Button, Table, Space, Input, DatePicker, Select,
-} from 'antd';
+import { Button, Table, Space, Input, DatePicker, Select } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 import PropTypes from 'prop-types';
 import moment from 'moment';
@@ -105,7 +103,8 @@ const HomeTable = ({ data }) => {
   };
 
   const jobStatusDropdownOptions = [];
-  Object.values(PHASES).forEach((phase) => jobStatusDropdownOptions.push({ value: phase, label: phase }),
+  Object.values(PHASES).forEach((phase) =>
+    jobStatusDropdownOptions.push({ value: phase, label: phase })
   );
 
   const columns = [
@@ -116,8 +115,8 @@ const HomeTable = ({ data }) => {
       show: homeTableState.columnManagement.showRunId,
       sorter: (a, b) => a.name.localeCompare(b.name),
       sortOrder:
-        homeTableState.sortInfo?.columnKey === 'name'
-        && homeTableState.sortInfo.order,
+        homeTableState.sortInfo?.columnKey === 'name' &&
+        homeTableState.sortInfo.order,
       children: [
         {
           title: (
@@ -139,8 +138,8 @@ const HomeTable = ({ data }) => {
       show: homeTableState.columnManagement.showWorkflowName,
       sorter: (a, b) => a.wf_name.localeCompare(b.wf_name),
       sortOrder:
-        homeTableState.sortInfo?.columnKey === 'wf_name'
-        && homeTableState.sortInfo.order,
+        homeTableState.sortInfo?.columnKey === 'wf_name' &&
+        homeTableState.sortInfo.order,
       children: [
         {
           title: (
@@ -162,8 +161,8 @@ const HomeTable = ({ data }) => {
       show: homeTableState.columnManagement.showDateSubmitted,
       sorter: (a, b) => a.submittedAt.localeCompare(b.submittedAt),
       sortOrder:
-        homeTableState.sortInfo?.columnKey === 'submittedAt'
-        && homeTableState.sortInfo.order,
+        homeTableState.sortInfo?.columnKey === 'submittedAt' &&
+        homeTableState.sortInfo.order,
       children: [
         {
           title: (
@@ -187,8 +186,8 @@ const HomeTable = ({ data }) => {
       key: 'phase',
       show: homeTableState.columnManagement.showJobStatus,
       sortOrder:
-        homeTableState.sortInfo?.columnKey === 'phase'
-        && homeTableState.sortInfo.order,
+        homeTableState.sortInfo?.columnKey === 'phase' &&
+        homeTableState.sortInfo.order,
       children: [
         {
           title: (
@@ -221,10 +220,16 @@ const HomeTable = ({ data }) => {
       title: 'Date/Time Finished',
       key: 'finishedAt',
       show: homeTableState.columnManagement.showDateFinished,
-      sorter: (a, b) => a.finishedAt.localeCompare(b.finishedAt),
+      sorter: (a, b) => {
+        if (!a.finishedAt) return -1;
+        else if (!b.finishedAt) return -1;
+        else {
+          return a?.finishedAt.localeCompare(b?.finishedAt);
+        }
+      },
       sortOrder:
-        homeTableState.sortInfo?.columnKey === 'finishedAt'
-        && homeTableState.sortInfo.order,
+        homeTableState.sortInfo?.columnKey === 'finishedAt' &&
+        homeTableState.sortInfo.order,
       dataIndex: 'finishedAt',
       children: [
         {
@@ -239,7 +244,8 @@ const HomeTable = ({ data }) => {
             />
           ),
           dataIndex: 'finishedAt',
-          render: (value) => (value ? <DateForTable utcFormattedDate={value} /> : '—/—/----'),
+          render: (value) =>
+            value ? <DateForTable utcFormattedDate={value} /> : '—/—/----',
         },
       ],
     },
@@ -299,7 +305,8 @@ const HomeTable = ({ data }) => {
     setFilteredData(filterTableData(data, homeTableState));
   }, [homeTableState, data]);
 
-  const checkForShownColumn = () => Object.values(homeTableState.columnManagement).includes(true);
+  const checkForShownColumn = () =>
+    Object.values(homeTableState.columnManagement).includes(true);
 
   return (
     <div className='home-table'>
@@ -308,9 +315,13 @@ const HomeTable = ({ data }) => {
           dataSource={isIterable(filteredData) && [...filteredData]}
           columns={columns}
           rowKey={(record) => record.uid}
-          rowClassName={(record) => record.uid === selectedRowData?.uid && 'selected-row'}
+          rowClassName={(record) =>
+            record.uid === selectedRowData?.uid && 'selected-row'
+          }
           onRow={(record) => ({
-            onClick: () => { setSelectedRowData(record); },
+            onClick: () => {
+              setSelectedRowData(record);
+            },
           })}
           onChange={handleTableChange}
           pagination={{
