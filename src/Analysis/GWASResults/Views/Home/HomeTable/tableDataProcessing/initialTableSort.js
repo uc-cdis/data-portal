cff373c5b11a012c4799bd8cd802f4ca6180b720
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-08-03 10:41:55 -0500

    fix(hiccupHometable): Began writing unit test

--- src/Analysis/GWASResults/Views/Home/HomeTable/tableDataProcessing/initialTableSort.js
+++ src/Analysis/GWASResults/Views/Home/HomeTable/tableDataProcessing/initialTableSort.js
@@ -1,7 +1,14 @@
 const initialTableSort = (data) => data.sort((a, b) => {
-  if (!a.finishedAt) return -1;
-  if (!b.finishedAt) return -1;
-  return b?.finishedAt.localeCompare(a?.finishedAt);
+  if (a.finishedAt === null && b.finishedAt === null) {
+    return 0;
+  } else if (a.finishedAt === null) {
+    return -1; // a comes first as it has finishedAt null
+  } else if (b.finishedAt === null) {
+    return 1; // b comes first as it has finishedAt null
+  } else {
+    // Sort by finishedAt in descending order
+    return new Date(b.finishedAt) - new Date(a.finishedAt);
+  }
 });
 
 export default initialTableSort;
