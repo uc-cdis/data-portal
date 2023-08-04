f9484d0529ac04398a7b73613d9def177c29bb0b
Jarvis <113449836+jarvisraymond-uchicago@users.noreply.github.com>
2023-08-04 10:09:11 -0500

    Fix/hiccup hometable (#1376)
    * fix(hiccupHometable): Began writing unit test
    
    * fix(hiccupHometable): Wrote unit test
    
    * fix(hiccupHometable): Formatted code for clarity
    
    * fix(hiccupHometable): updated test description

--- src/Analysis/GWASResults/Views/Home/HomeTable/tableDataProcessing/initialTableSort.js
+++ src/Analysis/GWASResults/Views/Home/HomeTable/tableDataProcessing/initialTableSort.js
@@ -1,7 +1,12 @@
 const initialTableSort = (data) => data.sort((a, b) => {
-  if (!a.finishedAt) return -1;
-  if (!b.finishedAt) return -1;
-  return b?.finishedAt.localeCompare(a?.finishedAt);
+  if (a.finishedAt === null && b.finishedAt === null) {
+    return 0;
+  } if (a.finishedAt === null) {
+    return -1;
+  } if (b.finishedAt === null) {
+    return 1;
+  }
+  return new Date(b.finishedAt) - new Date(a.finishedAt);
 });
 
 export default initialTableSort;
