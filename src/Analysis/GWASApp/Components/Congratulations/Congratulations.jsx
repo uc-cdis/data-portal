8e346fc907fd6e3513d4b22e3c5b09f35e306c31
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-09-12 12:46:56 -0500

    fix(nextButtonEnabled): Initial commit

--- src/Analysis/GWASApp/Components/Congratulations/Congratulations.jsx
+++ src/Analysis/GWASApp/Components/Congratulations/Congratulations.jsx
@@ -45,6 +45,10 @@ const Congratulations = ({
               type: ACTIONS.SET_CURRENT_STEP,
               payload: 3,
             });
+            dispatch({
+              type: ACTIONS.UPDATE_SELECTED_HARE,
+              payload: '',
+            });
           }}
         >
           Submit Similar (Stay Here)
