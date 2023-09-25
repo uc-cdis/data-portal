9dea63c3f7ad0cea8c00672b887ec582d409c4db
alilahrime <alahrime@uchicago.edu>
2023-09-25 14:06:37 -0400

    refactor(PXP-11087): add mantine react table

--- src/DataDictionary/table/MDataDictionaryTable/MDataDictionaryTable.tsx
+++ src/DataDictionary/table/MDataDictionaryTable/MDataDictionaryTable.tsx
@@ -1,4 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
+import {
+    MantineReactTable,
+    useMantineReactTable
+} from 'mantine-react-table';
+import { capitalize } from "lodash";
 
 export interface MDataDictionaryTableProps {
     dictionary: any
@@ -41,7 +46,8 @@ export const MDataDictionaryTable: React.FC<MDataDictionaryTableProps> = ({
     dictionary
 }: MDataDictionaryTableProps) => {
     const [categories, setCategories] = useState({});
-    const [expandedCategory, setExpandedCategory] = useState("");
+    const [selectedId, setSelectedId] = useState("");
+    const [selectedData, setSelectedData] = useState([]);
     // filters out ['_definitions', 'undefined', '_terms', 'data_release', 'metaschema', 'root']
     const categoryFilter = (id) => id.charAt(0) !== '_' && id === dictionary[id].id && dictionary[id].category && dictionary[id].id && dictionary[id].category.toLowerCase() !== "internal";
 
@@ -57,14 +63,50 @@ export const MDataDictionaryTable: React.FC<MDataDictionaryTableProps> = ({
         setCategories(reduced);
     }, [dictionary]);
 
-    return (<>{Object.keys(categories).map((c) => {
-        return <div>
-            <h1>{c}</h1>
-            <div>{categories[c].map(({ title, description, id }) => {
-                // todo columns for tables: "property", "type", "required", "description", "term"
-                return <div onClick={() => setExpandedCategory((i) => i === id ? "" : id)}><div>{title} {description}</div>{expandedCategory === id ? <div>table</div> : undefined}</div>
+    const columns = useMemo(
+        () => ["property", "type", "required", "description", "term"].map((key) => {
+            return {
+                accessorKey: key,
+                header: key.toLocaleUpperCase(),
+                Cell: ({ cell }) => (
+                    <span>{cell.getValue()}</span>
+                ),
+            }
+        }),
+        [],
+    );
+
+    const tableData = useMemo(() => {
+        let keys = dictionary[selectedId]?.properties ? Object.keys(dictionary[selectedId].properties) : [];
+        return keys.length ? keys.map((k) => {
+            const { properties, required } = dictionary[selectedId];
+            const row = properties[k];
+            return {
+                property: k.split("_").map((name) => capitalize(name)).join(" "),
+                type: Object.keys(row).includes("anyOf") ? row.anyOf.map(({ type }) => type).join(" ") : row.type,
+                required: required.includes(k) ? "Required" : "No",
+                description: row?.description ?? row?.term?.description ?? "No Description",
+                term: ""
+            }
+        }) : [];
+    }, [selectedId]);
+
+    const table = useMantineReactTable({ columns, data: tableData });
+
+    return (<>{Object.keys(categories).map((c, key) => {
+        return <div key={key}>
+            <h1>{c.split("_").map((name) => capitalize(name)).join(" ")}</h1>
+            <div>{categories[c].map(({ title, description, id }, key) => {
+                return <>
+                    <div key={key} onClick={() => {
+                        setSelectedId((i) => i === id ? "" : id);
+                    }}>
+                        {title} {description}
+                    </div>
+                    {selectedId === id ? (<div key={selectedId}>
+                        <MantineReactTable table={table} />
+                    </div>) : undefined}</>
             })}</div>
         </div>
     })}</>)
-
 }
