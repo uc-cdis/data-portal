a0744e2cedb4e0d426983adbede6233a0262e6fd
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-09-26 13:41:35 -0500

    chore(homeTableFilterTest): Finished writing remaining tests for filtering

--- src/Analysis/GWASResults/Views/Home/HomeTable/tableDataProcessing/filterTableData.test.js
+++ src/Analysis/GWASResults/Views/Home/HomeTable/tableDataProcessing/filterTableData.test.js
@@ -1,67 +1,64 @@
 import filterTableData from './filterTableData';
+import InitialHomeTableState from '../../HomeTableState/InitialHomeTableState';
+import PHASES from '../../../../Utils/PhasesEnumeration';
 
 describe('filterTableData', () => {
   const testData = [
     {
       name: 'name-test-1',
-      wf_name: 'Workflow 1',
+      wf_name: 'workflow-name-test-1',
       submittedAt: '2023-01-15T12:00:00Z',
       finishedAt: '2023-01-20T12:00:00Z',
-      phase: 'completed',
+      phase: PHASES.Succeeded,
     },
     {
       name: 'name-test-2',
-      wf_name: 'Workflow 2',
+      wf_name: 'workflow-name-test-2',
       submittedAt: '2023-02-15T12:00:00Z',
       finishedAt: '2023-02-20T12:00:00Z',
-      phase: 'in_progress',
+      phase: PHASES.Failed,
     },
   ];
-  const homeTableStateDefault = {
-    nameSearchTerm: '',
-    wfNameSearchTerm: '',
-    submittedAtSelections: [],
-    finishedAtSelections: [],
-    jobStatusSelections: [],
-  };
+
 
   it('should filter data by name search term', () => {
-    // const homeTableState = homeTableStateDefault;
-    // homeTableState.nameSearchTerm = "name-test";
-    const filteredData = filterTableData(testData, { ...homeTableStateDefault, ...{ nameSearchTerm: 'name-test-1' } });
+    const filteredData = filterTableData(testData,
+      { ...InitialHomeTableState, ...{ nameSearchTerm: 'name-test-1'}}
+    );
     expect(filteredData).toHaveLength(1);
     expect(filteredData[0].name).toBe('name-test-1');
   });
-  /*
 
   it('should filter data by workflow name search term', () => {
-    const homeTableState = homeTableStateDefault;
-    homeTableState.wfNameSearchTerm = "workflow-name-test";
-    const filteredData = filterTableData(testData, homeTableState);
+    const filteredData = filterTableData(testData,
+      { ...InitialHomeTableState, ...{wfNameSearchTerm: 'workflow-name-test-2'}}
+    );
     expect(filteredData).toHaveLength(1);
-    expect(filteredData[0].wf_name).toBe('Workflow 2');
+    expect(filteredData[0].wf_name).toBe('workflow-name-test-2');
   });
 
   it('should filter data by submitted date range', () => {
-    const homeTableState = homeTableStateDefault;
-    homeTableState.submittedAtSelections = [new Date('2023-01-01'), new Date('2023-02-01')];
-    const filteredData = filterTableData(testData, homeTableState);
+    const filteredData = filterTableData(testData,
+      { ...InitialHomeTableState, ...{submittedAtSelections: [new Date('2023-01-01'), new Date('2023-03-01')]}}
+    );
     expect(filteredData).toHaveLength(2);
+    expect(filteredData[0].submittedAt).toBe(testData[0].submittedAt);
+    expect(filteredData[1].submittedAt).toBe(testData[1].submittedAt);
   });
 
   it('should filter data by job statuses', () => {
-    const homeTableState = homeTableStateDefault;
-    homeTableState.jobStatusSelections = ['in_progress'];
-    const filteredData = filterTableData(testData, homeTableState);
+    const filteredData = filterTableData(testData,
+      { ...InitialHomeTableState, ...{jobStatusSelections: [PHASES.Succeeded]}}
+    );
     expect(filteredData).toHaveLength(1);
-    expect(filteredData[0].phase).toBe('in_progress');
+    expect(filteredData[0].phase).toBe(PHASES.Succeeded);
   });
 
   it('should filter data by finished date range', () => {
-    const homeTableState = homeTableStateDefault;
-    homeTableState.finishedAtSelections= [new Date('2023-01-15'), new Date('2023-01-22')];
-    const filteredData = filterTableData(testData, homeTableState);
+    const filteredData = filterTableData(testData,
+      { ...InitialHomeTableState, ...{finishedAtSelections: [new Date('2023-01-15'), new Date('2023-01-22')]}}
+    );
     expect(filteredData).toHaveLength(1);
-    expect(filteredData[0].name).toBe('John');
-  }); */
+    expect(filteredData[0].finishedAt).toBe('2023-01-20T12:00:00Z');
+  });
 });
