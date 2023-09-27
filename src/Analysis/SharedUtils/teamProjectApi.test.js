fc346efe16f53f90a8078b9a3016ce4cedc36fd0
pieterlukasse <pieterlukasse@gmail.com>
2023-09-27 21:56:09 +0200

    feat: unit tests for temProjectApi.js

---
+++ src/Analysis/SharedUtils/teamProjectApi.test.js
@@ -0,0 +1,53 @@
+import fetchArboristTeamProjectRoles from './teamProjectApi';
+import { authzMappingPath } from '../../configs';
+
+// Mocking the fetch function
+global.fetch = jest.fn();
+
+describe('fetchArboristTeamProjectRoles', () => {
+  beforeEach(() => {
+    // Clear the mock function calls and reset any mock implementation
+    jest.clearAllMocks();
+  });
+
+  it('fetches and returns team project roles successfully', async () => {
+    // Define the expected response data
+    const mockResponse = {
+      '/gwas_projects/project1': { abc: 'def' },
+      '/gwas_projects/project2': { abc: 'def' },
+      '/ohter/project3': { abc: 'def' },
+    };
+
+    // Mock the fetch function to return a successful response with the expected data
+    global.fetch.mockResolvedValueOnce({
+      ok: true,
+      json: () => mockResponse,
+    });
+
+    // Call the function and assert the result
+    const result = await fetchArboristTeamProjectRoles();
+
+    expect(result).toEqual({
+      teams: [
+        { teamName: '/gwas_projects/project1' },
+        { teamName: '/gwas_projects/project2' },
+      ],
+    });
+
+    // Ensure that fetch was called with the correct URL
+    expect(global.fetch).toHaveBeenCalledWith(authzMappingPath);
+  });
+
+  it('throws an error when fetch fails', async () => {
+    // Mock the fetch function to return an error response
+    global.fetch.mockRejectedValueOnce(new Error('Network error'));
+
+    // Call the function and expect it to throw an error
+    await expect(fetchArboristTeamProjectRoles()).rejects.toThrow(
+      'Network error'
+    );
+
+    // Ensure that fetch was called with the correct URL
+    expect(global.fetch).toHaveBeenCalledWith(authzMappingPath);
+  });
+});
