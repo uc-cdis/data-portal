091002ed3c95c6c35598eb661d2cc8be48461e30
alilahrime <alahrime@uchicago.edu>
2023-09-27 12:23:23 -0400

    refactor(PXP-11087): styling, stop event bubbling

--- src/DataDictionary/table/MDataDictionaryTable/MDataDictionaryTable.tsx
+++ src/DataDictionary/table/MDataDictionaryTable/MDataDictionaryTable.tsx
@@ -46,7 +46,6 @@ export const MDataDictionaryTable: React.FC<MDataDictionaryTableProps> = ({
 }: MDataDictionaryTableProps) => {
     const [categories, setCategories] = useState({});
     const [selectedId, setSelectedId] = useState('');
-
     // filters out ['_definitions', 'undefined', '_terms', 'data_release', 'metaschema', 'root']
     useEffect(() => {
         const categoryFilter = (id) => id.charAt(0) !== '_' && id === dictionary[id].id && dictionary[id].category && dictionary[id].id && dictionary[id].category.toLowerCase() !== 'internal';
@@ -66,7 +65,11 @@ export const MDataDictionaryTable: React.FC<MDataDictionaryTableProps> = ({
             accessorKey: key,
             header: key.toLocaleUpperCase(),
             Cell: ({ cell }) => (
-                <span>{cell.getValue()}</span>
+                <>
+                    {key === 'type' ? <>{<ul>{(cell.getValue()?.split(" ") || []).map((cell) => {
+                        return <li>{cell}</li>
+                    })}</ul>}</> : <span>{cell.getValue()}</span>}
+                </>
             ),
         })),
         [],
@@ -79,7 +82,7 @@ export const MDataDictionaryTable: React.FC<MDataDictionaryTableProps> = ({
             const row = properties[k];
             return {
                 property: k.split('_').map((name) => capitalize(name)).join(' '),
-                type: Object.keys(row).includes('anyOf') ? row.anyOf.map(({ type }) => type).join(' ') : row.type,
+                type: Object.keys(row).includes('anyOf') ? row.anyOf.map(({ type }) => type).join(" ") : row.type,
                 required: required.includes(k) ? 'Required' : 'No',
                 description: row?.description ?? row?.term?.description ?? 'No Description',
                 term: '',
@@ -91,27 +94,38 @@ export const MDataDictionaryTable: React.FC<MDataDictionaryTableProps> = ({
         setSelectedId((i) => (i === id ? '' : id));
     };
 
-    const table = useMantineReactTable({ columns, data: tableData });
+    const table = useMantineReactTable({
+        columns,
+        data: tableData,
+        enablePagination: false,
+        enableBottomToolbar: false,
+        enableTopToolbar: false
+    });
 
     return (
         <React.Fragment>{Object.keys(categories).map((c) => (
             <div>
-                <h1>{c.split('_').map((name) => capitalize(name)).join(' ')}</h1>
-                <div>{categories[c].map(({ title, description, id }, key) => (
-                    <React.Fragment>
+                <h2 style={{ color: "white", backgroundColor: "gray", border: "1px solid black", padding: "0 10px" }}>{c.split('_').map((name) => capitalize(name)).join(' ')}</h2>
+                <div style={{ border: "1px solid black" }}>{categories[c].map(({ title, description, id }, key) => (
+                    <div onClick={() => handleSelect(id)} style={{ display: "flex", flexDirection: "column", padding: 2 }}>
                         <div
                             key={key}
-                            onClick={() => handleSelect(id)}
-                            onKeyDown={() => handleSelect(id)}
-                        >
-                            {title} {description}
-                        </div>
-                        {selectedId === id ? (
-                            <div key={selectedId}>
-                                <MantineReactTable table={table} />
+                            style={{ display: "flex", justifyContent: "space-between", borderBottom: key < categories[c].length - 1 ? "1px solid black" : 0 }}>
+                            <div style={{ flexGrow: 0, flexShrink: 0, width: 260, padding: "0 10px" }}>{title}</div>
+                            <div style={{ flexGrow: 1, padding: "0px 10px" }}>{description}</div>
+                            <div style={{ display: "flex", fontSize: 12, flexGrow: 0, flexShrink: 0, justifyContent: "space-between" }}>
+                                <button onClick={(e) => e.stopPropagation()} style={{ height: 30, width: 60, margin: "0 5px" }}>TSV</button>
+                                <button onClick={(e) => e.stopPropagation()} style={{ height: 30, width: 60, margin: "0 5px" }}>JSON</button>
                             </div>
-                        ) : undefined}
-                    </React.Fragment>
+                        </div>
+                        <div>
+                            {selectedId === id ? (
+                                <div onClick={(e) => e.stopPropagation()} key={selectedId}>
+                                    <MantineReactTable table={table} />
+                                </div>
+                            ) : undefined}
+                        </div>
+                    </div>
                 ))}
                 </div>
             </div>
