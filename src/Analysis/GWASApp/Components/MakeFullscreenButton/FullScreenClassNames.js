99ce1d018e0e86b9daa0612c2a600ab77fba9d37
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-10-09 14:54:41 -0500

    feat(gwasAppFullScreen): Deleted working files to address case issue between FullScreen and fullscreen

--- src/Analysis/GWASApp/Components/MakeFullscreenButton/FullScreenClassNames.js
+++
@@ -1,9 +0,0 @@
-const FullScreenClassNames = [
-  '.top-bar',
-  '.nav-bar',
-  '.footer-container',
-  '.analysis-app__title',
-  '.analysis-app__description',
-];
-
-export default FullScreenClassNames;
