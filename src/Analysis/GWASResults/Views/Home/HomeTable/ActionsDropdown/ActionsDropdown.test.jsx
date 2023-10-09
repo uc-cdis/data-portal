cb100850dad5e9584c688ac6e1ba523f77fad643
Jarvis <113449836+jarvisraymond-uchicago@users.noreply.github.com>
2023-10-09 11:49:04 -0500

    Feat/results button disable (#1428)
    * feat(resultsButtonDisable): Initial commit
    
    * feat(resultsButtonDisable): Removed unneeded console log statement
    
    * feat(resultsButtonDisable): Ran linter
    
    * feat(resultsButtonDisable): Updated actions dropdown and test
    
    * feat(resultsButtonDisable): Ran linter
    
    * feat(resultsButtonDisable): Updated unit test

--- src/Analysis/GWASResults/Views/Home/HomeTable/ActionsDropdown/ActionsDropdown.test.jsx
+++ src/Analysis/GWASResults/Views/Home/HomeTable/ActionsDropdown/ActionsDropdown.test.jsx
@@ -7,24 +7,63 @@ import PHASES from '../../../../Utils/PhasesEnumeration';
 describe('ActionsDropdown', () => {
   it('should open the dropdown menu when the button is clicked and Retry option should be disabled for the Phase "running"', () => {
     const record = { phase: PHASES.Running };
-    const { getByRole, getByText } = render(<ActionsDropdown record={record} />);
+    const { getByRole, getByText } = render(
+      <ActionsDropdown record={record} />,
+    );
     const dropdownButton = getByRole('button');
     waitFor(() => {
       fireEvent.click(dropdownButton);
     });
     expect(getByText('Download')).toBeInTheDocument();
     expect(getByText('Retry')).toBeInTheDocument();
-    expect(getByText('Retry').parentElement.parentElement).toHaveClass('ant-dropdown-menu-item-disabled');
+    expect(getByText('Retry').parentElement.parentElement).toHaveClass(
+      'ant-dropdown-menu-item-disabled',
+    );
   });
   it('should open the dropdown menu when the button is clicked and Retry option should be enabled for the Phase "failed"', () => {
     const record = { phase: PHASES.Failed };
-    const { getByRole, getByText } = render(<ActionsDropdown record={record} />);
+    const { getByRole, getByText } = render(
+      <ActionsDropdown record={record} />,
+    );
     const dropdownButton = getByRole('button');
     waitFor(() => {
       fireEvent.click(dropdownButton);
     });
     expect(getByText('Download')).toBeInTheDocument();
     expect(getByText('Retry')).toBeInTheDocument();
-    expect(getByText('Retry').parentElement.parentElement).not.toHaveClass('ant-dropdown-menu-item-disabled');
+    expect(getByText('Retry').parentElement.parentElement).not.toHaveClass(
+      'ant-dropdown-menu-item-disabled',
+    );
+  });
+
+  it('should disabled Download option for the Phase "failed"', () => {
+    const record = { phase: PHASES.Failed };
+    const { getByRole, getByText } = render(
+      <ActionsDropdown record={record} />,
+    );
+    const dropdownButton = getByRole('button');
+    waitFor(() => {
+      fireEvent.click(dropdownButton);
+    });
+    expect(getByText('Download')).toBeInTheDocument();
+    expect(getByText('Retry')).toBeInTheDocument();
+    expect(getByText('Download').parentElement.parentElement).toHaveClass(
+      'ant-dropdown-menu-item-disabled',
+    );
+  });
+  it('should enabled Download option for the Phase "Succeeded"', () => {
+    const record = { phase: PHASES.Succeeded };
+    const { getByRole, getByText } = render(
+      <ActionsDropdown record={record} />,
+    );
+    const dropdownButton = getByRole('button');
+    waitFor(() => {
+      fireEvent.click(dropdownButton);
+    });
+    expect(getByText('Download')).toBeInTheDocument();
+    expect(getByText('Retry')).toBeInTheDocument();
+    expect(getByText('Download').parentElement.parentElement).not.toHaveClass(
+      'ant-dropdown-menu-item-disabled',
+    );
   });
 });
