cda807cacbd0ba2438d8107d9aaf3598fef13baa
Jarvis <113449836+jarvisraymond-uchicago@users.noreply.github.com>
2023-10-10 12:20:28 -0500

    Feat/gwas app full screen (#1430)
    * feat(gwasAppFullScreen): Initial commit
    
    * feat(gwasAppFullScreen): Ran linter
    
    * feat(gwasAppFullScreen): Move function outside of scope of other function
    
    * feat(gwasAppFullScreen): added comment
    
    * feat(gwasAppFullScreen): Added jest test and storybook
    
    * feat(gwasAppFullScreen): Added linter
    
    * feat(gwasAppFullScreen): Renamed files with consistent casing
    
    * feat(gwasAppFullScreen): Deleted working files to address case issue between FullScreen and fullscreen
    
    * feat(gwasAppFullScreen): added  working files to address case issue between FullScreen and fullscreen
    
    * feat(gwasAppFullScreen): updated import order for consistency in GWASContainer.jsx

--- src/Analysis/GWASApp/GWASContainer.jsx
+++ src/Analysis/GWASApp/GWASContainer.jsx
@@ -12,6 +12,7 @@ import ConfigureGWAS from './Steps/ConfigureGWAS/ConfigureGWAS';
 import SelectOutcome from './Steps/SelectOutcome/SelectOutcome';
 import SelectCovariates from './Steps/SelectCovariates/SelectCovariates';
 import DismissibleMessagesList from './Components/DismissibleMessagesList/DismissibleMessagesList';
+import MakeFullscreenButton from './Components/MakeFullscreenButton/MakeFullscreenButton';
 import './GWASApp.css';
 
 const GWASContainer = () => {
@@ -152,6 +153,7 @@ const GWASContainer = () => {
               </Button>
             )}
           </div>
+          <MakeFullscreenButton />
         </Space>
       </div>
     </SourceContextProvider>
