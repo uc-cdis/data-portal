0a0e743b16782a4df90ac6b38a3215ea5ffbe12f
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-10-13 14:33:36 -0500

    feat(dataDownloadListActions): Wrote styles to show buttons per design

--- src/Discovery/DiscoveryDetails/DataDownloadList/ActionBar/ActionBar.jsx
+++ src/Discovery/DiscoveryDetails/DataDownloadList/ActionBar/ActionBar.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
-import { Col, Row, List, Button } from 'antd';
+import { Col, Row, Button } from 'antd';
+import './ActionBar.css';
 
 const ActionBar = () => {
-    return (<>
-
-    <Row className="row">
-      <Col flex="1 0 auto" className="column Red">Red</Col>
-      <Col flex="1 0 auto" className="column Green">Green</Col>
-      <Col flex="1 0 auto" className="column Blue">Blue</Col>
-      <Col flex="1 0 auto" className="column Blue">Yellow</Col>
-    </Row>
-    </>)
-}
+  return (
+    <div className='discovery-modal_action-bar'>
+      <Row className='row'>
+        <Col flex='1 0 auto'>
+          <Button className='discovery-action-bar-button'>
+            Download <br />
+            Variable-Level Metadata
+          </Button>
+        </Col>
+        <Col flex='1 0 auto'>
+          <Button className='discovery-action-bar-button'>
+            Download <br />
+            Study-Level Metadata
+          </Button>
+        </Col>
+        <Col flex='1 0 auto'>
+          <Button className='discovery-action-bar-button'>
+            Download File Manifest
+          </Button>
+        </Col>
+        <Col flex='1 0 auto'>
+          <Button className='discovery-action-bar-button'>
+            Download All Files
+          </Button>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 export default ActionBar;
