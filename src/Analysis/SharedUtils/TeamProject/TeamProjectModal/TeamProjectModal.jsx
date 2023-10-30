3b5f9e5f8a491e9441a2bec9356ce6d16f46312c
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-10-30 08:56:00 -0500

    feat(teamProjectsHeader): Removed stray react fragment

--- src/Analysis/SharedUtils/TeamProject/TeamProjectModal/TeamProjectModal.jsx
+++ src/Analysis/SharedUtils/TeamProject/TeamProjectModal/TeamProjectModal.jsx
@@ -61,44 +61,42 @@ const TeamProjectModal = ({ isModalOpen, setIsModalOpen, setBannerText }) => {
   }
   if (data) {
     modalContent = (
-      <React.Fragment>
-        <Modal
-          className='team-project-modal'
-          title='Team Projects'
-          open={isModalOpen}
-          onCancel={() => setIsModalOpen(false)}
-          closable={localStorage.getItem('teamProject')}
-          maskClosable={localStorage.getItem('teamProject')}
-          keyboard={localStorage.getItem('teamProject')}
-          footer={[
-            <Button
-              key='submit'
-              type='primary'
-              disabled={!selectedTeamProject}
-              onClick={() => closeAndUpdateTeamProject()}
-            >
-              Submit
-            </Button>,
-          ]}
-        >
-          <div className='team-project-modal_modal-description'>
-            Please select your team.
-          </div>
-          <Select
-            id='input-selectTeamProjectDropDown'
-            labelInValue
-            defaultValue={selectedTeamProject}
-            onChange={(e) => setSelectedTeamProject(e.value)}
-            placeholder='-select one of the team projects below-'
-            fieldNames={{ label: 'teamName', value: 'teamName' }}
-            options={data.teams}
-            dropdownStyle={{ width: '100%' }}
-          />
-        </Modal>
-      </React.Fragment>
+      <Modal
+        className='team-project-modal'
+        title='Team Projects'
+        open={isModalOpen}
+        onCancel={() => setIsModalOpen(false)}
+        closable={localStorage.getItem('teamProject')}
+        maskClosable={localStorage.getItem('teamProject')}
+        keyboard={localStorage.getItem('teamProject')}
+        footer={[
+          <Button
+            key='submit'
+            type='primary'
+            disabled={!selectedTeamProject}
+            onClick={() => closeAndUpdateTeamProject()}
+          >
+            Submit
+          </Button>,
+        ]}
+      >
+        <div className='team-project-modal_modal-description'>
+          Please select your team.
+        </div>
+        <Select
+          id='input-selectTeamProjectDropDown'
+          labelInValue
+          defaultValue={selectedTeamProject}
+          onChange={(e) => setSelectedTeamProject(e.value)}
+          placeholder='-select one of the team projects below-'
+          fieldNames={{ label: 'teamName', value: 'teamName' }}
+          options={data.teams}
+          dropdownStyle={{ width: '100%' }}
+        />
+      </Modal>
     );
   }
-  return <React.Fragment>{modalContent}</React.Fragment>;
+  return <React.Fragment>{ modalContent }</React.Fragment>;
 };
 
 TeamProjectModal.propTypes = {
