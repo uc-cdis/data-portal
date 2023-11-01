9f619a1d56aa1a0e031845c61f5b93bbcb24a4cf
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-11-01 11:00:33 -0500

    feat(addTeamProjectHeaderToAnalysisApps): Removed unneeded waitFor import

--- src/Analysis/SharedUtils/TeamProject/TeamProjectHeader/TeamProjectHeader.test.jsx
+++ src/Analysis/SharedUtils/TeamProject/TeamProjectHeader/TeamProjectHeader.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
