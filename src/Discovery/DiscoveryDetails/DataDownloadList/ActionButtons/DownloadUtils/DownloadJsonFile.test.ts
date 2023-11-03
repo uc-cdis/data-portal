84c5da710ebf9c8ad4e63c4b0f538d305a61b35a
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-11-03 14:38:58 -0500

    feat(dataDownloadListActions): Wrote test for DownloadJsonFile

---
+++ src/Discovery/DiscoveryDetails/DataDownloadList/ActionButtons/DownloadUtils/DownloadJsonFile.test.ts
@@ -0,0 +1,20 @@
+import FileSaver from 'file-saver';
+import DownloadJsonFile from './DownloadJsonFile';
+
+jest.mock('file-saver', () => ({
+  saveAs: jest.fn(),
+}));
+
+describe('DownloadJsonFile', () => {
+  test('downloads JSON file with correct title', () => {
+    // Set up mock data
+    const fileName = 'testFile';
+    const testData = { key: 'value' };
+
+    // Call the DownloadJsonFile function
+    DownloadJsonFile(fileName, testData);
+
+    // Assert that saveAs is called with the correct arguments
+    expect(FileSaver.saveAs).toHaveBeenCalledWith(expect.any(Blob), 'testFile.json');
+  });
+});
