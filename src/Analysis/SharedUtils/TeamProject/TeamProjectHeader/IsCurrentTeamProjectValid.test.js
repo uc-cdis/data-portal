20ebc8ebed855af099d9f685ba861dacc192c455
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-11-21 13:59:42 -0600

    feat(currentTeamProjectValidation): Formatted code for clarity

--- src/Analysis/SharedUtils/TeamProject/TeamProjectHeader/IsCurrentTeamProjectValid.test.js
+++ src/Analysis/SharedUtils/TeamProject/TeamProjectHeader/IsCurrentTeamProjectValid.test.js
@@ -24,33 +24,22 @@ const sampleData = {
 };
 
 describe('IsCurrentTeamProjectValid', () => {
-  it('should return false if data doesn\'t contain teams', () => {
-    // Call the function with sample data
+  it(`should return false if data doesn't contain teams`, () => {
     const result = IsCurrentTeamProjectValid({ notTeams: [] });
-    // Expect the result to be false
     expect(result).toBe(false);
   });
 
   it('should return false if current team project is not valid', () => {
-    // Mock localStorage.getItem to return an invalid team project
     localStorageMock.getItem.mockReturnValue('InvalidTeamName');
-
-    // Call the function with sample data
     const result = IsCurrentTeamProjectValid(sampleData);
-
-    // Expect the result to be false
     expect(result).toBe(false);
-
-    // Expect localStorage.getItem to have been called with the correct argument
     expect(localStorageMock.getItem).toHaveBeenCalledWith('teamProject');
   });
+
   it('should return true if current team project is valid', () => {
     localStorageMock.getItem.mockReturnValue('ValidTeamName');
-    // Call the function with sample data
     const result = IsCurrentTeamProjectValid(sampleData);
-    // Expect the result to be true
     expect(result).toBe(true);
-    // Expect localStorage.getItem to have been called with the correct argument
     expect(localStorageMock.getItem).toHaveBeenCalledWith('teamProject');
   });
 });
