f4603d21f5474352d7b408866ddb79a46a6ee401
Jarvis Raymond <jarvisraymond@uchicago.edu>
2023-12-01 16:27:26 -0600

    feat(dataDownloadListActions): fixed syntax (extra comma)

--- src/Discovery/DiscoveryDetails/Utils/CheckHealLoginNeeded.ts
+++ src/Discovery/DiscoveryDetails/Utils/CheckHealLoginNeeded.ts
@@ -4,44 +4,41 @@ import { bundle } from '../../../localconf';
 // props.discovery.selectedResources
 //  props.user.fence_idp
 
-const CheckHealLoginNeeded = (
-  selectedResources,
-  fence_idp,
-): {} => {
+const CheckHealLoginNeeded = (selectedResources, fence_idp): boolean => {
   /* const healRequiredIdentityProviderLogic = useCallback(() => { */
   if (bundle === 'heal') {
     // HP-1233 Generalize IdP-based access control2
     // Find which resources Required IDP
     console.log(
       'selectedResources in HealRequiredIdentityProviderInfo',
-      selectedResources,
+      selectedResources
     );
     const requiredIdentityProvider: string[] = [];
-    selectedResources.forEach((resource) => resource?.tags.forEach((tag: { name: string; category: string }) => {
-      if (tag?.category === 'RequiredIDP' && tag?.name) {
-        // If any resources RequiredIDP check if logged in
-        switch (tag.name) {
-        case 'InCommon':
-          if (fence_idp === 'shibboleth') {
-            return; // do not add tag to list
+    selectedResources.forEach((resource) =>
+      resource?.tags.forEach((tag: { name: string; category: string }) => {
+        if (tag?.category === 'RequiredIDP' && tag?.name) {
+          // If any resources RequiredIDP check if logged in
+          switch (tag.name) {
+            case 'InCommon':
+              if (fence_idp === 'shibboleth') {
+                return; // do not add tag to list
+              }
+              break;
+            default:
+              // eslint-disable-next-line no-console
+              console.log(
+                `Required Identity Provider does not expect: ${tag?.name}`
+              );
+              return; // do not add tag to list
           }
-          break;
-        default:
-          // eslint-disable-next-line no-console
-          console.log(
-            `Required Identity Provider does not expect: ${tag?.name}`,
-          );
-          return; // do not add tag to list
+          requiredIdentityProvider.push(tag.name);
         }
-        requiredIdentityProvider.push(tag.name);
-      }
-    }),
+      })
     );
     // return requiredIdentityProvider;
-    return  Boolean(requiredIdentityProvider.length),
-
+    return Boolean(requiredIdentityProvider.length);
   }
-  return  false;
+  return false;
   // }, [selectedResources, fence_idp]);
   // return healRequiredIdentityProviderLogic;
 };
