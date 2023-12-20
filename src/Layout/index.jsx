b85de061e6791100256969e96780ad8fda09eb72
tianyunz <tianyunz@uchicago.edu>
2023-12-20 11:35:13 -0500

    PEDS-987 - initialize GA4 only once and set user id once only after user is fetched

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -3,24 +3,19 @@ import { Helmet } from 'react-helmet';
 import { useLocation } from 'react-router-dom';
 import { components } from '../params';
 import isEnabled from '../helpers/featureFlags';
-import useGoogleAnalytics from '../hooks/useGoogleAnalytics';
 import dictIcons from '../img/icons/index';
 import ReduxFooter from './ReduxFooter';
 import ScreenSizeWarning from '../components/ScreenSizeWarning';
 import ReduxTopBar from './ReduxTopBar';
 import ReduxNavBar from './ReduxNavBar';
 import './Layout.css';
-import { useAppSelector } from '../redux/hooks';
 
 /**
  * @param {Object} props
  * @param {React.ReactNode} props.children
  */
 function Layout({ children }) {
-  const userId = useAppSelector((state) => state.user.sub);
   const location = useLocation();
-  // @ts-ignore
-  useGoogleAnalytics(userId);
 
   const isDashboardPage =
     location.pathname.toLowerCase().startsWith('/dd') ||
