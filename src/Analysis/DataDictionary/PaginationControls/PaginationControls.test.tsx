8757d9615b66e0e074d5080e8aa2a76f5b1e11c8
Jarvis Raymond <jarvisraymond@uchicago.edu>
2024-01-17 09:58:21 -0600

    feat(vadcDataDictionaryTable): linted test file

--- src/Analysis/DataDictionary/PaginationControls/PaginationControls.test.tsx
+++ src/Analysis/DataDictionary/PaginationControls/PaginationControls.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import '@mantine/core/styles.css'; // ensure styles are loaded for testing
 import PaginationControls from './PaginationControls';
 
 describe('PaginationControls', () => {
@@ -15,35 +14,24 @@ describe('PaginationControls', () => {
 
   it('should render PaginationControls with correct initial state', () => {
     const { getByTestId, getByLabelText } = render(
-      <PaginationControls {...defaultProps} />
+      <PaginationControls {...defaultProps} />,
     );
-
     expect(getByTestId('pagination-controls')).toBeInTheDocument();
     expect(getByLabelText('Show')).toBeInTheDocument();
     expect(getByLabelText('entries')).toBeInTheDocument();
   });
 
   it('should call setEntriesShown when select value changes', () => {
-    const { getByTestId, getByLabelText } = render(
-      <PaginationControls {...defaultProps} />
-    );
-
+    const { getByTestId } = render(<PaginationControls {...defaultProps} />);
     const entriesSelect = getByTestId('entries-select');
-
     fireEvent.change(entriesSelect, { target: { value: '20' } });
-
     expect(defaultProps.setEntriesShown).toHaveBeenCalledWith(20);
   });
 
   it('should call setActivePage when pagination button is clicked', () => {
-    const { getByTestId, getAllByRole } = render(
-      <PaginationControls {...defaultProps} />
-    );
-
-    const nextButton = getAllByRole('button', { name: /2/i })[0];
-
-    fireEvent.click(nextButton);
-
+    const { getAllByRole } = render(<PaginationControls {...defaultProps} />);
+    const twoButton = getAllByRole('button', { name: /2/i })[0];
+    fireEvent.click(twoButton);
     expect(defaultProps.setActivePage).toHaveBeenCalledWith(2);
   });
 });
