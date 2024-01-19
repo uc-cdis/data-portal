7dc1649593a85aa14fec1c665910f57ea90a4627
Jarvis Raymond <jarvisraymond@uchicago.edu>
2024-01-19 11:45:41 -0600

    feat(downloadVariableMetadataButton): added function createUniqueDownloadErrorMsg

--- src/Discovery/DiscoveryDetails/DataDownloadList/ActionButtons/DownloadUtils/DownloadVariableMetadata/DownloadVariableMetadata.tsx
+++ src/Discovery/DiscoveryDetails/DataDownloadList/ActionButtons/DownloadUtils/DownloadVariableMetadata/DownloadVariableMetadata.tsx
@@ -14,12 +14,9 @@ interface IdataDictionaries {
 
 const DownloadVariableMetadata = async (
   resourceInfo: DiscoveryResource,
-  setDownloadStatus: Function,
+  setDownloadStatus: Function
 ) => {
   const zip = new JSZip();
-  // const studyID = resourceInfo['_hdp_uid']; ??????
-  // Need to follow up regarding how to get studyID?
-  // const studyID = 'HDP00001';
   const studyID = resourceInfo._hdp_uid;
   const projectTitle = resourceInfo.project_title;
 
@@ -30,8 +27,8 @@ const DownloadVariableMetadata = async (
       content: (
         <React.Fragment>
           <p>
-            Data Dictionary with name:<em> {projectTitle || studyID}</em> cannot
-            download variable-level metadata with ID:
+            Study with name:<em> {projectTitle || studyID}</em> cannot download
+            variable-level metadata with ID:
             <em> {studyID}</em>.
           </p>
           <p>Please try again later and contact support.</p>
@@ -40,67 +37,72 @@ const DownloadVariableMetadata = async (
       active: true,
     },
   };
+  const createUniqueDownloadErrorMsg = (key: string) => {
+    return {
+      ...DOWNLOAD_FAIL_INFO,
+      message: {
+        ...DOWNLOAD_FAIL_INFO.message,
+        content: (
+          <React.Fragment>
+            <p>
+              Data Dictionary with name <em>{key}</em> cannot download data
+              dictionary with name <em>{key}</em>.
+            </p>
+            <p>Please try again later and contact support.</p>
+          </React.Fragment>
+        ),
+      },
+    };
+  };
 
-  const fetchData = async (key: string, value: string) => new Promise((resolve, reject) => {
-    fetchWithCreds({ path: `${mdsURL}/${value}` }).then((statusResponse) => {
-      const { data } = statusResponse;
-      if (statusResponse.status !== 200 || !data) {
-        setDownloadStatus(DOWNLOAD_FAIL_INFO);
-        reject(new Error(`Issue with ${key}: ${value}`));
-      } else {
-        zip.file(key, JSON.stringify(data));
-        resolve(`Data resolved for ${key}: ${value}`);
-      }
+  const fetchData = async (key: string, value: string) =>
+    new Promise((resolve, reject) => {
+      fetchWithCreds({ path: `${mdsURL}/${value}` }).then((statusResponse) => {
+        const { data } = statusResponse;
+        if (statusResponse.status !== 200 || !data) {
+          setDownloadStatus(createUniqueDownloadErrorMsg(key));
+
+          reject(new Error(`Issue with ${key}: ${value}`));
+        } else {
+          zip.file(key, JSON.stringify(data));
+          resolve(`Data resolved for ${key}: ${value}`);
+        }
+      });
     });
-  });
 
   const fetchDataForAllFiles = async (dataDictionaries: IdataDictionaries) => {
     try {
       await Promise.all(
-        Object.entries(dataDictionaries).map(([key, value]) => fetchData(key, value),
-        ),
+        Object.entries(dataDictionaries).map(([key, value]) =>
+          fetchData(key, value)
+        )
       ).then(() => {
         zip.generateAsync({ type: 'blob' }).then((content) => {
           FileSaver.saveAs(content, 'variable-metadata.zip');
         });
       });
     } catch (error) {
-      setDownloadStatus(DOWNLOAD_FAIL_INFO);
       console.error('Error fetching data:', error);
     }
   };
 
-  if (studyID) {
-    console.log(studyID);
-    fetchWithCreds({ path: `${mdsURL}/${studyID}` }).then((statusResponse) => {
-      const { data } = statusResponse;
-      if (statusResponse.status !== 200 || !data) {
-        setDownloadStatus(DOWNLOAD_FAIL_INFO);
-      } else {
-        try {
-          const dataDictionaries: IdataDictionaries = data.data_dictionaries;
-          if (Object.keys(dataDictionaries).length !== 0) {
-            fetchDataForAllFiles(dataDictionaries);
-          }
-        } catch {
-          setDownloadStatus(DOWNLOAD_FAIL_INFO);
+  fetchWithCreds({ path: `${mdsURL}/${studyID}` }).then((statusResponse) => {
+    const { data } = statusResponse;
+    if (statusResponse.status !== 200 || !data) {
+      setDownloadStatus(DOWNLOAD_FAIL_INFO);
+    } else {
+      try {
+        const dataDictionaries: IdataDictionaries = data.data_dictionaries;
+        if (Object.keys(dataDictionaries).length !== 0) {
+          fetchDataForAllFiles(dataDictionaries);
         }
+      } catch (error) {
+        setDownloadStatus(DOWNLOAD_FAIL_INFO);
+        console.error('Error fetching data:', error);
       }
-    });
-  } else {
-    setDownloadStatus(DOWNLOAD_FAIL_INFO);
-  }
+    }
+  });
   return null;
 };
 
 export default DownloadVariableMetadata;
-
-/**
- * ISSUES:
- * Test study HDP00001 is not configured for DataDownloadList
- * fieldConfig.type === 'dataDownloadList' is false
- * Cannot find two now optional added into
- * “discoveryConfig.features.exportToWorkspace”: “variableMetadataFieldName” and “enableDownloadVariableMetadata”
- * As a result: Cannot find value from the value associated with the key variableMetadataFieldName
- * Not clear what [Name] means for Data Dictionary and Study.resourceInfo.project_title?
- */
