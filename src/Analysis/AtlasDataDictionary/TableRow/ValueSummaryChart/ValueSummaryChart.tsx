af741797fdaee296f54874403c367d1c95f01fb4
Jarvis Raymond <jarvisraymond@uchicago.edu>
2024-01-31 14:57:42 -0600

    feat(vadcDataDictionaryPreviewCharts): Fixed spacing issue for preview version of chart

--- src/Analysis/AtlasDataDictionary/TableRow/ValueSummaryChart/ValueSummaryChart.tsx
+++ src/Analysis/AtlasDataDictionary/TableRow/ValueSummaryChart/ValueSummaryChart.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import {
-  BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip,
-} from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 import * as d3Scale from 'd3-scale';
 import {
   FULLSIZE_CHART_HEIGHT,
@@ -45,9 +43,10 @@ const ValueSummaryChart = ({
   const xAxisAngle = chartType === 'Number' ? 0 : -25;
   const xAxisTextAnchor = chartType === 'Number' ? 'middle' : 'end';
 
-  const processedChartData = chartType === 'Number'
-    ? chartData.sort((a: any, b: any) => a.start - b.start)
-    : chartData;
+  const processedChartData =
+    chartType === 'Number'
+      ? chartData.sort((a: any, b: any) => a.start - b.start)
+      : chartData;
 
   const formatXAxisWithEllipsisIfTooLong = (tick: string) => {
     if (tick.length > X_AXIS_CHARACTER_CUT_OFF) {
@@ -59,7 +58,7 @@ const ValueSummaryChart = ({
   return (
     <div
       data-testid='value-summary-chart'
-      className={`value-summary-chart ${chartType}`}
+      className={`value-summary-chart ${chartType} ${preview ? 'preview' : ''}`}
       style={{ width: chartWidth }}
     >
       <BarChart
