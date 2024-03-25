4ce556398be1b645b6d1e5d3171877953c69ad1d
Mingfei Shao <2475897+mfshao@users.noreply.github.com>
2024-03-25 14:51:13 -0500

    HP-1416 Feat/disco details subheading (#1507)
    * add subheader field to disco details
    
    * revert some files
    
    * update component
    
    * missing file
    
    * style change
    
    * fix wrong heading
    
    * update test
    
    * regular text

--- src/Discovery/DiscoveryDetails/Components/StudyHeader.tsx
+++ src/Discovery/DiscoveryDetails/Components/StudyHeader.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import { Space } from 'antd';
 import jsonpath from 'jsonpath';
 
 const StudyHeader = ({ props }) => {
   const headerField = props.config.detailView?.headerField
-    || props.config.studyPageFields.header?.field
+    || props.config.studyPageFields?.header?.field
+    || '';
+  const subHeaderField = props.config.detailView?.subHeaderField
+    || props.config.studyPageFields?.subHeader?.field
     || '';
   return (
-    <Space align='baseline'>
-      <h3 className='discovery-modal__header-text'>
-        {jsonpath.query(props.modalData, `$.${headerField}`)}
-      </h3>
-    </Space>
+    <div className='discovery-modal__headers'>
+      {(headerField) && (
+        <h3 className='discovery-modal__header-text'>
+          {jsonpath.query(props.modalData, `$.${headerField}`)}
+        </h3>
+      )}
+      {(subHeaderField) && (
+        <div className='discovery-modal__subheader-text'>
+          {jsonpath.query(props.modalData, `$.${subHeaderField}`)}
+        </div>
+      )}
+    </div>
   );
 };
 
