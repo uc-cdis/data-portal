a6c73c99a19f87971ef0a87d8071801db7c4597b
Jarvis Raymond <jarvisraymond@uchicago.edu>
2024-04-08 10:19:53 -0500

    feat(vadcManageColumnsDataDictionary): Created separate HeaderItems object for reuse across different components

--- src/Analysis/AtlasDataDictionary/Components/ColumnHeaders/ColumnHeaders.tsx
+++ src/Analysis/AtlasDataDictionary/Components/ColumnHeaders/ColumnHeaders.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import HeaderItems from '../../Utils/HeaderItems';
 import Header from './Header';
 import {
   ISortConfig,
@@ -15,140 +16,34 @@ const ColumnHeaders = ({
   handleSort,
   sortConfig,
   columnManagementData,
-}: ITableHeadersProps) => {
-  const headerItems = [
-    {
-      headerKey: '',
-      show: true,
-      jsx: <React.Fragment />,
-    },
-    {
-      headerKey: 'vocabularyID',
-      show: columnManagementData.vocabularyID,
-      jsx: (
-        <span>
-          Vocabulary
-          <br />
-          ID
-        </span>
-      ),
-    },
-    {
-      headerKey: 'conceptID',
-      show: columnManagementData.conceptID,
-      jsx: (
-        <span>
-          Concept
-          <br />
-          ID
-        </span>
-      ),
-    },
-    {
-      headerKey: 'conceptCode',
-      show: columnManagementData.conceptCode,
-      jsx: (
-        <span>
-          Concept
-          <br />
-          Code
-        </span>
-      ),
-    },
-    {
-      headerKey: 'conceptName',
-      show: columnManagementData.conceptName,
-      jsx: (
-        <span>
-          Concept
-          <br />
-          Name
-        </span>
-      ),
-    },
-    {
-      headerKey: 'conceptClassID',
-      show: columnManagementData.conceptClassID,
-      jsx: (
-        <span>
-          Concept
-          <br />
-          Class&nbsp;ID
-        </span>
-      ),
-    },
-    {
-      headerKey: 'numberOfPeopleWithVariable',
-      show: columnManagementData.numberOfPeopleWithVariable,
-      jsx: (
-        <span>
-          #&nbsp;/&nbsp;%&nbsp;of&nbsp;People
-          <br />
-          with&nbsp;Variable
-        </span>
-      ),
-    },
-    {
-      headerKey: 'numberOfPeopleWhereValueIsFilled',
-      show: columnManagementData.numberOfPeopleWhereValueIsFilled,
-      jsx: (
-        <span>
-          #&nbsp;/&nbsp;%&nbsp;of&nbsp;People
-          <br />
-          where&nbsp;Value&nbsp;is&nbsp;Filled
-        </span>
-      ),
-    },
-    {
-      headerKey: 'numberOfPeopleWhereValueIsNull',
-      show: columnManagementData.numberOfPeopleWhereValueIsNull,
-      jsx: (
-        <span>
-          #&nbsp;/&nbsp;%&nbsp;of&nbsp;People
-          <br />
-          where&nbsp;Value&nbsp;is&nbsp;Null
-        </span>
-      ),
-    },
-    {
-      headerKey: 'valueStoredAs',
-      show: columnManagementData.valueStoredAs,
-      jsx: (
-        <span>
-          Value
-          <br />
-          Stored&nbsp;As
-        </span>
-      ),
-    },
-    {
-      headerKey: 'valueSummary',
-      show: columnManagementData.valueSummary,
-      jsx: <span>Value&nbsp;Summary</span>,
-    },
-  ];
-
-  return (
-    <thead className={'column-headers'} data-testid='column-headers'>
-      <tr>
-        {headerItems.map((item) => (
-          <React.Fragment key={item.headerKey}>
-            {item.show && (
-              <Header
-                handleSort={handleSort}
-                headerJSX={item.jsx}
-                headerKey={item.headerKey}
-                sortConfig={sortConfig}
-                sortable={
-                  item.headerKey !== 'valueSummary' && item.headerKey !== ''
-                }
-              />
-            )}
-          </React.Fragment>
-        ))}
-      </tr>
-    </thead>
-  );
-};
+}: ITableHeadersProps) => (
+  <thead className={'column-headers'} data-testid='column-headers'>
+    <tr>
+      {/* Empty header for dropdown buttons */}
+      <Header
+        handleSort={handleSort}
+        headerJSX={<React.Fragment />}
+        headerKey={''}
+        sortConfig={sortConfig}
+        sortable={false}
+      />
+      {HeaderItems.map((item) => (
+        <React.Fragment key={item.headerKey}>
+          {columnManagementData[item.headerKey] && (
+            <Header
+              handleSort={handleSort}
+              headerJSX={item.jsx}
+              headerKey={item.headerKey}
+              sortConfig={sortConfig}
+              sortable={
+                item.headerKey !== 'valueSummary'
+              }
+            />
+          )}
+        </React.Fragment>
+      ))}
+    </tr>
+  </thead>
+);
 
 export default ColumnHeaders;
