ab6606759fb7f36085da19862d6ce36e5bd0ab6c
Jarvis Raymond <jarvisraymond@uchicago.edu>
2024-04-12 14:56:58 -0500

    feat(inputValidationHealDataDictionaryField): removed destructuring from unit test - was causing test failure

--- src/Discovery/DiscoveryDetails/Components/FieldGrouping/TabField/DataDownloadList/ActionButtons/DownloadUtils/DownloadVariableMetadata/SanitizeFileName.ts
+++ src/Discovery/DiscoveryDetails/Components/FieldGrouping/TabField/DataDownloadList/ActionButtons/DownloadUtils/DownloadVariableMetadata/SanitizeFileName.ts
@@ -1,20 +1,15 @@
 import { validFileNameChecks } from '../../../../../../../../../utils';
 
-const {
-  fileNameCharactersCheckRegex,
-  invalidWindowsFileNames,
-  maximumAllowedFileNameLength,
-} = validFileNameChecks;
 
 const SanitizeFileName = (unSanitizedfileName: string) => {
   let fileName = unSanitizedfileName;
   const fileExtension = '.json';
   // Replace all invalid file name characters
-  fileName = fileName.replace(fileNameCharactersCheckRegex, '');
+  fileName = fileName.replace(validFileNameChecks.fileNameCharactersCheckRegex, '');
   // Trim excessive filename characters
-  fileName = fileName.substring(0, maximumAllowedFileNameLength - fileExtension.length);
+  fileName = fileName.substring(0, validFileNameChecks.maximumAllowedFileNameLength - fileExtension.length);
   // Ensure file name is not an invalid Window's file name
-  invalidWindowsFileNames.forEach((invalidName) => {
+  validFileNameChecks.invalidWindowsFileNames.forEach((invalidName) => {
     if (invalidName === fileName) {
       fileName += '_';
     }
