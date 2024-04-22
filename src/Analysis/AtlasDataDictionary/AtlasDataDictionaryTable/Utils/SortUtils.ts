588d0e7b9e11ff687c5ce681f2b4b6ad960c5f25
Jarvis Raymond <jarvisraymond@uchicago.edu>
2024-04-22 13:20:51 -0500

    feat(atlasDataDictionarySortNumbersAndStrings): Initial Commit

--- src/Analysis/AtlasDataDictionary/AtlasDataDictionaryTable/Utils/SortUtils.ts
+++ src/Analysis/AtlasDataDictionary/AtlasDataDictionaryTable/Utils/SortUtils.ts
@@ -1,7 +1,11 @@
 import { ISortConfig } from '../Interfaces/Interfaces';
+import ColumnsItems from './ColumnItems';
 
-const DetermineNextSortDirection = (sortConfig: ISortConfig, sortKey: string|null) => {
-  let direction:ISortConfig['direction'] = 'ascending';
+const DetermineNextSortDirection = (
+  sortConfig: ISortConfig,
+  sortKey: string | null
+) => {
+  let direction: ISortConfig['direction'] = 'ascending';
   if (sortConfig.sortKey === sortKey) {
     if (sortConfig.direction === 'ascending') {
       direction = 'descending';
@@ -14,14 +18,35 @@ const DetermineNextSortDirection = (sortConfig: ISortConfig, sortKey: string|nul
   return direction;
 };
 
-const SortDataWithDirection = (data:any, direction:ISortConfig['direction'], sortKey: string) => [...data].sort((a, b) => {
-  if (direction === 'ascending') {
-    return a[sortKey].toString().localeCompare(b[sortKey].toString());
+const determineSortType = (sortKey: string) => {
+  const objectWithSortKey = ColumnsItems.find(
+    (object) => object.headerKey === sortKey
+  );
+  return objectWithSortKey?.sortType;
+};
+const SortDataWithDirection = (
+  data: any,
+  direction: ISortConfig['direction'],
+  sortKey: string
+) => {
+  const sortType = determineSortType(sortKey);
+  if (sortType !== 'number' && sortType !== 'string') {
+    throw new Error('invalid sortType found within SortDataWithDirection');
   }
-  if (direction === 'descending') {
-    return b[sortKey].toString().localeCompare(a[sortKey].toString());
-  }
-  return 0;
-});
+  return () =>
+    [...data].sort((a, b) => {
+      if (direction === 'ascending') {
+        return sortType === 'string'
+          ? a[sortKey].toString().localeCompare(b[sortKey].toString())
+          : a[sortKey] - b[sortKey];
+      }
+      if (direction === 'descending') {
+        return sortType === 'string'
+          ? b[sortKey].toString().localeCompare(a[sortKey].toString())
+          : b[sortKey] - a[sortKey];
+      }
+      return 0;
+    });
+};
 
 export { DetermineNextSortDirection, SortDataWithDirection };
