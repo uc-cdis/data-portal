604941351f3e6ae1ce924d7820a71a1974bab039
Jarvis <113449836+jarvisraymond-uchicago@users.noreply.github.com>
2024-05-07 15:40:38 -0500

    VADC-1020 Feat/debounce vadc data dictionary search (#1531)
    * feat(debounceVADCDataDictionarySearch): Initial commit
    
    * feat(debounceVADCDataDictionarySearch): Updated unit test to account for debouncing
    
    * feat(debounceVADCDataDictionarySearch): Automate formatted code
    
    * feat(debounceVADCDataDictionarySearch): formatted to removed excessively long line
    
    * feat(debounceVADCDataDictionarySearch): Updated column names, added toLocalString() to numbers displayed to end user
    
    * feat(debounceVADCDataDictionarySearch): Formatted column names, used toLocaleString on remaining numeric outputs for consistentcy

--- src/Analysis/AtlasDataDictionary/AtlasDataDictionaryTable/Components/TableRow/NonNumericDetailsTable/NonNumericDetailsTable.tsx
+++ src/Analysis/AtlasDataDictionary/AtlasDataDictionaryTable/Components/TableRow/NonNumericDetailsTable/NonNumericDetailsTable.tsx
@@ -47,7 +47,7 @@ const NonNumericDetailsTable = ({ rowObject, searchTerm }) => (
                 searchTerm,
               )}
             >
-              {valueSummaryObj.personCount}
+              {valueSummaryObj.personCount.toLocaleString()}
             </td>
           </tr>
         ),
