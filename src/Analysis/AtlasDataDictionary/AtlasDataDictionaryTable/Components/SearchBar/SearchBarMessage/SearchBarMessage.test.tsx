29cde6fabf11664bd16fdc1a7b64de5c53907afa
Jarvis Raymond <jarvisraymond@uchicago.edu>
2024-05-20 13:55:13 -0500

    feat(hiddenColumnsSearchMessage): formatted code for readability

--- src/Analysis/AtlasDataDictionary/AtlasDataDictionaryTable/Components/SearchBar/SearchBarMessage/SearchBarMessage.test.tsx
+++ src/Analysis/AtlasDataDictionary/AtlasDataDictionaryTable/Components/SearchBar/SearchBarMessage/SearchBarMessage.test.tsx
@@ -5,13 +5,13 @@ import SearchBarMessage from './SearchBarMessage';
 import showSearchBarMessage from './showSearchBarMessage';
 import DefaultAtlasColumnManagement from '../../../Utils/DefaultAtlasColumnManagement';
 import TableData from '../../../TestData/TableData';
-import { IColumnManagementData, IRowData } from '../../../Interfaces/Interfaces';
+import { IRowData } from '../../../Interfaces/Interfaces';
 
 jest.mock('./showSearchBarMessage');
 const columnManagementResetMock = jest.fn();
 const searchTerm = 'search term';
 const paginatedData = TableData.data as IRowData[];
-const columnManagementData = { DefaultAtlasColumnManagement };
+const columnManagementData = DefaultAtlasColumnManagement;
 
 describe('SearchBarMessage', () => {
   afterEach(() => {
@@ -27,11 +27,13 @@ describe('SearchBarMessage', () => {
         columnManagementReset={columnManagementResetMock}
         searchTerm={searchTerm}
         paginatedData={paginatedData}
-        columnManagementData={columnManagementData as unknown as IColumnManagementData}
+        columnManagementData={columnManagementData}
       />,
     );
 
-    expect(screen.getByText('Matches found in hidden columns.')).toBeInTheDocument();
+    expect(
+      screen.getByText('Matches found in hidden columns.'),
+    ).toBeInTheDocument();
     fireEvent.click(screen.getByTestId('show-all-button'));
     expect(columnManagementResetMock).toHaveBeenCalled();
   });
@@ -45,7 +47,7 @@ describe('SearchBarMessage', () => {
         columnManagementReset={columnManagementResetMock}
         searchTerm={searchTerm}
         paginatedData={paginatedData}
-        columnManagementData={columnManagementData as unknown as IColumnManagementData}
+        columnManagementData={columnManagementData}
       />,
     );
     expect(container).toBeEmptyDOMElement();
--- src/Analysis/AtlasDataDictionary/AtlasDataDictionaryTable/Components/SearchBar/SearchBarMessage/showSearchBarMessage.test.ts
+++ src/Analysis/AtlasDataDictionary/AtlasDataDictionaryTable/Components/SearchBar/SearchBarMessage/showSearchBarMessage.test.ts
@@ -8,38 +8,90 @@ describe('showSearchBarMessage', () => {
     jest.clearAllMocks();
   });
   const paginatedData = [TableData.data[0]] as IRowData[];
-  it('returns true when searchTerm is found in hidden columns with checkIfCellContainsSearchTerm', () => {
+  it(`returns true when searchTerm is found in hidden columns
+   with checkIfCellContainsSearchTerm`, () => {
     const searchTerm = TableData.data[0].vocabularyID;
-    const columnManagementData = { ...DefaultAtlasColumnManagement, ...{ vocabularyID: false } };
-    const result = showSearchBarMessage(searchTerm, paginatedData, columnManagementData);
+    const columnManagementData = {
+      ...DefaultAtlasColumnManagement,
+      ...{ vocabularyID: false },
+    };
+    const result = showSearchBarMessage(
+      searchTerm,
+      paginatedData,
+      columnManagementData,
+    );
     expect(result).toBe(true);
   });
-  it('returns true when searchTerm is found in hidden cells with checkIfHiddenCellsContainSearchTerm', () => {
+  it(`returns true when searchTerm is found in hidden cells
+  with checkIfHiddenCellsContainSearchTerm`, () => {
     const searchTerm = TableData.data[0].valueSummary[0].personCount.toString();
-    const columnManagementData = { ...DefaultAtlasColumnManagement, ...{ valueSummary: false } };
-    const result = showSearchBarMessage(searchTerm, paginatedData, columnManagementData);
+    const columnManagementData = {
+      ...DefaultAtlasColumnManagement,
+      ...{ valueSummary: false },
+    };
+    const result = showSearchBarMessage(
+      searchTerm,
+      paginatedData,
+      columnManagementData,
+    );
     expect(result).toBe(true);
   });
   it('returns true when searchTerm is found in a percentage column', () => {
     const searchTerm = 3.1415;
-    const columnManagementData = { ...DefaultAtlasColumnManagement, ...{ numberOfPeopleWithVariable: false } };
-    const paginatedDataWithPercentageData = [{ ...TableData.data[0], ...{ numberOfPeopleWithVariablePercent: searchTerm } }] as IRowData[];
-    const result = showSearchBarMessage(searchTerm.toString(), paginatedDataWithPercentageData, columnManagementData);
+    const columnManagementData = {
+      ...DefaultAtlasColumnManagement,
+      ...{ numberOfPeopleWithVariable: false },
+    };
+    const paginatedDataWithPercentageData = [
+      {
+        ...TableData.data[0],
+        ...{ numberOfPeopleWithVariablePercent: searchTerm },
+      },
+    ] as IRowData[];
+    const result = showSearchBarMessage(
+      searchTerm.toString(),
+      paginatedDataWithPercentageData,
+      columnManagementData,
+    );
     expect(result).toBe(true);
   });
   it('returns false when searchTerm is not found in a percentage column', () => {
     const searchTerm = 3.1415;
-    const columnManagementData = { ...DefaultAtlasColumnManagement, ...{ numberOfPeopleWithVariable: false } };
-    const paginatedDataWithPercentageData = [{ ...TableData.data[0], ...{ numberOfPeopleWithVariablePercent: 3 } }] as IRowData[];
-    const result = showSearchBarMessage(searchTerm.toString(), paginatedDataWithPercentageData, columnManagementData);
+    const columnManagementData = {
+      ...DefaultAtlasColumnManagement,
+      ...{ numberOfPeopleWithVariable: false },
+    };
+    const paginatedDataWithPercentageData = [
+      {
+        ...TableData.data[0],
+        ...{ numberOfPeopleWithVariablePercent: 3 },
+      },
+    ] as IRowData[];
+    const result = showSearchBarMessage(
+      searchTerm.toString(),
+      paginatedDataWithPercentageData,
+      columnManagementData,
+    );
     expect(result).toBe(false);
   });
 
   it('returns false when searchTerm is not found in hidden columns', () => {
     const searchTerm = 3.1415;
-    const columnManagementData = { ...DefaultAtlasColumnManagement, ...{ numberOfPeopleWithVariable: false } };
-    const paginatedDataWithPercentageData = [{ ...TableData.data[0], ...{ numberOfPeopleWithVariablePercent: 3 } }] as IRowData[];
-    const result = showSearchBarMessage(searchTerm.toString(), paginatedDataWithPercentageData, columnManagementData);
+    const columnManagementData = {
+      ...DefaultAtlasColumnManagement,
+      ...{ numberOfPeopleWithVariable: false },
+    };
+    const paginatedDataWithPercentageData = [
+      {
+        ...TableData.data[0],
+        ...{ numberOfPeopleWithVariablePercent: 3 },
+      },
+    ] as IRowData[];
+    const result = showSearchBarMessage(
+      searchTerm.toString(),
+      paginatedDataWithPercentageData,
+      columnManagementData,
+    );
     expect(result).toBe(false);
   });
 });
