168b68b5b7ce0b3ceebe355a1a5a7d05e7ab1889
Jarvis Raymond <jarvisraymond@uchicago.edu>
2024-07-16 12:13:31 -0500

    feat(vadcWorkflowDashboard): Added styles

--- src/Analysis/SharedUtils/WorkflowLimitsDashboard/WorkflowLimitsDashboard.jsx
+++ src/Analysis/SharedUtils/WorkflowLimitsDashboard/WorkflowLimitsDashboard.jsx
@@ -1,30 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import { Progress } from 'antd';
+import { components } from '../../../params';
 import './WorkflowLimitsDashboard.css';
-const WorkflowLimitsDashboard = ({}) => {
-  const workflow_run = 59;
-  const workflow_limit = 50;
 
+const WorkflowLimitsDashboard = () => {
+  const workflowRun = 10;
+  const workflowLimit = 50;
+  const supportEmail = components.login?.email || 'support@datacommons.io';
   return (
     <React.Fragment>
       <div className='workflow-limits-dashboard row'>
         <div className='column'>
           <h3>Monthly Workflow Limit</h3>
           <div>
-            <strong>{workflow_run} used</strong> from {workflow_limit} Limit
+            <strong>{workflowRun} used</strong> from {workflowLimit} Limit
           </div>
         </div>
         <div className='column progress'>
-          {workflow_run >= workflow_limit && (
-            <div className='error-message'>
-              !&#x20DD; You have exceeded your monthly workflow limit. Please
-              contact support for assistance.
+          {workflowRun >= workflowLimit && (
+            <div>
+              <div className='error-message'>
+                You have exceeded your monthly workflow limit. Please contact
+                support for assistance:
+                <a href={`mailto:${supportEmail}`}>{supportEmail}</a>.
+              </div>
             </div>
           )}
           <Progress
-            percent={(workflow_run / workflow_limit) * 100}
+            percent={(workflowRun / workflowLimit) * 100}
             showInfo={false}
-            status={workflow_run >= workflow_limit ? 'exception' : 'success'}
+            status={workflowRun >= workflowLimit ? 'exception' : 'success'}
           />
         </div>
       </div>
