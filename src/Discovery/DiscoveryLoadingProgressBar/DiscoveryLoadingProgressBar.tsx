d727870c75a209b65236cd1f649db758db43f960
Jarvis Raymond <jarvisraymond@uchicago.edu>
2024-09-05 14:28:57 -0500

    feat(discoveryEnhancedLoading): Initial commit

---
+++ src/Discovery/DiscoveryLoadingProgressBar/DiscoveryLoadingProgressBar.tsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import { Progress } from 'antd';
+
+interface DiscoveryLoadingProgressBarProps {
+ batchLoadingInfo: { allBatchesAreLoaded: boolean }
+}
+
+// this should probably be done in a CSS for production:
+/*
+const style = document.createElement('style');
+style.type = 'text/css';
+const css = '.discovery-header__dropdown-tags-container {margin-top: 15px;} .discovery-header{align-items:start;} ';
+style.innerHTML = css;
+document.head.appendChild(style);
+*/
+
+const DiscoveryLoadingProgressBar = ({
+  batchLoadingInfo,
+}: DiscoveryLoadingProgressBarProps) => {
+  const [percent, setPercent] = useState(0);
+  const { allBatchesAreLoaded } = batchLoadingInfo;
+  const [displayProgressBar, setDisplayProgressBar] = useState(true);
+
+  // Fake loading UI
+  const percentUpdateInterval = 500;
+  const percentIncrementAmount = 5;
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPercent((prevPercent) => prevPercent + percentIncrementAmount);
+    }, percentUpdateInterval);
+    return () => clearInterval(interval);
+  }, [percent, allBatchesAreLoaded]);
+
+  // hide the bar with a transition delay after the batches are loaded,
+  // giving the browser some time to process the batch
+  const delayTimeBeforeHidingProgressBar = 2000;
+  useEffect(() => {
+    if (allBatchesAreLoaded) {
+      // Change displayProgressBar to false after delay
+      setTimeout(() => {
+        setDisplayProgressBar(false);
+      }, delayTimeBeforeHidingProgressBar);
+    }
+  }, [allBatchesAreLoaded]);
+
+  return (
+    <React.Fragment>
+      { displayProgressBar && (
+        <div className='discovery-loading-progress-bar' style={{ display: displayProgressBar ? 'block' : 'none' }}>
+          <Progress
+            width={80}
+            showInfo={false}
+            percent={allBatchesAreLoaded ? 100 : percent}
+            status='success'
+            strokeColor='#99286B'
+          />
+          <p className='discovery-header__stat-label'>
+            Loading studies...
+          </p>
+        </div>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default DiscoveryLoadingProgressBar;
