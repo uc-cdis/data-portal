53498162294d6306ef24cf2bcb25ff1b24a1dd6e
Jarvis Raymond <jarvisraymond@uchicago.edu>
2024-09-20 13:56:18 -0500

    feat(discoveryEnhancedLoading): updated MDSUtils.test.jsx

--- src/Discovery/MDSUtils.test.jsx
+++ src/Discovery/MDSUtils.test.jsx
@@ -4,12 +4,30 @@ import { mdsURL } from '../localconf';
 // Mocking the global fetch API
 global.fetch = jest.fn();
 
+const checkForFetchError = async (targetFunction) => {
+  const mockResponse = {
+    0: { gen3_discovery: { name: 'Study 1' } },
+    1: { gen3_discovery: { name: 'Study 2' } },
+  };
+  fetch.mockResolvedValueOnce({
+    status: 401,
+    json: jest.fn().mockResolvedValueOnce(mockResponse),
+  });
+  const expectedErrorMsg = 'Request for study data failed: Error';
+  let actualErrorMsg = null;
+  try {
+    const studies = await targetFunction();
+  } catch (e) {
+    actualErrorMsg = e.message;
+  }
+  expect(actualErrorMsg.toString().includes(expectedErrorMsg)).toBe(true);
+};
+
 describe('MDS Data Loading Functions', () => {
   afterEach(() => {
     jest.clearAllMocks(); // Clear mocks after each test
   });
 
-  /*
   describe('loadStudiesFromMDS', () => {
     it('should load studies successfully', async () => {
       const mockResponse = {
@@ -25,69 +43,59 @@ describe('MDS Data Loading Functions', () => {
       const studies = await loadStudiesFromMDS();
       expect(studies).toEqual([{ name: 'Study 1' }, { name: 'Study 2' }]);
       expect(fetch).toHaveBeenCalledTimes(1);
-      expect(fetch).toHaveBeenCalledWith(`${mdsURL}?data=True&_guid_type=discovery_metadata&limit=2000&offset=0`);
+      expect(fetch).toHaveBeenCalledWith(
+        `${mdsURL}?data=True&_guid_type=discovery_metadata&limit=2000&offset=0`
+      );
     });
 
-  it('should throw an error on fetch failure', async () => {
-    const mockResponse = {
-      0: { gen3_discovery: { name: 'Study 1' } },
-      1: { gen3_discovery: { name: 'Study 2' } },
-    };
-    fetch.mockResolvedValueOnce({
-      status: 401,
-      json: jest.fn().mockResolvedValueOnce(mockResponse),
+    it('should throw an error on fetch failure', async () => {
+      checkForFetchError(loadStudiesFromMDS);
     });
-    const expectedErrorMsg = 'Request for study data failed: Error';
-    let actualErrorMsg = null;
-    try {
+
+    it('should load up to 2000 studies, then load more with a secondary request', async () => {
+      const mockStudies = new Array(2500).fill({ mockStudy: 'info' });
+      // Simulate first fetch (2000 studies)
+      fetch.mockImplementationOnce(() =>
+        Promise.resolve({
+          status: 200,
+          json: () => Promise.resolve(mockStudies.slice(0, 2000)),
+        })
+      );
+
+      // Simulate second fetch (500 studies)
+      fetch.mockImplementationOnce(() =>
+        Promise.resolve({
+          status: 200,
+          json: () => Promise.resolve(mockStudies.slice(2000, 2500)),
+        })
+      );
       const studies = await loadStudiesFromMDS();
-    } catch (e) {
-      actualErrorMsg = e.message;
-    }
-    expect(actualErrorMsg.toString().includes(expectedErrorMsg)).toBe(true);
-  });
-
-  it('should load up to 2000 studies, then load more with a secondary request', async () => {
-    const mockStudies = new Array(2500).fill({mockStudy:'info'});
-    // Simulate first fetch (2000 studies)
-    fetch.mockImplementationOnce(() =>
-      Promise.resolve({
-        status: 200,
-        json: () => Promise.resolve(mockStudies.slice(0, 2000))
-      })
-    );
-
-    // Simulate second fetch (500 studies)
-    fetch.mockImplementationOnce(() =>
-      Promise.resolve({
-        status: 200,
-        json: () => Promise.resolve(mockStudies.slice(2000, 2500))
-      })
-    );
-    const studies = await loadStudiesFromMDS();
-    expect(fetch).toHaveBeenCalledTimes(2);
-    expect(studies.length).toBe(2500);
-  });
-  */
-
-  describe('getSomeStudiesFromMDS', () => {
-    it('should retrieve a limited number of studies', async () => {
-      const mockResponse = {
-        0: { gen3_discovery: { name: 'Study 1' } },
-        1: { gen3_discovery: { name: 'Study 2' } },
-      };
-
-      fetch.mockResolvedValueOnce({
-        status: 200,
-        json: jest.fn().mockResolvedValueOnce(mockResponse),
-      });
-
-      const studies = await getSomeStudiesFromMDS('discovery_metadata', 2);
-      expect(studies).toEqual([{ name: 'Study 1' }, { name: 'Study 2' }]);
-      expect(fetch).toHaveBeenCalledTimes(1);
-      expect(fetch).toHaveBeenCalledWith(`${mdsURL}?data=True&_guid_type=discovery_metadata&limit=2`);
+      expect(fetch).toHaveBeenCalledTimes(2);
+      expect(studies.length).toBe(2500);
     });
 
+    describe('getSomeStudiesFromMDS', () => {
+      it('should retrieve a limited number of studies', async () => {
+        const mockResponse = {
+          0: { gen3_discovery: { name: 'Study 1' } },
+          1: { gen3_discovery: { name: 'Study 2' } },
+        };
 
+        fetch.mockResolvedValueOnce({
+          status: 200,
+          json: jest.fn().mockResolvedValueOnce(mockResponse),
+        });
+
+        const studies = await getSomeStudiesFromMDS('discovery_metadata', 2);
+        expect(studies).toEqual([{ name: 'Study 1' }, { name: 'Study 2' }]);
+        expect(fetch).toHaveBeenCalledTimes(1);
+        expect(fetch).toHaveBeenCalledWith(
+          `${mdsURL}?data=True&_guid_type=discovery_metadata&limit=2`
+        );
+      });
+      it('should throw an error on fetch failure', async () => {
+        checkForFetchError(getSomeStudiesFromMDS);
+      });
+    });
   });
 });
