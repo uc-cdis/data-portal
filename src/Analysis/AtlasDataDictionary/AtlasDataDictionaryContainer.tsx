caf50109b1485bae2e2548eecca36ee9858d702a
pieterlukasse <pieterlukasse@gmail.com>
2024-10-23 22:18:07 +0200

    feat: enable old and new data dictionary for VA

--- src/Analysis/AtlasDataDictionary/AtlasDataDictionaryContainer.tsx
+++ src/Analysis/AtlasDataDictionary/AtlasDataDictionaryContainer.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useLocation, useHistory, useRouteMatch } from 'react-router-dom';
 import ProtectedContent from '../../Login/ProtectedContent';
 import AtlasDataDictionaryLoading from './AtlasDataDictionaryTable/AtlasDataDictionaryLoading';
+import AtlasDataDictionaryButton from './AtlasDataDictionaryButton/AtlasDataDictionaryButton';
 import './AtlasDataDictionary.css';
 
-const AtlasDataDictionaryContainer = () => {
+const AtlasDataDictionaryContainer = ({ dataDictionaryVersion }) => {
   const location = useLocation();
   const history = useHistory();
   const match = useRouteMatch();
+
+  if (!dataDictionaryVersion || !dataDictionaryVersion.includes('new')) {
+    // Default legacy component: render a div with AtlasDataDictionaryButton when
+    // no dataDictionaryVersion is set or it does not include 'new':
+    return (
+      <div style={{ width: '100%' }}><AtlasDataDictionaryButton /></div>
+    );
+  }
   return (
     <div className='atlas-data-dictionary-container'>
       <ProtectedContent
@@ -19,6 +29,14 @@ const AtlasDataDictionaryContainer = () => {
       />
     </div>
   );
+};
+
+AtlasDataDictionaryContainer.propTypes = {
+  dataDictionaryVersion: PropTypes.string,
+};
+
+AtlasDataDictionaryContainer.defaultProps = {
+  dataDictionaryVersion: null,
 };
 
 export default AtlasDataDictionaryContainer;
