2eee47982d9d315022a0b53cc5a9d85dae53c111
pieterlukasse <pieterlukasse@gmail.com>
2024-10-23 22:18:07 +0200

    feat: enable old and new data dictionary for VA

--- src/Analysis/AtlasDataDictionary/AtlasDataDictionaryContainer.tsx
+++ src/Analysis/AtlasDataDictionary/AtlasDataDictionaryContainer.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useLocation, useHistory, useRouteMatch } from 'react-router-dom';
 import ProtectedContent from '../../Login/ProtectedContent';
 import AtlasDataDictionaryLoading from './AtlasDataDictionaryTable/AtlasDataDictionaryLoading';
+import AtlasDataDictionaryButton from './AtlasDataDictionaryButton/AtlasDataDictionaryButton';
 import './AtlasDataDictionary.css';
 
-const AtlasDataDictionaryContainer = () => {
+const AtlasDataDictionaryContainer = ({ title }) => {
   const location = useLocation();
   const history = useHistory();
   const match = useRouteMatch();
-  return (
-    <div className='atlas-data-dictionary-container'>
-      <ProtectedContent
-        public
-        location={location}
-        history={history}
-        match={match}
-        component={() => <AtlasDataDictionaryLoading />}
-      />
-    </div>
-  );
+
+  if (!title || !title.includes('(new)')) {
+    // Legacy component: render a div with AtlasDataDictionaryButton when title does not include '(new)'
+    return (
+      <div style={{ width: '100%' }}><AtlasDataDictionaryButton /></div>
+    );
+  } else {
+    return (
+      <div className='atlas-data-dictionary-container'>
+        <ProtectedContent
+          public
+          location={location}
+          history={history}
+          match={match}
+          component={() => <AtlasDataDictionaryLoading />}
+        />
+      </div>
+    );
+  }
 };
 
+AtlasDataDictionaryContainer.propTypes = {
+  title: PropTypes.string,
+};
 export default AtlasDataDictionaryContainer;
