658498bf3bd03cde229cb491577f3f2d65d05b35
Jarvis Raymond <jarvisraymond@uchicago.edu>
2024-10-23 14:38:54 -0500

    fix(cardDescriptionParsing): ranner linter and fixed error

--- src/Analysis/AppCard.jsx
+++ src/Analysis/AppCard.jsx
@@ -15,7 +15,7 @@ class AppCard extends React.Component {
         <img
           className='app-card__image'
           src={(this.props.imageUrl) ? this.props.imageUrl : '/src/img/analysis-icons/default-app.png'}
-          alt=""
+          alt=''
         />
       </div>
     );
