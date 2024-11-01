dfd1ebb2ffdf199f24d61142576232528e536862
tianj7 <jtian1@uchicago.edu>
2024-11-01 11:43:38 -0500

    add sprint branch changes

--- src/Analysis/AtlasDataDictionary/AtlasDataDictionaryContainer.tsx
+++ src/Analysis/AtlasDataDictionary/AtlasDataDictionaryContainer.tsx
@@ -1,36 +1,24 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { useLocation, useHistory, useRouteMatch } from 'react-router-dom';
 import ProtectedContent from '../../Login/ProtectedContent';
 import AtlasDataDictionaryLoading from './AtlasDataDictionaryTable/AtlasDataDictionaryLoading';
-import AtlasDataDictionaryButton from './AtlasDataDictionaryButton/AtlasDataDictionaryButton';
 import './AtlasDataDictionary.css';
 
-const AtlasDataDictionaryContainer = ({ dataDictionaryVersion }) => {
+const AtlasDataDictionaryContainer = () => {
   const location = useLocation();
   const history = useHistory();
   const match = useRouteMatch();
-
-  if (!dataDictionaryVersion || !dataDictionaryVersion.includes('new')) {
-    // Default legacy component: render a div with AtlasDataDictionaryButton when
-    // no dataDictionaryVersion is set or it does not include 'new':
-    return (
-      <div style={{ width: '100%' }}><AtlasDataDictionaryButton /></div>
-    );
-  }
   return (
     <div className='atlas-data-dictionary-container'>
-      <AtlasDataDictionaryLoading />
+      <ProtectedContent
+        public
+        location={location}
+        history={history}
+        match={match}
+        component={() => <AtlasDataDictionaryLoading />}
+      />
     </div>
   );
-};
-
-AtlasDataDictionaryContainer.propTypes = {
-  dataDictionaryVersion: PropTypes.string,
-};
-
-AtlasDataDictionaryContainer.defaultProps = {
-  dataDictionaryVersion: null,
 };
 
 export default AtlasDataDictionaryContainer;
