06aa87719a7c4e3eed2b593dbbfe06076da9ca76
pieterlukasse <pieterlukasse@gmail.com>
2024-11-01 19:59:42 +0100

    fix: revert and bring back old data dictionary button component
    ...from https://github.com/uc-cdis/data-portal/tree/5dc09282573e93f8e11d44f53d65e3cf677256c6/src/Analysis/AtlasDataDictionary/AtlasDataDictionaryButton
    ...which in turn is a restore from older code https://github.com/uc-cdis/data-portal/pull/1478/

---
+++ src/Analysis/AtlasDataDictionary/AtlasLegacyDataDictionaryButton/AtlasLegacyDataDictionaryButton.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { Modal } from 'antd';
+import Button from '@gen3/ui-component/dist/components/Button';
+import './AtlasLegacyDataDictionaryButton.css';
+
+const AtlasLegacyDataDictionaryButton = () => {
+  const dataDictionaryURL = 'https://vincicentral.vinci.med.va.gov/SitePages/VINCI_University-VADC_Academy.aspx';
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div className='atlas-data-dictionary-button' data-testid='atlas-data-dictionary-button'>
+      <Modal
+        title="You're now leaving the VA Data Commons"
+        open={isModalOpen}
+        className='atlas-data-dictionary-button-modal'
+        onOk={() => {
+          window.open(dataDictionaryURL, '_blank');
+          handleCancel();
+        }}
+        onCancel={handleCancel}
+      >
+        <p>
+          The VADC website, privacy and security policies don&apos;t apply to the
+          site or app you&apos;re about to visit.
+        </p>
+      </Modal>
+      <Button
+        className='analysis-app__button'
+        onClick={showModal}
+        label='MVP Data Dictionary'
+        buttonType='secondary'
+        rightIcon='external-link'
+      />
+    </div>
+  );
+};
+export default AtlasLegacyDataDictionaryButton;
