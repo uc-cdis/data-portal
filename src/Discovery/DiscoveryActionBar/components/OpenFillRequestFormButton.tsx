88c751febbab97a9819e637a04329c1e48740b5a
urvi-occ <urvi@occ-data.org>
2024-11-07 10:41:49 -0600

    changes for failing test

--- src/Discovery/DiscoveryActionBar/components/OpenFillRequestFormButton.tsx
+++ src/Discovery/DiscoveryActionBar/components/OpenFillRequestFormButton.tsx
@@ -4,14 +4,23 @@ import { Popover, Button } from 'antd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const OpenFillRequestFormButton = (props) => {
-  if (
-    !props.config.features.exportToWorkspace?.enableFillRequestForm ||
-    !props.config.features.exportToWorkspace.fillRequestFormURL?.trim() ||
-    !props.config.features.exportToWorkspace.externalWebsiteURL?.trim() ||
-    !props.config.features.exportToWorkspace.externalWebsiteName?.trim()
-  ) {
-    return null; 
+  const { config, discovery } = props;
+
+  // Check if Fill Request Form button should be disabled based on configuration
+  const isFillRequestFormDisabled = !config?.features?.exportToWorkspace?.enableFillRequestForm
+    || !config.features.exportToWorkspace.fillRequestFormURL?.trim()
+    || !config.features.exportToWorkspace.externalWebsiteURL?.trim()
+    || !config.features.exportToWorkspace.externalWebsiteName?.trim();
+
+  if (isFillRequestFormDisabled) {
+    return null; // Return early if any required config values are missing
   }
+
+  // Define URLs and text for use in the popover and button
+  const {
+    fillRequestFormURL, externalWebsiteURL, externalWebsiteName, fillRequestFormDisplayText,
+  } = config.features.exportToWorkspace;
+  const { selectedResources } = discovery;
 
   return (
     <Popover
@@ -20,35 +29,31 @@ const OpenFillRequestFormButton = (props) => {
       title={(
         <React.Fragment>
           &nbsp;
-          <a
-            target='_blank'
-            rel='noreferrer'
-            href={props.config.features.exportToWorkspace.externalWebsiteURL}
-          >
-            {props.config.features.exportToWorkspace.externalWebsiteName}
+          <a target='_blank' rel='noreferrer' href={externalWebsiteURL}>
+            {externalWebsiteName}
           </a>
           <FontAwesomeIcon icon={'external-link-alt'} />
         </React.Fragment>
       )}
       content={(
         <span className='discovery-popover__text'>
-          After filling the request form, after your search selection, you would get an approval after which you can use the Gen3 Client
+          After filling the request form, once your search selection is approved, you can use the Gen3 Client
           to download the data from the selected studies to your local computer.
         </span>
       )}
     >
       <Button
         onClick={() => {
-          const combinedIds = props.discovery.selectedResources.map((item) => item._medical_sample_id).join(',');
-          const url = `${props.config.features.exportToWorkspace.fillRequestFormURL}?query=${encodeURIComponent(combinedIds)}`;
+          const combinedIds = selectedResources.map((item) => item._medical_sample_id).join(',');
+          const url = `${fillRequestFormURL}?query=${encodeURIComponent(combinedIds)}`;
           window.open(url, '_blank');
         }}
         type='default'
-        className={`discovery-action-bar-button${(props.discovery.selectedResources.length === 0) ? '--disabled' : ''}`}
-        disabled={props.discovery.selectedResources.length === 0}
+        className={`discovery-action-bar-button${(selectedResources.length === 0) ? '--disabled' : ''}`}
+        disabled={selectedResources.length === 0}
         icon={<FileTextOutlined />}
       >
-        {`Click Here to ${props.config.features.exportToWorkspace.fillRequestFormDisplayText || 'Request Information'}`}
+        {`Click Here to ${fillRequestFormDisplayText || 'Request Information'}`}
       </Button>
     </Popover>
   );
