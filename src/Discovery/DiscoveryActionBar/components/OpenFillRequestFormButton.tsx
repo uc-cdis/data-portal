2af582bdcde6892cd509e167cd591eb8b073acf3
Urvi Sheth <70549768+urvi-occ@users.noreply.github.com>
2024-11-11 16:22:11 -0600

    new changes for B3 WALDER project to make changes for discovery porta… (#1551)
    * new changes for B3 WALDER project to make changes for discovery portal in PRC
    
    * spacing changes for code
    
    * changes suggested by mingfei
    
    * changes made after mingfei's comments and meeting
    
    * eslint on openfillrequestformbutton
    
    * added missing variables in discovey config
    
    * added null condition and ran eslint
    
    * added check for null to pass npm test
    
    * added check for null to pass npm test
    
    * refactor: simplify button logic by removing redundant check
    
    * refactor: remove redundant  check from disabled condition
    
    * changes for failing test
    
    * new variable fillRequestFormCheckField added
    
    * changes for if condition
    
    * if condition fixes
    
    * new property added to allow checkboxes if not logged in
    
    * revert change
    
    * removed extra space
    
    * changed text for tooltip
    
    * fixes for props variable
    
    * Update src/Discovery/DiscoveryActionBar/components/OpenFillRequestFormButton.tsx
    
    Co-authored-by: Mingfei Shao <2475897+mfshao@users.noreply.github.com>
    
    * changes for tooltip if multiple flags are on
    
    * Update src/Discovery/DiscoveryListView.tsx
    
    Co-authored-by: Mingfei Shao <2475897+mfshao@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Mingfei Shao <2475897+mfshao@users.noreply.github.com>

---
+++ src/Discovery/DiscoveryActionBar/components/OpenFillRequestFormButton.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { FileTextOutlined } from '@ant-design/icons';
+import { Popover, Button } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const OpenFillRequestFormButton = (props) => {
+  const { config, discovery } = props.props;
+
+  // Check if Fill Request Form button should be disabled based on configuration
+  const isFillRequestFormDisabled = !config?.features?.exportToWorkspace?.enableFillRequestForm
+    || !config.features.exportToWorkspace.fillRequestFormURL?.trim()
+    || !config.features.exportToWorkspace.externalWebsiteURL?.trim()
+    || !config.features.exportToWorkspace.externalWebsiteName?.trim();
+
+  if (isFillRequestFormDisabled) {
+    return null; // Return early if any required config values are missing
+  }
+
+  // Define URLs and text for use in the popover and button
+  const {
+    fillRequestFormURL, externalWebsiteURL, externalWebsiteName, fillRequestFormDisplayText,
+  } = config.features.exportToWorkspace;
+  const { selectedResources } = discovery;
+
+  return (
+    <Popover
+      className='discovery-popover'
+      arrowPointAtCenter
+      title={(
+        <React.Fragment>
+          &nbsp;
+          <a target='_blank' rel='noreferrer' href={externalWebsiteURL}>
+            {externalWebsiteName}
+          </a>
+          <FontAwesomeIcon icon={'external-link-alt'} />
+        </React.Fragment>
+      )}
+      content={(
+        <span className='discovery-popover__text'>
+          After filling the request form, once your search selection is approved, you can use the Gen3 Client
+          to download the data from the selected studies to your local computer.
+        </span>
+      )}
+    >
+      <Button
+        onClick={() => {
+          const combinedIds = selectedResources.map((item) => item[config.features.exportToWorkspace.fillRequestFormCheckField]).join(',');
+          const url = `${fillRequestFormURL}?query=${encodeURIComponent(combinedIds)}`;
+          window.open(url, '_blank');
+        }}
+        type='default'
+        className={`discovery-action-bar-button${(selectedResources.length === 0) ? '--disabled' : ''}`}
+        disabled={selectedResources.length === 0}
+        icon={<FileTextOutlined />}
+      >
+        {`Click Here to ${fillRequestFormDisplayText || 'Request Information'}`}
+      </Button>
+
+    </Popover>
+  );
+};
+
+export default OpenFillRequestFormButton;
