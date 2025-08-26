391ffe19e21997d1d5c7d4714918387b27f0943e
Mingfei Shao <mshao1@uchicago.edu>
2025-08-26 15:31:53 -0500

    revert change

--- src/Discovery/Discovery.tsx
+++ src/Discovery/Discovery.tsx
@@ -311,7 +311,7 @@ const Discovery: React.FunctionComponent<Props> = (props: Props) => {
   const [searchMode, setSearchMode] = useState(SearchMode.FULL_TEXT);
 
   useEffect(() => {
-    if (!props.allBatchesAreReady) {
+    if (!props.studies.length) {
       return;
     }
     const cacheKey = JSON.stringify({
