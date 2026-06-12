a7611e2e35cf81312e78cfbe910665b384432169
avantol <avantol@uchicago.edu>
2026-06-12 11:46:47 -0500

    fix(deps): handle mjs

--- webpack.config.js
+++ webpack.config.js
@@ -336,6 +336,20 @@ module.exports = {
       test: /\.flow$/,
       loader: 'ignore-loader',
     },
+    // .mjs parsing needed on webpack 4 for fuse.js
+    {
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+      use: [
+        {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      ]
+    }
     ],
   },
   resolve: {
