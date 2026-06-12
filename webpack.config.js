1ab0c2540fb16b259c1476b359198c10e7a10748
avantol <avantol@uchicago.edu>
2026-06-12 10:57:47 -0500

    feat(webpack): updates for new version

--- webpack.config.js
+++ webpack.config.js
@@ -310,7 +310,7 @@ module.exports = {
     },
     {
       test: /\.less$/,
-      loaders: [
+      use: [
         'style-loader',
         'css-loader',
         'less-loader',
@@ -322,13 +322,13 @@ module.exports = {
     },
     {
       test: /\.svg$/,
-      // loaders: ['babel-loader', 'react-svg-loader'], // to address the `css-what` vulnerability issue, after updating to webpack 5 and latest `react-svg-loader` we can switch back to this
+      // use: ['babel-loader', 'react-svg-loader'], // to address the `css-what` vulnerability issue, after updating to webpack 5 and latest `react-svg-loader` we can switch back to this
       loader: 'svg-react-loader',
     },
     {
       test: /\.(png|jpg|gif|woff|ttf|eot)$/,
-      loaders: 'url-loader',
-      query: {
+      use: 'url-loader',
+      options: {
         limit: 8192,
       },
     },
