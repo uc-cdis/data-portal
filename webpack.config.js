400d8e186b762e27b847dd5e6ace00fa077c5449
avantol <avantol@uchicago.edu>
2026-06-12 11:31:50 -0500

    fix(deps): revert webpack upgrade

--- webpack.config.js
+++ webpack.config.js
@@ -310,7 +310,7 @@ module.exports = {
     },
     {
       test: /\.less$/,
-      use: [
+      loaders: [
         'style-loader',
         'css-loader',
         'less-loader',
@@ -322,13 +322,13 @@ module.exports = {
     },
     {
       test: /\.svg$/,
-      // use: ['babel-loader', 'react-svg-loader'], // to address the `css-what` vulnerability issue, after updating to webpack 5 and latest `react-svg-loader` we can switch back to this
+      // loaders: ['babel-loader', 'react-svg-loader'], // to address the `css-what` vulnerability issue, after updating to webpack 5 and latest `react-svg-loader` we can switch back to this
       loader: 'svg-react-loader',
     },
     {
       test: /\.(png|jpg|gif|woff|ttf|eot)$/,
-      use: 'url-loader',
-      options: {
+      loaders: 'url-loader',
+      query: {
         limit: 8192,
       },
     },
